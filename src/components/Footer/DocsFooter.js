ae9ea4b3f0127e1a16dd52ed1c9ecc6c9dcad366
Athreya Anand <athreyanand@gmail.com>
2020-07-08 15:52:16 -0700

    modified footer to include reach out sentence

--- src/components/Footer/DocsFooter.js
+++ src/components/Footer/DocsFooter.js
@@ -5,6 +5,7 @@ export function DocsFooter({ filename, title }) {
     return <div className='docs-footer'>
         <Row>
             <h3>Help and feedback</h3>
+            If you need help on any of the above, feel free to create an issue on <a href='https://github.com/PostHog/posthog'>our repo</a>, or <a href='https://join.slack.com/t/posthogusers/shared_invite/enQtOTY0MzU5NjAwMDY3LTc2MWQ0OTZlNjhkODk3ZDI3NDVjMDE1YjgxY2I4ZjI4MzJhZmVmNjJkN2NmMGJmMzc2N2U3Yjc3ZjI5NGFlZDQ'>join our Slack</a> where a member of our team can assist you! Chances are that if you have a problem or question, someone else does too - so please don't hesitate to create a new issue or ask us a question :) <br />
             <Col span={12}>
                 <strong>Docs</strong><br />
                 <a href={'https://github.com/PostHog/posthog.com/tree/master/contents' + filename}>Edit this page</a><br />
