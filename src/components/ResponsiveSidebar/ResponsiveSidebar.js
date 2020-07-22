5e60d2c51c588f5c78ebb4e66ae5fb37d712a23c
Bernt Glaser <berntglaser@Bernts-MacBook-Pro.local>
2020-07-22 16:20:31 +0200

    closes #204 Docs Sidebar Overflows into Footer

--- src/components/ResponsiveSidebar/ResponsiveSidebar.js
+++ src/components/ResponsiveSidebar/ResponsiveSidebar.js
@@ -1,25 +1,40 @@
 import React, { Component } from "react";
 import SidebarContents from "../SidebarContents";
+import logoGrey from '../../images/posthog-logo-dark-grey.svg'
+import { Link } from 'gatsby'
 
 class ResponsiveSidebar extends Component {
   render() {
 
     return (
       <div style={{
-        position: "relative",
         top: 80,
         left: 10,
         right: "80%",
         bottom: 0,
         zIndex:1, 
-      }} >
+        marginBottom: 10
+      }} >           
         <div style={{
-          position:"absolute", 
-          left:0,
-          right:10,
-          top:0,
-          bottom:0
+          marginRight: 10
         }}>
+          <Link
+            id="logo"
+            to="/"
+            style={{
+              //color: '#FFF',
+              textDecoration: 'none',
+            }}
+          >
+            <div style={{ 
+              height: 'auto', 
+              backgroundColor: '#C4C4C4', 
+              padding: 10,
+              marginRight: 10,
+              marginBottom: 80}}>
+              <img alt='logo' src={logoGrey} id='logo-image' style={{padding: '0 10', width: '20vw', height: 'auto'}}/>
+            </div>
+          </Link>
           <SidebarContents/>
         </div>
       </div>
