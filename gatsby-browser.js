9aadba7e071d61c7b5f00c1a6a5676eb65c56471
Yakko Majuri <yakko.majuri@gmail.com>
2020-08-25 08:08:38 -0300

    changed style for inline and blocks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,4 +10,4 @@ export const wrapRootElement = wrapWithProvider
 
 
 // gatsby-browser.js
-require('prismjs/themes/prism.css')
+require('prismjs/themes/prism-okaidia.css')
--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
               showLineNumbers: false,
               // If setting this to true, the parser won't handle and highlight inline
               // code used in markdown i.e. single backtick code like `this`.
-              noInlineHighlight: false,
+              noInlineHighlight: true,
               // This adds a new language definition to Prism or extend an already
               // existing language definition. More details on this option can be
               // found under the header "Add new language definition or extend an
