8dd75f64b3ded1074c96b60df28346ef0e1203a7
Yakko Majuri <yakko.majuri@gmail.com>
2020-09-16 11:08:13 +0000

    made search bar appear only on Docs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import Helmet from 'react-helmet'
 import Menu from '../Menu'
 import logo from '../../images/posthog-logo-150x29.svg'
 import whiteLogo from '../../images/posthog-logo-white.svg'
@@ -10,34 +9,34 @@ import docsearch from 'docsearch.js'
 import 'docsearch.js/dist/cdn/docsearch.min.css/'
 import { SearchOutlined } from '@ant-design/icons'
 
-
+const isDocsPage = () => { return window.location.pathname.split('/')[1] === "docs" }
 
 class Header extends Component {
   componentDidMount() {
-    docsearch({apiKey: '45e80dec3e5b55c400663a5cba911c4c', indexName: 'posthog', inputSelector: '#doc-search'})
+    docsearch({ apiKey: '45e80dec3e5b55c400663a5cba911c4c', indexName: 'posthog', inputSelector: '#doc-search' })
   }
   render() {
     const { sidebarDocked, onPostPage, sidebarHide, screenIsSmall, isBlogPage } = this.props
 
     return (
-      
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: 100
 
-        }}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              background: 'none'
-            }}>
-            {sidebarHide || !onPostPage ? (
-              <Link
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: 100
+
+      }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            background: 'none'
+          }}>
+          {sidebarHide || !onPostPage ? (
+            <Link
               id="logo"
               to="/"
               style={{
@@ -46,18 +45,18 @@ class Header extends Component {
                 verticalAlign: 'center'
               }}>
               {screenIsSmall ? (
-              <img alt="logo" src={logo} id="logo-image" style={{
-                display: 'flex',
-                top: 0,
-                left: 8
-              }} />
+                <img alt="logo" src={logo} id="logo-image" style={{
+                  display: 'flex',
+                  top: 0,
+                  left: 8
+                }} />
               ) : (
-                <img alt="logo" src={logo} id="logo-image"/>
-              )}
-              </Link>
-            ) : (
-                isBlogPage ? (
-                  <Link
+                  <img alt="logo" src={logo} id="logo-image" />
+                )}
+            </Link>
+          ) : (
+              isBlogPage ? (
+                <Link
                   id="logo"
                   to="/"
                   style={{
@@ -65,33 +64,34 @@ class Header extends Component {
                     textDecoration: 'none',
                     verticalAlign: 'center'
                   }}>
-                    {screenIsSmall ? (<img alt="logo" src={logo} id="logo-image" style={{
-                      display: 'flex',
-                      top: 0,
-                      left: 8
-                    }} />
-                    ) : (
-                      <img alt="logo" src={whiteLogo} id="logo-image"/>
+                  {screenIsSmall ? (<img alt="logo" src={logo} id="logo-image" style={{
+                    display: 'flex',
+                    top: 0,
+                    left: 8
+                  }} />
+                  ) : (
+                      <img alt="logo" src={whiteLogo} id="logo-image" />
                     )}
-                  </Link>
-                ) : (
-                  <div style={{height: 64, width: 0}}></div>
-              )
+                </Link>
+              ) : (
+                  <div style={{ height: 64, width: 0 }}></div>
+                )
             )}
-            <Menu 
+          <Menu
             sidebarDocked={sidebarDocked}
             sidebarHide={sidebarHide}
             isBlogPage={isBlogPage} />
-          </div>  
+        </div>
+        {isDocsPage() && (
           <form className="docSearchWrapper">
             <input
-            placeholder="Search our website"
-            id="doc-search"/>
-            <SearchOutlined className="docSearchIcon" type="submit"/>
+              placeholder="Search our Docs"
+              id="doc-search" />
+            <SearchOutlined className="docSearchIcon" type="submit" />
           </form>
-          
-        </div>
-      )
+        )}
+      </div>
+    )
   }
 }
 
