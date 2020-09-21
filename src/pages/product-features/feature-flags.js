dd8441965b7922aa4f551f7dd52c9ce240fd388c
Bernt Glaser <berntglaser@Bernts-MBP.lan>
2020-09-21 17:50:27 +0100

    finished all internal pages + cleaned up styling

--- src/pages/product-features/feature-flags.js
+++ src/pages/product-features/feature-flags.js
@@ -15,19 +15,19 @@ import retentionIcon from '../../images/retention-icon.svg'
 function ProductFeatures() {
     return (
         <Layout>
-            <div className="featuresWrapper retention">
-                <div className="head orange retention">
+            <div className="featuresWrapper featureFlags">
+                <div className="head yellow featureFlags">
                     <div className="headContents">
                         <Link to="/product-features" className="headNav top">
                             <Button icon="left" />
                             <p>Back to Features</p>
                         </Link>
-                        <h1>Retention</h1>
+                        <h1>Feature flags</h1>
                         <h3>
-                            Visualize users going through a multi-step process in your app and figure out where they’re
-                            dropping off.
+                            Release new features slowly to your users, see how they perform, and roll them back if you
+                            need to.
                         </h3>
-                        <Link to="/product-features/feature-flags" className="headNav bottom">
+                        <Link to="/product-features/self-hosted" className="headNav bottom">
                             <p>Next Feature</p>
                             <Button icon="right" />
                         </Link>
@@ -36,43 +36,43 @@ function ProductFeatures() {
                 <div className="row01 featuresRow">
                     <h2 className="number row01">01</h2>
                     <div className="rowContents row01">
-                        <h2>Visualize user behavior step-by-step</h2>
+                        <h2>Roll out features at your own pace</h2>
                         <hr className="redLine" />
                         <p>
-                            PostHog funnels offer you a visual representation of the flow of users through specific
-                            areas of your app, such as the sign up process. They allow you to understand what percentage
-                            of users go from one step to another, so you can identify any areas for improvement in
-                            multi-step processes.
+                            Releasing new features can be difficult. As much as your team may find the feature amazing,
+                            you cannot be sure of how it will impact your users. With feature flags, rolling out new
+                            features becomes less stressful. You can choose to slowly roll out features to a certain
+                            percentages of your users, and increase or decrease that percentage accordingly.
                         </p>
                     </div>
-                    <div className="rowImg row01 retention" />
+                    <div className="rowImg row01 featureFlags" />
                 </div>
                 <div className="row02 featuresRow">
-                    <div className="rowImg row02 retention" />
+                    <div className="rowImg row02 featureFlags" />
                     <h2 className="number row02">02</h2>
                     <div className="rowContents row02">
-                        <h2>Understand where users drop off</h2>
+                        <h2>See how new features affect the UX</h2>
                         <hr className="redLine" />
                         <p>
-                            Funnels are especially useful for understanding at which step of a process users are
-                            dropping off. They can help you identify points that need attention in order to improve
-                            conversion rates, or simply make the app more friendly to use.
+                            Maybe you want to see how your users react to different text on a button, or a new navbar
+                            style. With feature flags, you can roll out a change to only a percentage of users and see
+                            how that affects the UX through the various analytics tools PostHog provides. You can then
+                            decide to fully implement the change or simply roll it back.
                         </p>
                     </div>
                 </div>
                 <div className="row03 featuresRow">
                     <h2 className="number row03">03</h2>
                     <div className="rowContents row03">
-                        <h2>Improve your app’s UX</h2>
+                        <h2>Safely roll back features</h2>
                         <hr className="redLine" />
                         <p>
-                            In PostHog, you can see an aggregate view of all your users going through a funnel, filter
-                            them by a certain property, or even see each user individually! This makes it easy to
-                            identify accessibility issues, or areas where information might be unclear. With funnels,
-                            you can identify patterns, polish the UX, and improve conversion.
+                            Maybe your new feature didn’t turn out to be as exciting to your users, or perhaps it still
+                            needs a few tweaks. No problem. Safely roll back features without serious consequences and
+                            only make definite changes to your app when you’re certain they’re good to go!
                         </p>
                     </div>
-                    <div className="rowImg row03 retention" />
+                    <div className="rowImg row03 featureFlags" />
                 </div>
                 <div className="startNowWrapper">
                     <div className="startNowRow">
@@ -84,19 +84,17 @@ function ProductFeatures() {
                         <img src={rays} />
                     </div>
                 </div>
-                <div className="nextFeature retention yellow">
+                <div className="nextFeature featureFlags blue">
                     <div className="nextFeatureContents retention">
                         <Link to="/product-features" className="nextFeatureNav top">
                             <Button icon="left" />
                             <p>Back to Features</p>
                         </Link>
-                        <Link to="/product-features/feature-flags" className="nextFeatureText">
-                            <h2>Feature Flags</h2>
-                            <p>
-                                Understand what percentage of users you’re retaining, and who are the ones you’re not.
-                            </p>
+                        <Link to="/product-features/self-hosted" className="nextFeatureText">
+                            <h2>Self hosted</h2>
+                            <p>Powerful analytics on your own infrastructure, with all the features your team needs.</p>
                         </Link>
-                        <Link to="/product-features/feature-flags" className="nextFeatureNav bottom">
+                        <Link to="/product-features/self-hosted" className="nextFeatureNav bottom">
                             <p>Next Feature</p>
                             <Button icon="right" />
                         </Link>
@@ -106,23 +104,23 @@ function ProductFeatures() {
                     <h2>Other features</h2>
                     <div className="otherFeatures">
                         <div className="twoOtherFeatures">
-                            <Link to="/product-features/self-hosted">
-                                <img src={selfHostedIcon} />
-                                <h4>Self hosted</h4>
-                            </Link>
                             <Link to="/product-features/event-autocapture">
                                 <img src={eventAutocaptureIcon} />
                                 <h4>Event autocapture</h4>
                             </Link>
-                        </div>
-                        <div className="twoOtherFeatures">
                             <Link to="/product-features/trends">
                                 <img src={trendsIcon} />
-                                <h4>Retention</h4>
+                                <h4>Trends</h4>
                             </Link>
+                        </div>
+                        <div className="twoOtherFeatures">
                             <Link to="/product-features/funnels">
                                 <img src={funnelsIcon} />
                                 <h4>Funnels</h4>
+                            </Link>
+                            <Link to="/product-features/trends">
+                                <img src={retentionIcon} />
+                                <h4>Retention</h4>
                             </Link>
                         </div>
                     </div>
