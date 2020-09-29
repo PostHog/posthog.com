b3dee583192997aee2f65ab077043239ca610ed4
Tim Glaser <tim@glsr.nl>
2020-09-29 11:27:59 +0100

    Add options choice

--- src/pages/compensation.js
+++ src/pages/compensation.js
@@ -13,6 +13,8 @@ var formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   });
+
+let levelToOptionsCash = (level) => level_modifier[level] >= 1 ? 40000 : 20000
 let formatCur = (val) => formatter.format(val).replace('.00', '')
 
 const CompensationPage = () => {
@@ -21,6 +23,8 @@ const CompensationPage = () => {
     const [region, setRegion] = useState(localStorage.getItem('region'))
     const [level, setLevel] = useState(localStorage.getItem('level') || 'Senior')
     const [step, setStep] = useState(localStorage.getItem('step') || 'Thriving')
+    const prevOptions = parseInt(localStorage.getItem('options'));
+    const [options, setOptions] = useState(prevOptions > -1 ? prevOptions : levelToOptionsCash(level))
     const setItem = (type) => {
         return (value) => {
             if(type === 'job') setJob(value)
@@ -29,8 +33,12 @@ const CompensationPage = () => {
                 setItem('region')(false)
             }
             if(type === 'region') setRegion(value)
-            if(type === 'level') setLevel(value)
+            if(type === 'level') {
+                setLevel(value)
+                setItem('options')(levelToOptionsCash(value))
+            }
             if(type === 'step') setStep(value)
+            if(type === 'options') setOptions(value)
             localStorage.setItem(type, value)
         }
     }
@@ -83,10 +91,29 @@ const CompensationPage = () => {
             >
             {Object.entries(step_modifier).map(([step, modifier]) => <Radio.Button value={step} key={step}>{step} <span>{modifier}</span></Radio.Button>)}
         </Radio.Group>
-        <Statistic title="Base salary" value={(job && country && region) ? formatCur(sf_benchmark[job] * calculatedLocationFactor * level_modifier[level] * step_modifier[step]) : '--'} />
+        Options (based on seniority)
+        <Radio.Group
+            style={{width: '100%', marginBottom: '0.75rem'}}
+            value={options > 0 ? levelToOptionsCash(level) : 0}
+            buttonStyle="solid"
+            onChange={e => setItem('options')(e.target.value)}
+            >
+                {level_modifier[level] >= 1 ?
+                    <>
+                        <Radio.Button value={40000}>Options + Cash (0.1% + $40k/year)</Radio.Button>
+                        <Radio.Button value={0}>All options (0.5%)</Radio.Button>
+                    </> :
+                    <>
+                        <Radio.Button value={20000}>Options + Cash (0.05% + $20k/year)</Radio.Button>
+                        <Radio.Button value={0}>All options (0.25%)</Radio.Button>
+                    </>
+                }
+        </Radio.Group>
+        <Statistic title="Base salary" value={(job && country && region) ? formatCur(sf_benchmark[job] * calculatedLocationFactor * level_modifier[level] * step_modifier[step] + options) : '--'} />
         {job && country && region && <div>
             <Tag>SF Benchmark: {formatCur(sf_benchmark[job])}</Tag> x <Tag>Location factor: {calculatedLocationFactor}</Tag> x <Tag>Level modifier: {level_modifier[level]}</Tag>
             x <Tag>Step modifier: {step_modifier[step]}</Tag>
+            x <Tag>Options: {formatCur(options)}</Tag>
         </div>}
         
     </div>)
