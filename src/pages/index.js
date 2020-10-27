81d3abb7c452c2011cd2a36120c44477d661be36
yakkomajuri <yakko.majuri@gmail.com>
2020-10-27 10:43:11 +0000

    standardize colored bg components on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,29 +13,19 @@ import improveFunnels from '../images/retro-funnels-box.png'
 import enterprise01 from '../images/retro-self-managed.svg'
 import enterprise02 from '../images/enterprise-02.svg'
 import enterprise03 from '../images/enterprise-03.svg'
-import shelf4 from '../images/shelf-4.svg'
-import shelf2 from '../images/shelf-2.svg'
 import coolHedgehog from '../images/cool-hedgehog.svg'
-import stackAndroid from '../images/stack-android.svg'
-import stackPython from '../images/stack-python.svg'
-import stackIos from '../images/stack-ios.svg'
-import stackNode from '../images/stack-node.svg'
-import stackPhp from '../images/stack-php.svg'
-import stackRuby from '../images/stack-ruby.svg'
-import stackGatsby from '../images/stack-gatsby.svg'
-import stackJavascript from '../images/stack-javascript.svg'
-import stackGo from '../images/stack-go.svg'
-import stackApi from '../images/stack-api.svg'
 import retroAutocapture from '../images/retro-autocapture.svg'
 import trafficFlow from '../images/retro-how-traffic-flows.svg'
 import visualizeTrends from '../images/retro-product-trends.svg'
 import retroFlagsImg from '../images/retro-feature-flags.svg'
+import selfHostedImg from '../images/self-host.svg'
 import SEO from '../components/seo'
 import Layout from '../components/Layout'
 import { FeaturedSectionTextLeft } from '../components/Sections/FeaturedSectionTextLeft'
 import { FeaturedSectionTextRight } from '../components/Sections/FeaturedSectionTextRight'
 import { FeaturedSectionTripleImage } from '../components/Sections/FeaturedSectionTripleImage'
 import { Spacer } from '../components/Spacer'
+import { DesignedForYourStackBlock } from '../components/Sections/DesignedForYourStackBlock'
 
 function IndexPage() {
     const [modalIsOpen, setModalIsOpen] = useState(false)
@@ -178,7 +168,6 @@ function IndexPage() {
                         title3="Paths"
                     />
                     <Spacer />
-                    <Spacer onlyDesktop={true} />
                     <div className="fs-feature-flags-wrapper">
                         <FeaturedSectionTextLeft
                             headerText="Use feature flags to test new ideas"
@@ -188,103 +177,18 @@ function IndexPage() {
                             color="red"
                         />
                     </div>
-
-                    {/*Designed for your Stack */}
-                    <div className="designed4StackWrapper">
-                        <div className="designed4StackRow row-">
-                            <h2 align="middle" className="gosha">
-                                Designed for your stack
-                            </h2>
-                        </div>
-                        <div className="yourStackWrapper">
-                            <div className="yourStackLogosRow row" align="bottom">
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/python-integration">
-                                        <img alt="Python" className="imageShow" loading="lazy" src={stackPython} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/php-integration">
-                                        <img alt="PHP" className="imageShow" loading="lazy" src={stackPhp} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/android-integration">
-                                        <img alt="Android" className="imageShow" loading="lazy" src={stackAndroid} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/ios-integration">
-                                        <img alt="iOS" className="imageShow" loading="lazy" src={stackIos} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/node-integration">
-                                        <img alt="Node" className="imageShow" loading="lazy" src={stackNode} />
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="yourStackShelfRow row">
-                                <div className="yourStackShelf">
-                                    <img alt="shelf-2" className="imageShow shelf" loading="lazy" src={shelf2} />
-                                </div>
-                            </div>
-                            <div className="yourStackLogosRow row" align="bottom">
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/ruby-integration">
-                                        <img alt="Ruby" className="imageShow" loading="lazy" src={stackRuby} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/gatsby-integration">
-                                        <img alt="Gatsby" className="imageShow" loading="lazy" src={stackGatsby} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/js-integration">
-                                        <img
-                                            alt="Javascript"
-                                            className="imageShow"
-                                            loading="lazy"
-                                            src={stackJavascript}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/go-integration">
-                                        <img alt="Go" className="imageShow" loading="lazy" src={stackGo} />
-                                    </Link>
-                                </div>
-                                <div className="yourStackLogos">
-                                    <Link to="/docs/integrations/api">
-                                        <img alt="API" className="imageShow" loading="lazy" src={stackApi} />
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="yourStackShelfRow row">
-                                <div className="yourStackShelf">
-                                    <img alt="shelf-4" className="imageShow shelf" loading="lazy" src={shelf4} />
-                                </div>
-                            </div>
-                        </div>
+                    <DesignedForYourStackBlock />
+                    <Spacer />
+                    <div className="fs-self-host-wrapper">
+                        <FeaturedSectionTextLeft
+                            headerText="Self host available, with full underlying data access"
+                            listItem=""
+                            descriptionText="Huge user base? No problem. Eliminate the data protection risks and costs associated with sending millions of users' data to 3rd parties."
+                            image={selfHostedImg}
+                            color="blue"
+                        />
                     </div>
-
-                    {/* self-hosted full data*/}
-                    <div className="selfHostedDataWrapper wrapper">
-                        <div className="selfHostedDataRow row">
-                            <div className="selfHostedDataCol">
-                                <h2 className="gosha">
-                                    Self host available, <br /> with full underlying data access
-                                </h2>
-                                <hr className="blueLine" />
-                                <p className="darkBlueText">
-                                    Huge user base? No problem. Eliminate the data protection risks and costs associated
-                                    with sending millions of users' data to 3rd parties.
-                                </p>
-                            </div>
-                            <div className="selfHostedDataImg" />
-                        </div>
-                    </div>
+                    <Spacer onlyDesktop={true} />
 
                     {/* Why posthog */}
                     <div className="whyPosthogWrapper wrapper">
