99d075a4cdcdce9237a8f5fa43d58a21d1dacd68
yakkomajuri <yakko.majuri@gmail.com>
2020-11-23 18:54:43 +0000

    update form submission

--- src/pages/request_demo.js
+++ src/pages/request_demo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import { Col, Form, Input, Button } from 'antd'
 
@@ -8,6 +8,9 @@ const layout = {
 }
 
 const RequestDemoForm = ({ title, subtitle }) => {
+    const [wasFormSubmitted, setWasFormSubmitted] = useState(false)
+    const [formErrored, setFormErrored] = useState(false)
+
     const handleFormSubmission = (e) => {
         e.preventDefault()
         const formElement = e.target
@@ -25,8 +28,9 @@ const RequestDemoForm = ({ title, subtitle }) => {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams(formData).toString(),
         })
-            .then(() => console.log('Form successfully submitted'))
-            .catch((error) => alert(error))
+            .then(() => {})
+            .catch(() => setFormErrored(true))
+        setWasFormSubmitted(true)
     }
 
     return [
@@ -39,44 +43,61 @@ const RequestDemoForm = ({ title, subtitle }) => {
             )}
         </Col>,
         <Col align="middle" key="startup-form-2">
-            <Form
-                {...layout}
-                name="demo-request"
-                id="demo-request"
-                method="POST"
-                style={{ maxWidth: '80vw' }}
-                data-netlify="true"
-                onSubmit={(e) => handleFormSubmission(e)}
-            >
-                <input type="hidden" name="demo-request" value="demo-request" />
-                <Form.Item
-                    label="First Name"
-                    name="firstName"
-                    rules={[{ required: true, message: 'Please input your first name!' }]}
+            {!wasFormSubmitted ? (
+                <Form
+                    {...layout}
+                    name="demo-request"
+                    id="demo-request"
+                    method="POST"
+                    style={{ maxWidth: '80vw' }}
+                    data-netlify="true"
+                    onSubmit={(e) => handleFormSubmission(e)}
                 >
-                    <Input id="firstName" required />
-                </Form.Item>
-                <Form.Item
-                    label="Last Name"
-                    name="lastName"
-                    rules={[{ required: true, message: 'Please input your last name!' }]}
-                >
-                    <Input id="lastName" required />
-                </Form.Item>
-                <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
-                    <Input id="email" required type="email" />
-                </Form.Item>
-                <Form.Item
-                    label="Company"
-                    name="company"
-                    rules={[{ required: true, message: 'Please input your company!' }]}
-                >
-                    <Input id="company" required />
-                </Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form>
+                    <input type="hidden" name="demo-request" value="demo-request" />
+                    <Form.Item
+                        label="First Name"
+                        name="firstName"
+                        rules={[{ required: true, message: 'Please input your first name!' }]}
+                    >
+                        <Input id="firstName" name="firstName" required />
+                    </Form.Item>
+                    <Form.Item
+                        label="Last Name"
+                        name="lastName"
+                        rules={[{ required: true, message: 'Please input your last name!' }]}
+                    >
+                        <Input id="lastName" name="lastName" required />
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input id="email" name="email" required type="email" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Company"
+                        name="company"
+                        rules={[{ required: true, message: 'Please input your company!' }]}
+                    >
+                        <Input id="company" name="company" required />
+                    </Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form>
+            ) : (
+                <div className="centered" style={{ maxWidth: '80vw' }}>
+                    {formErrored ? (
+                        <p>
+                            Oops, there was an error when submitting your form. Please try again or contact
+                            _hey@posthog.com_ to schedule a demo.
+                        </p>
+                    ) : (
+                        <p>Form submitted successfully. We'll get back to you soon!</p>
+                    )}
+                </div>
+            )}
             <br />
             <br />
         </Col>,
