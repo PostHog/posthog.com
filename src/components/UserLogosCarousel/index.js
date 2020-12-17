70a6d638c9bbb1243e628aa13d3182a193226af2
yakkomajuri <yakko.majuri@gmail.com>
2020-12-17 11:01:25 -0300

    style updates

--- src/components/UserLogosCarousel/index.js
+++ src/components/UserLogosCarousel/index.js
@@ -1,17 +1,59 @@
 import React from 'react'
 import InfiniteCarousel from 'react-leaf-carousel'
-import hasuraLogo from '../../images/user-logos/hasura.png'
-import spacexLogo from '../../images/user-logos/spacex.png'
-import tinkoffLogo from '../../images/user-logos/tinkoff.png'
-import airbalticLogo from '../../images/user-logos/airbaltic.png'
-import ycLogo from '../../images/user-logos/yc.png'
-import landmarkLogo from '../../images/user-logos/landmark.png'
-import staplesLogo from '../../images/user-logos/staples.png'
-import webinyLogo from '../../images/user-logos/webiny.png'
+import hasuraLogo from '../../images/user-logos/hasura.svg'
+import spacexLogo from '../../images/user-logos/spacex.svg'
+import tinkoffLogo from '../../images/user-logos/tinkoff.svg'
+import airbalticLogo from '../../images/user-logos/airbaltic.svg'
+import ycLogo from '../../images/user-logos/yc.svg'
+import landmarkLogo from '../../images/user-logos/landmark.svg'
+import staplesLogo from '../../images/user-logos/staples.svg'
+import webinyLogo from '../../images/user-logos/webiny.svg'
 import './style.scss'
 
+const companies = [
+    {
+        logo: hasuraLogo,
+        name: 'Hasura',
+        website: 'https://hasura.io/',
+    },
+    {
+        logo: tinkoffLogo,
+        name: 'Tinkoff',
+        website: 'https://www.tinkoffgroup.com/',
+    },
+    {
+        logo: spacexLogo,
+        name: 'SpaceX',
+        website: 'https://spacex.com/',
+    },
+    {
+        logo: airbalticLogo,
+        name: 'AirBaltic',
+        website: 'https://airbaltic.com/',
+    },
+    {
+        logo: ycLogo,
+        name: 'YCombinator',
+        website: 'https://ycombinator.com/',
+    },
+    {
+        logo: staplesLogo,
+        name: 'Staples',
+        website: 'https://staples.com/',
+    },
+    {
+        logo: landmarkLogo,
+        name: 'Landmark Group',
+        website: 'https://landmarkgroup.com/',
+    },
+    {
+        logo: webinyLogo,
+        name: 'Webiny',
+        website: 'https://webiny.com/',
+    },
+]
+
 export const UserLogosCarousel = () => {
-    const logos = [hasuraLogo, tinkoffLogo, spacexLogo, airbalticLogo, ycLogo, staplesLogo, landmarkLogo, webinyLogo]
     return (
         <div className="centered logo-carousel-wrapper">
             <InfiniteCarousel
@@ -39,9 +81,11 @@ export const UserLogosCarousel = () => {
                 slidesToShow={5}
                 scrollOnDevice={true}
             >
-                {logos.map((logo) => (
-                    <div key={logo}>
-                        <img alt="user logo" src={logo} style={{ maxWidth: 200 }} />
+                {companies.map((company) => (
+                    <div key={company.name.toLowerCase()}>
+                        <a href={company.website} target="_blank" rel="noreferrer">
+                            <img alt="user logo" className="user-logos" src={company.logo} style={{ maxWidth: 200 }} />
+                        </a>
                     </div>
                 ))}
             </InfiniteCarousel>
