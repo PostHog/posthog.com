014a5fa2edcefdcc958bc6a747496029063a3d49
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-01-29 13:47:34 +0100

    Usage-based pricing for cloud (#900)

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -3,19 +3,21 @@ import { useLocation } from '@reach/router'
 import { Link } from 'gatsby'
 import queryString from 'query-string'
 import Layout from '../components/Layout'
-import { Row, Col, Button, Card, Collapse } from 'antd'
+import { Row, Col, Button, Card, Collapse, Slider } from 'antd'
 import SEO from '../components/seo'
 import './styles/pricing.scss'
 import 'antd/lib/collapse/style/css'
 import imgCloud from '../images/cloud.svg'
 import imgBuilding from '../images/building.svg'
 import imgChevronRight from '../images/chevron-right.svg'
-
-// Data for plans abstracted to pricing-data.js to reduce noise
+import imgCloudPlan from '../images/plan-cloud.svg'
+import 'antd/lib/slider/style/css'
 import { plans, faqs } from '../pages-content/pricing-data'
+import { DollarCircleTwoTone } from '@ant-design/icons'
 
 const PricingPage = () => {
     const [state, setState] = useState({ planOptions: 'cloud' })
+    const [priceSimulation, setPriceSimulation] = useState(250000)
     const comparisonRef = useRef()
     const location = useLocation()
     const { Panel } = Collapse
@@ -103,7 +105,7 @@ const PricingPage = () => {
                         </Col>
                     </Row>
                     <Row gutter={[24, 36]} type="flex" style={{ justifyContent: 'center', marginTop: '32px' }}>
-                        {plans[state.planOptions].map((plan) => (
+                        {(plans[state.planOptions] || []).map((plan) => (
                             <Col
                                 className="plan-card"
                                 md={24 / plans[state.planOptions].length}
@@ -152,14 +154,136 @@ const PricingPage = () => {
                                 </Card>
                             </Col>
                         ))}
+                        {state.planOptions === 'cloud' && (
+                            <div className="pricing-cloud">
+                                <h4>One Price. Pay only for what you use.</h4>
+                                <div>
+                                    Get all the features with a straight and transparent pricing. Pay based on the
+                                    events you <b>capture</b> every month.
+                                </div>
+
+                                <div>
+                                    <div className="main-price">
+                                        <div>
+                                            <DollarCircleTwoTone style={{ marginRight: 6 }} />
+                                            $0.000225/event
+                                            <span>per month</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <Slider
+                                            defaultValue={250000}
+                                            min={10000}
+                                            max={4000000}
+                                            step={20000}
+                                            tooltipVisible
+                                            tipFormatter={(value) => value.toLocaleString()}
+                                            onChange={(value) => setPriceSimulation(value)}
+                                        />
+                                        <div style={{ fontSize: '1rem', textAlign: 'right' }}>
+                                            {priceSimulation < 4000000 ? (
+                                                <>
+                                                    <span className="text-muted">Monthly estimate:</span>{' '}
+                                                    <b>
+                                                        $
+                                                        {Math.round(
+                                                            (priceSimulation - 10000) * 0.000225
+                                                        ).toLocaleString()}
+                                                    </b>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <a href="mailto:sales@posthog.com?title=Cloud%20Large%20Volumes%20Enquiry">
+                                                        Contact us
+                                                    </a>{' '}
+                                                    to talk pricing.
+                                                </>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div style={{ fontSize: 16, marginTop: 16 }}>
+                                    Looking for our free plan? First <b>10,000 events are free</b> every single month.
+                                    For everyone.
+                                </div>
+
+                                <div style={{ fontSize: 16, marginTop: 16 }}>
+                                    Have very large volumes? If you expect to capture for than 4 million events per
+                                    month,{' '}
+                                    <a href="mailto:sales@posthog.com?title=Cloud%20Large%20Volumes%20Enquiry">
+                                        contact us
+                                    </a>
+                                    .
+                                </div>
+
+                                <Card className="feature-card">
+                                    <div className="plan-image">
+                                        <img src={imgCloudPlan} alt="" />
+                                    </div>
+                                    <div className="text-center">
+                                        <h5>Features included</h5>
+                                    </div>
+                                    <ul style={{ listStyle: 'none' }}>
+                                        <li>
+                                            <b>Unlimited</b> event allocation. Pay only for what you use.
+                                        </li>
+                                        <li>
+                                            <b>Unlimited</b> tracked users
+                                        </li>
+                                        <li>
+                                            <b>Unlimited</b> team members
+                                        </li>
+                                        <li>
+                                            <b>Unlimited</b> projects
+                                        </li>
+                                        <li>
+                                            <b>7 years</b> of data retention
+                                            <span className="disclaimer">
+                                                <a href="#disclaimer-1">1</a>
+                                            </span>
+                                        </li>
+                                        <li>
+                                            <b>All core analytics features</b>
+                                        </li>
+                                        <li>
+                                            Session recording with unlimited storage
+                                            <span className="disclaimer">
+                                                <a href="#disclaimer-2">2</a>
+                                            </span>
+                                        </li>
+                                        <li>Feature flags</li>
+                                        <li>Plugins &amp; other integrations</li>
+                                        <li>Zapier integration</li>
+                                        <li>SSO/SAML</li>
+                                        <li>Export to data lakes</li>
+                                        <li>Community, Slack &amp; Email support</li>
+                                    </ul>
+                                    For companies using on average 1,000,000 events per month or more, we offer{' '}
+                                    <b>priority support</b>.
+                                </Card>
+                            </div>
+                        )}
                         <Col span={24} align="middle">
                             All prices in US Dollars (USD), excluding taxes
                         </Col>
                         <Col span={24} align="middle">
-                            <Link to="/docs/features" className="p-features-link">
+                            <Link to="/product-features" className="p-features-link">
                                 Full features details <img alt="" src={imgChevronRight} />
                             </Link>
                         </Col>
+                        {state.planOptions === 'cloud' && (
+                            <div className="disclaimer-details">
+                                <div id="disclaimer-1">
+                                    1. Data may be moved to cold storage after 12 months. Queries that involve data in
+                                    cold storage can take longer than normal to run.
+                                </div>
+                                <div id="disclaimer-2">
+                                    2. While there is no restriction on session recording storage, session recording
+                                    information is captured and stored as normal events and therefore billed as such.
+                                </div>
+                            </div>
+                        )}
                     </Row>
                     <br />
                     <Row gutter={[24, 24]} style={{ marginTop: '32px' }}>
@@ -190,7 +314,7 @@ const PricingPage = () => {
                                         <li>You want to get automatic updates with all the latest features.</li>
                                     </ul>
                                     <div className="p-comparison-btn">
-                                        <a href="https://app.posthog.com/signup?plan=starter">
+                                        <a href="https://app.posthog.com/signup">
                                             <Button type="primary" size="large">
                                                 Start my 30-day free trial
                                             </Button>
