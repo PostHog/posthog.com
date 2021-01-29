17544041ea6a0e99de9c012bb357699cf6fb3c7c
yakkomajuri <yakko.majuri@gmail.com>
2021-01-29 08:43:20 +0000

    add mdx page creation to /contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,19 @@ module.exports = {
                 path: `${__dirname}/src/images`,
             },
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `contents`,
+                path: `${__dirname}/contents`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-page-creator',
+            options: {
+                path: `${__dirname}/contents`,
+            },
+        },
         `gatsby-plugin-mdx`,
         `gatsby-transformer-json`,
         {
@@ -26,13 +39,6 @@ module.exports = {
             options: {
                 name: `menuItems`,
                 path: `${__dirname}/src/menuItems`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `contents`,
-                path: `${__dirname}/contents`,
             },
         },
         {
