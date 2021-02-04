d0c6aa274d1a3cf6e57de3faa8a051e43652769e
eltjehelene <75622766+eltjehelene@users.noreply.github.com>
2021-02-04 10:27:50 +0000

    Update sf_benchmark.js

--- src/pages-content/compensation_data/sf_benchmark.js
+++ src/pages-content/compensation_data/sf_benchmark.js
@@ -3,7 +3,7 @@
 const sf_benchmark = {
     Designer: 128400,
     Engineer: 192000,
-    'Growth Engineer': 192000,
+    //'Growth Engineer': 192000,
     'Product Designer': 174000,
     'Customer Success Lead': 190000,
     //Recruiter: 144000,
