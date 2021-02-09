09d96adab2c5d5fd4fe1263b038aedb05e322468
yakkomajuri <yakko.majuri@gmail.com>
2021-02-09 08:54:33 +0000

    Add MDX query to blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,10 +7,18 @@ import SEO from '../components/seo'
 const BlogPage = ({
     data: {
         allMarkdownRemark: { edges },
+        allMdx,
     },
 }) => {
-    const posts = edges
+    // Standardize position of 'slug' across MDX and MD
+    const parsedMdxData = allMdx.edges.map((edge) => ({
+        ...edge,
+        node: { ...edge.node, fields: { slug: `/${edge.node.slug}` } },
+    }))
+
+    const posts = [...edges, ...parsedMdxData] // Merge MDX and MD data into one array
         .filter((edge) => !!edge.node.frontmatter.date)
+        .sort((a, b) => new Date(b.node.frontmatter.date) - new Date(a.node.frontmatter.date)) // Resort based on dates following merge
         .map((edge) => <PostCard key={edge.node.id} post={edge.node} />)
     return (
         <Layout>
@@ -33,6 +41,23 @@ export const pageQuery = graphql`
                     fields {
                         slug
                     }
+                    id
+                    excerpt(pruneLength: 250)
+                    frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        title
+                        rootPage
+                    }
+                }
+            }
+        }
+        allMdx(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { frontmatter: { rootPage: { eq: $path } } }
+        ) {
+            edges {
+                node {
+                    slug
                     id
                     excerpt(pruneLength: 250)
                     frontmatter {
