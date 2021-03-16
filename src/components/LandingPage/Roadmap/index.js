3d1e07c00305d3c1554999e1a069ff79da7d325d
Cory Watilo <corywatilo@gmail.com>
2021-03-16 10:49:27 -0400

    rebuilding timeline

--- src/components/LandingPage/Roadmap/index.js
+++ src/components/LandingPage/Roadmap/index.js
@@ -1,41 +1,39 @@
 import React from 'react'
-import roadmapImg from '../images/mountains.png'
+import mountainsImg from '../images/mountains.png'
 import sunImg from '../images/sun.png'
+import cityImg from '../images/city.png'
 import timelineImg from '../images/timeline.png'
-import subwayImg from '../images/subway.png'
 import { CallToAction } from '../../CallToAction'
 
 export const Roadmap = () => (
     <div className="w-screen mt-24 relative">
         <div className="w-screen mt-24 relative city-wrapper">
-            <div className="max-w-3xl mx-auto text-center text-white relative z-50">
-                <h2>Getting better every day</h2>
-                <p className="mt-1 text-opacity-80">
-                    We built a ton in 2020. We’re going to build even more in 2021. <br />
-                    Everything we build is based off your feedback.
-                </p>
+            <div className="scene-wrapper">
+                <div className="text-wrapper max-w-3xl mx-auto text-center text-white relative z-50">
+                    <h2>Getting better every day</h2>
+                    <p className="mt-1 text-opacity-80">
+                        We built a ton in 2020. We’re going to build even more in 2021. <br />
+                        Everything we build is based off your feedback.
+                    </p>
 
-                <CallToAction
-                    icon="roadmap"
-                    to="/handbook/strategy/roadmap"
-                    type="custom"
-                    width="72"
-                    className="bg-purpleish text-white hover:bg-purpleish-dark hover:text-white border-purpleish-dark mx-auto"
-                >
-                    Explore Roadmap
-                </CallToAction>
+                    <CallToAction
+                        icon="roadmap"
+                        to="/handbook/strategy/roadmap"
+                        type="custom"
+                        width="72"
+                        className="bg-purpleish text-white hover:bg-purpleish-dark hover:text-white border-purpleish-dark mx-auto"
+                    >
+                        Explore Roadmap
+                    </CallToAction>
+                </div>
+                <img src={mountainsImg} className="w-screen mx-auto relative z-10 mountains" />
+
+                <img src={sunImg} className="sun absolute" />
             </div>
 
-            <img src={roadmapImg} className="w-screen mx-auto relative z-10 mountains" />
-
-            <img src={sunImg} className="sun absolute" />
-
-            <div className="timeline-wrapper overflow-x-scroll w-full relative lg:absolute z-40 lg:max-w-full lg:w-full lg:overflow-x-hidden lg:bottom-0">
-                <img src={timelineImg} className="timeline lg:w-full lg:max-w-full" />
-            </div>
-
-            <div className="subway-wrapper w-full relative z-40 lg:max-w-full lg:w-full">
-                <img src={subwayImg} className="subway " />
+            <div className="timeline-wrapper">
+                <img src={timelineImg} className="timeline" />
+                <img src={cityImg} className="city" />
             </div>
         </div>
     </div>
