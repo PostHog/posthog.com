d4e62a3e41cb96f8755d9df6f4e9a3b832e2643c
Sean Packham <sean@seanpackham.com>
2021-03-23 11:48:23 +0000

    Add blog link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -79,6 +79,9 @@ export const Header = ({ onPostPage }: { onPostPage: boolean }) => {
                     <NavbarLink to="/pricing" textLight={textLight}>
                         Pricing
                     </NavbarLink>
+                    <NavbarLink to="/blog" textLight={textLight}>
+                        Blog
+                    </NavbarLink>
                     <NavbarLink href="https://github.com/posthog/posthog" textLight={textLight}>
                         GitHub
                     </NavbarLink>
