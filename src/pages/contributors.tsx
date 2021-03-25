0c045d986917ec379a6ede4330754d07df0eadde
yakkomajuri <yakko.majuri@gmail.com>
2021-03-25 15:22:57 +0000

    wip stats

--- src/pages/contributors.tsx
+++ src/pages/contributors.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import { Spacer } from '../components/Spacer'
-import { Row } from 'antd'
+import { Row, Tabs } from 'antd'
 import { useValues } from 'kea'
 import { contributorsLogic } from '../logic/contributorsLogic'
 import { SEO } from '../components/seo'
@@ -11,8 +11,11 @@ import { ContributorCard } from 'components/ContributorCard'
 import { Contributor } from 'types'
 import { ContributorSearch } from 'components/ContributorSearch'
 
+const { TabPane } = Tabs
+
 export const ContributorsPage = () => {
     const { filteredContributors } = useValues(contributorsLogic)
+    const [activeTab, setActiveTab] = useState('list')
 
     return (
         <div className="contributors-page-wrapper">
@@ -25,30 +28,40 @@ export const ContributorsPage = () => {
                 <div className="centered" style={{ margin: 'auto' }}>
                     <Spacer />
                     <h1 className="center">Contributors</h1>
-                    <ContributorSearch />
+                    <Tabs activeKey={activeTab} onChange={(key) => setActiveTab(key)}>
+                        <TabPane tab="List" key="list" />
+                        <TabPane tab="Stats" key="stats" />
+                    </Tabs>
                     <Spacer height={20} />
-                    <Row gutter={16} style={{ marginTop: 16, marginRight: 10, marginLeft: 10, minHeight: 600 }}>
-                        <ContributorCard
-                            key="cool-hedgehog"
-                            name="the-cool-hedgehog"
-                            link="https://github.com/PostHog/posthog"
-                            imageSrc="https://posthog.com/static/cool-hedgehog-2e771b8385a05bfe25cfdea4bbb775a3.svg"
-                            contributions={['code', 'doc', 'plugin', 'bug']}
-                            mvpWins={2}
-                            contributorLevel={99}
-                        />
-                        {filteredContributors.map((contributor: Contributor) => (
-                            <ContributorCard
-                                key={contributor.login}
-                                name={contributor.login}
-                                link={contributor.profile}
-                                imageSrc={contributor.avatar_url}
-                                contributions={contributor.contributions}
-                                mvpWins={contributor.mvpWins}
-                                contributorLevel={contributor.level}
-                            />
-                        ))}
-                    </Row>
+
+                    {activeTab === 'list' ? (
+                        <>
+                            <ContributorSearch />
+                            <Spacer height={20} />
+                            <Row gutter={16} style={{ marginTop: 16, marginRight: 10, marginLeft: 10, minHeight: 600 }}>
+                                <ContributorCard
+                                    key="cool-hedgehog"
+                                    name="the-cool-hedgehog"
+                                    link="https://github.com/PostHog/posthog"
+                                    imageSrc="https://posthog.com/static/cool-hedgehog-2e771b8385a05bfe25cfdea4bbb775a3.svg"
+                                    contributions={['code', 'doc', 'plugin', 'bug']}
+                                    mvpWins={2}
+                                    contributorLevel={99}
+                                />
+                                {filteredContributors.map((contributor: Contributor) => (
+                                    <ContributorCard
+                                        key={contributor.login}
+                                        name={contributor.login}
+                                        link={contributor.profile}
+                                        imageSrc={contributor.avatar_url}
+                                        contributions={contributor.contributions}
+                                        mvpWins={contributor.mvpWins}
+                                        contributorLevel={contributor.level}
+                                    />
+                                ))}
+                            </Row>
+                        </>
+                    ) : null}
                 </div>
                 <Spacer />
             </Layout>
