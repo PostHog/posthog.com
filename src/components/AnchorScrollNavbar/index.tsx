1c64b76c093ebd6b431641fd8950672b519c862c
Mike Nicklas <mike.nicklas@gmail.com>
2021-03-26 07:39:35 -0400

    Setup smooth scrolling and mobile select

--- src/components/AnchorScrollNavbar/index.tsx
+++ src/components/AnchorScrollNavbar/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'gatsby'
+import scrollTo from 'gatsby-plugin-smoothscroll'
 
 const ButtonLink = ({
     section,
@@ -12,10 +14,20 @@ const ButtonLink = ({
     const baseClasses = 'px-3 py-2 rounded'
     const classList =
         section == currentSection
-            ? baseClasses.concat(' bg-primary text-white')
-            : baseClasses.concat(' text-white text-opacity-80 hover:bg-gray-100 hover:bg-opacity-10')
+            ? baseClasses.concat(' bg-primary text-white hover:text-white')
+            : baseClasses.concat(' text-white text-opacity-80 hover:bg-gray-100 hover:bg-opacity-10 hover:text-white')
+    const sectionSelector = `#${section}`
 
-    return <button className={classList}>{children}</button>
+    const clickHandler = (e) => {
+        e.preventDefault()
+        scrollTo(sectionSelector)
+    }
+
+    return (
+        <Link to={sectionSelector} className={classList} onClick={clickHandler}>
+            {children}
+        </Link>
+    )
 }
 
 interface AnchorScrollNavbarProps {
@@ -23,14 +35,14 @@ interface AnchorScrollNavbarProps {
 }
 
 export const AnchorScrollNavbar = ({ className = '' }: AnchorScrollNavbarProps) => {
-    const baseClasses = 'rounded w-full max-w-3xl mx-auto flex justify-between items-stetch p-3 sticky top-3 z-10'
+    const baseClasses =
+        'rounded w-11/12 max-w-3xl mx-auto justify-between items-stetch p-3 sticky top-3 z-10 hidden lg:flex'
     const classList = [baseClasses, className].join(' ')
 
     const [currentSection, setCurrentSection] = useState('why-were-here')
 
     useEffect(() => {
         // @todo - throttle this
-
         const sections = {
             culture: document.getElementById('culture')?.offsetTop,
             interviewProcess: document.getElementById('interview-process')?.offsetTop,
@@ -59,25 +71,41 @@ export const AnchorScrollNavbar = ({ className = '' }: AnchorScrollNavbarProps) 
     }, [])
 
     return (
-        <div className={classList} style={{ backgroundColor: '#202038' }}>
-            <ButtonLink section="why-were-here" currentSection={currentSection}>
-                Why we're here
-            </ButtonLink>
-            <ButtonLink section="culture" currentSection={currentSection}>
-                Our culture
-            </ButtonLink>
-            <ButtonLink section="interview-process" currentSection={currentSection}>
-                Interview process
-            </ButtonLink>
-            <ButtonLink section="benefits" currentSection={currentSection}>
-                Benefits
-            </ButtonLink>
-            <ButtonLink section="working-at-posthog" currentSection={currentSection}>
-                Working at PostHog
-            </ButtonLink>
-            <ButtonLink section="open-roles" currentSection={currentSection}>
-                Open roles
-            </ButtonLink>
-        </div>
+        <>
+            <div className={classList} style={{ backgroundColor: '#202038' }}>
+                <ButtonLink section="why-were-here" currentSection={currentSection}>
+                    Why we're here
+                </ButtonLink>
+                <ButtonLink section="culture" currentSection={currentSection}>
+                    Our culture
+                </ButtonLink>
+                <ButtonLink section="interview-process" currentSection={currentSection}>
+                    Interview process
+                </ButtonLink>
+                <ButtonLink section="benefits" currentSection={currentSection}>
+                    Benefits
+                </ButtonLink>
+                <ButtonLink section="working-at-posthog" currentSection={currentSection}>
+                    Working at PostHog
+                </ButtonLink>
+                <ButtonLink section="open-roles" currentSection={currentSection}>
+                    Open roles
+                </ButtonLink>
+            </div>
+
+            <select
+                className="appearance-none text-white w-11/12 p-3 sticky top-3 z-10 mx-auto block max-w-3xl rounded lg:hidden"
+                style={{ backgroundColor: '#202038' }}
+                value={currentSection}
+                onChange={(e) => scrollTo(`#${e.target.value}`)}
+            >
+                <option value="why-were-here">Why we're here</option>
+                <option value="culture">Our culture</option>
+                <option value="interview-process">Interview process</option>
+                <option value="benefits">Benefits</option>
+                <option value="working-at-posthog">Working at PostHog</option>
+                <option value="open-roles">Open roles</option>
+            </select>
+        </>
     )
 }
