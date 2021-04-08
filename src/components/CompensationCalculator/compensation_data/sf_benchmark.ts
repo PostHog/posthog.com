2649998a64585433d9682f5aa70b0d775e3396f1
eltjehelene <75622766+eltjehelene@users.noreply.github.com>
2021-04-08 20:06:37 +0100

    Update sf_benchmark.ts (#1163)
    * Update sf_benchmark.ts
    
    * Update sf_benchmark.ts

--- src/components/CompensationCalculator/compensation_data/sf_benchmark.ts
+++ src/components/CompensationCalculator/compensation_data/sf_benchmark.ts
@@ -1,8 +1,11 @@
 export const sfBenchmark: Record<string, number> = {
-    Engineer: 216000,
+    'Full Stack Engineer': 216000,
     'Sales Engineer': 190000,
     'Site Reliability Engineer': 205000,
+    'Mobile Engineer': 200000,
     'Product Marketer': 195000,
+    'Product Designer': 186000,
+  
     // 'Content Marketer': 165000,
     // 'Recruiter': 144000,
     // 'Technical Writer': 138000,
