dff1125dc341b6038ad03edfe04e8ee81ae411b3
Mike Nicklas <mike.nicklas@gmail.com>
2021-04-16 08:13:32 -0400

    Dark mode category links

--- src/components/Blog/BlogSidebar/index.tsx
+++ src/components/Blog/BlogSidebar/index.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { BlogCategories, CategoryInterface } from '../constants/categories'
 
+const linkClassList =
+    'block text-gray-900 dark:text-gray-100 my-2 text-sm hover:underline hover:text-gray-900 dark:hover:text-gray-100'
+
 export const BlogSidebar = () => {
     const blogCategoryLinks = BlogCategories.map((category: CategoryInterface) => {
         return (
-            <Link
-                to={category.link}
-                key={category.slug}
-                className="block text-gray-900 my-2 text-sm hover:underline hover:text-gray-900"
-            >
+            <Link to={category.link} key={category.slug} className={linkClassList}>
                 {category.title}
             </Link>
         )
@@ -21,21 +20,21 @@ export const BlogSidebar = () => {
             {blogCategoryLinks}
 
             <header className="mt-12 text-xs text-gray-400 uppercase">More Reads</header>
-            <a href="/tutorials" className="block text-gray-900 my-2 text-sm hover:underline hover:text-gray-900">
+            <a href="/tutorials" className={linkClassList}>
                 Tutorials
             </a>
-            <a href="/release-notes" className="block text-gray-900 my-2 text-sm hover:underline hover:text-gray-900">
+            <a href="/release-notes" className={linkClassList}>
                 Release notes
             </a>
 
             <header className="mt-12 text-xs text-gray-400 uppercase">Follow Us</header>
-            <a href="/twitter" className="block text-gray-900 my-2 text-sm hover:underline hover:text-gray-900">
+            <a href="/twitter" className={linkClassList}>
                 Twitter
             </a>
-            <a href="/linked-in" className="block text-gray-900 my-2 text-sm hover:underline hover:text-gray-900">
+            <a href="/linked-in" className={linkClassList}>
                 LinkedIn
             </a>
-            <a href="/youtube" className="block text-gray-900 my-2 text-sm hover:underline hover:text-gray-900">
+            <a href="/youtube" className={linkClassList}>
                 YouTube
             </a>
         </div>
