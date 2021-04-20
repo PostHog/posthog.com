1c6b880eb3e323e4517b4ecd70ea631e8adcc51d
eltjehelene <75622766+eltjehelene@users.noreply.github.com>
2021-04-20 09:58:00 +0100

    Add FE Dev and recruitment ops salary

--- src/components/CompensationCalculator/compensation_data/sf_benchmark.ts
+++ src/components/CompensationCalculator/compensation_data/sf_benchmark.ts
@@ -1,10 +1,12 @@
 export const sfBenchmark: Record<string, number> = {
     'Full Stack Engineer': 216000,
+    'Front End Developer': 199000,
     'Sales Engineer': 190000,
     'Site Reliability Engineer': 205000,
     'Mobile Engineer': 200000,
     'Product Marketer': 195000,
     'Product Designer': 186000,
+    ' Recruitment & Operations Coordinator': 99000,
 
     // 'Content Marketer': 165000,
     // 'Recruiter': 144000,
