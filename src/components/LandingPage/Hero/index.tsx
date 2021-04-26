a87fe294e07979b4307f3806d64b4c7f40931999
Cory Watilo <corywatilo@gmail.com>
2021-04-26 15:28:58 -0400

    new hero icons, headline

--- src/components/LandingPage/Hero/index.tsx
+++ src/components/LandingPage/Hero/index.tsx
@@ -7,28 +7,23 @@ export const Hero = () => {
     return (
         <div className="hero py-12 sm:py-24">
             <div className="w-11/12 mx-auto text-center relative z-10 rounded">
-                <div className="max-w-3xl mx-auto">
-                    <h1 className="text-white mb-0 pb-0">More than product analytics</h1>
-                    <p className="text-white mt-4 max-w-xl mx-auto">
-                        <span className="opacity-70">PostHog is an</span>{' '}
-                        <a
-                            href="https://github.com/PostHog/"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="text-orange opacity-90 hover:opacity-100 hover:text-orange"
-                        >
-                            open source platform
-                        </a>{' '}
-                        <span className="opacity-70">
-                            designed to help you understand customers, quantify value, and ship new features faster.
-                        </span>
-                    </p>
+                <div className="max-w-full mx-auto">
+                    <h1 className="text-white mb-0 pb-0 leading-tight">
+                        The <span className="relative open-source">platform</span> for{' '}
+                        <span className="text-orange">building better products</span>
+                    </h1>
                 </div>
 
                 <div className="max-w-4xl mx-auto">
                     <ProductFeatureIcons />
                 </div>
+
+                <p className="text-white mt-4 max-w-xl mx-auto">
+                    <span className="opacity-70">+ Heatmaps, Session Browsing, Autocapture &amp; more</span>
+                </p>
+
                 <LandingPageCallToAction />
+
                 <SocialProof />
             </div>
         </div>
--- src/components/LandingPage/ProductFeatureIcons/index.tsx
+++ src/components/LandingPage/ProductFeatureIcons/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 
+import eventPipelineImg from '../images/ProductFeatureIcons/eventPipeline.svg'
 import analyticsImg from '../images/ProductFeatureIcons/analytics.svg'
-import heatmapsImg from '../images/ProductFeatureIcons/heatmaps.svg'
 import sessionReplayImg from '../images/ProductFeatureIcons/session-replay.svg'
 import featureFlagsImg from '../images/ProductFeatureIcons/feature-flags.svg'
-import abTestingImg from '../images/ProductFeatureIcons/ab-testing.svg'
-import userFeedbackImg from '../images/ProductFeatureIcons/user-feedback.svg'
-import revenueTrackingImg from '../images/ProductFeatureIcons/revenue-tracking.svg'
+import dataLakesImg from '../images/ProductFeatureIcons/dataLakes.svg'
 
 interface ProductIconData {
     label: string
     icon: string
+    subtitle?: string
     expectedLaunchDate?: string
 }
 
-const ProductFeatureIcon = ({ label, icon, expectedLaunchDate }: ProductIconData) => {
+const ProductFeatureIcon = ({ label, icon, subtitle, expectedLaunchDate }: ProductIconData) => {
     const opacity = expectedLaunchDate ? 'opacity-50' : 'opacity-100'
     const expectedLabel = expectedLaunchDate ? (
         <span className="text-white uppercase text-2xs opacity-75">{expectedLaunchDate}</span>
@@ -26,6 +25,7 @@ const ProductFeatureIcon = ({ label, icon, expectedLaunchDate }: ProductIconData
                 <img src={icon} alt={label} className="mb-0 block mx-auto" />
             </div>
             <span className="text-white block mt-1 font-bold">{label}</span>
+            <div className="flex-grow text-white">{subtitle}</div>
             <div className="flex-grow">{expectedLabel}</div>
         </div>
     )
@@ -33,14 +33,12 @@ const ProductFeatureIcon = ({ label, icon, expectedLaunchDate }: ProductIconData
 
 export const ProductFeatureIcons = () => {
     return (
-        <div className="product-features justify-center mt-8 mb-8 lg:my-16 grid lg:grid-cols-8">
-            <ProductFeatureIcon label="Analytics" icon={analyticsImg} />
-            <ProductFeatureIcon label="Heatmaps" icon={heatmapsImg} />
-            <ProductFeatureIcon label="Session Replay" icon={sessionReplayImg} />
-            <ProductFeatureIcon label="Feature Flags" icon={featureFlagsImg} />
-            <ProductFeatureIcon label="A/B Testing" icon={abTestingImg} expectedLaunchDate="May" />
-            <ProductFeatureIcon label="User Feedback" icon={userFeedbackImg} expectedLaunchDate="June" />
-            <ProductFeatureIcon label="Revenue Tracking" icon={revenueTrackingImg} expectedLaunchDate="August" />
+        <div className="product-features justify-center mt-8 mb-8 lg:my-16 inline-grid">
+            <ProductFeatureIcon label="Ingest product data" icon={eventPipelineImg} subtitle="with event pipeline" />
+            <ProductFeatureIcon label="Learn" icon={analyticsImg} subtitle="with analytics suite" />
+            <ProductFeatureIcon label="Test &amp; iterate" icon={featureFlagsImg} subtitle="with feature flags" />
+            <ProductFeatureIcon label="Watch adoption" icon={sessionReplayImg} subtitle="with session recordings" />
+            <ProductFeatureIcon label="Export" icon={dataLakesImg} subtitle="to data lakes" />
         </div>
     )
 }
