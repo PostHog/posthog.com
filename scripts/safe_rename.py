494c7757edb070bff8561bb440a867d64e4be47f
yakkomajuri <yakko.majuri@gmail.com>
2021-04-27 11:13:59 +0000

    never fail bot

--- scripts/safe_rename.py
+++ scripts/safe_rename.py
@@ -1,31 +1,34 @@
 import re
 import requests
 
-with open("./pr_diff", "r") as git_diff_file:
-    git_diff = git_diff_file.read()
+try:
+    with open("./pr_diff", "r") as git_diff_file:
+        git_diff = git_diff_file.read()
 
-rename_from_regex = r'rename from contents(.*).md'
-rename_to_regex = r'rename to contents(.*).md'
+    rename_from_regex = r'rename from contents(.*).md'
+    rename_to_regex = r'rename to contents(.*).md'
 
 
-from_paths = re.findall(rename_from_regex, git_diff)
-to_paths = re.findall(rename_to_regex, git_diff)
+    from_paths = re.findall(rename_from_regex, git_diff)
+    to_paths = re.findall(rename_to_regex, git_diff)
 
-redirect_text = '''
+    redirect_text = '''
 
-[[redirects]]
-    from = "{}"
-    to = "{}"
-'''
+    [[redirects]]
+        from = "{}"
+        to = "{}"
+    '''
 
-if len(from_paths) > 0 and len(from_paths) == len(to_paths):
-    netlify_config_text = requests.get('https://raw.githubusercontent.com/PostHog/posthog.com/master/netlify.toml').text
-    
-    new_redirects = ''
+    if len(from_paths) > 0 and len(from_paths) == len(to_paths):
+        netlify_config_text = requests.get('https://raw.githubusercontent.com/PostHog/posthog.com/master/netlify.toml').text
 
-    for i in range(len(from_paths)):
-        if from_paths[i] not in netlify_config_text:
-            new_redirects += redirect_text.format(from_paths[i], to_paths[i])
-    
-    with open("./netlify.toml", "a") as netlify_config:
-        netlify_config.write(new_redirects)
+        new_redirects = ''
+
+        for i in range(len(from_paths)):
+            if from_paths[i] not in netlify_config_text:
+                new_redirects += redirect_text.format(from_paths[i], to_paths[i])
+
+        with open("./netlify.toml", "a") as netlify_config:
+            netlify_config.write(new_redirects)
+except Exception as e:
+    print(e)
