cf29917259f13988aba99b67485618e0fa8ffc52
Mike Nicklas <mike.nicklas@gmail.com>
2021-05-13 07:32:09 -0700

    Strip out some of the content

---
+++ src/components/Pricing/FAQs/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Structure } from '../../Structure'
+
+export const FAQs = () => {
+    return (
+        <div className="pricing-hero text-white text-center">
+            <Structure.Section width="4xl" className="py-12">
+                <Structure.SectionHeader titleTag="h3" title="FAQ" />
+            </Structure.Section>
+        </div>
+    )
+}
---
+++ src/pages/pricing-new.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { SEO } from '../components/seo'
+import { PricingHero } from '../components/Pricing/PricingHero'
+import { PricingTable } from '../components/Pricing/PricingTable'
+import { RocketHorizontalRule } from '../components/RocketHorizontalRule'
+import { PricingComparison } from '../components/Pricing/PricingComparison'
+import { Savings } from '../components/Pricing/Savings'
+import { FAQs } from '../components/Pricing/FAQs'
+import { Footer } from '../components/Footer'
+
+import '../components/Pricing/styles/index.scss'
+
+const PricingNew = (): JSX.Element => {
+    return (
+        <div className="pricing-hero">
+            <SEO title="PostHog Pricing" description="Find out how much it costs to use PostHog" />
+
+            <PricingHero />
+            <PricingTable />
+            <RocketHorizontalRule />
+            <PricingComparison />
+            <Savings />
+            <FAQs />
+            <Footer onPostPage={false} transparentBg />
+        </div>
+    )
+}
+
+export default PricingNew
