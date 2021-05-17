ac824eb5fc9c66eef0192ad702f9179cd639534f
kunal <kunal@Kunals-iMac.hsd1.ca.comcast.net>
2021-05-17 09:51:12 -0700

    Linkify latest post title

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -40,7 +40,12 @@ const FeaturedPost = ({ post }: { post: PostType }) => {
             <div className="w-full md:w-1/2 md:pr-8 py-4 mx-auto">
                 <span className="text-gray-400 text-xs uppercase">Latest Post</span>
                 <header className="text-4xl text-gray-900 dark:text-gray-100 font-gosha mt-1">
-                    {post.frontmatter.title}
+                    <Link
+                        to={post.fields.slug}
+                        className="text-gray-900 hover:text-gray-900 dark:text-gray-100 dark:hover:text-gray-100 hover:underline"
+                    >
+                        {post.frontmatter.title}
+                    </Link>
                 </header>
                 <div className="text-gray-500 dark:text-gray-300 mt-2 text-sm leading-relaxed font-inter">
                     {post.excerpt}
