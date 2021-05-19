170541c3a603362e0ee2652bc7cd24474585e9d8
Gianluca Esposito <g@esposi.to>
2021-05-19 19:05:26 +0200

    Add StaticImage component in ContributorAvatars

--- src/components/ContributorAvatars/index.js
+++ src/components/ContributorAvatars/index.js
@@ -1,65 +1,91 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export const ContributorAvatars = () => {
     return (
         <div className="flex flex-wrap justify-center contributor-images -mx-10 md:w-full md:mx-0">
-            <img
+            <StaticImage
                 src="https://github.com/bhavish-agarwal.png?v=3&s=100"
-                title="bhavish-agarwal"
-                width="50"
-                height="50"
+                alt="bhavish-agarwal"
+                width={50}
+                height={50}
             />
-            <img src="https://github.com/Tannergoods.png?v=3&s=100" title="Tannergoods" width="50" height="50" />
-            <img src="https://github.com/ungless.png?v=3&s=100" title="ungless" width="50" height="50" />
-            <img src="https://github.com/gzog.png?v=3&s=100" title="gzog" width="50" height="50" />
-            <img src="https://github.com/Tmunayyer.png?v=3&s=100" title="Tmunayyer" width="50" height="50" />
-            <img src="https://github.com/adamb70.png?v=3&s=100" title="adamb70" width="50" height="50" />
-            <img src="https://github.com/samcaspus.png?v=3&s=100" title="samcaspus" width="50" height="50" />
-            <img src="https://github.com/SanketDG.png?v=3&s=100" title="SanketDG" width="50" height="50" />
-            <img src="https://github.com/dependabot-bot.png?v=3&s=100" title="dependabot[bot]" width="50" height="50" />
-            <img src="https://github.com/J0.png?v=3&s=100" title="J0" width="50" height="50" />
-            <img src="https://github.com/14MR.png?v=3&s=100" title="14MR" width="50" height="50" />
-            <img src="https://github.com/03difoha.png?v=3&s=100" title="03difoha" width="50" height="50" />
-            <img src="https://github.com/ahtik.png?v=3&s=100" title="ahtik" width="50" height="50" />
-            <img src="https://github.com/Algogator.png?v=3&s=100" title="Algogator" width="50" height="50" />
-            <img src="https://github.com/GalDayan.png?v=3&s=100" title="GalDayan" width="50" height="50" />
-            <img src="https://github.com/Kacppian.png?v=3&s=100" title="Kacppian" width="50" height="50" />
-            <img src="https://github.com/FUSAKLA.png?v=3&s=100" title="FUSAKLA" width="50" height="50" />
-            <img src="https://github.com/iMerica.png?v=3&s=100" title="iMerica" width="50" height="50" />
-            <img src="https://github.com/pedroapfilho.png?v=3&s=100" title="pedroapfilho" width="50" height="50" />
-            <img src="https://github.com/eLRuLL.png?v=3&s=100" title="eLRuLL" width="50" height="50" />
-            <img
-                src="https://github.com/stevenphaedonos.png?v=3&s=100"
-                title="stevenphaedonos"
-                width="50"
-                height="50"
-            />
-            <img src="https://github.com/tapico-weyert.png?v=3&s=100" title="tapico-weyert" width="50" height="50" />
-            <img
-                src="https://github.com/adamschoenemann.png?v=3&s=100"
-                title="adamschoenemann"
-                width="50"
-                height="50"
-            />
-            <img
-                src="https://github.com/AlexandreBonaventure.png?v=3&s=100"
-                title="AlexandreBonaventure"
-                width="50"
-                height="50"
-            />
-            <img src="https://github.com/dan-dr.png?v=3&s=100" title="dan-dr" width="50" height="50" />
-            <img src="https://github.com/dts.png?v=3&s=100" title="dts" width="50" height="50" />
-            <img src="https://github.com/jamiehaywood.png?v=3&s=100" title="jamiehaywood" width="50" height="50" />
-            <img
+            <StaticImage src="https://github.com/Tannergoods.png?v=3&s=100" alt="Tannergoods" width={50} height={50} />
+            <StaticImage src="https://github.com/ungless.png?v=3&s=100" alt="ungless" width={50} height={50} />
+            <StaticImage src="https://github.com/gzog.png?v=3&s=100" alt="gzog" width={50} height={50} />
+            <StaticImage src="https://github.com/Tmunayyer.png?v=3&s=100" alt="Tmunayyer" width={50} height={50} />
+            <StaticImage src="https://github.com/adamb70.png?v=3&s=100" alt="adamb70" width={50} height={50} />
+            <StaticImage src="https://github.com/samcaspus.png?v=3&s=100" alt="samcaspus" width={50} height={50} />
+            <StaticImage src="https://github.com/SanketDG.png?v=3&s=100" alt="SanketDG" width={50} height={50} />
+            <StaticImage
                 src="https://github.com/dependabot-bot.png?v=3&s=100"
-                title="dependabot-preview[bot]"
-                width="50"
-                height="50"
+                alt="dependabot[bot]"
+                width={50}
+                height={50}
             />
-            <img src="https://github.com/rushabhnagda11.png?v=3&s=100" title="rushabhnagda11" width="50" height="50" />
-            <img src="https://github.com/weyert.png?v=3&s=100" title="weyert" width="50" height="50" />
-            <img src="https://github.com/casio.png?v=3&s=100" title="casio" width="50" height="50" />
-            <img src="https://github.com/Hungsiro506.png?v=3&s=100" title="Hungsiro506" width="50" height="50" />
+            <StaticImage src="https://github.com/J0.png?v=3&s=100" alt="J0" width={50} height={50} />
+            <StaticImage src="https://github.com/14MR.png?v=3&s=100" alt="14MR" width={50} height={50} />
+            <StaticImage src="https://github.com/03difoha.png?v=3&s=100" alt="03difoha" width={50} height={50} />
+            <StaticImage src="https://github.com/ahtik.png?v=3&s=100" alt="ahtik" width={50} height={50} />
+            <StaticImage src="https://github.com/Algogator.png?v=3&s=100" alt="Algogator" width={50} height={50} />
+            <StaticImage src="https://github.com/GalDayan.png?v=3&s=100" alt="GalDayan" width={50} height={50} />
+            <StaticImage src="https://github.com/Kacppian.png?v=3&s=100" alt="Kacppian" width={50} height={50} />
+            <StaticImage src="https://github.com/FUSAKLA.png?v=3&s=100" alt="FUSAKLA" width={50} height={50} />
+            <StaticImage src="https://github.com/iMerica.png?v=3&s=100" alt="iMerica" width={50} height={50} />
+            <StaticImage
+                src="https://github.com/pedroapfilho.png?v=3&s=100"
+                alt="pedroapfilho"
+                width={50}
+                height={50}
+            />
+            <StaticImage src="https://github.com/eLRuLL.png?v=3&s=100" alt="eLRuLL" width={50} height={50} />
+            <StaticImage
+                src="https://github.com/stevenphaedonos.png?v=3&s=100"
+                alt="stevenphaedonos"
+                width={50}
+                height={50}
+            />
+            <StaticImage
+                src="https://github.com/tapico-weyert.png?v=3&s=100"
+                alt="tapico-weyert"
+                width={50}
+                height={50}
+            />
+            <StaticImage
+                src="https://github.com/adamschoenemann.png?v=3&s=100"
+                alt="adamschoenemann"
+                width={50}
+                height={50}
+            />
+            <StaticImage
+                src="https://github.com/AlexandreBonaventure.png?v=3&s=100"
+                alt="AlexandreBonaventure"
+                width={50}
+                height={50}
+            />
+            <StaticImage src="https://github.com/dan-dr.png?v=3&s=100" alt="dan-dr" width={50} height={50} />
+            <StaticImage src="https://github.com/dts.png?v=3&s=100" alt="dts" width={50} height={50} />
+            <StaticImage
+                src="https://github.com/jamiehaywood.png?v=3&s=100"
+                alt="jamiehaywood"
+                width={50}
+                height={50}
+            />
+            <StaticImage
+                src="https://github.com/dependabot-bot.png?v=3&s=100"
+                alt="dependabot-preview[bot]"
+                width={50}
+                height={50}
+            />
+            <StaticImage
+                src="https://github.com/rushabhnagda11.png?v=3&s=100"
+                alt="rushabhnagda11"
+                width={50}
+                height={50}
+            />
+            <StaticImage src="https://github.com/weyert.png?v=3&s=100" alt="weyert" width={50} height={50} />
+            <StaticImage src="https://github.com/casio.png?v=3&s=100" alt="casio" width={50} height={50} />
+            <StaticImage src="https://github.com/Hungsiro506.png?v=3&s=100" alt="Hungsiro506" width={50} height={50} />
         </div>
     )
 }
