8036a5b075a468d65ea06b31a0f087c3bdb2a5f9
Gianluca Esposito <g@esposi.to>
2021-05-19 19:04:01 +0200

    Update avatars' urls

--- src/components/ContributorAvatars/index.js
+++ src/components/ContributorAvatars/index.js
@@ -4,97 +4,62 @@ export const ContributorAvatars = () => {
     return (
         <div className="flex flex-wrap justify-center contributor-images -mx-10 md:w-full md:mx-0">
             <img
-                src="https://avatars.githubusercontent.com/u/14195048?v=4"
+                src="https://github.com/bhavish-agarwal.png?v=3&s=100"
                 title="bhavish-agarwal"
                 width="50"
                 height="50"
             />
+            <img src="https://github.com/Tannergoods.png?v=3&s=100" title="Tannergoods" width="50" height="50" />
+            <img src="https://github.com/ungless.png?v=3&s=100" title="ungless" width="50" height="50" />
+            <img src="https://github.com/gzog.png?v=3&s=100" title="gzog" width="50" height="50" />
+            <img src="https://github.com/Tmunayyer.png?v=3&s=100" title="Tmunayyer" width="50" height="50" />
+            <img src="https://github.com/adamb70.png?v=3&s=100" title="adamb70" width="50" height="50" />
+            <img src="https://github.com/samcaspus.png?v=3&s=100" title="samcaspus" width="50" height="50" />
+            <img src="https://github.com/SanketDG.png?v=3&s=100" title="SanketDG" width="50" height="50" />
+            <img src="https://github.com/dependabot-bot.png?v=3&s=100" title="dependabot[bot]" width="50" height="50" />
+            <img src="https://github.com/J0.png?v=3&s=100" title="J0" width="50" height="50" />
+            <img src="https://github.com/14MR.png?v=3&s=100" title="14MR" width="50" height="50" />
+            <img src="https://github.com/03difoha.png?v=3&s=100" title="03difoha" width="50" height="50" />
+            <img src="https://github.com/ahtik.png?v=3&s=100" title="ahtik" width="50" height="50" />
+            <img src="https://github.com/Algogator.png?v=3&s=100" title="Algogator" width="50" height="50" />
+            <img src="https://github.com/GalDayan.png?v=3&s=100" title="GalDayan" width="50" height="50" />
+            <img src="https://github.com/Kacppian.png?v=3&s=100" title="Kacppian" width="50" height="50" />
+            <img src="https://github.com/FUSAKLA.png?v=3&s=100" title="FUSAKLA" width="50" height="50" />
+            <img src="https://github.com/iMerica.png?v=3&s=100" title="iMerica" width="50" height="50" />
+            <img src="https://github.com/pedroapfilho.png?v=3&s=100" title="pedroapfilho" width="50" height="50" />
+            <img src="https://github.com/eLRuLL.png?v=3&s=100" title="eLRuLL" width="50" height="50" />
             <img
-                src="https://avatars.githubusercontent.com/u/60791437?v=f"
-                title="Tannergoods"
-                width="50"
-                height="50"
-            />
-            <img src="https://avatars.githubusercontent.com/u/8397061?v=4" title="ungless" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/1487006?v=4" title="gzog" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/29887304?v=4" title="Tmunayyer" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/11885987?v=4" title="adamb70" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/19220113?v=4" title="samcaspus" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/8980971?v=4" title="SanketDG" width="50" height="50" />
-            <img
-                src="https://avatars.githubusercontent.com/in/29110?v=4"
-                title="dependabot[bot]"
-                width="50"
-                height="50"
-            />
-            <img src="https://avatars.githubusercontent.com/u/8011761?v=4" title="J0" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/5824170?v=4" title="14MR" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/8876615?v=4" title="03difoha" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/140952?v=4" title="ahtik" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/1433469?v=4" title="Algogator" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/24251369?v=4" title="GalDayan" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/14990078?v=4" title="Kacppian" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/6112562?v=4" title="FUSAKLA" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/487897?v=4" title="iMerica" width="50" height="50" />
-            <img
-                src="https://avatars.githubusercontent.com/u/13142568?v=4"
-                title="pedroapfilho"
-                width="50"
-                height="50"
-            />
-            <img src="https://avatars.githubusercontent.com/u/1459486?v=4" title="eLRuLL" width="50" height="50" />
-            <img
-                src="https://avatars.githubusercontent.com/u/12955616?v=4"
+                src="https://github.com/stevenphaedonos.png?v=3&s=100"
                 title="stevenphaedonos"
                 width="50"
                 height="50"
             />
+            <img src="https://github.com/tapico-weyert.png?v=3&s=100" title="tapico-weyert" width="50" height="50" />
             <img
-                src="https://avatars.githubusercontent.com/u/70971917?v=4"
-                title="tapico-weyert"
-                width="50"
-                height="50"
-            />
-            <img
-                src="https://avatars.githubusercontent.com/u/2095226?v=4"
+                src="https://github.com/adamschoenemann.png?v=3&s=100"
                 title="adamschoenemann"
                 width="50"
                 height="50"
             />
             <img
-                src="https://avatars.githubusercontent.com/u/4596409?v=4"
+                src="https://github.com/AlexandreBonaventure.png?v=3&s=100"
                 title="AlexandreBonaventure"
                 width="50"
                 height="50"
             />
-            <img src="https://avatars.githubusercontent.com/u/6669808?v=4" title="dan-dr" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/273856?v=4" title="dts" width="50" height="50" />
+            <img src="https://github.com/dan-dr.png?v=3&s=100" title="dan-dr" width="50" height="50" />
+            <img src="https://github.com/dts.png?v=3&s=100" title="dts" width="50" height="50" />
+            <img src="https://github.com/jamiehaywood.png?v=3&s=100" title="jamiehaywood" width="50" height="50" />
             <img
-                src="https://avatars.githubusercontent.com/u/26779712?v=4"
-                title="jamiehaywood"
-                width="50"
-                height="50"
-            />
-            <img
-                src="https://avatars.githubusercontent.com/in/2141?v=4"
+                src="https://github.com/dependabot-bot.png?v=3&s=100"
                 title="dependabot-preview[bot]"
                 width="50"
                 height="50"
             />
-            <img
-                src="https://avatars.githubusercontent.com/u/3235568?v=4"
-                title="rushabhnagda11"
-                width="50"
-                height="50"
-            />
-            <img src="https://avatars.githubusercontent.com/u/7049?v=4" title="weyert" width="50" height="50" />
-            <img src="https://avatars.githubusercontent.com/u/29784?v=4" title="casio" width="50" height="50" />
-            <img
-                src="https://avatars.githubusercontent.com/u/10346923?v=4"
-                title="Hungsiro506"
-                width="50"
-                height="50"
-            />
+            <img src="https://github.com/rushabhnagda11.png?v=3&s=100" title="rushabhnagda11" width="50" height="50" />
+            <img src="https://github.com/weyert.png?v=3&s=100" title="weyert" width="50" height="50" />
+            <img src="https://github.com/casio.png?v=3&s=100" title="casio" width="50" height="50" />
+            <img src="https://github.com/Hungsiro506.png?v=3&s=100" title="Hungsiro506" width="50" height="50" />
         </div>
     )
 }
