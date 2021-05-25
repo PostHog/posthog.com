cf9603ba31e47c128ad524b5503d7507e67430d0
pietrodevpiccini <78323924+pietrodevpiccini@users.noreply.github.com>
2021-05-26 01:43:21 +0500

    add FAQ, improve slider (#1406)
    * add FAQ, improve slider
    
    * styling faqs
    
    Co-authored-by: Cory Watilo <corywatilo@gmail.com>

--- src/components/PricingSlider/LogSlider.tsx
+++ src/components/PricingSlider/LogSlider.tsx
@@ -27,14 +27,26 @@ const prettyInt = (x: number): string => {
 export const sliderCurve = Math.exp
 export const inverseCurve = Math.log
 
+const SI_SYMBOL = ['', 'k', 'M']
+
+const abbreviateNumber = (number: number): string => {
+    const tier = (Math.log10(Math.abs(number)) / 3) | 0
+    if (tier == 0) return `${number}`
+    const suffix = SI_SYMBOL[tier]
+    const scale = Math.pow(10, tier * 3)
+    const scaled = number / scale
+    return `${scaled.toFixed(0) + suffix}`
+}
+
 const makeMarks = (marks: number[]): Record<number, string> => {
     return marks.reduce((acc, cur) => {
-        acc[inverseCurve(cur)] = prettyInt(cur)
+        acc[inverseCurve(cur)] = abbreviateNumber(cur)
         return acc
     }, {} as Record<number, string>)
 }
 
 export const LogSlider = ({ min, max, marks, stepsInRange, onChange }: LogSliderProps): JSX.Element => {
+    console.log(marks, makeMarks(marks))
     const { sliderValue } = useValues(pricingSliderLogic)
     return (
         <MySlider
