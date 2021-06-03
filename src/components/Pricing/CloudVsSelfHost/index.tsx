1a515eadcb024ce89f100d0227d2af7ada8e27e3
Cory Watilo <corywatilo@gmail.com>
2021-06-03 05:02:46 -0400

    added faq about free plan soft limits, consistency for PostHog Cloud and Self-hosted naming

--- src/components/Pricing/CloudVsSelfHost/index.tsx
+++ src/components/Pricing/CloudVsSelfHost/index.tsx
@@ -8,7 +8,7 @@ export const CloudVsSelfHost = () => {
     return (
         <div className="pricing-hero text-white text-center relative">
             <Structure.Section width="4xl" className="py-12">
-                <h3 className="text-center text-white">PostHog Cloud vs. self-hosting</h3>
+                <h3 className="text-center text-white">PostHog Cloud vs. self-hosted</h3>
                 <div className="bg-deep-blue bg-opacity-20 border border-white border-opacity-10 py-4 px-6 md:px-10 mx-auto mt-4 mb-10 max-w-4xl rounded-lg items-baseline text-left text-white backdrop-filter backdrop-blur">
                     <dl className="flex flex-col md:flex-row flex-wrap">
                         <dt style={{ flex: '0 0 33%' }}>
@@ -19,7 +19,7 @@ export const CloudVsSelfHost = () => {
                             Start using immediately and receive automatic upgrades as we release new features.
                         </dd>
                         <dt style={{ flex: '0 0 33%' }}>
-                            <h4 className="mb-0">Self-hosting</h4>
+                            <h4 className="mb-0">Self-hosted</h4>
                         </dt>
                         <dd className="text-base mb-0" style={{ flexBasis: '67%' }}>
                             <strong className="block mb-2 pt-2">User data stays on your infrastructure.</strong>
