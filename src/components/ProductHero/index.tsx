0f0cabfbafcf81ac78609f15b1e7dfeb2d6cbeda
Cory Watilo <corywatilo@gmail.com>
2021-07-02 15:54:01 -0400

    mobile product hero

--- src/components/ProductHero/index.tsx
+++ src/components/ProductHero/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { LandingPageCallToAction } from '../LandingPage/LandingPageCallToAction'
+import HeroAnimation720x1300 from '../productFeature/images/svgs/hero-720x1300.svg'
 import HeroAnimation1920x1080 from '../productFeature/images/svgs/hero-1920x1080.svg'
 import HeroAnimation1920x720 from '../productFeature/images/svgs/hero-1920x720.svg'
 
@@ -23,7 +24,7 @@ export const ProductHero = ({
     const backgroundColorClass = `bg-${bgColor}`
     return (
         <div className={`${componentKey}`}>
-            <div className="mt-[54%] xl:mt-[37.5%]">
+            <div className="mt-[180%] sm:mt-[54%] xl:mt-[37.5%]">
                 <div className="flex justify-center w-11/12 mx-auto mb-8 pt-12 flex-col relative z-10 text-center">
                     <div className="max-w-full hero-headline mt-4">
                         <div className="font-osiris lowercase text-base mb-4 text-white text-opacity-75">
@@ -48,11 +49,12 @@ export const ProductHero = ({
                 </div>
             </div>
 
-            <a href="#" className="absolute bottom-4 mb-12 xl:mb-4 z-20 p-8">
+            <a href="#" className="absolute bottom-0 sm:bottom-4 sm:mb-12 xl:mb-4 z-20 p-4 sm:p-8">
                 <span className="chevron"></span>
             </a>
 
-            <HeroAnimation1920x1080 className="xl:hidden absolute h-full w-full top-0 left-0" />
+            <HeroAnimation720x1300 className="sm:hidden absolute h-full w-full top-0 left-0" />
+            <HeroAnimation1920x1080 className="hidden sm:block xl:hidden absolute h-full w-full top-0 left-0" />
             <HeroAnimation1920x720 className="hidden xl:block absolute h-full w-full top-0 left-0" />
         </div>
     )
