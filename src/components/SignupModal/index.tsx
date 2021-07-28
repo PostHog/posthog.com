04375c620615c6042a5068671a9c48a5fb57d12d
Sam Winslow <sammywinslow@gmail.com>
2021-07-28 10:42:49 -0400

    build out form components

--- src/components/SignupModal/index.tsx
+++ src/components/SignupModal/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Modal from 'react-modal'
 import './SignupModal.scss'
-import { Input } from 'antd'
+import { Button, Input } from 'antd'
 
-export const SignupModal = () => {
+export const SignupModal = (): JSX.Element => {
     return (
         <Modal
             isOpen={true}
@@ -14,8 +13,24 @@ export const SignupModal = () => {
             ariaHideApp={false}
         >
             <h3 className="font-sans">Unlock insights.</h3>
-            <p>What's your email?</p>
-            <Input bordered={false} /* TODO older antd doesn't support bordered prop. */ />
+            <form
+                name="gated-signup"
+                onSubmit={(data) => {
+                    console.log({ data }) //TODO
+                }}
+            >
+                <p>What's your email?</p>
+                <Input name="email" type="email" className="signup-email-field" placeholder="Email address" />
+                <Button htmlType="submit" type="primary" block>
+                    Next
+                </Button>
+                <div className="bottom-actions">
+                    <Button htmlType="submit" type="link">
+                        <strong>Next: </strong> Choose your edition
+                    </Button>
+                    <Button type="link">Skip</Button>
+                </div>
+            </form>
         </Modal>
     )
 }
