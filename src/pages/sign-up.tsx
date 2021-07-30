ee2ef4b25c6182b8772dd4c65388554d2e269a1b
Sam Winslow <sammywinslow@gmail.com>
2021-07-30 09:35:08 -0400

    custom icon lockups

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -7,7 +7,9 @@ import { SignupModal } from 'components/SignupModal'
 import { useActions, useValues } from 'kea'
 import { Realm, signupLogic, SignupModalView } from 'logic/signupLogic'
 import './styles/sign-up.scss'
+import posthogIcon from '../../contents/images/logos/posthog-icon-black.svg'
 import { ButtonBlock } from 'components/ButtonBlock/ButtonBlock'
+import { BadgeWrapper, CloudIcon, OpenSourceIcon, ServerIcon, UpAndToTheRightIcon } from 'components/Icons/Icons'
 
 const BenefitsList = ({ items }: { items: (string | React.ReactNode)[] }): JSX.Element => {
     return (
@@ -53,8 +55,37 @@ const SignUpPage = (): JSX.Element => {
                         </div>
                         <div className="grid grid-cols-2 gap-4 pricing-grid">
                             <div className="pricing-card">
-                                <h2 className="bg-yellow">Self-host</h2>
+                                <h2 className="bg-yellow">
+                                    <ServerIcon />
+                                    Self-host
+                                </h2>
                                 <div className="card-body">
+                                    <div className="icon-flex-row">
+                                        <div className="icon-lockup">
+                                            <BadgeWrapper
+                                                style={{
+                                                    background: 'rgba(251, 79, 13, 0.1)',
+                                                    color: '#F75A01',
+                                                }}
+                                            >
+                                                <OpenSourceIcon />
+                                            </BadgeWrapper>
+                                            <h3 className="font-sans">Open source</h3>
+                                            <p>Free forever</p>
+                                        </div>
+                                        <div className="icon-lockup">
+                                            <BadgeWrapper
+                                                style={{
+                                                    background: 'rgba(58, 94, 244, 0.1)',
+                                                    color: '#3A5EF4',
+                                                }}
+                                            >
+                                                <UpAndToTheRightIcon />
+                                            </BadgeWrapper>
+                                            <h3 className="font-sans">Enterprise</h3>
+                                            <p>From $2k/mo</p>
+                                        </div>
+                                    </div>
                                     <p>Benefits</p>
                                     <BenefitsList
                                         items={[
@@ -74,8 +105,18 @@ const SignUpPage = (): JSX.Element => {
                                 <footer>Self serve. Deploy with Docker, AWS, GCS + more</footer>
                             </div>
                             <div className="pricing-card">
-                                <h2 className="bg-blue">Hosted</h2>
+                                <h2 className="bg-blue">
+                                    <CloudIcon />
+                                    Hosted
+                                </h2>
                                 <div className="card-body">
+                                    <div className="icon-flex-row">
+                                        <div className="icon-lockup">
+                                            <img src={posthogIcon} style={{ height: 36 }} className="posthog-icon" />
+                                            <h3 className="font-sans">PostHog Cloud</h3>
+                                            <p>Pay per event</p>
+                                        </div>
+                                    </div>
                                     <p>Benefits</p>
                                     <BenefitsList
                                         items={[
