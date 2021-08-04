043b616a8395748dfbd2182f2b059822d50743da
Sam Winslow <sammywinslow@gmail.com>
2021-08-03 22:15:12 -0400

    introduce feature flags to constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const FEATURE_FLAGS = {
+    EMAIL_GATED_SIGNUP_OLD_FLOW: 'email-gated-signup-old-flow',
+    EMAIL_GATED_SIGNUP_NOT_SKIPPABLE: 'email-gated-signup-not-skippable',
+    EMAIL_GATED_SIGNUP_SKIPPABLE: 'email-gated-signup-skippable',
+}
