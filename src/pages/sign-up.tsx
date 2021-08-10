defa7735b3c5ba67f4fc33afc7b5c49bd15cd106
Sam Winslow <sammywinslow@gmail.com>
2021-08-10 16:06:49 -0400

    render nothing on the intermediate page if a redirect should occur

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -10,7 +10,7 @@ import './styles/sign-up.scss'
 import { DeploymentOptionsGrid } from 'components/DeploymentOptionsGrid/DeploymentOptionsGrid'
 
 const SignUpPage = (): JSX.Element => {
-    const { modalView } = useValues(signupLogic)
+    const { modalView, hasEmailGatedSignup } = useValues(signupLogic)
     const { onRenderSignupPage } = useActions(signupLogic)
 
     useEffect(() => {
@@ -23,31 +23,35 @@ const SignUpPage = (): JSX.Element => {
                 title="Sign Up • PostHog"
                 description="Unlock insights. Our Open Source, Scale, and Cloud editions provide flexible deployment of reliable analytics."
             />
-            <Header onPostPage={false} logoOnly transparentBackground />
-            <div className="w-full h-full relative flex items-center justify-center">
-                {modalView === SignupModalView.EMAIL_PROMPT && (
-                    <div className="signup-modal-view-email">
-                        <img
-                            src={funnelsScreenshot}
-                            alt="PostHog Insights interface showing new Funnels features"
-                            style={{ maxWidth: '75vw' }}
-                        />
-                        <div className="signup-callout-message">
-                            Unlock a deeper level of insights with Funnels 2.0{' '}
-                            <strong className="text-badge">New</strong>
-                        </div>
-                        <SignupBorderGraphic />
-                        <SignupModal />
+            {hasEmailGatedSignup && (
+                <>
+                    <Header onPostPage={false} logoOnly transparentBackground />
+                    <div className="w-full h-full relative flex items-center justify-center">
+                        {modalView === SignupModalView.EMAIL_PROMPT && (
+                            <div className="signup-modal-view-email">
+                                <img
+                                    src={funnelsScreenshot}
+                                    alt="PostHog Insights interface showing new Funnels features"
+                                    style={{ maxWidth: '75vw' }}
+                                />
+                                <div className="signup-callout-message">
+                                    Unlock a deeper level of insights with Funnels 2.0{' '}
+                                    <strong className="text-badge">New</strong>
+                                </div>
+                                <SignupBorderGraphic />
+                                <SignupModal />
+                            </div>
+                        )}
+                        {modalView === SignupModalView.DEPLOYMENT_OPTIONS && (
+                            <div className="signup-modal-view-deployment">
+                                <SignupCircleGraphic />
+                                <SignupBorderGraphic />
+                                <DeploymentOptionsGrid />
+                            </div>
+                        )}
                     </div>
-                )}
-                {modalView === SignupModalView.DEPLOYMENT_OPTIONS && (
-                    <div className="signup-modal-view-deployment">
-                        <SignupCircleGraphic />
-                        <SignupBorderGraphic />
-                        <DeploymentOptionsGrid />
-                    </div>
-                )}
-            </div>
+                </>
+            )}
         </div>
     )
 }
