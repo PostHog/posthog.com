338d5f1ec17067d01ab61bba0c56daa64c687dec
Sam Winslow <sammywinslow@gmail.com>
2021-08-11 12:21:58 -0400

    make nav and category view more consistent

--- src/components/Blog/BlogCategoriesList/index.tsx
+++ src/components/Blog/BlogCategoriesList/index.tsx
@@ -3,23 +3,33 @@ import { Link } from 'gatsby'
 import { BlogCategories, CategoryInterface } from '../constants/categories'
 
 const linkClassList =
-    'border-2 rounded-lg text-gray-900 dark:text-white px-2 py-1 mx-1 my-2 text-sm hover:bg-white hover:bg-opacity-50'
+    'border-2 rounded-lg text-white dark:text-white px-2 py-1 mx-1 my-2 text-sm hover:bg-white hover:bg-opacity-50 transition-colors'
 
-export function BlogCategoriesList(): JSX.Element {
-    const blogCategoryLinks = BlogCategories.filter((cat) => !cat.hideFromNavigation).map(
-        (category: CategoryInterface) => (
-            <Link to={category.link} key={category.slug} className={linkClassList}>
-                {category.title}
-            </Link>
-        )
-    )
+export function BlogCategoriesList({ activeSlug = 'blog' }: { activeSlug?: string }): JSX.Element {
+    const listObjects = [
+        ...BlogCategories,
+        {
+            title: 'Tutorials',
+            slug: 'tutorials',
+            link: '/docs/tutorials',
+        },
+    ]
 
     return (
         <div className="text-center mb-2">
-            {blogCategoryLinks}
-            <Link to="/docs/tutorials/overview" className={linkClassList}>
-                Tutorials
-            </Link>
+            {listObjects
+                .filter((cat) => !cat.hideFromNavigation)
+                .map((category: CategoryInterface) => {
+                    const isActive = category.slug === activeSlug
+                    const statefulClasses = isActive
+                        ? 'bg-white hover:bg-opacity-100 text-black dark:text-black'
+                        : 'opacity-75'
+                    return (
+                        <Link to={category.link} key={category.slug} className={`${linkClassList} ${statefulClasses}`}>
+                            {category.title}
+                        </Link>
+                    )
+                })}
         </div>
     )
 }
--- src/pages/blog/categories/[category].js
+++ src/pages/blog/categories/[category].js
@@ -61,19 +61,16 @@ const BlogCategoryPage = ({
                         <DarkModeToggle />
                     </div>
 
-                    <Structure.Section className="py-12" width="5xl">
+                    <Structure.Section width="5xl">
                         <Structure.SectionHeader title={`Blog: ${title}`} titleTag="h1" titleClassName="text-center" />
-
+                        <BlogCategoriesList activeSlug={category} />
                         {latestPost}
                         <NewsletterForm
                             compact
                             className="bg-offwhite-purple dark:bg-darkmode-purple text-gray-900 dark:text-white"
                         />
 
-                        <div className="w-11/12 max-w-3xl mx-auto flex flex-col lg:flex-row justify-between items-start">
-                            <div className="hidden lg:block lg:w-1/4 lg:pr-8">
-                                <BlogCategoriesList />
-                            </div>
+                        <div className="max-w-3xl mx-auto flex flex-col lg:flex-row justify-between items-start">
                             <div className="w-full lg:w-3/4 lg:pl-8">
                                 <header className="text-xs text-gray-400 uppercase">Recent Posts</header>
                                 {nonLatestPosts}
