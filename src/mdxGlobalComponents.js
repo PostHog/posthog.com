d945252a2ba83756a4513bc2ca027e3822aa10d8
Eli Kinsey <eli@ekinsey.dev>
2021-08-21 16:53:45 -0700

    ignore unused components in mdx import generator

--- src/mdxGlobalComponents.js
+++ src/mdxGlobalComponents.js
@@ -1,193 +1,23 @@
 // AUTO GENERATED FILE
 
 import { Endpoint } from './components/APIDocs/Endpoint'
-import { MethodTags } from './components/APIDocs/MethodTags'
-import { AllTheFeaturesCloud } from './components/AllTheFeaturesCloud'
-import { AnchorScrollNavbar } from './components/AnchorScrollNavbar'
 import { ArrayCTA } from './components/ArrayCTA'
 import { BasicHedgehogImage } from './components/BasicHedgehogImage'
-import { BlogAuthor } from './components/Blog/BlogAuthor'
-import { BlogCategoriesList } from './components/Blog/BlogCategoriesList'
-import { BlogFeaturedImage } from './components/Blog/BlogFeaturedImage'
-import { BlogIntro } from './components/Blog/BlogIntro'
-import { BlogPostLayout } from './components/Blog/BlogPostLayout'
-import { BlogPosts } from './components/Blog/BlogPosts'
-import { BlogShareButtons } from './components/Blog/BlogShareButtons'
-import { BlogFooter } from './components/BlogFooter'
 import { CallToAction } from './components/CallToAction'
-import { Benefits } from './components/Careers/Benefits'
-import { CareersHero } from './components/Careers/CareersHero'
-import { InterviewProcess } from './components/Careers/InterviewProcess'
-import { OpenRoles } from './components/Careers/OpenRoles'
-import { Transparency } from './components/Careers/Transparency'
-import { WhyWereHere } from './components/Careers/WhyWereHere'
-import { WorkingAtPostHog } from './components/Careers/WorkingAtPostHog'
-import { CodeBlock } from './components/CodeBlock'
 import { CompensationCalculator } from './components/CompensationCalculator'
-import { Container } from './components/Container'
-import { ContributorAvatars } from './components/ContributorAvatars'
-import { ContributorCard } from './components/ContributorCard'
-import { ContributorSearch } from './components/ContributorSearch'
-import { ContributorsChart } from './components/ContributorsChart'
-import { DarkModeToggle } from './components/DarkModeToggle'
-import { DemoScheduler } from './components/DemoScheduler'
-import { DocsPageSurvey } from './components/DocsPageSurvey'
-import { DocsSearch } from './components/DocsSearch'
-import { Doodle } from './components/Doodle'
-import { DoodleCircle } from './components/DoodleCircle'
-import { DoodleRectangle } from './components/DoodleRectangle'
-import { DoodleTriangle } from './components/DoodleTriangle'
-import { DoodleZigzag } from './components/DoodleZigzag'
-import { FeaturesComparisonTable } from './components/FeaturesComparisonTable'
-import { FeaturesNav } from './components/FeaturesNav'
-import { FeaturesSelfHost } from './components/FeaturesSelfHost'
-import { Footer } from './components/Footer'
-import { GetStartedModal } from './components/GetStartedModal'
 import { GithubIcon } from './components/GithubIcon'
 import { HiddenSection } from './components/HiddenSection'
-import { HostingOption } from './components/HostingOption'
-import { Blockquote } from './components/LandingPage/Blockquote'
-import { Features } from './components/LandingPage/Features'
-import { Hero } from './components/LandingPage/Hero'
-import { LandingPageCallToAction } from './components/LandingPage/LandingPageCallToAction'
-import { PrivateCloud } from './components/LandingPage/PrivateCloud'
-import { ProductFeatureIcons } from './components/LandingPage/ProductFeatureIcons'
-import { RecentBlogPosts } from './components/LandingPage/RecentBlogPosts'
-import { Roadmap } from './components/LandingPage/Roadmap'
-import { SocialProof } from './components/LandingPage/SocialProof'
-import { Tutorials } from './components/LandingPage/Tutorials'
 import { LibraryStats } from './components/LibraryStats'
-import { MdxAnchorHeaders } from './components/MdxAnchorHeaders'
-import { NewsletterForm } from './components/NewsletterForm'
-import { NotFoundPage } from './components/NotFoundPage'
-import { OtherFeaturesBlock } from './components/OtherFeaturesBlock'
 import { OverflowXSection } from './components/OverflowXSection'
-import { PageHeader } from './components/PageHeader'
-import { PlanComparisonTable } from './components/PlanComparisonTable'
-import { PostCard } from './components/PostCard'
-import { CloudVsSelfHost } from './components/Pricing/CloudVsSelfHost'
-import { FAQs } from './components/Pricing/FAQs'
-import { PlanComparison } from './components/Pricing/PlanComparison'
-import { PricingHero } from './components/Pricing/PricingHero'
-import { PricingTable } from './components/Pricing/PricingTable'
-import { Quote } from './components/Pricing/Quote'
-import { Savings } from './components/Pricing/Savings'
-import { PricingSlider } from './components/PricingSlider'
-import { ProductAnchorNavbar } from './components/ProductAnchorNavbar'
-import { ProductFeature } from './components/ProductFeature'
-import { ProductFeaturePlugin } from './components/ProductFeaturePlugin'
-import { ProductFooter } from './components/ProductFooter'
-import { ProductHero } from './components/ProductHero'
-import { ProductSectionHeader } from './components/ProductSectionHeader'
-import { ResponsiveAnchor } from './components/ResponsiveAnchor'
-import { ResponsiveSidebar } from './components/ResponsiveSidebar'
-import { ResponsiveTopBar } from './components/ResponsiveTopBar'
-import { RocketHorizontalRule } from './components/RocketHorizontalRule'
-import { DesignedForYourStackBlock } from './components/Sections/DesignedForYourStackBlock'
-import { FeaturedSectionTextLeft } from './components/Sections/FeaturedSectionTextLeft'
-import { FeaturedSectionTextRight } from './components/Sections/FeaturedSectionTextRight'
-import { FeaturedSectionTripleImage } from './components/Sections/FeaturedSectionTripleImage'
-import { SignupModal } from './components/SignupModal'
-import { Spacer } from './components/Spacer'
-import { StarRepoButton } from './components/StarRepoButton'
-import { StartNowButton } from './components/StartNowButton'
-import { Structure } from './components/Structure'
-import { TableOfContents } from './components/TableOfContents'
-import { WorkableSnippet } from './components/WorkableSnippet'
 
 export const shortcodes = {
     Endpoint,
-    MethodTags,
-    AllTheFeaturesCloud,
-    AnchorScrollNavbar,
     ArrayCTA,
     BasicHedgehogImage,
-    BlogAuthor,
-    BlogCategoriesList,
-    BlogFeaturedImage,
-    BlogIntro,
-    BlogPostLayout,
-    BlogPosts,
-    BlogShareButtons,
-    BlogFooter,
     CallToAction,
-    Benefits,
-    CareersHero,
-    InterviewProcess,
-    OpenRoles,
-    Transparency,
-    WhyWereHere,
-    WorkingAtPostHog,
-    CodeBlock,
     CompensationCalculator,
-    Container,
-    ContributorAvatars,
-    ContributorCard,
-    ContributorSearch,
-    ContributorsChart,
-    DarkModeToggle,
-    DemoScheduler,
-    DocsPageSurvey,
-    DocsSearch,
-    Doodle,
-    DoodleCircle,
-    DoodleRectangle,
-    DoodleTriangle,
-    DoodleZigzag,
-    FeaturesComparisonTable,
-    FeaturesNav,
-    FeaturesSelfHost,
-    Footer,
-    GetStartedModal,
     GithubIcon,
     HiddenSection,
-    HostingOption,
-    Blockquote,
-    Features,
-    Hero,
-    LandingPageCallToAction,
-    PrivateCloud,
-    ProductFeatureIcons,
-    RecentBlogPosts,
-    Roadmap,
-    SocialProof,
-    Tutorials,
     LibraryStats,
-    MdxAnchorHeaders,
-    NewsletterForm,
-    NotFoundPage,
-    OtherFeaturesBlock,
     OverflowXSection,
-    PageHeader,
-    PlanComparisonTable,
-    PostCard,
-    CloudVsSelfHost,
-    FAQs,
-    PlanComparison,
-    PricingHero,
-    PricingTable,
-    Quote,
-    Savings,
-    PricingSlider,
-    ProductAnchorNavbar,
-    ProductFeature,
-    ProductFeaturePlugin,
-    ProductFooter,
-    ProductHero,
-    ProductSectionHeader,
-    ResponsiveAnchor,
-    ResponsiveSidebar,
-    ResponsiveTopBar,
-    RocketHorizontalRule,
-    DesignedForYourStackBlock,
-    FeaturedSectionTextLeft,
-    FeaturedSectionTextRight,
-    FeaturedSectionTripleImage,
-    SignupModal,
-    Spacer,
-    StarRepoButton,
-    StartNowButton,
-    Structure,
-    TableOfContents,
-    WorkableSnippet,
 }
