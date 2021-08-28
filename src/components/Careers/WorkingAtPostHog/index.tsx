3f475f2e1322b47847e942d81bff6273fcee3ae9
Cory Watilo <corywatilo@gmail.com>
2021-08-28 11:48:54 +0200

    styled lottie video

--- src/components/Careers/WorkingAtPostHog/index.tsx
+++ src/components/Careers/WorkingAtPostHog/index.tsx
@@ -4,17 +4,26 @@ import { mergeClassList } from '../../../lib/utils'
 
 export const WorkingAtPostHog = () => {
     return (
-        <div className="pt-24 text-white text-center" id="working-at-posthog">
-            <Structure.Section width="5xl">
-                <header className="text-white text-lg">Watch a day in the life of our graphic designer, Lottie!</header>
-                <div className="mt-8 h-0 pb-fluid-video relative">
-                    <iframe
-                        src="https://www.youtube.com/embed/xlODCLrZyvM"
-                        className="absolute top-0 left-0 w-full h-full rounded-lg"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen=""
-                        frameBorder="0"
-                    ></iframe>
+        <div id="working-at-posthog">
+            <Structure.Section
+                width="4xl"
+                className="py-12 border-gray-accent-light border-dashed border-l-0 border-r-0 border-t border-b flex items-center"
+            >
+                <div className="flex-0 max-w-xs mr-4">
+                    <h3 className="text-4xl">
+                        Watch a day in the life of our graphic designer, <span className="text-orange">Lottie!</span>
+                    </h3>
+                </div>
+                <div className="flex-1">
+                    <div className="h-0 pb-fluid-video relative">
+                        <iframe
+                            src="https://www.youtube.com/embed/xlODCLrZyvM"
+                            className="absolute top-0 left-0 w-full h-full rounded-lg"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen=""
+                            frameBorder="0"
+                        ></iframe>
+                    </div>
                 </div>
             </Structure.Section>
         </div>
