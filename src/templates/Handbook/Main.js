03cfde0194419ce5166790a192f4ef1e0bf5a07e
Eli Kinsey <eli@ekinsey.dev>
2021-08-30 15:13:45 -0700

    docs/handbook cleanup

--- src/templates/Handbook/Main.js
+++ src/templates/Handbook/Main.js
@@ -25,6 +25,23 @@ const Blockquote = (props) => (
     <blockquote {...props} className="p-6 rounded bg-gray-accent-light dark:bg-gray-accent-dark" />
 )
 
+const SectionLinksBottom = ({ previous, next }) => {
+    return (
+        <>
+            <hr className="w-[calc(100vw-2rem)] m-0 bg-transparent border-t border-r-0 border-l-0 border-b-0 border-dashed border-gray-accent-light dark:border-gray-accent-dark" />
+            <SectionLinks
+                className="mb-16 2xl:max-w-[800px] xl:max-w-[650px] max-w-full mx-auto mt-9"
+                previous={previous}
+                next={next}
+            />
+        </>
+    )
+}
+
+const SectionLinksTop = ({ previous, next }) => {
+    return <SectionLinks className="mt-9" previous={previous} next={next} />
+}
+
 export default function Main({
     handleMobileMenuClick,
     filePath,
@@ -53,20 +70,23 @@ export default function Main({
     const mainEl = useRef()
     return (
         <div className="relative">
+            <SectionLinksTop next={next} previous={previous} />
             <div className="dark:text-white flex max-w-screen-3xl mx-auto items-start relative z-10 mt-8">
                 <div className="sticky top-20 flex-1">
-                    <SectionLink link={previous} previous className="hidden md:flex mb-8" />
                     <MainSidebar
                         mainEl={mainEl}
                         menu={menu}
                         slug={slug}
-                        className="hidden md:block w-full transition-opacity md:opacity-60 hover:opacity-100"
+                        className="hidden md:block w-full transition-opacity md:opacity-60 hover:opacity-100 mb-14"
                     />
                 </div>
-                <main ref={mainEl} className={`relative md:pl-16 xl:px-16 2xl:px-32 ${showToc ? '' : 'flex-grow'}`}>
-                    <article className="2xl:max-w-[800px] xl:max-w-[650px] max-w-full pb-14">
+                <main ref={mainEl} className="overflow-scroll">
+                    <article
+                        style={!showToc ? { maxWidth: '100%', paddingRight: 0 } : {}}
+                        className="2xl:max-w-[800px] xl:max-w-[650px] max-w-full pb-14 relative md:pl-16 xl:px-16 2xl:px-32 box-content"
+                    >
                         <section className="mb-8 xl:mb-14 relative">
-                            <h1 className="dark:text-white text-5xl mt-0 mb-2">{title}</h1>
+                            <h1 className="dark:text-white text-3xl sm:text-5xl mt-0 mb-2">{title}</h1>
                             <p className="mt-1 mb-0 opacity-50">
                                 Last updated: <time>{lastUpdated}</time>
                             </p>
@@ -88,19 +108,10 @@ export default function Main({
                                 className="border-r border-dashed border-gray-accent-light dark:border-gray-accent-dark absolute bottom-0  right-0 hidden xl:flex justify-center"
                             />
                         )}
-                        {next && (
-                            <SectionLinks
-                                previous={previous}
-                                next={next}
-                                hideAnchor={!showToc}
-                                breakpoints={breakpoints}
-                            />
-                        )}
                     </article>
                 </main>
 
                 <div className="sticky top-20 flex-1">
-                    <SectionLink className="hidden md:flex justify-end absolute top-0 right-0" link={next} />
                     {!breakpoints.lg && showToc && (
                         <InternalSidebar
                             className="mt-[20vh] mb-14 hidden xl:block"
@@ -109,6 +120,7 @@ export default function Main({
                     )}
                 </div>
             </div>
+            {next && <SectionLinksBottom next={next} previous={previous} />}
         </div>
     )
 }
--- src/templates/Handbook/SectionLinks.js
+++ src/templates/Handbook/SectionLinks.js
@@ -1,28 +1,11 @@
 import React from 'react'
 import SectionLink from './SectionLink'
 
-export default function NextArticle({ hideAnchor, breakpoints, next, previous }) {
+export default function NextArticle({ next, previous, className = '' }) {
     return (
-        <>
-            <section>
-                <hr
-                    style={{
-                        width:
-                            hideAnchor || breakpoints.lg
-                                ? '100%'
-                                : `calc(100% + ${breakpoints.xl ? '224px + 8rem' : '224px + 16rem'})`,
-                        marginLeft:
-                            hideAnchor || breakpoints.lg
-                                ? '0px'
-                                : `calc(${breakpoints.xl ? '-224px - 4rem' : '-224px - 8rem'})`,
-                    }}
-                    className="my-9 bg-transparent border-t-2 border-r-0 border-l-0 border-b-0 border-dashed border-gray-accent-light dark:border-gray-accent-dark"
-                />
-                <div className="flex justify-between">
-                    <SectionLink link={previous} previous />
-                    <SectionLink link={next} />
-                </div>
-            </section>
-        </>
+        <div className={`flex justify-between ${className}`}>
+            <SectionLink link={previous} previous />
+            <SectionLink link={next} />
+        </div>
     )
 }
