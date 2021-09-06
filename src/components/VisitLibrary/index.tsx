f5bfe29e90dc4b39b8cc37f3a6ab978cd0afd432
Cory Watilo <corywatilo@gmail.com>
2021-09-06 07:57:55 -0500

    use VisitLibrary component for all client and server libraries

--- src/components/VisitLibrary/index.tsx
+++ src/components/VisitLibrary/index.tsx
@@ -4,17 +4,21 @@ import { CallToAction } from '../CallToAction'
 interface VisitLibraryProps {
     name: string
     to: string
-    official?: string
+    official?: boolean
 }
 
 export const VisitLibrary = ({ name, to, official }: VisitLibraryProps) => {
     return (
-        <div className="flex space-between items-center w-full">
-            <p>
-                The {official} {name} library is hosted on GitHub.
+        <div className="md:flex justify-between items-center w-full p-6 rounded bg-gray-accent-light dark:bg-gray-accent-dark mb-5">
+            <p className="text-center md:text-left md:mb-0 md:mr-4" style={{ lineHeight: '1.5' }}>
+                This is the{' '}
+                <strong>
+                    {official ? 'official' : ''} PostHog library for {name}
+                </strong>{' '}
+                to capture and send events to any PostHog instance.
             </p>
 
-            <CallToAction icon="none" href={to} className="mb-4">
+            <CallToAction icon="none" href={to} className="whitespace-nowrap w-full !block md:!inline-block md:md-auto">
                 View on GitHub
             </CallToAction>
         </div>
