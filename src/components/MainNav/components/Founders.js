05ef71a41f36da53cd7e183efa3353d5e382af37
Eli Kinsey <eli@ekinsey.dev>
2021-09-20 15:02:02 -0700

    copy/image updates

--- src/components/MainNav/components/Founders.js
+++ src/components/MainNav/components/Founders.js
@@ -4,14 +4,21 @@ import React from 'react'
 export default function Founders() {
     return (
         <div className="self-center flex-shrink-0">
-            <StaticImage
-                placeholder="none"
-                alt="Tim and James"
-                quality={100}
-                width={150}
-                className="mb-4 md:mb-0 pointer-events-none"
-                src="./images/founders.png"
-            />
+            <figure>
+                <StaticImage
+                    placeholder="none"
+                    alt="Tim and James"
+                    quality={100}
+                    width={150}
+                    className="mb-4 md:mb-0 pointer-events-none"
+                    src="./images/founders.png"
+                />
+                <figcaption className="leading-none text-primary dark:text-white">
+                    <strong className="text-[15px]">Tim & James</strong>
+                    <br />
+                    <span className="text-[13px]">Co-founders</span>
+                </figcaption>
+            </figure>
         </div>
     )
 }
