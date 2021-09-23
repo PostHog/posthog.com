fd6887d3545995bf041b26c23555f03e0702b4c1
Sam Winslow <sammywinslow@gmail.com>
2021-09-23 11:46:03 -0400

    change yc-onboarding-wrapper -> get-in-touch-wrapper; set pane from url hash

--- src/pages/schedule-demo.tsx
+++ src/pages/schedule-demo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import { DemoScheduler } from '../components/DemoScheduler'
 import { Spacer } from '../components/Spacer'
@@ -6,26 +6,45 @@ import './styles/yc-onboarding.scss'
 import { SEO } from '../components/seo'
 import { Link } from 'gatsby'
 
+type PaneOption = 'demo' | 'contact'
+
+const isPaneOption = (hash: string): hash is PaneOption => ['demo', 'contact'].includes(hash)
+
 export const ScheduleDemo = () => {
+    const [activePane, setActivePane] = useState('demo' as PaneOption)
+    const hash = typeof window !== 'undefined' ? window.location.hash.replace('#', '') : ''
+    useEffect(() => {
+        if (isPaneOption(hash)) {
+            setActivePane(hash)
+        }
+    }, [hash])
+
     return (
         <Layout>
             <SEO title="Schedule Demo • PostHog" />
-            <div className="yc-onboarding-wrapper">
+            <div className="get-in-touch-wrapper">
                 <Spacer />
-                <h1 className="centered">Schedule Demo</h1>
-                <p>
-                    Use the widget below to schedule a demo call with one of our engineers. Please note that this is not
-                    a Sales call. For sales enquiries, please{' '}
-                    <a href="https://share.hsforms.com/1-IVCY9gNRvaZBajMt_UPIg4559u">contact us</a>.
-                </p>
-                <br />
-                <p>
-                    PostHog demos are done in group sessions and are an introduction to the platform. You can also find
-                    a lot of information about the product in our <Link to="/docs">Docs</Link> and{' '}
-                    <a href="https://www.youtube.com/channel/UCoP6ql8QkyOoVpBU4P8LM6w">secondary YouTube channel</a>.
-                </p>
-                <br />
-                <DemoScheduler iframeSrc="https://calendly.com/jamesefhawkins/posthog-demo" />
+                <h1 className="centered">Get in touch</h1>
+                {activePane === 'demo' && (
+                    <>
+                        <p>
+                            Use the widget below to schedule a demo call with one of our engineers. Please note that
+                            this is not a Sales call. For sales enquiries, please{' '}
+                            <a href="https://share.hsforms.com/1-IVCY9gNRvaZBajMt_UPIg4559u">contact us</a>.
+                        </p>
+                        <p>
+                            PostHog demos are done in group sessions and are an introduction to the platform. You can
+                            also find a lot of information about the product in our <Link to="/docs">Docs</Link> and{' '}
+                            <a href="https://www.youtube.com/channel/UCoP6ql8QkyOoVpBU4P8LM6w">
+                                secondary YouTube channel
+                            </a>
+                            .
+                        </p>
+                        <br />
+                        <DemoScheduler iframeSrc="https://calendly.com/jamesefhawkins/posthog-demo" />
+                    </>
+                )}
+                {activePane === 'demo' && <>{/* TODO <form> */}</>}
             </div>
         </Layout>
     )
