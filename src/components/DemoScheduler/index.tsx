839313fc91dc00322c9eac010a83468985cfb009
Sam Winslow <sammywinslow@gmail.com>
2021-09-28 15:24:31 -0400

    track successful calendly bookings

--- src/components/DemoScheduler/index.tsx
+++ src/components/DemoScheduler/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useValues } from 'kea'
+import { posthogAnalyticsLogic } from 'logic/posthogAnalyticsLogic'
+import React, { useEffect } from 'react'
 import { Spacer } from '../Spacer'
 import './style.scss'
 
@@ -6,7 +8,20 @@ export const DemoScheduler = ({
     iframeSrc = 'https://calendly.com/yakko/yc-onboarding-group',
 }: {
     iframeSrc?: string
-}) => {
+}): JSX.Element => {
+    const { posthog } = useValues(posthogAnalyticsLogic)
+    useEffect(() => {
+        typeof window !== 'undefined' &&
+            window.addEventListener('message', function (e) {
+                const { event, payload = null } = e.data
+                if (event === 'calendly.event_scheduled') {
+                    posthog?.capture(event, {
+                        calendly_event_uri: payload?.event.uri,
+                        calendly_invitee_uri: payload?.invitee.uri,
+                    })
+                }
+            })
+    })
     return (
         <>
             <div>
