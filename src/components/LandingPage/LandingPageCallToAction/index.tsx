2e8b432ebffc1fce2b54372d1fced2e5e9051f37
Eli Kinsey <eli@ekinsey.dev>
2021-09-28 15:53:21 -0700

    update cta links

--- src/components/LandingPage/LandingPageCallToAction/index.tsx
+++ src/components/LandingPage/LandingPageCallToAction/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { CallToAction } from 'components/CallToAction'
+import React from 'react'
 
 export const LandingPageCallToAction = (): JSX.Element => {
     return (
         <div className="flex flex-col justify-center items-center space-y-2 md:space-y-3">
-            <CallToAction type="primary" width="56" to="/docs/self-host/">
+            <CallToAction type="primary" width="56" to="/signup">
                 Get started
             </CallToAction>
             <CallToAction type="outline" width="56" to="/schedule-demo">
