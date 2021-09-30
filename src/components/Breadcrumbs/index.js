d25c2c64d7354e91bd6b4b8f1028ca65d9376df9
Eli Kinsey <eli@ekinsey.dev>
2021-09-30 12:57:25 -0700

    extend breadcrumbs to allow link color changes/conditional rendering on demo page

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'gatsby'
 import cntl from 'cntl'
+import { Link } from 'gatsby'
+import React from 'react'
 
 const crumbText = (classes = '') => cntl`
     font-bold
@@ -11,16 +11,28 @@ const crumbText = (classes = '') => cntl`
     ${classes}
 `
 
-function Crumb({ url, title, className }) {
+function Crumb({ url, title, className, onClick, linkColor }) {
     // If crumbs get more complex, create a conditional wrapper component to keep code DRY
     return (
         <li
             className={`border-r border-gray-accent-light dark:border-gray-accent-dark border-dashed text-primary dark:text-primary-dark ${className}`}
         >
             {url ? (
-                <Link className={crumbText(`text-yellow hover:text-yellow`)} to={url}>
+                <Link
+                    style={linkColor ? { color: linkColor } : {}}
+                    className={crumbText(`text-yellow hover:text-yellow`)}
+                    to={url}
+                >
                     {title}
                 </Link>
+            ) : onClick ? (
+                <button
+                    onClick={onClick}
+                    style={linkColor ? { color: linkColor } : {}}
+                    className={crumbText(`text-yellow hover:text-yellow`)}
+                >
+                    {title}
+                </button>
             ) : (
                 <span className={crumbText()}>{title}</span>
             )}
@@ -28,12 +40,12 @@ function Crumb({ url, title, className }) {
     )
 }
 
-export default function Breadcrumbs({ crumbs }) {
+export default function Breadcrumbs({ crumbs, linkColor }) {
     return (
         <ul className="list-none p-0 m-0 flex border-gray-accent-light dark:border-gray-accent-dark border-dashed border-t border-b">
             {crumbs &&
                 crumbs.map((crumb, index) => {
-                    return <Crumb key={index} {...crumb} />
+                    return <Crumb linkColor={linkColor} key={index} {...crumb} />
                 })}
         </ul>
     )
