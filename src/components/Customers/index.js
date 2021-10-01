1412fbb72da5af8919eb3198b72556edb2ba43fd
Eli Kinsey <eli@ekinsey.dev>
2021-10-01 10:28:12 -0700

    add seo

--- src/components/Customers/index.js
+++ src/components/Customers/index.js
@@ -3,6 +3,7 @@ import { RightArrow } from 'components/Icons/Icons'
 import Layout from 'components/Layout'
 import Link from 'components/Link'
 import { Quote } from 'components/Pricing/Quote'
+import { SEO } from 'components/seo'
 import { graphql, useStaticQuery } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
@@ -54,78 +55,84 @@ export default function Customers() {
     const customers = data?.customers?.nodes
     const { featuredCustomer } = data
     return (
-        <Layout>
-            <section className="px-4">
-                <h1 className={heading('md', 'primary', 'mt-16 md:mt-28 mb-9')}>Customers</h1>
-                <FeaturedCustomer customer={featuredCustomer} />
-            </section>
-            <section className="bg-black dark py-16 md:py-32 px-4">
-                <div>
-                    <Quote
-                        logo="/images/customers/pry.svg"
-                        name="Tiffany Wong"
-                        title="Co-founder, Pry Financials"
-                        image={<StaticImage width={100} quality={100} src="./images/tiffany.png" />}
-                        quote={
-                            <span>
-                                “Hosting PostHog on our own infrastructure was easy to do and means{' '}
-                                <span className="text-red">
-                                    we can be confident that our data is safe and shared with as few platforms as
-                                    possible.
-                                </span>{' '}
-                                This is really important to us, as we’re often dealing with sensitive data.”
-                            </span>
-                        }
-                    />
-                </div>
-            </section>
-            <section className="max-w-[1140px] mx-auto -mt-6 md:-mt-12 px-4">
-                <ul className="list-none p-0 m-0 grid md:grid-cols-2 gap-y-5 md:gap-y-10 gap-x-10">
-                    {customers.map((customer, index) => {
-                        const {
-                            slug,
-                            frontmatter: { title, logo, featuredImage },
-                        } = customer
-                        return (
-                            <li key={index}>
-                                <Card className="inline-block relative h-full" url={slug}>
-                                    <span className="absolute inset-0 w-full h-full z-0 before:absolute before:inset-0 before:w-full before:h-full before:bg-white before:bg-opacity-80">
-                                        <img src={featuredImage?.publicURL} className="w-full h-full object-cover" />
-                                    </span>
-                                    <div className="relative px-9 py-9 flex flex-col h-full items-start">
-                                        <img src={logo?.publicURL} />
-                                        <h3 className="text-2xl mb-24 mt-4">{title}</h3>
-                                        <div className="text-yellow hover:text-yellow font-bold flex space-x-1 items-center text-[17px] mt-auto">
-                                            <span>Read case study</span>
-                                            <RightArrow className="w-5 h-5 bounce" />
+        <>
+            <SEO title={`Customers - PostHog`} />
+            <Layout>
+                <section className="px-4">
+                    <h1 className={heading('md', 'primary', 'mt-16 md:mt-28 mb-9')}>Customers</h1>
+                    <FeaturedCustomer customer={featuredCustomer} />
+                </section>
+                <section className="bg-black dark py-16 md:py-32 px-4">
+                    <div>
+                        <Quote
+                            logo="/images/customers/pry.svg"
+                            name="Tiffany Wong"
+                            title="Co-founder, Pry Financials"
+                            image={<StaticImage width={100} quality={100} src="./images/tiffany.png" />}
+                            quote={
+                                <span>
+                                    “Hosting PostHog on our own infrastructure was easy to do and means{' '}
+                                    <span className="text-red">
+                                        we can be confident that our data is safe and shared with as few platforms as
+                                        possible.
+                                    </span>{' '}
+                                    This is really important to us, as we’re often dealing with sensitive data.”
+                                </span>
+                            }
+                        />
+                    </div>
+                </section>
+                <section className="max-w-[1140px] mx-auto -mt-6 md:-mt-12 px-4">
+                    <ul className="list-none p-0 m-0 grid md:grid-cols-2 gap-y-5 md:gap-y-10 gap-x-10">
+                        {customers.map((customer, index) => {
+                            const {
+                                slug,
+                                frontmatter: { title, logo, featuredImage },
+                            } = customer
+                            return (
+                                <li key={index}>
+                                    <Card className="inline-block relative h-full" url={slug}>
+                                        <span className="absolute inset-0 w-full h-full z-0 before:absolute before:inset-0 before:w-full before:h-full before:bg-white before:bg-opacity-80">
+                                            <img
+                                                src={featuredImage?.publicURL}
+                                                className="w-full h-full object-cover"
+                                            />
+                                        </span>
+                                        <div className="relative px-9 py-9 flex flex-col h-full items-start">
+                                            <img src={logo?.publicURL} />
+                                            <h3 className="text-2xl mb-24 mt-4">{title}</h3>
+                                            <div className="text-yellow hover:text-yellow font-bold flex space-x-1 items-center text-[17px] mt-auto">
+                                                <span>Read case study</span>
+                                                <RightArrow className="w-5 h-5 bounce" />
+                                            </div>
                                         </div>
-                                    </div>
-                                </Card>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </section>
-            <section className="my-16 md:my-32 px-4">
-                <div>
-                    <Quote
-                        logo="/images/customers/mention-me.svg"
-                        name="Lleo Harress"
-                        title="Software Engineer"
-                        image={<StaticImage width={100} quality={100} src="./images/lleo.png" />}
-                        quote={
-                            <span>
-                                “The fact that PostHog is open source is really helpful. The cloud formation templates
-                                made deployment easy and{' '}
-                                <span className="text-red">we were up and running within a day,</span> but we can also
-                                open the codebase and build on it. We’ve iterated an entire stack for ourselves since
-                                launch.”
-                            </span>
-                        }
-                    />
-                </div>
-            </section>
-        </Layout>
+                                    </Card>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </section>
+                <section className="my-16 md:my-32 px-4">
+                    <div>
+                        <Quote
+                            logo="/images/customers/mention-me.svg"
+                            name="Lleo Harress"
+                            title="Software Engineer"
+                            image={<StaticImage width={100} quality={100} src="./images/lleo.png" />}
+                            quote={
+                                <span>
+                                    “The fact that PostHog is open source is really helpful. The cloud formation
+                                    templates made deployment easy and{' '}
+                                    <span className="text-red">we were up and running within a day,</span> but we can
+                                    also open the codebase and build on it. We’ve iterated an entire stack for ourselves
+                                    since launch.”
+                                </span>
+                            }
+                        />
+                    </div>
+                </section>
+            </Layout>
+        </>
     )
 }
 
--- src/templates/Customer.js
+++ src/templates/Customer.js
@@ -4,6 +4,7 @@ import Breadcrumbs from 'components/Breadcrumbs'
 import { FloatedImage } from 'components/FloatedImage'
 import { ImageBlock } from 'components/ImageBlock'
 import Layout from 'components/Layout'
+import { SEO } from 'components/seo'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import React from 'react'
@@ -37,41 +38,50 @@ export default function Customer({ data }) {
     const {
         customerData: {
             body,
+            excerpt,
             frontmatter: { title, customer, logo, description, industries, users, toolsUsed, featuredImage },
         },
     } = data
     return (
-        <Layout>
-            <div className="px-4 sticky top-[-2px] bg-tan dark:bg-primary z-10">
-                <Breadcrumbs
-                    crumbs={[
-                        {
-                            title: 'Customers',
-                            url: '/customers',
-                        },
-                        {
-                            title: customer,
-                        },
-                    ]}
-                />
-            </div>
-            <div className="max-w-screen-lg mx-auto px-4 flex flex-col md:flex-row items-start mt-16 md:mt-20">
-                <aside className="md:mr-9 mb-9 md:mb-0 md:sticky top-20 md:pr-9 md:border-r border-dashed border-gray-accent-light w-full md:w-auto">
-                    {logo && <img className="w-full max-w-[150px]" src={logo.publicURL} />}
-                    <ul className="list-none flex-col flex space-y-8 p-0 mt-10 min-w-[250px]">
-                        <Tags title="Industry" tags={industries} />
-                        <Tags title="Users" tags={users} />
-                        <Tags title="Tools used" tags={toolsUsed} />
-                    </ul>
-                </aside>
-                <section className="customer-content">
-                    <h1 className="text-3xl">{title}</h1>
-                    <MDXProvider components={components}>
-                        <MDXRenderer>{body}</MDXRenderer>
-                    </MDXProvider>
-                </section>
-            </div>
-        </Layout>
+        <>
+            <SEO
+                title={`${title} - PostHog`}
+                description={description || excerpt}
+                article
+                image={featuredImage?.publicURL}
+            />
+            <Layout>
+                <div className="px-4 sticky top-[-2px] bg-tan dark:bg-primary z-10">
+                    <Breadcrumbs
+                        crumbs={[
+                            {
+                                title: 'Customers',
+                                url: '/customers',
+                            },
+                            {
+                                title: customer,
+                            },
+                        ]}
+                    />
+                </div>
+                <div className="max-w-screen-lg mx-auto px-4 flex flex-col md:flex-row items-start mt-16 md:mt-20">
+                    <aside className="md:mr-9 mb-9 md:mb-0 md:sticky top-20 md:pr-9 md:border-r border-dashed border-gray-accent-light w-full md:w-auto">
+                        {logo && <img className="w-full max-w-[150px]" src={logo.publicURL} />}
+                        <ul className="list-none flex-col flex space-y-8 p-0 mt-10 min-w-[250px]">
+                            <Tags title="Industry" tags={industries} />
+                            <Tags title="Users" tags={users} />
+                            <Tags title="Tools used" tags={toolsUsed} />
+                        </ul>
+                    </aside>
+                    <section className="customer-content">
+                        <h1 className="text-3xl">{title}</h1>
+                        <MDXProvider components={components}>
+                            <MDXRenderer>{body}</MDXRenderer>
+                        </MDXProvider>
+                    </section>
+                </div>
+            </Layout>
+        </>
     )
 }
 
