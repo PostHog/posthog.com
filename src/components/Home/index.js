743d8de7771ab3963735c9b07d31dd3e36e87fcc
Eli Kinsey <eli@ekinsey.dev>
2021-10-01 17:53:35 -0700

    create and implement tutorials component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { posthogAnalyticsLogic } from 'logic/posthogAnalyticsLogic'
 import { useValues } from 'kea'
-import { SEO } from '../seo'
+import { posthogAnalyticsLogic } from 'logic/posthogAnalyticsLogic'
+import React from 'react'
 import Layout from '../Layout'
-import Hero from './Hero'
-import Customers from './Customers'
+import { SEO } from '../seo'
 import BeforeAndAfter from './BeforeAndAfter'
-import Features from './Features'
-import Pipelines from './Pipelines'
-import Quote from './Quote'
 import Community from './Community'
 import CTA from './CTA'
+import Customers from './Customers'
+import Features from './Features'
+import Hero from './Hero'
+import Pipelines from './Pipelines'
+import Quote from './Quote'
+import Tutorials from './Tutorials'
 
 const Home = () => {
     useValues(posthogAnalyticsLogic) // mount this logic
@@ -21,16 +22,15 @@ const Home = () => {
                 title="PostHog - Open-Source Product Analytics"
                 description="Self-hosted product analytics stack, to deploy on your infrastructure."
             />
-            <main>
-                <Hero />
-                <Customers />
-                <BeforeAndAfter />
-                <Features />
-                <Pipelines />
-                <Quote />
-                <Community />
-                <CTA />
-            </main>
+            <Hero />
+            <Customers />
+            <BeforeAndAfter />
+            <Features />
+            <Pipelines />
+            <Quote />
+            <Community />
+            <Tutorials />
+            <CTA />
         </Layout>
     )
 }
