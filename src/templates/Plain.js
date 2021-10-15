62dae43f89ddbaea36dd26edb41a776ab8fa10de
Eli Kinsey <eli@ekinsey.dev>
2021-10-14 21:18:08 -0700

    add and implement article width frontmatter

--- src/templates/Plain.js
+++ src/templates/Plain.js
@@ -5,23 +5,23 @@ import { MDXProvider } from '@mdx-js/react'
 import { SEO } from 'components/seo'
 import { CodeBlock } from '../components/CodeBlock'
 import { shortcodes } from '../mdxGlobalComponents'
-import { H1, H2, H3, H4, H5, H6 } from 'components/MdxAnchorHeaders'
 import Hero from 'components/Hero'
 import { Section } from 'components/Section'
 import FeatureSnapshot from 'components/FeatureSnapshot'
 import { Check, Close } from 'components/Icons/Icons'
 
+const articleWidth = {
+    lg: 'max-w-screen-2xl',
+    md: 'max-w-5xl',
+    sm: 'max-w-2xl',
+    full: 'w-full',
+}
+
 export default function Plain({ data }) {
     const { pageData } = data
     const { body, excerpt } = pageData
-    const { title, featuredImage, description, showTitle } = pageData?.frontmatter
+    const { title, featuredImage, description, showTitle, width = 'sm' } = pageData?.frontmatter
     const components = {
-        h1: H1,
-        h2: H2,
-        h3: H3,
-        h4: H4,
-        h5: H5,
-        h6: H6,
         pre: CodeBlock,
         Hero,
         Section,
@@ -38,7 +38,7 @@ export default function Plain({ data }) {
                 article
                 image={featuredImage?.publicURL}
             />
-            <article className="max-w-screen-2xl mx-auto my-12 md:my-24 px-4 article-content">
+            <article className={`mx-auto my-12 md:my-24 px-4 article-content ${articleWidth[width || 'sm']}`}>
                 {showTitle && <h1 className="text-center">{title}</h1>}
                 <MDXProvider components={components}>
                     <MDXRenderer>{body}</MDXRenderer>
@@ -64,6 +64,7 @@ export const query = graphql`
                 featuredImage {
                     publicURL
                 }
+                width
             }
         }
     }
