b6edf1c80547c182f6f64b26c40287d3bee9a69b
yakkomajuri <yakko.majuri@gmail.com>
2021-10-26 16:40:15 +0000

    update homepage numbers

--- src/components/Home/Community.js
+++ src/components/Home/Community.js
@@ -23,8 +23,8 @@ export default function Community() {
                 <h3 className={heading('sm', 'tan')}>*4,600+ stars on GitHub</h3>
                 <ul className="grid sm:grid-cols-3 text-white m-0 p-0 list-none my-8 sm:my-20 divide-gray-accent-light divide-y-1 sm:divide-y-0 sm:divide-x-1 divide-dashed">
                     <CommunityStat title="15k+" description="Developer community" />
-                    <CommunityStat title="140" description="Contributors" />
-                    <CommunityStat title="10b" description="Events tracked" />
+                    <CommunityStat title="180" description="Contributors" />
+                    <CommunityStat title="25b" description="Events tracked" />
                 </ul>
                 <CallToAction type="outline" width="56" href="https://github.com/PostHog/posthog">
                     Browse on GitHub
