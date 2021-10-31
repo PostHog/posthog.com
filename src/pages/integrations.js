56076c9831dfcf0f47c8913ffed4cde4c120428b
Eli Kinsey <eli@ekinsey.dev>
2021-10-31 10:43:30 -0700

    add filter sidebar

--- src/pages/integrations.js
+++ src/pages/integrations.js
@@ -1,10 +1,11 @@
 import Layout from 'components/Layout'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { heading, section } from 'components/Home/classes'
 import { Segment, Zapier, Sentry, Check } from 'components/Icons/Icons'
 import Card from 'components/Card'
 import { motion } from 'framer-motion'
 import { SEO } from 'components/seo'
+import Logo from 'components/Logo'
 
 const logos = {
     Segment,
@@ -15,14 +16,7 @@ const logos = {
 const maintainerIcons = {
     official: (
         <span title="Officially maintained">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-yellow"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-            >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
+            <Logo noText className="w-5 h-5" />
         </span>
     ),
     community: (
@@ -41,73 +35,136 @@ const maintainerIcons = {
 
 export default function Integrations({
     data: {
-        allIntegrations: { integrations },
+        allIntegrations: { integrations, maintainers },
     },
 }) {
+    const [filteredIntegrations, setFilteredIntegrations] = useState(null)
+    const [filters, setFilters] = useState({
+        maintainer: [],
+    })
+
+    const handleChange = (e, filter) => {
+        const { value, checked } = e.target
+        let newFilters = { ...filters }
+        if (!checked) {
+            newFilters[filter].splice(newFilters[filter].indexOf(value), 1)
+        } else {
+            newFilters = { ...newFilters, [filter]: [...newFilters[filter], value] }
+        }
+        setFilters(newFilters)
+    }
+
+    useEffect(() => {
+        const filtered = integrations.filter((integration) => {
+            return Object.keys(filters).every((key) => {
+                return filters[key].some((filterBy) => {
+                    return integration[key] === filterBy
+                })
+            })
+        })
+        setFilteredIntegrations(filtered.length <= 0 ? null : filtered)
+    }, [filters])
+
     return (
         <Layout>
             <SEO title="Integrate PostHog" description="Keep your entire product stack in sync with PostHog" />
-            <section>
-                <div className={section()}>
-                    <h1 className={heading()}>Integrations</h1>
-                    <h2 className={heading('sm')}>Keep your entire product stack in sync with PostHog</h2>
-                </div>
-                <div className={section()}>
-                    <motion.ul
-                        initial="hidden"
-                        animate="show"
-                        variants={{
-                            hidden: { opacity: 0 },
-                            show: {
-                                opacity: 1,
-                                transition: {
-                                    staggerChildren: 0.05,
-                                },
-                            },
-                        }}
-                        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 list-none p-0 m-0"
+            <div>
+                <section>
+                    <div className={section()}>
+                        <h1 className={heading()}>Integrations</h1>
+                        <h2 className={heading('sm')}>Keep your entire product stack in sync with PostHog</h2>
+                    </div>
+                    <div
+                        className={section(
+                            'flex space-y-12 md:space-y-0 md:space-x-4 flex-col md:flex-row items-start relative'
+                        )}
                     >
-                        {integrations.map((integration) => {
-                            const { id, name, description, url, maintainer, verified } = integration
-                            const Logo = logos[name]
-                            const MaintainerIcon = maintainerIcons[maintainer]
-                            return (
-                                <motion.li
-                                    className="list-none"
-                                    variants={{
-                                        hidden: { translateY: '-20%', opacity: 0 },
-                                        show: { translateY: 0, opacity: 1 },
-                                    }}
-                                    key={id}
-                                >
-                                    <Card
-                                        url={url}
-                                        className="text-primary hover:text-primary p-6 relative block w-full h-full"
+                        <aside className="w-[300px] md:sticky top-4">
+                            <h5 className="border-b border-dashed border-gray-accent-light inline-block">Maintainer</h5>
+                            <ul className="list-none p-0 m-0 flex flex-col space-y-2 mt-2">
+                                {maintainers.map((maintainer) => {
+                                    const { type } = maintainer
+                                    return (
+                                        <li
+                                            key={type}
+                                            className="flex items-center space-x-2 text-base relative font-semibold"
+                                        >
+                                            <span>
+                                                <input
+                                                    onChange={(e) => handleChange(e, 'maintainer')}
+                                                    value={type}
+                                                    id={type}
+                                                    aria-describedby={`${type}-description`}
+                                                    name={type}
+                                                    type="checkbox"
+                                                    className="focus:ring-primary h-4 w-4 text-primary border border-primary bg-primary rounded checkbox appearance-none bg-opacity-0 absolute transition-all"
+                                                />
+                                                <Check className="w-4 h-4 relative invisible text-white pointer-events-none" />
+                                            </span>
+                                            <label className="flex" htmlFor={type}>
+                                                <span>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+                                            </label>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </aside>
+                        <motion.ul
+                            initial="hidden"
+                            animate="show"
+                            variants={{
+                                hidden: { opacity: 0 },
+                                show: {
+                                    opacity: 1,
+                                    transition: {
+                                        staggerChildren: 0.05,
+                                    },
+                                },
+                            }}
+                            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 list-none p-0 md:pl-6 m-0 flex-grow w-full"
+                        >
+                            {(filteredIntegrations || integrations).map((integration) => {
+                                const { id, name, description, url, maintainer, verified } = integration
+                                const Logo = logos[name]
+                                const MaintainerIcon = maintainerIcons[maintainer]
+                                return (
+                                    <motion.li
+                                        className="list-none"
+                                        variants={{
+                                            hidden: { translateY: '-20%', opacity: 0 },
+                                            show: { translateY: 0, opacity: 1 },
+                                        }}
+                                        key={id}
                                     >
-                                        <h3 className="flex items-center">
-                                            {Logo && (
-                                                <span>
-                                                    <Logo className="mr-2" />
-                                                </span>
-                                            )}
-                                            <span>{name}</span>
-                                        </h3>
-                                        <p>{description}</p>
-                                        <span className="absolute right-3 top-3 flex space-x-1">
-                                            {verified && (
-                                                <span title="Verified">
-                                                    <Check className="text-[green] h-5 w-5" />
-                                                </span>
-                                            )}
-                                            {MaintainerIcon && MaintainerIcon}
-                                        </span>
-                                    </Card>
-                                </motion.li>
-                            )
-                        })}
-                    </motion.ul>
-                </div>
-            </section>
+                                        <Card
+                                            url={url}
+                                            className="text-primary hover:text-primary p-6 relative block w-full h-full"
+                                        >
+                                            <h3 className="flex items-center">
+                                                {Logo && (
+                                                    <span>
+                                                        <Logo className="mr-2" />
+                                                    </span>
+                                                )}
+                                                <span>{name}</span>
+                                            </h3>
+                                            <p>{description}</p>
+                                            <span className="absolute right-3 top-3 flex space-x-1">
+                                                {verified && (
+                                                    <span title="Verified">
+                                                        <Check className="text-[green] h-5 w-5" />
+                                                    </span>
+                                                )}
+                                                {MaintainerIcon && MaintainerIcon}
+                                            </span>
+                                        </Card>
+                                    </motion.li>
+                                )
+                            })}
+                        </motion.ul>
+                    </div>
+                </section>
+            </div>
         </Layout>
     )
 }
@@ -115,6 +172,9 @@ export default function Integrations({
 export const query = graphql`
     query IntegrationsQuery {
         allIntegrations {
+            maintainers: group(field: maintainer) {
+                type: fieldValue
+            }
             integrations: nodes {
                 id
                 name
