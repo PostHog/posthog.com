e29829620ab70a26c033d56816b12e5443854fc8
Eli Kinsey <eli@ekinsey.dev>
2021-11-18 05:56:27 -0800

    add product launch banner

---
+++ src/components/ProductHuntBadge/index.js
@@ -0,0 +1,58 @@
+import { Close } from 'components/Icons/Icons'
+import { motion } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
+
+export const ProductHuntBadgeLayoutWrapper = ({ children }) => {
+    return (
+        <>
+            <ProductHuntBadge />
+            {children}
+        </>
+    )
+}
+
+function ProductHuntBadge() {
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const hideProductHuntBadge = window?.localStorage?.getItem('hide-product-hunt-badge')
+            setVisible(hideProductHuntBadge !== 'true')
+        }
+    }, [])
+
+    const handleClick = () => {
+        setVisible(false)
+        if (typeof window !== 'undefined') {
+            window.localStorage.setItem('hide-product-hunt-badge', 'true')
+        }
+    }
+
+    return (
+        visible && (
+            <div className="text-white flex items-center justify-center space-x-4 py-3 px-4 relative">
+                <motion.div
+                    initial={{ translateX: '-100%' }}
+                    animate={{ translateX: 0 }}
+                    className="bg-red absolute inset-0 w-full h-full transition-all duration-1000"
+                />
+                <p className="m-0 font-bold">Nov 18: Check us out on ProductHunt.</p>
+                <a
+                    className="relative"
+                    href="https://www.producthunt.com/posts/posthog-3?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-posthog-3"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img
+                        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=320054&theme=light"
+                        alt="PostHog - An all-in-one product analytics suite you can self-host | Product Hunt"
+                        className="max-w-[200px]"
+                    />
+                </a>
+                <button onClick={handleClick} className="absolute right-4">
+                    <Close className="w-4 h-4" />
+                </button>
+            </div>
+        )
+    )
+}
