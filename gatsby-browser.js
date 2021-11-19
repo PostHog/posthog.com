c49c4c618e2b24a787ed913f12ac3e9cf8939f7f
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2021-11-19 13:04:00 +0200

    Revert "Product Hunt banner (#2471)"
    This reverts commit 823a7c8ca4a58a5e54ab8e4cf9b3f0b61982ef28.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,17 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import React from 'react'
 import { initKea, wrapElement } from './kea'
-import { ProductHuntBadgeLayoutWrapper } from './src/components/ProductHuntBadge'
 import './src/styles/global.css'
 
 initKea(false)
 
 export const wrapRootElement = wrapElement
-export const wrapPageElement = ({ element, props }) => (
-    <ProductHuntBadgeLayoutWrapper {...props}>{element}</ProductHuntBadgeLayoutWrapper>
-)
 export const onRouteUpdate = ({ location, prevLocation }) => {
     // This is checked and set on initial load in the body script set in gatsby-ssr.js
     // Checking for prevLocation prevents this from happening twice
