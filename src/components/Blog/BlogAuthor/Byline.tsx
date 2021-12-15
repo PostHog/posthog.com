592a9f1a7bd02ea5659aa3eef87700b4e10551bc
Eli Kinsey <eli@ekinsey.dev>
2021-12-14 19:48:13 -0800

    New post layout (#2616)
    * support for multiple authors on blog posts
    
    * support for multiple authors on blog/category listing pages
    
    * cleanup
    
    * update rss
    
    * update documentation
    
    * new post layout refactor from tutorials layout
    
    * apply new post layout to blog posts
    
    * Fix typos
    
    * remove border on mobile
    
    * update yarn.lock
    
    * cleanup
    
    Co-authored-by: PostHog <hey@posthog.com>

--- src/components/Blog/BlogAuthor/Byline.tsx
+++ src/components/Blog/BlogAuthor/Byline.tsx
@@ -1,25 +1,16 @@
-import moment from 'moment'
 import React from 'react'
 import { AuthorsData } from 'types'
 
 interface AuthorDetails {
-    authorDetails?: AuthorsData
+    authorDetails?: [AuthorsData]
     date?: string
     classes?: string
 }
 
 function Byline({ authorDetails, date, classes }: AuthorDetails): JSX.Element {
-    const { name, handle } = authorDetails || {}
-    const fragments: string[] = []
-    if (date && moment(date).isValid()) {
-        fragments.push(moment(date).format('MMM D, YYYY'))
-    }
-    if (name && handle) {
-        fragments.push(`by ${name}`)
-    }
+    const authorNames = (authorDetails && ` by ${authorDetails?.map((author) => author.name).join(', ')}`) || ''
     // TODO: Add "N minute read" when reading length plugin is installed
-    const bylineText = fragments.join(' ')
-    return <div className={`${classes} mt-1 mb-0 opacity-50`}>{bylineText}</div>
+    return <div className={`${classes} mt-1 mb-0 opacity-50`}>{`${date}${authorNames}`}</div>
 }
 
 export default Byline
