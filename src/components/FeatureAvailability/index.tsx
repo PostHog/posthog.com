9c996d00f1a96ebdbdb47bda0b1ddab523fe1cce
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-12-14 21:13:05 -0600

    add availability to features

--- src/components/FeatureAvailability/index.tsx
+++ src/components/FeatureAvailability/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './FeatureAvailability.scss'
-import { CheckCircleFilled, CloseCircleFilled, InfoCircleOutlined } from '@ant-design/icons'
+import { CheckCircleFilled, CloseCircleFilled, InfoCircleOutlined, MinusCircleFilled } from '@ant-design/icons'
 
 type AvailablePlans = 'free' | 'standard' | 'enterpriseCloud' | 'startup' | 'openSource' | 'scale' | 'enterprise'
 
@@ -16,16 +16,16 @@ const PLANS: Record<'cloud' | 'hosted', PlanInterface[]> = {
             name: 'Free',
         },
         {
+            key: 'startup',
+            name: 'Startup',
+        },
+        {
             key: 'standard',
             name: 'Standard',
         },
         {
             key: 'enterpriseCloud',
             name: 'Enterprise',
-        },
-        {
-            key: 'startup',
-            name: 'Startup',
         },
     ],
     hosted: [
@@ -47,65 +47,72 @@ const PLANS: Record<'cloud' | 'hosted', PlanInterface[]> = {
 interface FeatureAvailabilityProps {
     allPlans?: boolean
     availablePlans?: AvailablePlans[]
+    restrictedPlans?: AvailablePlans[]
 }
 
-function Plan({ available, name }: { available?: boolean; name: string }): JSX.Element {
+function Plan({
+    available,
+    name,
+    restricted,
+}: {
+    available?: boolean
+    name: string
+    restricted?: boolean
+}): JSX.Element {
     return (
-        <li className={!available ? 'unavailable' : ''}>
-            {available ? <CheckCircleFilled /> : <CloseCircleFilled />}
-            {name} plan
+        <li className={restricted ? 'restricted' : available ? '' : 'unavailable'}>
+            {restricted ? <MinusCircleFilled /> : available ? <CheckCircleFilled /> : <CloseCircleFilled />}
+            {name} plan{restricted && '*'}
         </li>
     )
 }
 
-export function FeatureAvailability({ allPlans, availablePlans }: FeatureAvailabilityProps): JSX.Element {
+export function FeatureAvailability({
+    allPlans,
+    availablePlans,
+    restrictedPlans,
+}: FeatureAvailabilityProps): JSX.Element {
     return (
         <div className="feature-availability">
             <h4>Where is this feature available?</h4>
 
             <div className="feature-availability-inner">
-                {allPlans ? (
+                <div>
+                    <h5>
+                        Cloud
+                        <a href="/pricing?realm=cloud">
+                            <InfoCircleOutlined />
+                        </a>
+                    </h5>
                     <ul>
-                        <li>Available on all plans</li>
+                        {PLANS.cloud.map((plan) => (
+                            <Plan
+                                key={plan.key}
+                                available={allPlans || availablePlans?.includes(plan.key)}
+                                name={plan.name}
+                                restricted={restrictedPlans?.includes(plan.key)}
+                            />
+                        ))}
                     </ul>
-                ) : (
-                    <>
-                        <div>
-                            <h5>
-                                Cloud
-                                <a href="/pricing?realm=cloud">
-                                    <InfoCircleOutlined />
-                                </a>
-                            </h5>
-                            <ul>
-                                {PLANS.cloud.map((plan) => (
-                                    <Plan
-                                        key={plan.key}
-                                        available={availablePlans?.includes(plan.key)}
-                                        name={plan.name}
-                                    />
-                                ))}
-                            </ul>
-                        </div>
-                        <div>
-                            <h5>
-                                Self-hosted
-                                <a href="/pricing?realm=self-hosted">
-                                    <InfoCircleOutlined />
-                                </a>
-                            </h5>
-                            <ul>
-                                {PLANS.hosted.map((plan) => (
-                                    <Plan
-                                        key={plan.key}
-                                        available={availablePlans?.includes(plan.key)}
-                                        name={plan.name}
-                                    />
-                                ))}
-                            </ul>
-                        </div>
-                    </>
-                )}
+                </div>
+                <div>
+                    <h5>
+                        Self-hosted
+                        <a href="/pricing?realm=self-hosted">
+                            <InfoCircleOutlined />
+                        </a>
+                    </h5>
+                    <ul>
+                        {PLANS.hosted.map((plan) => (
+                            <Plan
+                                key={plan.key}
+                                available={allPlans || availablePlans?.includes(plan.key)}
+                                name={plan.name}
+                                restricted={restrictedPlans?.includes(plan.key)}
+                            />
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
