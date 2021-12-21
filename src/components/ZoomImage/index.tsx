52f0770176008ebe7f5c300d09762803c0b69342
Eli Kinsey <eli@ekinsey.dev>
2021-12-21 12:52:32 -0800

    fix hydration issue with zoom image component

--- src/components/ZoomImage/index.tsx
+++ src/components/ZoomImage/index.tsx
@@ -4,8 +4,10 @@ import 'react-medium-image-zoom/dist/styles.css'
 
 export const ZoomImage = ({ children, ...other }: { children: any }) => {
     return (
-        <Zoom overlayBgColorEnd="rgb(0 0 0 / 85%)" overlayBgColorStart="rgb(0 0 0 / 80%)">
-            {children || <img {...other} />}
-        </Zoom>
+        <span>
+            <Zoom wrapElement="span" overlayBgColorEnd="rgb(0 0 0 / 85%)" overlayBgColorStart="rgb(0 0 0 / 80%)">
+                {children || <img {...other} />}
+            </Zoom>
+        </span>
     )
 }
