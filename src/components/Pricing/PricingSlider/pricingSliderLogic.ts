1999e87088cb936b845042685703d0577471fcc8
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-01-18 07:51:04 -0600

    fix pricing

--- src/components/Pricing/PricingSlider/pricingSliderLogic.ts
+++ src/components/Pricing/PricingSlider/pricingSliderLogic.ts
@@ -2,13 +2,12 @@ import { kea } from 'kea'
 import { SCALE_MINIMUM_EVENTS, SCALE_MINIMUM_PRICING } from '../constants'
 import { sliderCurve } from './LogSlider'
 
-export type PricingOptionType = 'vpc' | 'self-hosted' | 'cloud'
+export type PricingOptionType = 'self-hosted' | 'cloud'
 
 export const pricingSliderLogic = kea({
     actions: {
         setSliderValue: (value: number) => ({ value }),
         setPricingOption: (option: PricingOptionType) => ({ option }),
-        setAdditionalUnitPrice: (value: number) => ({ value }),
     },
     reducers: {
         eventNumber: [
@@ -29,46 +28,45 @@ export const pricingSliderLogic = kea({
                 setPricingOption: (_: null, { option }: { option: string }) => option,
             },
         ],
-        additionalUnitPrice: [
-            0.000225,
-            {
-                setAdditionalUnitPrice: (_: null, { value }: { value: number }) => value,
-            },
-        ],
     },
-    selectors: ({ actions }) => ({
+    selectors: () => ({
         finalCost: [
             (s) => [s.eventNumber, s.pricingOption],
             (eventNumber: number, pricingOption: PricingOptionType) => {
                 let finalCost = 0
-                let unitPricing = 0.000225
+                let alreadyCountedEvents = 0
+
+                const thresholdPrices =
+                    pricingOption === 'self-hosted'
+                        ? [
+                              [10_000_000, 0.000225],
+                              [100_000_000, 0.000045],
+                              [Number.MAX_SAFE_INTEGER, 0.000009],
+                          ]
+                        : [
+                              [1_000_000, 0],
+                              [10_000_000, 0.000225],
+                              [100_000_000, 0.000075],
+                              [Number.MAX_SAFE_INTEGER, 0.000025],
+                          ]
+                console.log('-----------------------------')
+                for (const [threshold, unitPricing] of thresholdPrices) {
+                    console.log(
+                        threshold,
+                        unitPricing,
+                        Math.max(0, Math.min(eventNumber - alreadyCountedEvents, threshold - alreadyCountedEvents))
+                    )
+                    finalCost =
+                        finalCost +
+                        Math.max(0, Math.min(eventNumber - alreadyCountedEvents, threshold - alreadyCountedEvents)) *
+                            unitPricing
+                    alreadyCountedEvents = threshold
+                }
+
                 if (pricingOption === 'self-hosted') {
-                    const estimatedCost = eventNumber * unitPricing
-                    finalCost = estimatedCost > SCALE_MINIMUM_PRICING ? estimatedCost : SCALE_MINIMUM_PRICING
-
-                    if (eventNumber >= 10_000_000 && eventNumber < 100_000_000) {
-                        unitPricing = 0.000045
-                        finalCost = 10_000_000 * 0.000225 + (eventNumber - 10_000_000) * unitPricing
-                    } else if (eventNumber >= 100_000_000) {
-                        unitPricing = 0.000009
-                        finalCost =
-                            10_000_000 * 0.000225 + 90_000_000 * 0.000045 + (eventNumber - 100_000_000) * unitPricing
-                    }
-
-                    actions.setAdditionalUnitPrice(unitPricing)
-                    return Math.round(finalCost).toLocaleString()
+                    finalCost = finalCost > SCALE_MINIMUM_PRICING ? finalCost : SCALE_MINIMUM_PRICING
                 }
 
-                // Cloud
-                threshold_to_price = [[1_000_000, 0], [10_000_000, 0.000225], [100_000_000, 0.000075], [Number.MAX_SAFE_INTEGER, 0.000025]]
-                finalCost = 0
-                already_counted_events = 0
-                for ([threshold, unitPricing] of threshold_to_price) {
-                    finalCost = finalCost + max(0, min(eventNumber - already_counted_events, threshold)) * unitPricing
-                    already_counted_events = threshold
-                }
-                }
-                actions.setAdditionalUnitPrice(unitPricing)
                 return Math.round(finalCost).toLocaleString()
             },
         ],
