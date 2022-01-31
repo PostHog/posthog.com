f1aa807e7fde8605c5b31647e23bf491b98d7b9d
Eli Kinsey <eli@ekinsey.dev>
2022-01-31 01:44:10 -0800

    hosthog landing page

---
+++ src/components/Subscribe/index.js
@@ -0,0 +1,84 @@
+import { Check } from 'components/Icons/Icons'
+import { StaticImage } from 'gatsby-plugin-image'
+import addToMailchimp from 'gatsby-plugin-mailchimp'
+import React, { useState } from 'react'
+import GitHubButton from 'react-github-btn'
+
+const Form = () => {
+    const [email, setEmail] = useState(null)
+    const [subscribed, setSubscribed] = useState(null)
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (email) {
+            await addToMailchimp(email)
+            setSubscribed(true)
+        }
+    }
+    return subscribed ? (
+        <>
+            <p className="flex justify-center items-center space-x-1 font-semibold text-[#43AF79] m-0">
+                <span className=" w-[24px] h-[24px] bg-[#43AF79] rounded-full flex justify-center items-center">
+                    <Check className="w-[12px] h-[12px] text-white" />
+                </span>
+                <span>You're subscribed!</span>
+            </p>
+            <h6 className="mt-2 mb-3 text-center">
+                Be sure to check us out on <a href="https://github.com/PostHog/posthog">Github.com</a>
+            </h6>
+            <p className="m-0 flex justify-center items-centertext-white font-semibold">
+                <GitHubButton
+                    className="text-white hover:text-white"
+                    href="https://github.com/posthog/posthog"
+                    data-size="large"
+                    data-show-count="true"
+                    aria-label="Star posthog/posthog on GitHub"
+                >
+                    Star
+                </GitHubButton>
+            </p>
+        </>
+    ) : (
+        <>
+            <h4>Join our mailing list</h4>
+            <form
+                className="flex items-end sm:space-x-2 sm:space-y-0 sm:flex-row flex-col space-y-2 m-0"
+                onSubmit={handleSubmit}
+            >
+                <span className="flex-grow w-full">
+                    <label htmlFor="email" className="text-base">
+                        Email address
+                    </label>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        name="email"
+                        className="py-[12px] block px-[13px] text-[15px] rounded-sm border border-gray-accent-light mt-1 w-full"
+                        type="email"
+                        required
+                        placeholder="Email address..."
+                    />
+                </span>
+                <button
+                    className="bg-red py-[12px] px-[30px] font-bold border border-red text-white text-[15px] rounded-sm sm:w-auto w-full"
+                    type="submit"
+                >
+                    Subscribe
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default function Subscribe() {
+    return (
+        <div className="max-w-[800px] sm:mt-0 mt-12 w-full flex sm:space-x-9 sm:space-y-0 space-y-4 relative bg-white py-9 px-7 border border-gray-accent-light rounded-md sm:flex-row flex-col">
+            <div className="w-[233px] sm:mt-0 -mt-24">
+                <div className="sm:absolute bottom-0">
+                    <StaticImage width={233} src="./hog-the-builder.png" />
+                </div>
+            </div>
+            <div className="flex-grow">
+                <Form />
+            </div>
+        </div>
+    )
+}
---
+++ src/templates/HostHog.js
@@ -0,0 +1,210 @@
+import { MDXProvider } from '@mdx-js/react'
+import { FeatureSnapshot } from 'components/FeatureSnapshot'
+import { Hero } from 'components/Hero'
+import { Check, Close, LinkedIn } from 'components/Icons/Icons'
+import Layout from 'components/Layout'
+import Link from 'components/Link'
+import { Section } from 'components/Section'
+import Subscribe from 'components/Subscribe'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import React from 'react'
+import { CodeBlock } from '../components/CodeBlock'
+import { shortcodes } from '../mdxGlobalComponents'
+
+const A = (props) => <Link {...props} className="text-red hover:text-red font-semibold" />
+
+const Title = () => (
+    <h1 className="text-4xl md:text-6xl m-0 mt-3 -mb-5">
+        Join us for our first community meetup <span className="text-red">in London</span>
+    </h1>
+)
+
+const Disclaimer = () => (
+    <p className="mt-3">
+        <small className="text-black opacity-75">HostHog events are free to attend, but space is limited.</small>
+    </p>
+)
+
+export default function HostHog({ data }) {
+    const {
+        id,
+        body,
+        frontmatter: { date, venue, from, to, city, agenda, speakers },
+    } = data.mdx
+
+    const WhereWhen = () => {
+        return (
+            <div className="flex space-x-8 border-t border-dashed border-gray-accent-light pt-4 sm:mb-0 mb-4">
+                <div>
+                    <h4 style={{ margin: '0px 0px 4px 0px', fontSize: 14 }} className="text-black opacity-50">
+                        Where
+                    </h4>
+                    <p style={{ fontSize: 14, lineHeight: '1.5', margin: 0 }}>
+                        <strong>{venue.name}</strong>
+                        <br />
+                        {venue.address}
+                    </p>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-[15px] font-bold inline-flex items-center space-x-1"
+                        href={`https://maps.google.com/?q=${venue.address}`}
+                    >
+                        <span>Map</span>
+                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M8.32514 0.338357L2.16485 0.337936C1.59733 0.337936 1.12426 0.811012 1.12426 1.37853C1.12426 1.94606 1.59733 2.41913 2.16485 2.41913L5.81269 2.43017L0.586328 7.65653C0.176498 8.06636 0.176498 8.71849 0.586329 9.12832C0.996159 9.53815 1.64829 9.53815 2.05812 9.12832L7.28448 3.90196L7.28448 7.5396C7.28448 8.10712 7.75755 8.5802 8.32508 8.5802C8.6191 8.5802 8.88234 8.46481 9.07165 8.27549C9.26097 8.08618 9.37677 7.82336 9.37635 7.52892L9.37635 1.38966C9.37635 1.11616 9.27123 0.842657 9.07165 0.643081C8.87208 0.443507 8.60932 0.327677 8.32514 0.338357Z"
+                                fill="#BFBFBC"
+                            />
+                        </svg>
+                    </a>
+                </div>
+                <div>
+                    <h4 style={{ margin: '0px 0px 4px 0px', fontSize: 14 }} className="text-black opacity-50">
+                        When
+                    </h4>
+                    <p style={{ fontSize: 14, lineHeight: '1.5', margin: 0 }}>
+                        <strong>{date}</strong>
+                        <br />
+                        18:00 - 20:30
+                    </p>
+                </div>
+            </div>
+        )
+    }
+
+    const Agenda = () => {
+        return (
+            <div>
+                <h3>Agenda</h3>
+                {agenda.map(({ to, from, description, emoji }) => {
+                    return (
+                        <>
+                            <h6 style={{ marginTop: 22, marginBottom: 0 }}>
+                                {from} - {to} {emoji && emoji}
+                            </h6>
+                            <p style={{ margin: 0, fontSize: 15 }}>{description}</p>
+                        </>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    const Speakers = () => {
+        return (
+            <div className="sm:mt-0 mt-6">
+                <h3>Speakers</h3>
+                <ul
+                    className="flex space-x-4 justify-between flex-wrap"
+                    style={{ listStyle: 'none', margin: 0, padding: 0 }}
+                >
+                    {speakers.map(({ name, title, company, linkedIn, image }) => {
+                        return (
+                            <li key={name}>
+                                <GatsbyImage image={getImage(image)} />
+                                <h5 style={{ marginTop: 22, fontSize: 18, margin: 0 }}>{name}</h5>
+                                <p
+                                    style={{ fontSize: 16, color: 'black', opacity: '.50', margin: 0 }}
+                                    className="font-semibold"
+                                >
+                                    {title}
+                                </p>
+                                {linkedIn && (
+                                    <a
+                                        style={{
+                                            color: '#151515',
+                                            marginTop: 12,
+                                            display: 'inline-block',
+                                            opacity: 0.5,
+                                        }}
+                                        href={linkedIn}
+                                    >
+                                        <LinkedIn className="w-[21px] h-[21px]" />
+                                    </a>
+                                )}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+
+    const components = {
+        pre: CodeBlock,
+        Hero,
+        Section,
+        FeatureSnapshot,
+        Check,
+        Close,
+        a: A,
+        Title,
+        WhereWhen,
+        Disclaimer,
+        Agenda,
+        Speakers,
+        ...shortcodes,
+    }
+
+    return (
+        <Layout>
+            <div className="max-w-[1070px] mx-auto mt-14 px-5">
+                <article>
+                    <p className="text-[20px] font-semibold opacity-50 text-black m-0">HostHog: {city} 2022</p>
+                    <section className="article-content">
+                        <MDXProvider components={components}>
+                            <MDXRenderer>{body}</MDXRenderer>
+                        </MDXProvider>
+                    </section>
+                </article>
+                <div className="border-t border-gray-accent-light border-dashed pt-10">
+                    <h3 className="m-0">Can’t make it to {city}?</h3>
+                    <p className="mt-1 mb-16 sm:mb-8 text-base">
+                        Our mailing list is the first to find out when we’re planning a HostHog event closer to you.
+                    </p>
+                    <Subscribe />
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query HostHogQuery($id: String!) {
+        mdx(id: { eq: $id }) {
+            id
+            body
+            excerpt(pruneLength: 150)
+            frontmatter {
+                date(formatString: "dddd, Do MMMM")
+                venue {
+                    name
+                    address
+                }
+                agenda {
+                    from
+                    to
+                    description
+                    emoji
+                }
+                speakers {
+                    name
+                    title
+                    company
+                    linkedIn
+                    image {
+                        childImageSharp {
+                            gatsbyImageData(width: 100, height: 124)
+                        }
+                    }
+                }
+                city
+                from
+                to
+            }
+        }
+    }
+`
