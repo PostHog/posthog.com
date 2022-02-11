af728619a8f19159dc04d8a2d0baceb96a1a72d1
Eli Kinsey <eli@ekinsey.dev>
2022-02-11 14:19:01 -0800

    add valentines day banner

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import { posthogAnalyticsLogic } from 'logic/posthogAnalyticsLogic'
 import React from 'react'
 import Layout from '../Layout'
 import { SEO } from '../seo'
+import ValentinesDay from '../ValentinesDay'
 import BeforeAndAfter from './BeforeAndAfter'
 import Community from './Community'
 import CTA from './CTA'
@@ -26,6 +27,7 @@ const Home = () => {
                     description="PostHog is the all-in-one platform for building better products. Heatmaps, funnels, feature flags, session replays and more. Try for free."
                     image="/images/home.png"
                 />
+                <ValentinesDay />
                 <Hero />
                 <Customers />
                 <BeforeAndAfter />
