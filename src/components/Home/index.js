6883c80c400246e4b5bfbbf6be68488fe34fc7bc
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-02-15 11:45:55 +0000

    The ugliest valentine

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,6 @@ import { posthogAnalyticsLogic } from 'logic/posthogAnalyticsLogic'
 import React from 'react'
 import Layout from '../Layout'
 import { SEO } from '../seo'
-import ValentinesDay from '../ValentinesDay'
 import BeforeAndAfter from './BeforeAndAfter'
 import Community from './Community'
 import CTA from './CTA'
@@ -27,7 +26,6 @@ const Home = () => {
                     description="PostHog is the all-in-one platform for building better products. Heatmaps, funnels, feature flags, session replays and more. Try for free."
                     image="/images/home.png"
                 />
-                <ValentinesDay />
                 <Hero />
                 <Customers />
                 <BeforeAndAfter />
