729066813fc27cc743770e519566c649d65e4589
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-04-21 15:27:34 +0100

    tweak

--- src/components/DocsCTA/index.tsx
+++ src/components/DocsCTA/index.tsx
@@ -3,7 +3,7 @@ import { CallToAction } from 'components/CallToAction'
 
 export const DocsCTA = () => (
     <>
-        <h3 className="text-white mb-5 !mt-0">Ready to find out more?</h3>
+        <h3 className="text-white mb-5 !mt-0">Want to give it a go?</h3>
         <div className="flex flex-col md:flex-row gap-2 xl:gap-4">
             <CallToAction width="56" to="/signup">
                 Try PostHog today
