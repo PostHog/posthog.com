b8a24d7c02c8dfb1b2608d034089c7d9ebe8d0b8
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-04-21 10:06:41 +0100

    Update index.tsx

--- src/components/ArrayCTA/index.tsx
+++ src/components/ArrayCTA/index.tsx
@@ -3,10 +3,14 @@ import { CallToAction } from 'components/CallToAction'
 
 export const ArrayCTA = () => (
     <>
-        <br />
-        <CallToAction width="56" to="https://app.posthog.com/signup">
-            Try PostHog Cloud
-        </CallToAction>
-        <br />
+        <p className="px-4 font-bold text-center z-10 relative mb-0 text-sm md:text-base">Ready to find out more?</p>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
+            <CallToAction width="56" to="/signup">
+                Try PostHog today
+            </CallToAction>
+            <CallToAction type="outline" width="56" to="/book-a-demo">
+                Schedule a demo
+            </CallToAction>
+        </div>
     </>
 )
