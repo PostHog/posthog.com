aa5206cda3c243f06eb663e715293588dd008ddb
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-04-27 14:46:48 +0000

    Comparison session recordings (#3343)
    * Update session-recording.mdx
    
    * Update session-recording.mdx
    
    * Create new Array CTA for blog
    
    * Update session-recording.mdx
    
    * Update session-recording.mdx
    
    * styled session recording table, evened out global table padding
    
    Co-authored-by: Cory Watilo <corywatilo@gmail.com>

--- src/components/ArrayCTA/index.tsx
+++ src/components/ArrayCTA/index.tsx
@@ -3,6 +3,7 @@ import { CallToAction } from 'components/CallToAction'
 
 export const ArrayCTA = () => (
     <>
+        <br />
         <p className="px-4 font-bold text-center z-10 relative mb-0 text-sm md:text-base">Ready to find out more?</p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
             <CallToAction width="56" to="/signup">
@@ -12,5 +13,6 @@ export const ArrayCTA = () => (
                 Schedule a demo
             </CallToAction>
         </div>
+        <br />
     </>
 )
