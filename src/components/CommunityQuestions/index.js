0b6dddecd82e2e92260f694e89ac3658cdf101ea
Eli Kinsey <eli@ekinsey.dev>
2022-05-04 06:15:53 -0700

    add dark mode toggle support

--- src/components/CommunityQuestions/index.js
+++ src/components/CommunityQuestions/index.js
@@ -3,7 +3,7 @@ import { Squeak } from 'squeak-react'
 
 export default function CommunityQuestions({ questions }) {
     return (
-        <div className="max-w-[450px]">
+        <div className="max-w-[450px] mt-12">
             <Squeak
                 apiHost="https://squeak.cloud"
                 apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4aXBrcXV2d3FhYXVudXpqb2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk3MjE3ODUsImV4cCI6MTk2NTI5Nzc4NX0.SxdOpxHjVwap7sDUptK2TFJl7WK3v3HLuKbzb0JKeKg"
