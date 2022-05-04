f18d8dcdae337442afd3446467daaf124d505780
Eli Kinsey <eli@ekinsey.dev>
2022-05-04 07:37:36 -0700

    add toc and footer links

--- src/components/CommunityQuestions/index.js
+++ src/components/CommunityQuestions/index.js
@@ -3,7 +3,10 @@ import { Squeak } from 'squeak-react'
 
 export default function CommunityQuestions({ questions }) {
     return (
-        <div className="max-w-[450px] mt-12">
+        <div className="max-w-[600px] mt-12">
+            <h3 id="squeak-questions" className="mb-4">
+                Questions?
+            </h3>
             <Squeak
                 apiHost="https://squeak.cloud"
                 apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4aXBrcXV2d3FhYXVudXpqb2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk3MjE3ODUsImV4cCI6MTk2NTI5Nzc4NX0.SxdOpxHjVwap7sDUptK2TFJl7WK3v3HLuKbzb0JKeKg"
