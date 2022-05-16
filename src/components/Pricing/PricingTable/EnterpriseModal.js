945b0d345b72af6b6b8ff9ad96f46f2c251ae594
Simon Fisher <simfish85@googlemail.com>
2022-05-16 12:56:16 +0100

    Switched Enterprise plans pricing to no minumum

--- src/components/Pricing/PricingTable/EnterpriseModal.js
+++ src/components/Pricing/PricingTable/EnterpriseModal.js
@@ -46,10 +46,10 @@ export default function EnterpriseModal({ setOpen, open, hideActions, hideBadge 
                                 </div>
 
                                 <PricingSlider
-                                    marks={[10000000, 20000000, 50000000, 100000000, 1000000000, 10000000000]}
-                                    min={10000000}
+                                    marks={[1000000, 10000000, 100000000, 1000000000, 10000000000]}
+                                    min={1000000}
                                     max={10000000000}
-                                    defaultValue={10000000}
+                                    defaultValue={1000000}
                                     pricingOption={'enterprise'}
                                 />
                             </div>
@@ -60,8 +60,8 @@ export default function EnterpriseModal({ setOpen, open, hideActions, hideBadge 
                                     <div className="opacity-50 text-2xs text-right">Monthly price per event</div>
                                 </div>
                                 <dl className="flex justify-between mb-0 p-2">
-                                    <dt className="mb-0 opacity-75 text-xs">First 10 million</dt>
-                                    <dd className="mb-0 font-bold text-xs">$4,500</dd>
+                                    <dt className="mb-0 opacity-75 text-xs">0-10 million</dt>
+                                    <dd className="mb-0 font-bold text-xs">$0.00045</dd>
                                 </dl>
                                 <dl className="flex justify-between mb-0 p-2">
                                     <dt className="mb-0 opacity-75 text-xs">10-100 million</dt>
