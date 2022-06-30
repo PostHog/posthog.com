42e0e2e49133fb316339514386fee92d72605acc
Eli Kinsey <eli@ekinsey.dev>
2022-06-29 22:23:26 -0700

    slides/buttons

--- src/components/Home/Slider/Slides.js
+++ src/components/Home/Slider/Slides.js
@@ -1,11 +1,17 @@
 import {
+    Android,
     DiagonalArrow,
     Experimentation,
     Funnels,
+    Ios,
+    JS,
     Lifecycle,
+    NodeJS,
     PathAnalysis,
+    ReactIcon,
     Retention,
     RightArrow,
+    Ruby,
     Stickiness,
     Trends,
 } from 'components/Icons/Icons'
@@ -45,8 +51,19 @@ export const ProductAnalytics = () => {
     return (
         <div className="grid grid-cols-5 gap-7 pt-5">
             <div className="col-span-3 h-[400px]">
-                <motion.div initial={{ translateY: '100%' }} animate={{ translateY: 0 }}>
-                    <StaticImage className="w-full" src="./images/product-analytics.png" />
+                <motion.div
+                    transition={{ delay: 0.4 }}
+                    className="h-full"
+                    initial={{ translateY: '100%' }}
+                    animate={{ translateY: 0 }}
+                >
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        objectFit="contain"
+                        className="w-full h-full"
+                        src="./images/product-analytics.png"
+                    />
                 </motion.div>
             </div>
             <div className="col-span-2 relative flex flex-col">
@@ -77,6 +94,8 @@ export const ProductAnalytics = () => {
                     </div>
                     <div className="relative">
                         <StaticImage
+                            placeholder="none"
+                            quality={100}
                             className="max-w-[360px] w-full flex-shrink-0"
                             src="./images/product-analytics-hog.png"
                         />
@@ -91,12 +110,23 @@ export const SessionRecording = () => {
     return (
         <div className="grid grid-cols-2 gap-7 pt-5">
             <div className="h-[400px]">
-                <motion.div initial={{ translateY: '100%' }} animate={{ translateY: 0 }}>
-                    <StaticImage className="w-full" src="./images/session-recording.png" />
+                <motion.div
+                    transition={{ delay: 0.4 }}
+                    className="h-full"
+                    initial={{ translateY: '100%' }}
+                    animate={{ translateY: 0 }}
+                >
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        objectFit="contain"
+                        className="w-full h-full"
+                        src="./images/session-recording.png"
+                    />
                 </motion.div>
             </div>
             <div className="relative flex flex-col">
-                <div>
+                <div className="max-w-[420px]">
                     <Title title={'Session recording'} />
                     <Subtitle subtitle="with console logs. Watch a group of sessions for users in a cohort." />
 
@@ -110,7 +140,12 @@ export const SessionRecording = () => {
                         </Link>
                     </div>
                     <div className="relative w-3/4">
-                        <StaticImage className="w-full" src="./images/session-recording-hog.png" />
+                        <StaticImage
+                            placeholder="none"
+                            quality={100}
+                            className="w-full"
+                            src="./images/session-recording-hog.png"
+                        />
                     </div>
                 </div>
             </div>
@@ -121,33 +156,38 @@ export const SessionRecording = () => {
 export const FeatureFlags = () => {
     return (
         <div className="grid grid-cols-2 gap-7 pt-5">
-            <div className="h-[400px] relative">
+            <div className="h-[400px] relative overflow-hidden">
                 <motion.div
+                    transition={{ delay: 0.4 }}
                     className="absolute left-0 bottom-0 w-3/4"
                     initial={{ translateY: '100%' }}
                     animate={{ translateY: 0 }}
                 >
-                    <StaticImage className="w-full" src="./images/feature-flags-2.png" />
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        className="w-full"
+                        src="./images/feature-flags-2.png"
+                    />
                 </motion.div>
                 <motion.div
+                    transition={{ delay: 0.4 }}
                     className="absolute right-0 top-0 w-3/4"
                     initial={{ translateY: '100%' }}
                     animate={{ translateY: 0 }}
                 >
-                    <StaticImage className="w-full" src="./images/feature-flags-1.png" />
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        className="w-full"
+                        src="./images/feature-flags-1.png"
+                    />
                 </motion.div>
             </div>
             <div className="relative flex flex-col">
-                <div>
+                <div className="max-w-[420px]">
                     <Title title={'Feature flags'} />
-                    <Subtitle
-                        subtitle={
-                            <>
-                                with multivariate testing. Roll out features to <br />
-                                groups or specific users.
-                            </>
-                        }
-                    />
+                    <Subtitle subtitle="with multivariate testing. Roll out features to groups or specific users." />
 
                     <CTA url="/product/feature-flags" title="See how it works" />
                 </div>
@@ -160,7 +200,12 @@ export const FeatureFlags = () => {
                     </div>
                     <div className="relative w-3/4">
                         <div className="absolute bottom-0 right-0">
-                            <StaticImage className="w-full max-w-[480px]" src="./images/feature-flags-hog.png" />
+                            <StaticImage
+                                placeholder="none"
+                                quality={100}
+                                className="w-full max-w-[480px]"
+                                src="./images/feature-flags-hog.png"
+                            />
                         </div>
                     </div>
                 </div>
@@ -177,24 +222,26 @@ export const ABTesting = () => {
 
     return (
         <div className="grid grid-cols-2 gap-7 pt-5">
-            <div className="h-[400px] relative">
+            <div className="h-[400px] relative overflow-hidden">
                 <motion.div
+                    transition={{ delay: 0.4 }}
                     className="absolute left-0 top-5 w-11/12"
                     initial={{ translateY: '100%' }}
                     animate={{ translateY: 0 }}
                 >
-                    <StaticImage className="w-full" src="./images/ab-testing-2.png" />
+                    <StaticImage placeholder="none" quality={100} className="w-full" src="./images/ab-testing-2.png" />
                 </motion.div>
                 <motion.div
+                    transition={{ delay: 0.4 }}
                     className="absolute right-5 top-0 w-3/4"
                     initial={{ translateY: '100%' }}
                     animate={{ translateY: 0 }}
                 >
-                    <StaticImage className="w-full" src="./images/ab-testing-1.png" />
+                    <StaticImage placeholder="none" quality={100} className="w-full" src="./images/ab-testing-1.png" />
                 </motion.div>
             </div>
             <div className="relative flex flex-col">
-                <div>
+                <div className="max-w-[420px]">
                     <Title title={'A/B testing'} />
                     <Subtitle subtitle="with multivariate testing" />
                     <ul className="list-none m-0 p-0 mt-5 inline-block">
@@ -222,8 +269,164 @@ export const ABTesting = () => {
                     </div>
                     <div className="relative w-3/4">
                         <div className="absolute bottom-0 right-0">
-                            <StaticImage className="w-full max-w-[480px]" src="./images/ab-testing-hog.png" />
+                            <StaticImage
+                                placeholder="none"
+                                quality={100}
+                                className="w-full max-w-[480px]"
+                                src="./images/ab-testing-hog.png"
+                            />
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const EventPipelines = () => {
+    const data = [
+        { Icon: JS, url: '' },
+        { Icon: ReactIcon, url: '' },
+        { Icon: NodeJS, url: '' },
+        { Icon: Ruby, url: '' },
+        { Icon: Ios, url: '' },
+        { Icon: Android, url: '' },
+    ]
+
+    return (
+        <div className="grid grid-cols-2 gap-7 pt-5">
+            <div className="h-[400px] relative">
+                <motion.div
+                    transition={{ delay: 0.4 }}
+                    className="h-full"
+                    initial={{ translateY: '100%' }}
+                    animate={{ translateY: 0 }}
+                >
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        objectFit="contain"
+                        className="w-full h-full"
+                        src="./images/event-pipelines.png"
+                    />
+                </motion.div>
+            </div>
+            <div className="relative flex flex-col">
+                <div className="max-w-[420px]">
+                    <Title title={'Event pipelines'} />
+                    <Subtitle subtitle="Enrich customer profiles in sales and marketing clouds with event data you send to PostHog." />
+                    <ul className="list-none m-0 p-0 mt-5 flex space-x-2 items-center">
+                        {data.map(({ Icon, url }, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link className="text-primary hover:text-primary" to={url}>
+                                        <Icon className="w-[22px]" />
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <CTA url="/docs/integrate" title="Browse all libraries" />
+                </div>
+                <div className="flex items-end mt-auto w-full">
+                    <div className="mb-3 flex-grow">
+                        <hr className="w-[20px] h-[3px] rounded-full" />
+                        <p className="text-[14px] m-0">Use another service?</p>
+                        <Link className="text-[14px]" to="/docs/apps/build">
+                            Build an app connector
+                        </Link>
+                    </div>
+                    <div className="relative w-3/4">
+                        <div className="absolute bottom-0 right-0">
+                            <StaticImage
+                                placeholder="none"
+                                quality={100}
+                                className="w-full max-w-[480px]"
+                                src="./images/event-pipelines-hog.png"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const DataWarehouse = () => {
+    return (
+        <div className="grid grid-cols-2 gap-7 pt-5">
+            <div className="h-[400px] relative">
+                <motion.div
+                    transition={{ delay: 0.4 }}
+                    className="h-full"
+                    initial={{ translateY: '100%' }}
+                    animate={{ translateY: 0 }}
+                >
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        objectFit="contain"
+                        className="w-full h-full"
+                        src="./images/data-warehouse.png"
+                    />
+                </motion.div>
+            </div>
+            <div className="relative flex flex-col">
+                <div className="max-w-[420px]">
+                    <Title title={'Data warehouse'} />
+                    <Subtitle subtitle="Sync with your warehouse. Or keep customer data in-house by self-hosting and using PostHog’s built-in data warehouse." />
+
+                    <CTA url="/docs/api" title="Explore what you can do" />
+                </div>
+                <div className="mt-auto w-full">
+                    <div className="w-full">
+                        <StaticImage
+                            placeholder="none"
+                            quality={100}
+                            className="w-full"
+                            src="./images/data-warehouse-hog.png"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const SelfHosting = () => {
+    return (
+        <div className="grid grid-cols-2 gap-7 pt-5">
+            <div className="h-[400px] relative">
+                <motion.div
+                    transition={{ delay: 0.4 }}
+                    className="h-full"
+                    initial={{ translateY: '100%' }}
+                    animate={{ translateY: 0 }}
+                >
+                    <StaticImage
+                        placeholder="none"
+                        quality={100}
+                        objectFit="contain"
+                        className="w-full h-full"
+                        src="./images/self-hosting.png"
+                    />
+                </motion.div>
+            </div>
+            <div className="relative flex flex-col">
+                <div className="max-w-[420px]">
+                    <Title title={'Self hosting'} />
+                    <Subtitle subtitle="Host on-prem so customer data never leaves your infrastructure. Plus get full SQL access." />
+
+                    <CTA url="/docs/self-host" title="Learn more" />
+                </div>
+                <div className="mt-auto w-full">
+                    <div className="w-full">
+                        <StaticImage
+                            placeholder="none"
+                            quality={100}
+                            className="w-full max-w-[225px]"
+                            src="./images/self-hosting-hog.png"
+                        />
                     </div>
                 </div>
             </div>
--- src/components/Home/Slider/index.js
+++ src/components/Home/Slider/index.js
@@ -1,7 +1,16 @@
+import { RightArrow } from 'components/Icons/Icons'
 import React, { useState } from 'react'
 import SliderComponent from 'react-slick'
 import { slideButtons } from './slideButtons'
-import { ABTesting, FeatureFlags, ProductAnalytics, SessionRecording } from './Slides'
+import {
+    ABTesting,
+    DataWarehouse,
+    EventPipelines,
+    FeatureFlags,
+    ProductAnalytics,
+    SelfHosting,
+    SessionRecording,
+} from './Slides'
 
 const SlideButton = ({ title, Icon, activeSlide, index }) => {
     const active = activeSlide === index
@@ -27,7 +36,7 @@ export default function Slider() {
     const [buttonRef, setButtonRef] = useState()
     const [slideRef, setSlideRef] = useState()
     const [activeSlide, setActiveSlide] = useState(0)
-    console.log(slideButtons)
+
     const handleChange = (_oldIndex, newIndex) => {
         setActiveSlide(newIndex)
     }
@@ -35,32 +44,56 @@ export default function Slider() {
     return (
         <div>
             <div className="border-t border-b border-gray-accent-light border-dashed">
-                <SliderComponent
-                    beforeChange={handleChange}
-                    ref={(buttonRef) => setButtonRef(buttonRef)}
-                    asNavFor={slideRef}
-                    arrows={false}
-                    slidesToShow={7}
-                    focusOnSelect
-                    className="home-slider list-none max-w-7xl mx-auto m-0 p-0 border-r border-l border-gray-accent-light border-dashed"
-                >
-                    {slideButtons.map((slide, index) => {
-                        return <SlideButton index={index} activeSlide={activeSlide} key={index} {...slide} />
-                    })}
-                </SliderComponent>
-                <SliderComponent
-                    ref={(slideRef) => setSlideRef(slideRef)}
-                    asNavFor={buttonRef}
-                    arrows={false}
-                    slidesToShow={1}
-                    infinite
-                    className="list-none max-w-7xl mx-auto m-0 p-0 flex items-center justify-center divide divide-x divide-gray-accent-light divide-dashed overflow-auto border-r border-l border-gray-accent-light border-dashed"
-                >
-                    <ProductAnalytics />
-                    <SessionRecording />
-                    <FeatureFlags />
-                    <ABTesting />
-                </SliderComponent>
+                <div className="border-b border-dashed border-gray-accent-light">
+                    <SliderComponent
+                        beforeChange={handleChange}
+                        ref={(buttonRef) => setButtonRef(buttonRef)}
+                        asNavFor={slideRef}
+                        arrows={false}
+                        slidesToShow={7}
+                        focusOnSelect
+                        className="home-slider list-none max-w-7xl mx-auto m-0 p-0 border-r border-l border-gray-accent-light border-dashed"
+                    >
+                        {slideButtons.map((slide, index) => {
+                            return <SlideButton index={index} activeSlide={activeSlide} key={index} {...slide} />
+                        })}
+                    </SliderComponent>
+                </div>
+                <div className="flex items-center">
+                    <div className="flex-grow flex justify-center items-center">
+                        <button
+                            onClick={() => slideRef.slickPrev()}
+                            className="px-3 py-2 bg-white rounded-md shadow-md text-primary"
+                        >
+                            <RightArrow className="rotate-180 w-[14px]" />
+                        </button>
+                    </div>
+                    <SliderComponent
+                        lazyLoad="ondemand"
+                        ref={(slideRef) => setSlideRef(slideRef)}
+                        asNavFor={buttonRef}
+                        arrows={false}
+                        slidesToShow={1}
+                        infinite
+                        className="flex-shrink-0 list-none max-w-7xl w-full mx-auto m-0 p-0 flex items-center justify-center divide divide-x divide-gray-accent-light divide-dashed overflow-auto border-l border-r border-gray-accent-light border-dashed"
+                    >
+                        <ProductAnalytics />
+                        <SessionRecording />
+                        <FeatureFlags />
+                        <ABTesting />
+                        <EventPipelines />
+                        <DataWarehouse />
+                        <SelfHosting />
+                    </SliderComponent>
+                    <div className="flex-grow flex justify-center items-center">
+                        <button
+                            onClick={() => slideRef.slickNext()}
+                            className="px-3 py-2 bg-white rounded-md shadow-md text-primary"
+                        >
+                            <RightArrow className="w-[14px]" />
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
