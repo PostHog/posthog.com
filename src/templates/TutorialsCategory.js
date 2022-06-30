9b8fb3325a1f138dc33375ad51802edf47b729ac
Paul Hultgren <pjhultgren5@gmail.com>
2022-06-29 18:22:41 -0700

    import graphql from gatsby
    Using the global graphql is no longer supported after gatsby v3

--- src/templates/TutorialsCategory.js
+++ src/templates/TutorialsCategory.js
@@ -2,8 +2,10 @@ import Tutorials from 'components/Tutorials'
 
 export default Tutorials
 
+import { graphql } from 'gatsby'
+
 export const pageQuery = graphql`
-    query($activeFilter: String) {
+    query ($activeFilter: String) {
         tutorials: allMdx(
             sort: { order: DESC, fields: [frontmatter___date] }
             filter: { frontmatter: { topics: { in: [$activeFilter] } }, fields: { slug: { regex: "/^/tutorials/" } } }
