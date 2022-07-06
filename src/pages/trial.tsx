813c2f5502b15265aab41e6dd131f37f69bfe481
Paul Hultgren <pjhultgren5@gmail.com>
2022-07-06 10:53:27 -0700

    finalize trial page

--- src/pages/trial.tsx
+++ src/pages/trial.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Layout from 'components/Layout'
+import Layout from '../components/Layout'
 import { CallToAction } from 'components/CallToAction'
-import { Cloud, ServerIcon } from 'components/Icons/Icons'
+import { CloudFilled, HddFilled } from '@ant-design/icons'
 import { SEO } from '../components/seo'
 
 const TrialPage = () => (
@@ -13,30 +13,30 @@ const TrialPage = () => (
 
             <div className="md:grid md:grid-cols-2 md:grid-flow-row-dense auto-rows-min gap-x-16 mt-8 md:mt-16">
                 <h2 className="flex items-center space-x-2 col-start-1">
-                    <Cloud /> <span>Cloud</span>
+                    <CloudFilled /> <span>Cloud</span>
                 </h2>
-                <h3 className="col-start-1">Just create an account.</h3>
-                <p className="col-start-1">
+                <h4 className="col-start-1 mt-2">Just create an account.</h4>
+                <p className="col-start-1 mt-6">
                     Select this option if you want to quickly try the PostHog features and don't want to worry about
                     installing it yourself.
                 </p>
                 <CallToAction
                     type="primary"
                     width="56"
-                    to="/https://app.posthog.com/signup"
+                    to="https://app.posthog.com/signup"
                     className="col-start-1 h-12"
                 >
                     Sign Up
                 </CallToAction>
 
                 <h2 className="flex items-center space-x-2 mt-16 md:mt-0">
-                    <ServerIcon /> <span>Open Source</span>
+                    <HddFilled /> <span>Open Source</span>
                 </h2>
-                <h3>Host your own instance.</h3>
-                <p className="h-16">
+                <h4 className="mt-2">Host your own instance.</h4>
+                <p className="h-16 mt-6">
                     Select this option if you want to install our open source platform on your own infrastructure.
                 </p>
-                <CallToAction type="primary" width="56" className="h-12" to="/https://app.posthog.com/signup">
+                <CallToAction type="primary" width="56" className="h-12" to="/docs/self-host">
                     Self Deploy
                 </CallToAction>
             </div>
