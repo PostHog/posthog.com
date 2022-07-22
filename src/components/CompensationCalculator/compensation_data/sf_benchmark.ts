5e1758309f6e676d3ef277736ba1b8ed2c8e7411
Charles Cook <charles@posthog.com>
2022-07-22 18:02:54 +0100

    Add missing benchmarks (#3785)
    Add data eng and backend eng benchmarks to compensation calculator

--- src/components/CompensationCalculator/compensation_data/sf_benchmark.ts
+++ src/components/CompensationCalculator/compensation_data/sf_benchmark.ts
@@ -1,7 +1,9 @@
 export const sfBenchmark: Record<string, number> = {
     // 'Community Engineer': 199000,
+    'Backend Engineer': 236000,
     'Content Marketer': 165000,
     'Customer Success Manager': 192000,
+    'Data Engineer': 236000,
     'Developer Advocate': 236000,
     'Front End Developer': 212000,
     'Full Stack Engineer': 236000,
@@ -12,7 +14,7 @@ export const sfBenchmark: Record<string, number> = {
     'Product Designer': 186000,
     'Product Manager': 213600,
     'Product Marketer': 195000,
-    Recruiter: 173000,
+    'Recruiter': 173000,
     'Site Reliability Engineer': 236000,
     'Talent Partner': 173000,
     // 'Technical Writer': 150000,
