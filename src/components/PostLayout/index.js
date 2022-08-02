72c250b08fc83fad052a24be2c30918569461f32
Eli Kinsey <eli@ekinsey.dev>
2022-08-01 21:07:09 -0700

    dark mode

--- src/components/PostLayout/index.js
+++ src/components/PostLayout/index.js
@@ -337,11 +337,11 @@ const NextPost = ({ contentContainerClasses = '', excerpt, frontmatter, fields }
     return (
         <div className="py-6 border-t border-gray-accent-light dark:border-gray-accent-dark border-dashed mt-5">
             <div className={contentContainerClasses}>
-                <p className="text-lg text-black/40 m-0 font-bold">Next article</p>
+                <p className="text-lg text-black/40 dark:text-white/40 m-0 font-bold">Next article</p>
                 <h3 className="text-xl font-bold m-0 my-1">{frontmatter?.title}</h3>
                 <p className="relative max-h-24 overflow-hidden">
                     {excerpt}
-                    <span className="bg-gradient-to-t from-tan to-transparent absolute w-full h-full inset-0" />
+                    <span className="bg-gradient-to-t from-tan dark:from-primary to-transparent absolute w-full h-full inset-0" />
                 </p>
                 <CallToAction to={fields?.slug}>Read next article</CallToAction>
             </div>
