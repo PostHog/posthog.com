e672a474877c5205e3011bf62a6e5ab104517a1e
Paul Hultgren <pjhultgren5@gmail.com>
2022-08-12 10:20:40 -0700

    add unified sidebar to tutorials and move to tsx

--- src/templates/tutorials/TutorialsCategory.tsx
+++ src/templates/tutorials/TutorialsCategory.tsx
@@ -3,13 +3,10 @@ import Tutorials from 'components/Tutorials'
 export default Tutorials
 
 export const pageQuery = graphql`
-    query($activeFilter: String) {
+    query ($activeFilter: String) {
         tutorials: allMdx(
             sort: { order: DESC, fields: [frontmatter___date] }
-            filter: {
-                fields: { slug: { regex: "/^/tutorials/" } }
-                frontmatter: { authorData: { elemMatch: { name: { eq: $activeFilter } } } }
-            }
+            filter: { frontmatter: { topics: { in: [$activeFilter] } }, fields: { slug: { regex: "/^/tutorials/" } } }
         ) {
             nodes {
                 ...TutorialsFragment
