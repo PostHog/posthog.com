4a95d5c5d3d00e6d331f11c8b8274fb18ab3e896
Eli Kinsey <eli@ekinsey.dev>
2022-08-17 14:53:49 -0700

    update squeak-react (#4012)

--- src/components/MainNav/Submenus/Community/index.tsx
+++ src/components/MainNav/Submenus/Community/index.tsx
@@ -96,8 +96,6 @@ export default function Docs({ referenceElement }: { referenceElement: HTMLDivEl
                                         topics={false}
                                         slug={null}
                                         apiHost="https://squeak.cloud"
-                                        apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4aXBrcXV2d3FhYXVudXpqb2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk3MjE3ODUsImV4cCI6MTk2NTI5Nzc4NX0.SxdOpxHjVwap7sDUptK2TFJl7WK3v3HLuKbzb0JKeKg"
-                                        url="https://pxipkquvwqaaunuzjoge.supabase.co"
                                         organizationId="a898bcf2-c5b9-4039-82a0-a00220a8c626"
                                     />
                                 </div>
