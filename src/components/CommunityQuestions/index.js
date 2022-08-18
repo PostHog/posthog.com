ed0d570fa5707750a1dcc0cfc52f2523756b3078
Eli Kinsey <eli@ekinsey.dev>
2022-08-18 09:41:19 -0700

    enable squeak

--- src/components/CommunityQuestions/index.js
+++ src/components/CommunityQuestions/index.js
@@ -2,5 +2,12 @@ import React from 'react'
 import { Squeak } from 'squeak-react'
 
 export default function CommunityQuestions() {
-    return null
+    return (
+        <div className="max-w-[600px] mt-12">
+            <h3 id="squeak-questions" className="mb-4">
+                Questions?
+            </h3>
+            <Squeak apiHost="https://squeak.cloud" organizationId="a898bcf2-c5b9-4039-82a0-a00220a8c626" />
+        </div>
+    )
 }
