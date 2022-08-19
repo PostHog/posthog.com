3cce82e9131a2d7e836a0f44dc18ed9088f1f989
Eli Kinsey <eli@ekinsey.dev>
2022-08-19 10:50:09 -0700

    disable dark mode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -18,9 +18,7 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
     // Checking for prevLocation prevents this from happening twice
     if (typeof window !== 'undefined' && prevLocation) {
         var slug = location.pathname.substring(1)
-        var theme = /^handbook|^docs|^blog|^integrations|^product|^tutorials|^questions|^manual|^using-posthog/.test(
-            slug
-        )
+        var theme = /^handbook|^docs|^blog|^integrations|^tutorials|^questions|^manual|^using-posthog/.test(slug)
             ? window.__theme
             : 'light'
         document.body.className = theme
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -49,7 +49,7 @@ export const onRenderBody = function ({ setPreBodyComponents }) {
     })
     try {
         preferredTheme =
-            (/^handbook|^docs|^blog|^integrations|^product|^tutorials|^questions|^using-posthog|^manual/.test(slug) &&
+            (/^handbook|^docs|^blog|^integrations|^tutorials|^questions|^using-posthog|^manual/.test(slug) &&
                 (localStorage.getItem('theme') || (darkQuery.matches ? 'dark' : 'light'))) ||
             'light'
     } catch (err) {}
