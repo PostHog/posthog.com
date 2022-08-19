34b4e9fd6194e92e90a509133a04ad200eaa9999
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-08-19 13:17:26 +0100

    Update eu-cloud.js (#4023)

--- src/pages/signup/eu-cloud.js
+++ src/pages/signup/eu-cloud.js
@@ -42,12 +42,9 @@ export default function SelfHost() {
         >
             <SEO title="Get in touch - PostHog" />
             <section className="px-4">
-
                 <div className="max-w-5xl mx-auto mt-12">
-
                     <div className="md:flex gap-8 xl:gap-16">
                         <aside className="grow-0 shrink-0 basis-[428px] pt-6">
-                
                             <StaticImage
                                 width={428}
                                 height={503}
@@ -61,37 +58,42 @@ export default function SelfHost() {
                                 <Plan
                                     className="px-0"
                                     title="While you're waiting..."
-                                    subtitle="We'll get back to you when PostHog Cloud EU is ready, but you can get started today on PostHog Cloud (US), or you can self-hostm, then migrate your event data when we're ready for you."
+                                    subtitle="We'll get back to you when PostHog Cloud EU is ready. In the mean time, you can use PostHog Cloud (US), or you can self-host, then migrate data when we're ready for you."
                                 >
                                     <div className="mt-6 flex justify-between items-center bg-gray-accent-light px-[18px] py-[16px] rounded-md flex-col xl:flex-row space-y-2 xl:space-y-0 ">
-                                        <p className="m-0 font-bold">Create a free PostHog Cloud account</p>
+                                        <p className="m-0 font-bold">Try PostHog Cloud for free</p>
                                         <CallToAction
                                             size="sm"
                                             type="primary"
                                             className="shadow-md"
                                             to="https://app.posthog.com/signup"
                                         >
-                                            Get started - free
+                                            Get started
                                         </CallToAction>
                                     </div>
                                     <div className="mt-6 flex justify-between items-center bg-gray-accent-light px-[18px] py-[16px] rounded-md flex-col xl:flex-row space-y-2 xl:space-y-0 ">
-                                        <p className="m-0 font-bold">Deploy a self-hosted instance</p>
+                                        <p className="m-0 font-bold">Self-host PostHog for free</p>
                                         <CallToAction
                                             size="sm"
                                             type="primary"
                                             className="shadow-md"
                                             to="/signup/self-host/deploy"
                                         >
-                                            Get started - free
+                                            Deploy today
                                         </CallToAction>
                                     </div>
                                 </Plan>
                             ) : (
                                 <>
                                     <h1 className="text-4xl md:text-5xl mb-2">
-                                        PostHog Cloud <span className="inline-block rounded px-2 py-1 text-[#FFCC00] bg-[#003399]">EU</span>
+                                        PostHog Cloud{' '}
+                                        <span className="inline-block rounded px-2 py-1 text-[#FFCC00] bg-[#003399]">
+                                            EU
+                                        </span>
                                     </h1>
-                                    <h2 className="text-xl md:text-2xl opacity-70 font-semibold mb-4">Same product suite. New hosting option.</h2>
+                                    <h2 className="text-xl md:text-2xl opacity-70 font-semibold mb-4">
+                                        Same product suite. New hosting option.
+                                    </h2>
                                     <p className="font-semibold opacity-70">Estimated arrival: Winter 2022</p>
 
                                     <h3 className="leading-tight">Get notified when PostHog Cloud EU is ready</h3>
