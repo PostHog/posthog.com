9ab56820228d7fe814ac0ae51a443f6f51d8e6d1
Eli Kinsey <eli@ekinsey.dev>
2022-08-28 23:29:54 -0700

    client side ff

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,28 +1,24 @@
 import Control from 'components/Pricing/Control'
-import React from 'react'
-import { PostHog } from 'posthog-node'
+import React, { useEffect, useState } from 'react'
 import Test from 'components/Pricing/Test'
 
-const PricingNew = ({ serverData }): JSX.Element => {
-    return serverData?.featureFlag === 'test' ? <Test /> : <Control />
+import { useValues } from 'kea'
+
+import { posthogAnalyticsLogic } from '../../logic/posthogAnalyticsLogic'
+
+const PricingNew = () => {
+    const { posthog } = useValues(posthogAnalyticsLogic)
+    const [loading, setLoading] = useState(true)
+    const [featureFlagEnabled, setFeatureFlagEnabled] = useState(false)
+
+    useEffect(() => {
+        if (posthog && posthog.isFeatureEnabled && posthog.isFeatureEnabled('highlight-open-source')) {
+            setFeatureFlagEnabled(true)
+        }
+        setLoading(false)
+    }, [])
+
+    return loading ? null : featureFlagEnabled ? <Test /> : <Control />
 }
 
 export default PricingNew
-
-export async function getServerData(context) {
-    function getCookieValue(cookie, name) {
-        const value = `; ${cookie}`
-        const parts = value.split(`; ${name}=`)
-        if (parts.length === 2) return parts.pop().split(';').shift()
-    }
-    const phCookie = getCookieValue(context.headers?.get('cookie'), 'ph_sTMFPsFhdP1Ssg_posthog')
-    const decoded = phCookie && decodeURIComponent(phCookie)
-    const distinctId = decoded && JSON.parse(decoded).distinct_id
-
-    const client = new PostHog('sTMFPsFhdP1Ssg')
-    const featureFlag = await client.getFeatureFlag('highlight-open-source', distinctId)
-    console.log(featureFlag)
-    return {
-        props: { featureFlag },
-    }
-}
