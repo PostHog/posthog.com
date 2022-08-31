0c44487aa63209d5ece4222da9517f7475a00c45
Paul Hultgren <pjhultgren5@gmail.com>
2022-08-31 14:09:17 -0700

    add more languages and create mappings

---
+++ src/components/CodeBlock/languages.tsx
@@ -0,0 +1,147 @@
+import React from 'react'
+import type { Language } from 'prism-react-renderer'
+import Prism from 'prism-react-renderer/prism'
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+
+require('prismjs/components/prism-php')
+require('prismjs/components/prism-ruby')
+require('prismjs/components/prism-swift')
+require('prismjs/components/prism-dart')
+
+type LanguageMap = {
+    [key: string]: {
+        language: Language | 'html' | 'xml' | 'shell' | 'php' | 'ruby' | 'swift' | 'dart'
+        label: React.ReactNode
+    }
+}
+
+const languageMap: LanguageMap = {
+    js: {
+        language: 'javascript',
+        label: 'JavaScript',
+    },
+    ts: {
+        language: 'typescript',
+        label: 'TypeScript',
+    },
+    javascript: {
+        language: 'javascript',
+        label: 'JavaScript',
+    },
+    json: {
+        language: 'json',
+        label: 'JSON',
+    },
+    swift: {
+        language: 'swift',
+        label: 'Swift',
+    },
+    dart: {
+        language: 'dart',
+        label: 'Dart',
+    },
+    node: {
+        language: 'javascript',
+        label: 'Node.js',
+    },
+    go: {
+        language: 'go',
+        label: 'Go',
+    },
+    php: {
+        language: 'php',
+        label: 'PHP',
+    },
+    java: {
+        language: 'cpp',
+        label: 'Java',
+    },
+    ruby: {
+        language: 'ruby',
+        label: 'Ruby',
+    },
+    xml: {
+        language: 'xml',
+        label: 'XML',
+    },
+    jsx: {
+        language: 'jsx',
+        label: 'JSX',
+    },
+    shell: {
+        language: 'shell',
+        label: (
+            <div className="flex items-center space-x-1.5">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-3 h-3"
+                >
+                    <polyline points="4 17 10 11 4 5" />
+                    <line x1="12" y1="19" x2="20" y2="19" />
+                </svg>
+                <span className="font-semibold">Terminal</span>
+            </div>
+        ),
+    },
+    bash: {
+        language: 'bash',
+        label: (
+            <div className="flex items-center space-x-1.5">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-3 h-3"
+                >
+                    <polyline points="4 17 10 11 4 5" />
+                    <line x1="12" y1="19" x2="20" y2="19" />
+                </svg>
+                <span className="font-semibold">Terminal</span>
+            </div>
+        ),
+    },
+    android: {
+        language: 'clike',
+        label: 'Android',
+    },
+    objectivec: {
+        language: 'objectivec',
+        label: 'Objective-C',
+    },
+    html: {
+        language: 'html',
+        label: 'HTML',
+    },
+    yaml: {
+        language: 'yaml',
+        label: 'YAML',
+    },
+    python: {
+        language: 'python',
+        label: 'Python',
+    },
+    ios: {
+        language: 'objectivec',
+        label: 'iOS',
+    },
+    'react-native': {
+        language: 'jsx',
+        label: 'React Native',
+    },
+}
+
+export default languageMap
