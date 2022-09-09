34674e394e5b6ea539ac5f1442c08abe73958c54
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-09-09 00:03:52 -0300

    fix broken link (#4152)

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -433,6 +433,7 @@ module.exports = exports.createPages = async ({ actions: { createPage }, graphql
             },
         })
     })
+
     result.data.apps.nodes.forEach((node) => {
         const { slug } = node.fields
         const { documentation } = node.frontmatter
