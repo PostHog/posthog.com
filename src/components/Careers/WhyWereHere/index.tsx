1340b5307127085eaf4b7f9a1979f9993657d477
Cory Watilo <corywatilo@gmail.com>
2022-09-11 13:35:24 -0700

    added tim to careers letter

--- src/components/Careers/WhyWereHere/index.tsx
+++ src/components/Careers/WhyWereHere/index.tsx
@@ -4,11 +4,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 import { AnchorScrollNavbarTop } from './AnchorScrollNavbarTop'
 
 const P = ({ children }) => {
-    return (
-        <p className="text-justify text-[17px] leading-relaxed">
-            {children}
-        </p>
-    )
+    return <p className="text-justify text-[17px] leading-relaxed">{children}</p>
 }
 
 export const WhyWereHere = () => {
@@ -16,36 +12,44 @@ export const WhyWereHere = () => {
         <div id="introduction" className="mb-16">
             <div className="text-left">
                 <Structure.Section width="5xl" className="text-lg">
-
                     <div className="lg:grid grid-cols-4 gap-8">
-                        <div className="text-opacity-80 text-gray-100 bg-white/90 backdrop-blur shadow-xl py-12 px-12 md:px-20 rounded col-span-3">
+                        <div className="text-opacity-80 text-gray-100 bg-white/90 backdrop-blur shadow-xl pt-6 pb-12 px-12 md:px-20 rounded col-span-3">
                             <div className="before:bg-[url('/images/letterhead.svg')] before:left-0 before:w-[6px] before:top-0 before:h-full before:absolute">
-
-                                <div className="flex space-between w-full pb-2 mb-8 border-b border-dashed border-gray-accent-light">
+                                <div className="flex items-center space-between w-full pb-4 mb-8 border-b border-dashed border-gray-accent-light">
                                     <div className="flex-1">
-                                        <em className="text-sm opacity-60">From the desk of</em>
-                                        <h4 className="mb-0">James Hawkins</h4>
-                                        <p className="text-gray-100 opacity-75 text-[15px]">CEO &amp; Co-Founder</p>
+                                        <p className="opacity-50 text-[17px] font-semibold mb-0 leading-tight">
+                                            A note from our co-founders...
+                                        </p>
                                     </div>
-                                    <figure>
-                                        <StaticImage src="../../../../contents/images/team/james.png" alt="James Hawkins" className="w-20 h-20 rounded-full bg-yellow" placeholder="blurred" />
+                                    <figure className="mb-0">
+                                        <StaticImage
+                                            src="../../../../contents/images/team/james.png"
+                                            alt="James Hawkins"
+                                            className="w-14 h-14 rounded-full bg-yellow border-2 border-solid border-white"
+                                            placeholder="blurred"
+                                        />
+                                        <StaticImage
+                                            src="../../../../contents/images/team/tim.png"
+                                            alt="Tim Glaser"
+                                            className="w-14 h-14 rounded-full bg-red border-2 border-solid border-white -ml-4"
+                                            placeholder="blurred"
+                                        />
                                     </figure>
                                 </div>
                                 <div>
                                     <h3>An introduction</h3>
                                     <P>
-                                    Until now, tools for building products have been fragmented. Product analytics,
-                                        heatmaps, session recording, feature flags and A/B testing are all helpful, but no
-                                        one wants to buy, send data to, and integrate multiple products.
+                                        Until now, tools for building products have been fragmented. Product analytics,
+                                        heatmaps, session recording, feature flags and A/B testing are all helpful, but
+                                        no one wants to buy, send data to, and integrate multiple products.
                                     </P>
                                     <P>
-                                        PostHog is different by offering these tools (and more) in a single platform that
-                                        you can host yourself.
+                                        PostHog is different by offering these tools (and more) in a single platform
+                                        that you can host yourself.
                                     </P>
 
                                     <P>
-                                        We started PostHog during Y Combinator's W20 cohort  and had the most successful 
-
+                                        We started PostHog during Y Combinator's W20 cohort and had the most successful
                                         <aside className="float-right w-36 md:w-64 mt-2 -mr-6 md:-mr-10 pl-4 md:pl-12">
                                             <StaticImage
                                                 src="images/this-hog-ships.png"
@@ -53,18 +57,20 @@ export const WhyWereHere = () => {
                                                 height={390}
                                                 width={410}
                                             />
-                                        </aside> B2B software launch on Hacker News since 2012 - with a product that was just 4 weeks
-                                        old.
+                                        </aside>{' '}
+                                        B2B software launch on Hacker News since 2012 - with a product that was just 4
+                                        weeks old.
                                     </P>
                                     <P>
                                         We now have over 10,000 customers, we're{' '}
-                                        <a href="http://www.paulgraham.com/aord.html">default alive</a>, we grow 97% through
-                                        word of mouth, and we have a{' '}
+                                        <a href="http://www.paulgraham.com/aord.html">default alive</a>, we grow 97%
+                                        through word of mouth, and we have a{' '}
                                         <a href="https://github.com/posthog/posthog">very popular repo</a>.
                                     </P>
                                     <P>
                                         We believe in product-led growth, where we build something awesome and let our
-                                        product bring the users, rather than an outbound sales team and regular cold calls.
+                                        product bring the users, rather than an outbound sales team and regular cold
+                                        calls.
                                     </P>
                                     <P>
                                         The company operates transparently - we've made our{' '}
@@ -73,28 +79,64 @@ export const WhyWereHere = () => {
                                     </P>
                                     <P>
                                         What does this mean for you? We have a lot of capital (and from the world's best
-                                        investors), but we're a lean, strong team - so you've got the opportunity to have a
-                                        huge impact.
+                                        investors), but we're a lean, strong team - so you've got the opportunity to
+                                        have a huge impact.
                                     </P>
-                                    <StaticImage src="../../../images/initials-james.png" alt="by James Hawkins" width={60.5} height={57} quality={100} placeholder="none" />
+
+                                    <div className="flex items-center space-x-3">
+                                        <StaticImage
+                                            src="../../../images/signature-hyphen.png"
+                                            alt="-"
+                                            width={11.5}
+                                            height={3}
+                                            quality={100}
+                                            placeholder="none"
+                                        />
+                                        <StaticImage
+                                            src="../../../images/signature-initials-james.png"
+                                            alt="-"
+                                            width={48.5}
+                                            height={57}
+                                            quality={100}
+                                            placeholder="none"
+                                        />
+                                        <StaticImage
+                                            src="../../../images/signature-plus.png"
+                                            alt="-"
+                                            width={17}
+                                            height={20}
+                                            quality={100}
+                                            placeholder="none"
+                                        />
+                                        <StaticImage
+                                            src="../../../images/signature-initials-tim.png"
+                                            alt="-"
+                                            width={58.5}
+                                            height={55}
+                                            quality={100}
+                                            placeholder="none"
+                                            className="!-ml-0"
+                                        />
+                                    </div>
                                 </div>
                             </div>
-
                         </div>
 
-
                         <div className="px-5 mt-48 lg:px-8 lg:pb-4 lg:block hidden">
-                            <h4 className="text-black dark:text-white font-semibold opacity-25 m-0 mb-1 text-sm">Jump to:</h4>
+                            <h4 className="text-black dark:text-white font-semibold opacity-25 m-0 mb-1 text-sm">
+                                Jump to:
+                            </h4>
                             <ul className="list-none m-0 p-0 flex flex-col">
                                 <li className="relative leading-none m-0 active-product">
                                     <span className="block">
-                                        <a className="text-almost-black leading-tight font-medium hover:text-red dark:text-white dark:hover:text-red cursor-pointer hover:opacity-100 opacity-60 text-[14px] py-1 block relative active:top-[0.5px] active:scale-[.99]">Introduction</a>
+                                        <a className="text-almost-black leading-tight font-medium hover:text-red dark:text-white dark:hover:text-red cursor-pointer hover:opacity-100 opacity-60 text-[14px] py-1 block relative active:top-[0.5px] active:scale-[.99]">
+                                            Introduction
+                                        </a>
                                     </span>
                                 </li>
                                 <AnchorScrollNavbarTop />
                             </ul>
                         </div>
-
                     </div>
                 </Structure.Section>
             </div>
