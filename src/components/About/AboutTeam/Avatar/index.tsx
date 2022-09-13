df1d0dec925743d2a39be7fad3613a591de02d27
Cory Watilo <corywatilo@gmail.com>
2022-09-13 11:12:10 -0400

    somewhat of an avatar

--- src/components/About/AboutTeam/Avatar/index.tsx
+++ src/components/About/AboutTeam/Avatar/index.tsx
@@ -11,7 +11,6 @@ const sizes = {
 export const circle = (className = '', size = 'lg') => cntl`
     rounded-full 
     relative 
-    overflow-hidden 
     border-gray-accent-light 
     border-solid 
     inline-block 
@@ -23,6 +22,9 @@ export const circle = (className = '', size = 'lg') => cntl`
 export const Avatar = ({ size = 'lg' }) => {
     return (
         <div className={circle(size)}>
+            <div className="absolute -right-2 -bottom-1 bg-white w-8 h-8 rounded-full border-2 text-lg border-gray-accent-light border-solid flex items-center justify-center">
+                🇺🇸
+            </div>
             <img
                 src="https://posthog.com/static/99172eb231efe4d9c969fd91dd0a7011/a7ca0/lottie.webp"
                 className="w-full h-full border border-solid border-white box-border rounded-full"
