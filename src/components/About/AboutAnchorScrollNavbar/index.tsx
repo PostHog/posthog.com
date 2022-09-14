945fa8e2793b2ee33e2c437855135f3cb8fca91b
Eli Kinsey <eli@ekinsey.dev>
2022-09-13 18:37:17 -0700

    new anchorscrollnavbar

--- src/components/About/AboutAnchorScrollNavbar/index.tsx
+++ src/components/About/AboutAnchorScrollNavbar/index.tsx
@@ -1,140 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import scrollTo from 'gatsby-plugin-smoothscroll'
-import { mergeClassList } from '../../../lib/utils'
-import downIcon from '../../../images/icons/down-caret.svg'
-import Chip from 'components/Chip'
+import React from 'react'
+import { useBreakpoint } from 'gatsby-plugin-breakpoints'
 import NotProductIcons from 'components/NotProductIcons'
+import AnchorScrollNavbar from 'components/AnchorScrollNavbar'
 
-const ButtonLink = ({
-    section,
-    currentSection,
-    children,
-}: {
-    section: string
-    currentSection: string
-    children: React.ReactNode
-}) => {
-    const sectionSelector = `#${section}`
-    const clickHandler = (e: { preventDefault: () => void }) => {
-        e.preventDefault()
-        scrollTo(sectionSelector)
-    }
-
-    return (
-        <Chip active={section == currentSection} onClick={clickHandler}>
-            {children}
-        </Chip>
-    )
-}
-
-const inPageLinks = [
+const menu = [
     {
-        label: 'Overview',
-        section: 'overview',
+        name: 'Our story',
+        url: 'our-story',
+        icon: NotProductIcons.story,
     },
     {
-        label: 'Our story',
-        section: 'story',
+        name: "Why we're different",
+        url: 'transparency',
+        icon: NotProductIcons.transparency,
     },
     {
-        label: 'Transparency',
-        section: 'transparency',
+        name: 'Core team',
+        url: 'team',
+        icon: NotProductIcons.coreTeam,
     },
     {
-        label: 'Team',
-        section: 'team',
-    },
-    {
-        label: 'Investors',
-        section: 'investors',
+        name: 'Investors',
+        url: 'investors',
+        icon: NotProductIcons.investors,
     },
 ]
 
-interface AboutAnchorScrollNavbarProps {
-    className?: string
-}
-
-export const AboutAnchorScrollNavbar = ({ className = '' }: AboutAnchorScrollNavbarProps) => {
-    const baseClasses =
-        'space-x-2 w-full mx-auto justify-center p-2 sticky top-[-1px] bg-tan/80 backdrop-blur border-t border-b border-dashed border-gray-accent-light z-30 hidden md:inline-flex'
-    const classList = mergeClassList(baseClasses, className)
-
-    const [currentSection, setCurrentSection] = useState('why-were-here')
-
-    useEffect(() => {
-        const scrollThreshold = 100
-
-        const scrollHandler = () => {
-            const sections = {
-                overview: document.getElementById('overview')!.offsetTop,
-                story: document.getElementById('story')!.offsetTop,
-                transparency: document.getElementById('transparency')!.offsetTop,
-            }
-            const offset = window.scrollY
-
-            if (offset < sections.overview - scrollThreshold) {
-                setCurrentSection('overview')
-            } else if (offset < sections.story - scrollThreshold) {
-                setCurrentSection('story')
-            } else if (offset < sections.transparency - scrollThreshold) {
-                setCurrentSection('transparency')
-            } else {
-                setCurrentSection('overview')
-            }
-        }
-
-        document.addEventListener('scroll', scrollHandler)
-        window.addEventListener('resize', scrollHandler)
-
-        return () => {
-            document.removeEventListener('scroll', scrollHandler)
-            window.removeEventListener('resize', scrollHandler)
-        }
-    }, [])
-
-    const navbarLinks = inPageLinks.map(({ label, section }) => (
-        <ButtonLink section={section} currentSection={currentSection} key={section}>
-            {label}
-        </ButtonLink>
-    ))
-
-    const selectOptions = inPageLinks.map(({ label, section }) => (
-        <option value={section} key={section}>
-            {label}
-        </option>
-    ))
-
+export const AboutAnchorScrollNavbar = () => {
+    const breakpoints = useBreakpoint()
     return (
-        <>
-            <div className={`${classList} mb-12`}>
-                <div className="divide-x divide-gray-accent-light divide-dashed flex">
-                    <div className="flex items-center space-x-2 px-4 py-2 font-bold hover:bg-gray-accent-light rounded mx-[1px]">
-                        <span className="w-5 h-5">{NotProductIcons.story}</span>
-                        <span>Our story</span>
-                    </div>
-                    <div className="flex items-center space-x-2 px-4 py-2 font-semibold hover:bg-gray-accent-light rounded mx-[1px] opacity-50">
-                        <span className="w-5 h-5">{NotProductIcons.transparency}</span>
-                        <span>Why we're different</span>
-                    </div>
-                    <div className="flex items-center space-x-2 px-4 py-2 font-semibold hover:bg-gray-accent-light rounded mx-[1px] opacity-50">
-                        <span className="w-5 h-5">{NotProductIcons.coreTeam}</span>
-                        <span>Core team</span>
-                    </div>
-                </div>
-                {navbarLinks}
-            </div>
-
-            <div className="w-11/12 mb-8 sticky top-3 z-10 mx-auto block max-w-3xl md:hidden border border-1 border-tan/25 rounded">
-                <select
-                    className="appearance-none text-white bg-primary block p-3 w-full rounded font-bold"
-                    value={currentSection}
-                    onChange={(e) => scrollTo(`#${e.target.value}`)}
-                >
-                    {selectOptions}
-                </select>
-
-                <img src={downIcon} alt="expand menu" className="absolute top-2 right-2 mt-1" />
-            </div>
-        </>
+        <div className="bg-tan py-2 sticky top-0 z-10 mb-6">
+            <AnchorScrollNavbar
+                autoScroll={breakpoints.md}
+                className="max-w-screen-xl mx-auto lg:px-8 px-4"
+                menu={menu}
+            />
+        </div>
     )
 }
