fe0d34602296a815fdcd9d47003fdb06421cd97f
Paul Hultgren <pjhultgren5@gmail.com>
2022-09-23 13:29:18 -0700

    denote features that are not the same between free and open source

--- src/components/FeatureAvailability/index.tsx
+++ src/components/FeatureAvailability/index.tsx
@@ -7,6 +7,7 @@ import Link from 'components/Link'
 type FeatureAvailabilityProps = {
     availability:
         | {
+              openSource?: boolean
               free: boolean
               selfServe: boolean
               enterprise: boolean
@@ -23,16 +24,38 @@ const renderAvailabilityIcon = (isAvailable: boolean) => {
 }
 
 export function FeatureAvailability({ availability }: FeatureAvailabilityProps): JSX.Element {
+    const diffOpenSource = typeof availability !== 'boolean' && 'openSource' in availability
+
     return (
-        <div className="border-t border-b border-dashed border-gray-accent-light dark:border-gray-accent-dark py-2 space-y-1 mt-2 mb-5 ">
+        <div className="border-t border-b border-dashed border-gray-accent-light dark:border-gray-accent-dark py-2 space-y-2 mt-2 mb-5 ">
             <h6 className="text-primary/50 dark:text-primary-dark/50 !my-0 font-semibold text-base">
                 Where is this feature available?
             </h6>
 
-            <div className="grid grid-flow-col-dense grid-rows-3 grid-cols-2 sm:grid-flow-row-dense sm:grid-cols-3 sm:grid-rows-2 gap-x-4 items-center">
+            <div
+                className={`grid grid-flow-col-dense ${
+                    diffOpenSource
+                        ? 'grid-rows-4 grid-cols-2 sm:grid-cols-4 sm:grid-rows-2'
+                        : 'grid-rows-3 grid-cols-2 sm:grid-cols-3 sm:grid-rows-2'
+                } sm:grid-flow-row-dense gap-x-4 items-center`}
+            >
+                {diffOpenSource ? (
+                    <div>
+                        <h5 className="flex items-center space-x-1.5 text-base !my-0">
+                            <span>Open-source</span>
+                            <Link
+                                to="/pricing?realm=self-hosted"
+                                className="!pb-0 group hover:!bg-none active:!bg-none focus:!bg-none"
+                            >
+                                <InfoIcon className="w-4 h-4 opacity-75 group-hover:opacity-100 relative transform transition-all group-hover:scale-[1.2] active:top-[1px] active:scale-[1.1]" />
+                            </Link>
+                        </h5>
+                    </div>
+                ) : null}
+
                 <div>
-                    <h5 className="flex items-center space-x-1 text-base !my-0">
-                        <span>Free / Open-source</span>
+                    <h5 className="flex items-center space-x-1.5 text-base !my-0">
+                        {diffOpenSource ? <span>Free</span> : <span>Free / Open-source</span>}
                         <Link
                             to="/pricing?realm=self-hosted"
                             className="!pb-0 group hover:!bg-none active:!bg-none focus:!bg-none"
@@ -41,8 +64,9 @@ export function FeatureAvailability({ availability }: FeatureAvailabilityProps):
                         </Link>
                     </h5>
                 </div>
-                <div className="">
-                    <h5 className="flex items-center space-x-1 text-base !my-0">
+
+                <div>
+                    <h5 className="flex items-center space-x-1.5 text-base !my-0">
                         <span>Self-serve</span>
                         <Link
                             to="/pricing?realm=cloud"
@@ -53,8 +77,8 @@ export function FeatureAvailability({ availability }: FeatureAvailabilityProps):
                     </h5>
                 </div>
 
-                <div className="">
-                    <h5 className="flex items-center space-x-1 text-base !my-0">
+                <div>
+                    <h5 className="flex items-center space-x-1.5 text-base !my-0">
                         <span>Enterprise</span>
                         <Link
                             to="/pricing?realm=cloud"
@@ -64,6 +88,8 @@ export function FeatureAvailability({ availability }: FeatureAvailabilityProps):
                         </Link>
                     </h5>
                 </div>
+
+                {diffOpenSource ? renderAvailabilityIcon(availability.openSource || false) : null}
 
                 {renderAvailabilityIcon(typeof availability === 'boolean' ? availability : availability.free)}
 
