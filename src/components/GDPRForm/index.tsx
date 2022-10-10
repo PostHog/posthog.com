770fd991f9e2506f1dd633ba8d9e752d7437ff19
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-10-10 11:17:49 +0100

    Removes waitlist flow, changes GDPRForm to point to landing page

--- src/components/GDPRForm/index.tsx
+++ src/components/GDPRForm/index.tsx
@@ -5,11 +5,11 @@ export const GDPRForm = () => (
     <>
         <br />
         <p className="px-4 font-bold text-center z-10 relative mb-0 text-sm md:text-lg">
-            Want PostHog hosted on an EU Cloud?
+            Worried about GDPR? Host PostHog on an EU Cloud!
         </p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
-            <CallToAction width="106" to="/signup/eu-cloud">
-                Join the waitlist!
+            <CallToAction width="106" to="/eu">
+                Find out more
             </CallToAction>
         </div>
         <br />
