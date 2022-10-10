1618e3ffacdbaa688c9fa70b301257d99c3dd6ca
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-10 14:04:56 -0700

    polish /docs

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -38,7 +38,7 @@ const quickLinks = [
         icon: 'feature-flags',
         name: 'API',
         to: '/docs/api',
-        description: 'Interact with PostHog programmatically through our API.',
+        description: 'Interact with PostHog programmatically.',
     },
     {
         icon: 'faq2',
@@ -54,7 +54,7 @@ const deployment = [
     { name: 'Google Cloud', to: '/docs/self-host/deploy/gcp', icon: 'gcs', badge: undefined },
     { name: 'DigitalOcean', to: '/docs/self-host/deploy/digital-ocean', icon: 'digital ocean', badge: undefined },
     { name: 'Azure', to: '/docs/self-host/deploy/azure', icon: 'azure', badge: 'beta' },
-    { name: 'Hobby', to: '/docs/self-host/deploy/hobby', icon: 'hobby', badge: undefined },
+    { name: 'Hobby', to: '/docs/self-host/deploy/hobby', icon: 'docker', badge: undefined },
 ]
 
 const libraries = [
@@ -75,19 +75,80 @@ const apps = [
     { name: 'Zapier', to: '/apps/zapier-connector', icon: 'zapier' },
 ]
 
+const otherLinks = [
+    {
+        name: 'Integrate',
+        links: [
+            { name: 'Live events', to: '/docs/integrate/ingest-live-data' },
+            { name: 'Historical events', to: '/docs/integrate/ingest-historic-data' },
+            { name: 'Identifying users', to: '/docs/integrate/identifying-users' },
+            { name: 'Libraries', to: '/docs/integrate/libraries' },
+            { name: 'Proxying events', to: '/docs/integrate/proxy' },
+        ],
+    },
+    {
+        name: 'Manual',
+        links: [
+            { name: 'Trends', to: '/manual/trends' },
+            { name: 'Funnels', to: '/manual/funnels' },
+            { name: 'Retention', to: '/manual/retention' },
+            { name: 'Session recording', to: '/manual/recordings' },
+            { name: 'Feature flags', to: '/manual/feature-flags' },
+            { name: 'Experimentation', to: '/manual/experimentation' },
+        ],
+    },
+    {
+        name: 'Self-host',
+        links: [
+            { name: 'Deployment', to: '/docs/self-host' },
+            { name: 'Runbook', to: '/docs/runbook' },
+            { name: 'Environment variables', to: '/docs/self-host/configure/environment-variables' },
+            { name: 'Monitoring', to: '/docs/self-host/configure/monitoring-with-grafana' },
+            { name: 'Upgrading', to: '/docs/runbook/upgrading-posthog' },
+            { name: 'Troubleshooting', to: '/docs/self-host/deploy/troubleshooting' },
+        ],
+    },
+    {
+        name: 'Apps',
+        links: [
+            { name: 'All apps', to: '/docs/apps' },
+            { name: 'Building apps', to: '/docs/apps/build' },
+            { name: 'Developer reference', to: '/docs/apps/build/reference' },
+        ],
+    },
+    {
+        name: 'Developers',
+        links: [
+            { name: 'REST API', to: '/docs/api' },
+            { name: 'Developing locally', to: '/handbook/engineering/developing-locally' },
+            { name: 'Contributing', to: '/docs/contribute' },
+            { name: 'PostHog internals', to: '/docs/how-posthog-works' },
+        ],
+    },
+    {
+        name: 'Privacy & Compliance',
+        links: [
+            { name: 'GDPR', to: '/docs/privacy/gdpr-compliance' },
+            { name: 'HIPAA', to: '/docs/privacy/hipaa-compliance' },
+            { name: 'CCPA', to: '/docs/privacy/ccpa-compliance' },
+            { name: 'Data deletion', to: '/docs/privacy/data-deletion' },
+        ],
+    },
+]
+
 export const DocsIndex: React.FC = () => {
     return (
         <Layout>
             <SEO title="Documentation - PostHog" />
 
-            <PostLayout article={false} survey={false} title={'Docs'} menu={docs} hideSidebar>
-                <div className="space-y-16 lg:space-y-24 lg:-mt-12">
+            <PostLayout article={false} title={'Docs'} menu={docs} hideSidebar hideSurvey>
+                <div className="space-y-16 lg:space-y-20 lg:-mt-12 mb-8">
                     <section>
-                        <div className="flex justify-start relative pb-12 md:pt-12 md:pb-20 lg:pt-16 lg:pb-32 items-center -mx-px">
+                        <div className="flex justify-start relative pb-12 md:pt-12 md:pb-20 lg:pt-16 lg:pb-32 items-center -mx-px h-80">
                             <div className="w-full z-20">
                                 <h1 className="font-bold text-5xl mb-2">Documentation</h1>
                                 <h5 className="opacity-60 font-semibold leading-tight mb-8">
-                                    In-depth tutorials, references, and <br className="hidden md:block 2xl:hidden" />
+                                    In-depth tutorials, references, and <br className="hidden md:block xl:hidden" />
                                     examples for everything PostHog
                                 </h5>
 
@@ -99,13 +160,13 @@ export const DocsIndex: React.FC = () => {
                                     <StaticImage
                                         src="../../contents/images/search-hog-3.png"
                                         alt="This hog has an answer"
-                                        width={500}
+                                        width={400}
                                         placeholder="blurred"
                                     />
                                 </span>
                             </div>
                         </div>
-                        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 border-l border-t border-dashed border-gray-accent-light dark:border-gray-accent-dark mt-8">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 border-l border-t border-dashed border-gray-accent-light dark:border-gray-accent-dark">
                             {quickLinks.map((link) => {
                                 return (
                                     <Link
@@ -127,7 +188,7 @@ export const DocsIndex: React.FC = () => {
                         </div>
                     </section>
 
-                    <section className="space-y-10">
+                    <section className="space-y-8">
                         <div className="text-center">
                             <h2 className="font-bold mb-1">Get started</h2>
                             <p className="text-gray font-medium">Information on how to get PostHog up and running</p>
@@ -187,7 +248,7 @@ export const DocsIndex: React.FC = () => {
                                 <h4 className="font-bold mb-0">
                                     <span className="text-gray text-lg">3.</span> Customize
                                 </h4>
-                                <p className="text-base text-gray">Customize your installation with apps</p>
+                                <p className="text-base text-gray">Customize your installation</p>
                                 <ul className="grid grid-cols-2 xl:grid-cols-1 w-full list-none m-0 p-0 space-y-1">
                                     {apps.map((app) => {
                                         return (
@@ -204,6 +265,35 @@ export const DocsIndex: React.FC = () => {
                                     })}
                                 </ul>
                             </div>
+                        </div>
+                    </section>
+
+                    <section className="space-y-8">
+                        <div className="text-center">
+                            <h2 className="font-bold mb-1">Important links</h2>
+                            <p className="text-gray font-medium">More information on every part of PostHog</p>
+                        </div>
+
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 border-t border-l border-dashed border-gray-accent-light dark:border-gray-accent-dark">
+                            {otherLinks.map((category) => {
+                                return (
+                                    <div
+                                        key={category.name}
+                                        className="space-y-2 py-4 md:py-6 px-4 md:px-8 border-dashed border-b border-r border-gray-accent-light dark:border-gray-accent-dark"
+                                    >
+                                        <h4 className="mb-0">{category.name}</h4>
+                                        <ul className="p-0 space-y-1">
+                                            {category.links.map((link) => {
+                                                return (
+                                                    <li key={link.to} className="list-none">
+                                                        <Link to={link.to}>{link.name}</Link>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                                )
+                            })}
                         </div>
                     </section>
                 </div>
