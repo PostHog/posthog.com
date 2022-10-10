b2b29798547b704108270f0c8699b3a6348b0470
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-10-10 14:32:58 +0100

    Update src/components/GDPRForm/index.tsx
    Co-authored-by: Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>

--- src/components/GDPRForm/index.tsx
+++ src/components/GDPRForm/index.tsx
@@ -5,7 +5,7 @@ export const GDPRForm = () => (
     <>
         <br />
         <p className="px-4 font-bold text-center z-10 relative mb-0 text-sm md:text-lg">
-            Interested in PostHog Cloud EU?
+            Need analytics hosted in the EU?
         </p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
             <CallToAction width="106" to="/eu">
