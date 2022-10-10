48f809e5e83c0f16f538efd4d795c57ccb9e5efe
Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>
2022-10-10 11:07:13 +0100

    PostHog Cloud EU blog

--- src/components/GDPRForm/index.tsx
+++ src/components/GDPRForm/index.tsx
@@ -5,11 +5,11 @@ export const GDPRForm = () => (
     <>
         <br />
         <p className="px-4 font-bold text-center z-10 relative mb-0 text-sm md:text-lg">
-            Want PostHog hosted on an EU Cloud?
+            Interested in PostHog Cloud EU?
         </p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
-            <CallToAction width="106" to="/signup/eu-cloud">
-                Join the waitlist!
+            <CallToAction width="106" to="/eu">
+                Signup now!
             </CallToAction>
         </div>
         <br />
