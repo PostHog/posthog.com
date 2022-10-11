9264c47f3ac9d12dce3918b1b0f9ca3ee1f967c5
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-10-11 10:27:15 +0100

    Update src/components/GDPRForm/index.tsx

--- src/components/GDPRForm/index.tsx
+++ src/components/GDPRForm/index.tsx
@@ -8,7 +8,7 @@ export const GDPRForm = () => (
             Need analytics hosted in the EU?
         </p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
-            <CallToAction width="106" to="/eu">
+            <CallToAction width="106" to="/signup">
                 Try PostHog Cloud EU – free
             </CallToAction>
         </div>
