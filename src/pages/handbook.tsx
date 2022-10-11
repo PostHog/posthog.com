8df1b0794689d4fb4d7e78a0aff8f29fc802a7af
Cory Watilo <corywatilo@gmail.com>
2022-10-11 13:38:22 -0400

    updated top links

--- src/pages/handbook.tsx
+++ src/pages/handbook.tsx
@@ -13,19 +13,8 @@ const otherLinks = [
         links: [
             { name: 'Story', to: '/handbook/company/story' },
             { name: 'Team', to: '/handbook/company/team' },
-            { name: 'Investors', to: '/handbook/strategy/investors' },
-            { name: 'Compensation', to: '/handbook/people/compensation' },
-            { name: 'Benefits', to: '/handbook/people/benefits' },
-            { name: 'Hiring', to: '/handbook/people/hiring-process' },
-        ],
-    },
-    {
-        name: 'Strategy',
-        links: [
-            { name: 'Overview', to: '/handbook/strategy/overview' },
-            { name: 'Business model', to: '/handbook/strategy/business-model' },
+            { name: 'Strategy overview', to: '/handbook/strategy/overview' },
             { name: 'Objectives', to: '/handbook/strategy/objectives' },
-            { name: 'Brand', to: '/handbook/strategy/brand' },
         ],
     },
     {
@@ -33,17 +22,24 @@ const otherLinks = [
         links: [
             { name: 'Culture', to: '/handbook/company/culture' },
             { name: 'Values', to: '/handbook/company/values' },
-            { name: 'Values', to: '/handbook/company/values' },
-            { name: 'Goal setting', to: '/handbook/company/goal-setting' },
-            { name: 'Diversity and inclusion', to: '/handbook/company/diversity' },
+            { name: 'Team structure', to: '/handbook/company/small-teams' },
             { name: 'Management', to: '/handbook/company/management' },
+        ],
+    },
+    {
+        name: 'People',
+        links: [
+            { name: 'Compensation', to: '/handbook/people/compensation' },
+            { name: 'Benefits', to: '/handbook/people/benefits' },
+            { name: 'Time off', to: '/handbook/people/time-off' },
+            { name: 'Spending money', to: '/handbook/people/spending-money' },
         ],
     },
     {
         name: 'Engineering',
         links: [
             { name: 'Developing locally', to: '/handbook/engineering/developing-locally' },
-            { name: 'Project structure', to: '/handbook/engineering/project-structure' },
+            { name: 'Support hero', to: '/handbook/engineering/support-hero' },
             { name: 'Feature ownership', to: '/handbook/engineering/feature-ownership' },
             { name: 'Releasing a new version', to: '/handbook/engineering/release-new-version' },
         ],
@@ -53,18 +49,17 @@ const otherLinks = [
         links: [
             { name: 'Philosophy', to: '/handbook/design/philosophy' },
             { name: 'Product design', to: '/handbook/design/process' },
-            { name: 'Website design', to: '/handbook/design/designing-posthog-website' },
+            { name: 'Working with product design', to: '/handbook/engineering/product-design' },
+            { name: 'PostHog.com', to: '/handbook/design/designing-posthog-website' },
         ],
     },
     {
-        name: 'Customer success',
+        name: 'Sales & marketing',
         links: [
-            { name: 'Strategy', to: '/handbook/growth/strategy' },
-            { name: 'Support', to: '/handbook/growth/customer-support' },
+            { name: 'Marketing overview', to: '/handbook/growth/marketing' },
+            { name: 'Content & SEO', to: '/handbook/growth/marketing/blog' },
             { name: 'Inbound model', to: '/handbook/growth/sales/overview' },
-            { name: 'Demos', to: '/handbook/growth/sales/demos' },
-            { name: 'Billing', to: '/handbook/growth/sales/billing' },
-            { name: 'CRM', to: '/handbook/growth/sales/crm' },
+            { name: 'Sales operations', to: '/handbook/growth/sales/sales-operations' },
         ],
     },
 ]
@@ -79,9 +74,8 @@ export const Handbook: React.FC = () => {
                     <section>
                         <div className="flex justify-start relative items-center -mx-px h-80">
                             <div className="w-full z-20">
-                                <h1 className="font-bold text-5xl mb-2">Handbook</h1>
+                                <h1 className="font-bold text-5xl mb-2">Company handbook</h1>
                                 <h5 className="opacity-60 font-semibold leading-tight mb-8">
-                                    Welcome to the PostHog Company Handbook! <br />
                                     This explains how we operate as a company.
                                 </h5>
 
@@ -103,6 +97,8 @@ export const Handbook: React.FC = () => {
                                 </span>
                             </div>
                         </div>
+
+                        <h4>Top links</h4>
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 border-t border-l border-dashed border-gray-accent-light dark:border-gray-accent-dark">
                             {otherLinks.map((category) => {
                                 return (
