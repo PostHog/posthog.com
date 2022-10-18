e917a32231139b508573488e509733f0e2e666b1
Cory Watilo <corywatilo@gmail.com>
2022-10-18 13:57:45 -0400

    removes demo link from product footer CTA

--- src/components/DemoLink/index.tsx
+++ src/components/DemoLink/index.tsx
@@ -7,7 +7,7 @@ export const DemoLink = ({ dark = false, className = '' }: { dark?: boolean; cla
         <CallToAction
             type="secondary"
             to="/book-a-demo"
-            className={` ${className} text-[15px] group justify-center font-semibold text-white py-2 px-3 rounded-sm hover:text-white hover:bg-white/30 inline-flex space-x-2 items-center relative active:top-[0.5px] active:scale-[.98] !w-full md:!w-44 shadow-xl`}
+            className={` ${className} text-[15px] group justify-center font-semibold text-white py-2 px-3 rounded-sm hover:!text-white bg-white/0 border border-solid border-white/20 hover:bg-white/30 inline-flex space-x-2 items-center relative active:top-[0.5px] active:scale-[.98] !w-full md:!w-44 shadow-xl`}
         >
             <span>Schedule a call</span>
         </CallToAction>
