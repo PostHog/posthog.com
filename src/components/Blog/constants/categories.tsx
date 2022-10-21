f81473e9d606d92850d7dce6829a19a83ebd03d6
Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>
2022-10-21 10:59:44 +0100

    Update categories.tsx

--- src/components/Blog/constants/categories.tsx
+++ src/components/Blog/constants/categories.tsx
@@ -38,6 +38,11 @@ export const BlogCategories: CategoryInterface[] = [
         slug: 'open-source',
         link: '/blog/categories/open-source',
     },
+        {
+        title: 'CEO diaries',
+        slug: 'ceo-diaries',
+        link: '/blog/categories/ceo-diaries',
+    },
 ]
 
 interface LinksInterface {
