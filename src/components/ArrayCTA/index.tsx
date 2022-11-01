c93182be8b46dc9097718d63866d08edb55b06f6
Eli Kinsey <eli@ekinsey.dev>
2022-11-01 12:47:07 -0700

    update /signup links

--- src/components/ArrayCTA/index.tsx
+++ src/components/ArrayCTA/index.tsx
@@ -6,7 +6,7 @@ export const ArrayCTA = () => (
         <br />
         <p className="px-4 font-bold text-center z-10 relative mb-0 text-sm md:text-lg">Ready to find out more?</p>
         <div className="flex flex-col md:flex-row justify-center items-center gap-2 xl:gap-4">
-            <CallToAction width="56" to="/signup">
+            <CallToAction width="56" to="https://app.posthog.com/signup">
                 Try PostHog today
             </CallToAction>
             <CallToAction type="outline" width="56" to="/book-a-demo">
