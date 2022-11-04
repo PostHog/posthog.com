d891ff852c8bd541a73472bfc173b1e2df2a6c70
Eli Kinsey <eli@ekinsey.dev>
2022-11-03 17:15:11 -0700

    update links

--- src/pages/community/profiles/[id].tsx
+++ src/pages/community/profiles/[id].tsx
@@ -175,28 +175,49 @@ const ProfileSidebar = ({
         <div>
             {profile.github || profile.twitter || profile.linkedin || profile.website ? (
                 <SidebarSection title="Links">
-                    <ul className="space-y-2 p-0">
+                    <ul className="p-0 flex space-x-2 items-center list-none m-0">
                         {profile.website && (
-                            <li className="flex items-center space-x-2">
-                                <GitHub className="w-4 h-4" /> <a href="#">{profile.website}</a>
+                            <li>
+                                <Link to={profile.website}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="w-6 h-6 text-black dark:text-white opacity-60 hover:opacity-100 transition-hover"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                                        />
+                                    </svg>
+                                </Link>
                             </li>
                         )}
 
                         {profile.github && (
-                            <li className="flex items-center space-x-2">
-                                <GitHub className="w-4 h-4" /> <a href="#">{profile.github}</a>
+                            <li>
+                                <Link to={profile.github}>
+                                    <GitHub className="w-6 h-6 text-black dark:text-white opacity-60 hover:opacity-100 transition-hover" />
+                                </Link>
                             </li>
                         )}
 
                         {profile.twitter && (
-                            <li className="flex items-center space-x-2">
-                                <Twitter className="w-4 h-4" /> <a href="#">{profile.twitter}</a>
+                            <li>
+                                <Link to={profile.twitter}>
+                                    <Twitter className="w-6 h-6 text-black dark:text-white opacity-60 hover:opacity-100 transition-hover" />
+                                </Link>
                             </li>
                         )}
 
                         {profile.linkedin && (
-                            <li className="flex items-center space-x-2">
-                                <LinkedIn className="w-4 h-4" /> <a href="#">{name}</a>
+                            <li>
+                                <Link to={profile.linkedin}>
+                                    <LinkedIn className="w-5 h-5 text-black dark:text-white opacity-60 hover:opacity-100 transition-hover" />
+                                </Link>
                             </li>
                         )}
                     </ul>
