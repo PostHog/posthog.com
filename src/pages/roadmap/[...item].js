f8d09e74cc3f63be03c957fa269ec571f11eccb1
James Hawkins <47497682+jamesefhawkins@users.noreply.github.com>
2022-11-05 14:11:09 -0500

    created roadmap page for sidecar

---
+++ src/pages/roadmap/[...item].js
@@ -0,0 +1,155 @@
+import Roadmap from 'components/Roadmap'
+import Layout from 'components/Layout'
+import { graphql, useStaticQuery } from 'gatsby'
+import React from 'react'
+import groupBy from 'lodash.groupby'
+import Link from 'components/Link'
+import { SEO } from 'components/seo'
+import PostLayout from 'components/PostLayout'
+import { OrgProvider, UserProvider } from 'squeak-react'
+import Section from 'components/ProductPage/Section'
+import { FeatureSnapshot } from 'components/FeatureSnapshot'
+
+export default function RoadmapPage({ params }) {
+    const itemName = params[`item`]
+
+    const HomePage = ({ data }) => {
+        return <div>{data.site.siteMetadata.description}</div>
+    }
+
+    return (
+        <Layout>
+            <SEO title="PostHog Roadmap" />
+            <OrgProvider
+                value={{ organizationId: 'a898bcf2-c5b9-4039-82a0-a00220a8c626', apiHost: 'https://squeak.cloud' }}
+            >
+                <UserProvider>
+                    <div className="border-t border-dashed border-gray-accent-light">
+                        <PostLayout
+                            contentWidth={'100%'}
+                            article={false}
+                            title={'Roadmap'}
+                            hideSearch
+                            hideSurvey
+                            menu={[
+                                { name: 'Questions', url: '/questions' },
+                                { name: 'Roadmap', url: '/roadmap' },
+                                { name: 'Contributors', url: '/contributors' },
+                                { name: 'Core team', url: '/handbook/company/team' },
+                            ]}
+                        >
+                            <h1 className="font-bold text-5xl mb-8 xl:mt-0">{itemName}</h1>
+                            <p className="font-bold text-1xl mb-8 xl:mt-0">
+                                A chrome extension that shows your users' data in any other SaaS tool.
+                            </p>
+
+                            <Section title="Test updates safely with feature flags" titleSize="md">
+                                <FeatureSnapshot
+                                    image="/images/sidecar.png"
+                                    features={[
+                                        <>
+                                            <strong>More context for debugging</strong> get to session recordings,
+                                            feature flags and events from any other SaaS product.
+                                        </>,
+                                        <>
+                                            <strong>Context for support teams</strong> know if users are paying
+                                            customers, which features they use and product actions they've taken.
+                                        </>,
+                                        <>
+                                            <strong>User data in any tool</strong> with zero integration needed.
+                                        </>,
+                                    ]}
+                                />
+                            </Section>
+
+                            <h2 className="font-bold text-5xl mb-8 xl:mt-0">How to install</h2>
+                            <ol>
+                                <li>
+                                    Join our <Link to="/slack">community chat</Link>, and join the
+                                    #team-website-and-docs channel - the zip file is pinned in the channel.
+                                </li>
+                                <li>Download the zip file and unzip it.</li>
+                                <li>
+                                    Open the extensions menu in Chrome, switch to developer mode, then choose load
+                                    unpacked extension and upload it.
+                                </li>
+                                <li>
+                                    Chrome's extension store can be buggy - just restart after you enable developer mode
+                                    if you have any issues.
+                                </li>
+                            </ol>
+
+                            <h2 className="font-bold text-5xl mb-8 xl:mt-0">
+                                I found a bug, have feedback or want to contribute
+                            </h2>
+
+                            <p>
+                                For feedback, please join our <Link to="/slack">community chat</Link>, and post it in
+                                the #team-website-and-docs channel.
+                            </p>
+                            <p>
+                                For bugs,{' '}
+                                <Link to="https://github.com/PostHog/sidecar/issues/new">create an issue</Link> in the
+                                Sidecar repo
+                            </p>
+                            <p>
+                                To contribute code, we're grateful for{' '}
+                                <Link to="https://github.com/PostHog/sidecar">pull requests in the sidecar repo</Link>.
+                                To make sure we can merge your work, we recommend talking about it with us in the Slack
+                                channel. We give merch for cool features.
+                            </p>
+
+                            <h2 className="font-bold text-5xl mb-8 xl:mt-0">Feature ideas</h2>
+
+                            <ul>
+                                <li>Inserting data into a page automatically, so there's no need to right click</li>
+                                <li>Inserting react components from PostHog into a page automatically</li>
+                                <li>Editable product-specific templates</li>
+                                <li>Community product-specific templates</li>
+                                <li>Inserting react components into the toolbar automatically</li>
+                                <li>
+                                    Ability to update a user's data in the toolbar, and pushing it back into PostHog
+                                </li>
+                                <li>
+                                    Ability to add customer user fields in the toolbar, and pushing it back into PostHog
+                                </li>
+                                <li>Ability to scrape a page and automatically update a user's profile</li>
+                            </ul>
+                        </PostLayout>
+                    </div>
+                </UserProvider>
+            </OrgProvider>
+        </Layout>
+    )
+}
+
+const query = graphql`
+    {
+        allSqueakRoadmap {
+            nodes {
+                beta_available
+                complete
+                date_completed
+                title
+                description
+                team {
+                    name
+                }
+                otherLinks
+                githubPages {
+                    title
+                    html_url
+                    number
+                    closed_at
+                    reactions {
+                        hooray
+                        heart
+                        eyes
+                        _1
+                    }
+                }
+                projected_completion_date
+            }
+        }
+    }
+`
