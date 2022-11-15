daafc64bca888368d65068f3fb23b79bb6576fc4
Cory Watilo <corywatilo@gmail.com>
2022-11-14 19:02:39 -0500

    improve responsiveness of team members

--- src/components/TeamMembers/index.tsx
+++ src/components/TeamMembers/index.tsx
@@ -16,16 +16,16 @@ export default function TeamMembers({ team }: { team: string }) {
     return (
         <>
             <h4>Team members</h4>
-            <ul className="list-none m-0 p-0 grid sm:grid-cols-2">
+            <ul className="list-none m-0 p-0 grid sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
                 {teamMembers.map((member) => {
                     const { name, headshot, jobTitle, teamLead, country } = member?.frontmatter
                     return (
                         <li className="!m-0 flex space-x-4 items-center py-4" key={name}>
-                            <span>
+                            <figure className="mb-0">
                                 <ContributorImage image={getImage(headshot)} />
-                            </span>
-                            <span>
-                                <span className="flex md:flex-row flex-col md:space-x-2 space-x-0 md:space-y-0 space-y-1 md:items-center items-start">
+                            </figure>
+                            <div>
+                                <span className="flex items-center md:flex-row space-x-2">
                                     <p className="!text-lg !font-bold !m-0 !leading-none">{name}</p>
                                     {country && (
                                         <span className="!leading-none">
@@ -37,13 +37,13 @@ export default function TeamMembers({ team }: { team: string }) {
                                         </span>
                                     )}
                                     {teamLead && (
-                                        <span className="inline-block border-2 border-red/50 rounded-md text-[12px] px-2 py-1 !leading-none font-semibold text-red bg-white">
-                                            Team Lead
+                                        <span className="inline-block border-2 border-red/50 rounded-sm text-[12px] px-2 py-1 !leading-none font-semibold text-red bg-white">
+                                            Team lead
                                         </span>
                                     )}
                                 </span>
                                 <p className="!text-sm !mb-0 opacity-50 !leading-none !mt-1">{jobTitle}</p>
-                            </span>
+                            </div>
                         </li>
                     )
                 })}
