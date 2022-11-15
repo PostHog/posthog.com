d917a11331f551edae0504f8e49749273dabce76
Paul Hultgren <pjhultgren5@gmail.com>
2022-11-15 15:39:52 -0800

    update to use snippet in html.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Header } from '../Header/Header'
 import { Footer } from '../Footer/Footer'
 import CookieBanner from 'components/CookieBanner'
--- src/html.tsx
+++ src/html.tsx
@@ -31,6 +31,15 @@ export default function HTML(props: HTMLProps): JSX.Element {
                     crossOrigin="anonymous"
                 />
 
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+                        posthog.init("phc_X8B6bhR1QgQKP1WdpFLN82LxLxgZ7WPXDgJyRyvIpib", {api_host: "https://app.posthog.com",capture_pageview: false})
+                `,
+                    }}
+                />
+
                 {props.headComponents}
             </head>
             <body {...props.bodyAttributes} className="light">
@@ -40,13 +49,4 @@ export default function HTML(props: HTMLProps): JSX.Element {
             </body>
         </html>
     )
-}
-
-HTML.propTypes = {
-    htmlAttributes: PropTypes.object,
-    headComponents: PropTypes.array,
-    bodyAttributes: PropTypes.object,
-    preBodyComponents: PropTypes.array,
-    body: PropTypes.string,
-    postBodyComponents: PropTypes.array,
 }
