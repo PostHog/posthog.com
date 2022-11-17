549d910b61701d315e716589e32f0044576e3f2b
Cory Watilo <corywatilo@gmail.com>
2022-11-17 12:58:50 -0500

    fix sizing, use h2s

--- src/components/BorderWrapper/index.js
+++ src/components/BorderWrapper/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const BorderWrapper = ({ children }) => {
-    return <div className="border-t border-b border-dashed border-gray-accent-light py-6 mt-9 mb-0">{children}</div>
+    return <div className="border-t border-b border-dashed border-gray-accent-light py-6 my-9">{children}</div>
 }
