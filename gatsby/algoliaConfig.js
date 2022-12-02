b6e13a95d8eafcb04048c78e609052bae2e988e8
Paul Hultgren <pjhultgren5@gmail.com>
2022-12-02 00:35:13 -0800

    fetch rawBody on mdx page

--- gatsby/algoliaConfig.js
+++ gatsby/algoliaConfig.js
@@ -14,6 +14,7 @@ const retrievePages = (type, regex) => {
                   fields {
                     pageViews
                   }
+                  rawBody
                   excerpt
                   frontmatter {
                     title
