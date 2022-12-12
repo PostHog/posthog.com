d00815160fa9852f7a88db87a278ec220a377b60
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2022-12-12 16:22:52 +0000

    Year in PostHog banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,9 +5,9 @@ export default function Banner() {
     return (
         <div>
             <p className="text-center py-4 bg-gray-accent-light dark:bg-gray-accent-dark flex sm:flex-row flex-col justify-center sm:space-x-1 font-semibold m-0">
-                <span>📣 Just released: </span>
-                <Link to="/blog/the-posthog-array-1-42-0" className="text-red">
-                    PostHog 1.42.0!
+                <span>🎄 Available now: </span>
+                <Link to="https://app.posthog.com/login" className="text-red">
+                    Your Year in PostHog!
                 </Link>
             </p>
         </div>
