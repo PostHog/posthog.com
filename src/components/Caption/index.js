fae6956fd3876c0045cefdd57098594b15ca2f28
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2023-01-01 20:45:13 +0000

    Corrects caption component margin

--- src/components/Caption/index.js
+++ src/components/Caption/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Caption = ({ children }) => {
     return (
         <div className="text-center">
-            <caption className="inline-block px-2 py-1 rounded-sm text-sm bg-gray-accent-light text-black/75 dark:bg-gray-accent-dark dark:text-white/75">
+            <caption className="inline-block px-2 py-1 mb-3 rounded-sm text-sm bg-gray-accent-light text-black/75 dark:bg-gray-accent-dark dark:text-white/75">
                 {children}
             </caption>
         </div>
