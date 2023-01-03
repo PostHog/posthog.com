e33abb3ade37bc962841c7f078f1baa70d9f6cde
Cory Watilo <corywatilo@gmail.com>
2023-01-03 09:53:38 -0500

    make caption margin work with more varied elements, make caption inline

--- src/components/Caption/index.js
+++ src/components/Caption/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export const Caption = ({ children }) => {
     return (
-        <div className="text-center">
-            <caption className="inline-block px-2 py-1 mb-3 rounded-sm text-sm bg-gray-accent-light text-black/75 dark:bg-gray-accent-dark dark:text-white/75">
-                {children}
+        <div className="caption text-center">
+            <caption className="inline text-sm">
+                <span className="px-2 py-2 rounded-sm bg-gray-accent-light text-black/75 dark:bg-gray-accent-dark dark:text-white/75">
+                    {children}
+                </span>
             </caption>
         </div>
     )
