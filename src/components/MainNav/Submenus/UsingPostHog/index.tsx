7ea98d72898f3f62149fdf6a9dc7f01734e682ba
Eli Kinsey <eli@ekinsey.dev>
2023-01-14 09:36:57 -0800

    update menu links

--- src/components/MainNav/Submenus/UsingPostHog/index.tsx
+++ src/components/MainNav/Submenus/UsingPostHog/index.tsx
@@ -129,23 +129,16 @@ export default function UsingPosthog({ referenceElement }: { referenceElement: H
                             <div className="opacity-70">
                                 <h3 className="text-[18px] font-bold m-0 text-black ">Latest tutorials</h3>
                             </div>
-                            <ul className="m-0 list-none p-0 mt-2 grid grid-cols-2 gap-4">
+                            <ul className="m-0 list-none p-0 mt-2 grid grid-cols-1 gap-y-2">
                                 {nodes.map(({ fields: { slug }, frontmatter: { title, featuredImage } }: Tutorial) => {
                                     const image = featuredImage && getImage(featuredImage)
                                     return (
                                         <li key={slug}>
                                             <Link
-                                                className="inline-block relative active:top-[1px] active:scale-[.99] before:h-full before:left-0 before:right-0 before:top-0 before:z-[1] before:absolute before:bg-gradient-to-b before:from-black/75 [text-shadow:0_2px_10px_rgba(0,0,0,0.4)]"
+                                                className="inline-block text-sm leading-tight m-0 text-red font-semibold"
                                                 to={slug}
                                             >
-                                                <GatsbyImage
-                                                    className="rounded bg-tan dark:bg-[#444] pointer-events-none"
-                                                    image={image}
-                                                    alt={title || ''}
-                                                />
-                                                <p className="absolute top-0 text-white text-sm my-2 mx-2 z-10">
-                                                    {title}
-                                                </p>
+                                                {title}
                                             </Link>
                                         </li>
                                     )
@@ -185,7 +178,7 @@ const query = graphql`
     {
         tutorials: allMdx(
             filter: { fields: { slug: { regex: "/^/tutorials/" } } }
-            limit: 2
+            limit: 3
             sort: { fields: frontmatter___date, order: DESC }
         ) {
             nodes {
