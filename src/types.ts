7be1b7c5f20cdc6748b1c40cf08f62f21e007097
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-01-27 14:40:43 -0800

    Add billing v2 types

--- src/types.ts
+++ src/types.ts
@@ -62,3 +62,97 @@ export interface AuthorsData {
 }
 
 export type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+
+export interface BillingV2FeatureType {
+    key: string
+    name: string
+    description?: string
+    unit?: string
+    limit?: number
+    note?: string
+    group?: AvailableFeature
+}
+
+export interface BillingV2TierType {
+    unit_amount_usd: string
+    current_amount_usd?: string | null
+    up_to: number | null
+}
+
+export interface BillingProductV2Type {
+    type: 'events' | 'recordings' | 'enterprise' | 'base'
+    name: string
+    description?: string
+    price_description?: string
+    image_url?: string
+    free_allocation?: number
+    tiers?: BillingV2TierType[]
+    tiered?: boolean
+    current_usage?: number
+    projected_usage?: number
+    percentage_usage: number
+    current_amount_usd?: string
+    usage_limit?: number
+    unit_amount_usd: string | null
+    feature_groups: {
+        group: string
+        name: string
+        features: BillingV2FeatureType[]
+    }[]
+}
+
+export interface BillingV2PlanType {
+    key: string
+    name: string
+    description: string
+    is_free?: boolean
+    products: BillingProductV2Type[]
+}
+
+export enum AvailableFeature {
+    EVENTS = 'events',
+    TRACKED_USERS = 'tracked_users',
+    DATA_RETENTION = 'data_retention',
+    SUBSCRIPTIONS = 'subscriptions',
+    DASHBOARD_COLLABORATION = 'dashboard_collaboration',
+    DASHBOARD_PERMISSIONING = 'dashboard_permissioning',
+    INGESTION_TAXONOMY = 'ingestion_taxonomy',
+    PATHS_ADVANCED = 'paths_advanced',
+    CORRELATION_ANALYSIS = 'correlation_analysis',
+    GROUP_ANALYTICS = 'group_analytics',
+    TAGGING = 'tagging',
+    BEHAVIORAL_COHORT_FILTERING = 'behavioral_cohort_filtering',
+    SESSION_RECORDINGS = 'session_recordings',
+    RECORDINGS_PLAYLISTS = 'recordings_playlists',
+    RECORDINGS_PERFORMANCE = 'recordings_performance',
+    RECORDINGS_FILE_EXPORT = 'recordings_file_export',
+    BOOLEAN_FLAGS = 'boolean_flags',
+    MULTIVARIATE_FLAGS = 'multivariate_flags',
+    EXPERIMENTATION = 'experimentation',
+    APPS = 'apps',
+    SLACK_INTEGRATION = 'slack_integration',
+    MICROSOFT_TEAMS_INTEGRATION = 'microsoft_teams_integration',
+    DISCORD_INTEGRATION = 'discord_integration',
+    ZAPIER = 'zapier',
+    APP_METRICS = 'app_metrics',
+    TEAM_MEMBERS = 'team_members',
+    API_ACCESS = 'api_access',
+    ORGANIZATIONS_PROJECTS = 'organizations_projects',
+    PROJECT_BASED_PERMISSIONING = 'project_based_permissioning',
+    ROLE_BASED_ACCESS = 'role_based_access',
+    GOOGLE_LOGIN = 'google_login',
+    SAML = 'saml',
+    SSO_ENFORCEMENT = 'sso_enforcement',
+    WHITE_LABELLING = 'white_labelling',
+    COMMUNITY_SUPPORT = 'community_support',
+    DEDICATED_SUPPORT = 'dedicated_support',
+    EMAIL_SUPPORT = 'email_support',
+    ACCOUNT_MANAGER = 'account_manager',
+    TRAINING = 'training',
+    CONFIGURATION_SUPPORT = 'configuration_support',
+    TERMS_AND_CONDITIONS = 'terms_and_conditions',
+    SECURITY_ASSESSMENT = 'security_assessment',
+    BESPOKE_PRICING = 'bespoke_pricing',
+    INVOICE_PAYMENTS = 'invoice_payments',
+    SUPPORT_SLAS = 'support_slas',
+}
