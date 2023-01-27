060735ec0daba4fc0f5d5767ad91b084cf3ce3e8
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-01-27 07:33:56 -0800

    Get rid of a bunch of old tests

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,22 +1,8 @@
 import Control from 'components/Pricing/Control'
-import React, { useEffect, useState } from 'react'
-import Test from 'components/Pricing/Test'
-import usePostHog from '../../hooks/usePostHog'
+import React from 'react'
 
 const PricingNew = () => {
-    const posthog = usePostHog()
-    const [loading, setLoading] = useState(true)
-
-    const [featureFlagEnabled, setFeatureFlagEnabled] = useState(false)
-
-    useEffect(() => {
-        if (posthog && posthog.getFeatureFlag && posthog.getFeatureFlag('highlight-open-source') === 'test') {
-            setFeatureFlagEnabled(true)
-        }
-        setLoading(false)
-    }, [])
-
-    return loading ? null : featureFlagEnabled ? <Test /> : <Control />
+    return <Control />
 }
 
 export default PricingNew
