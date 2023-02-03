1d706220b99713496577a851ad6fa53f1189cab1
Cory Watilo <cww@watilo.com>
2023-02-03 13:39:00 -0500

    all in one styling

--- src/components/Home/AllInOne.js
+++ src/components/Home/AllInOne.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const featureClasses = `m-0 text-sm font-semibold`
+const featureClasses = `m-0 font-semibold`
 
 const features = [
     {
@@ -8,7 +8,7 @@ const features = [
         content: (
             <>
                 <p className={featureClasses}>SaaS products in one</p>
-                <p className="m-0 text-xs">(All designed to work natively together)</p>
+                <p className="m-0 text-sm">(All designed to work natively together)</p>
             </>
         ),
     },
@@ -28,18 +28,22 @@ const features = [
 
 const DataSquare = ({ children }) => {
     return (
-        <div className="w-[45px] h-[45px] flex items-center justify-center bg-black rounded-sm">
-            <p className="m-0 text-2xl text-white">{children}</p>
+        <div className="w-20 h-20 flex items-center justify-center bg-black rounded-sm relative before:absolute before:top-0 before:left-0 before:w-full before:bottom-0 before:content-[''] before:bg-gradient-to-b before:from-white/0 before:via-white/20 before:to-white/0 after:content-[''] after:h-[1px] after:left-0 after:w-full after:top-1/2 after:bg-white/25 after:absolute shadow-xl">
+            <p className="m-0 text-4xl font-bold text-yellow relative">{children}</p>
         </div>
     )
 }
 
 export default function AllInOne() {
     return (
-        <section className="my-16 mx-auto max-w-[850px] px-4">
-            <h2 className="m-0 text-3xl md:text-5xl text-center">
-                <span className="text-red">Still</span> paying for 4+ different services...{' '}
-                <span className="text-red">and</span> tools to connect them all?
+        <section className="my-24 mx-auto max-w-4xl px-4">
+            <h2 className="m-0 text-3xl md:text-6xl text-center leading-none">
+                <span className="inline-block">
+                    <span className="text-red">Still</span> paying 4+ different vendors...
+                </span>{' '}
+                <span className="inline-block">
+                    <span className="text-red inline-block">and</span> tools to connect them all?
+                </span>
             </h2>
             <p className="text-center m-0 mt-4 font-semibold opacity-70 text-base sm:text-lg">
                 (They’ll never play together as nicely as this.)
