bb18a3dc2b3e1f05f2520386501d9845deef3c18
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-02-03 08:44:24 -0800

    Move AllPlans component to control folder

--- src/components/Pricing/control/AllPlans/index.tsx
+++ src/components/Pricing/control/AllPlans/index.tsx
@@ -1,7 +1,7 @@
 import { TrackedCTA } from 'components/CallToAction'
 import React, { useState } from 'react'
-import { PlanComparison } from '../control/PlanComparison'
-import { CloudIcon, SelfHostIcon } from '../Calculator/index'
+import { PlanComparison } from '../PlanComparison'
+import { CloudIcon, SelfHostIcon } from '../../Calculator/index'
 import { motion } from 'framer-motion'
 
 interface IPlan {
