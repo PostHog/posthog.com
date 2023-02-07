5ceca8678c623395e2acba82e6798e1c9f6d28d7
Paul Hultgren <pjhultgren5@gmail.com>
2023-02-07 13:21:51 -0800

    don't generate og images

--- gatsby-config.js
+++ gatsby-config.js
@@ -410,7 +410,6 @@ module.exports = {
         ...(!process.env.GATSBY_ALGOLIA_APP_ID || !process.env.ALGOLIA_API_KEY || !process.env.GATSBY_ALGOLIA_INDEX_NAME
             ? []
             : [algoliaConfig]),
-        'gatsby-generate-og-images',
         '@vercel/gatsby-plugin-vercel-builder',
     ],
 }
