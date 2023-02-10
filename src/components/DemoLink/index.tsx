672443d348f321e25af84cdc09916db57a5d63bb
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-02-10 13:14:27 -0800

    fix: test top CTA to go to pricing not book a call (#5268)
    * test top CTA to go to pricing not book a call
    
    * make the placeholder element take up some space

--- src/components/DemoLink/index.tsx
+++ src/components/DemoLink/index.tsx
@@ -1,23 +1,40 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import { CallToAction } from 'components/CallToAction'
+import { TrackedCTA } from 'components/CallToAction'
+import { RenderInClient } from 'components/RenderInClient'
+import usePostHog from '../../hooks/usePostHog'
 
 export const DemoLink = ({ dark = false, className = '' }: { dark?: boolean; className?: string }): JSX.Element => {
-    return dark ? (
-        <CallToAction
-            type="secondary"
-            to="/book-a-demo"
-            className={` ${className} text-[15px] group justify-center font-semibold text-white py-2 px-3 rounded-sm hover:!text-white bg-white/0 border border-solid border-white/20 hover:bg-white/30 inline-flex space-x-2 items-center relative active:top-[0.5px] active:scale-[.98] !w-full md:!w-44 shadow-xl`}
-        >
-            <span>Schedule a call</span>
-        </CallToAction>
-    ) : (
-        <CallToAction
-            type="secondary"
-            to="/book-a-demo"
-            className={` ${className} text-[15px] group justify-center font-semibold py-2 px-3 rounded-sm inline-flex space-x-2 items-center relative active:top-[0.5px] active:scale-[.98]  !w-full md:!w-44 shadow-xl`}
-        >
-            <span>Schedule a call</span>
-        </CallToAction>
+    const posthog = usePostHog()
+    const darkClasses = `${className} text-[15px] group justify-center font-semibold text-white py-2 px-3 rounded-sm hover:!text-white bg-white/0 border border-solid border-white/20 hover:bg-white/30 inline-flex space-x-2 items-center relative active:top-[0.5px] active:scale-[.98] !w-full md:!w-44 shadow-xl`
+    const lightClasses = `${className} text-[15px] group justify-center font-semibold py-2 px-3 rounded-sm inline-flex space-x-2 items-center relative active:top-[0.5px] active:scale-[.98]  !w-full md:!w-44 shadow-xl`
+
+    return (
+        <RenderInClient
+            placeholder={
+                <TrackedCTA
+                    event={{ name: `clicked to view pricing`, type: 'homepage' }}
+                    type="secondary"
+                    to={'/pricing'}
+                    className={dark ? darkClasses : lightClasses}
+                >
+                    <span>-</span>
+                </TrackedCTA>
+            }
+            render={() => (
+                <TrackedCTA
+                    event={{
+                        name: `clicked to view ${
+                            posthog?.isFeatureEnabled('web-pricing-cta') ? 'pricing' : 'demo booking page'
+                        }`,
+                        type: 'homepage',
+                    }}
+                    type="secondary"
+                    to={posthog?.isFeatureEnabled('web-pricing-cta') ? '/pricing' : '/book-a-demo'}
+                    className={dark ? darkClasses : lightClasses}
+                >
+                    <span>{posthog?.isFeatureEnabled('web-pricing-cta') ? 'View our pricing' : 'Schedule a call'}</span>
+                </TrackedCTA>
+            )}
+        />
     )
 }
