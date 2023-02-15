52c93d03b34a3a47d05ad152d1704095023efd96
Paul Hultgren <pjhultgren5@gmail.com>
2023-02-15 12:08:20 -0800

    un-ignore more components

--- src/mdxGlobalComponents.js
+++ src/mdxGlobalComponents.js
@@ -1,6 +1,17 @@
 // AUTO GENERATED FILE 
 
+import { ArrayCTA } from './components/ArrayCTA'
+import { CallToAction } from './components/CallToAction'
+import { Caption } from './components/Caption'
+import { FeatureAvailability } from './components/FeatureAvailability'
+import { NewsletterTutorial } from './components/NewsletterTutorial'
+import { Quote } from './components/Pricing/Quote'
 
 export const shortcodes = {
-	
+	ArrayCTA,
+	CallToAction,
+	Caption,
+	FeatureAvailability,
+	NewsletterTutorial,
+	Quote
 }
