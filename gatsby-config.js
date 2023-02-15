fb1e19a3e96867ac7a5b874dadfb5c36f8d0b1f5
Paul Hultgren <pjhultgren5@gmail.com>
2023-02-15 11:56:34 -0800

    remove mermaid

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,9 +79,6 @@ module.exports = {
                 gatsbyRemarkPlugins: [
                     `gatsby-remark-static-images`,
                     { resolve: 'gatsby-remark-autolink-headers', options: { icon: false } },
-                    {
-                        resolve: require.resolve(`./plugins/gatsby-remark-mermaid`),
-                    },
                 ],
                 plugins: [`gatsby-remark-static-images`],
             },
@@ -225,7 +222,6 @@ module.exports = {
                             escapeEntities: {},
                         },
                     },
-                    `gatsby-remark-mermaid`,
                 ],
             },
         },
