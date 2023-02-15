b9d319d6b2eda402ef4c3670e596a2327f3589ff
Paul Hultgren <pjhultgren5@gmail.com>
2023-02-15 13:04:04 -0800

    disable cache

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -37,5 +37,6 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ sta
                 logic: path.resolve(__dirname, 'src', 'logic'),
             },
         },
+        cache: false,
     })
 }
