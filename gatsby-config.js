93196a81b74bc4a4b9917154d571ead60352f023
Eli Kinsey <eli@ekinsey.dev>
2023-02-27 14:46:40 -0800

    use old version of gatsby-plugin-vercel-builder

--- gatsby-config.js
+++ gatsby-config.js
@@ -323,5 +323,6 @@ module.exports = {
         ...(!process.env.GATSBY_ALGOLIA_APP_ID || !process.env.ALGOLIA_API_KEY || !process.env.GATSBY_ALGOLIA_INDEX_NAME
             ? []
             : [algoliaConfig]),
+        '@vercel/gatsby-plugin-vercel-builder',
     ],
 }
