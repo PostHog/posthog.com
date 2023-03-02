fc3e9356d8ea529ee96e0bb386ab2fb60a7270f0
Paul Hultgren <pjhultgren5@gmail.com>
2023-03-02 12:29:05 -0800

    strip html tags from headings

--- src/components/Docs/InternalSidebarLink.js
+++ src/components/Docs/InternalSidebarLink.js
@@ -6,6 +6,13 @@ import { Link } from 'react-scroll'
 export default function InternalSidebarLink({ url, name, depth, onClick, className = '', style = {} }) {
     const key = typeof window !== 'undefined' ? window.location.pathname : 'gatsby-ssr-context'
     const { reportScrollUpdated } = useActions(scrollspyCaptureLogic({ key }))
+    // Remove HTML tags from the name and trim
+    const cleanName = name.replace(/<[^>]*>[\w\d\s]*<\/[^>]*>/gm, '').trim()
+
+    if (cleanName !== name) {
+        console.warn('Sidebar link name contains HTML tags. These will be stripped from the final heading.', url, name)
+    }
+
     return (
         <span className="block" style={{ marginLeft: `${depth / 1.5}rem` }}>
             <Link
@@ -22,7 +29,7 @@ export default function InternalSidebarLink({ url, name, depth, onClick, classNa
                     reportScrollUpdated(url)
                 }}
             >
-                {name}
+                {cleanName}
             </Link>
         </span>
     )
