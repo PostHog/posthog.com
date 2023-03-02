73e2d0bd30954d520ec79bb57c16c1cb7abdfcae
Paul Hultgren <pjhultgren5@gmail.com>
2023-03-01 23:58:03 -0800

    fix build

---
+++ src/components/SignupLink/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import usePostHog from '../../hooks/usePostHog'
+
+export default function SignupLink() {
+    const posthog = usePostHog()
+
+    return (
+        <Link
+            to={`https://${
+                posthog?.isFeatureEnabled && posthog?.isFeatureEnabled('direct-to-eu-cloud') ? 'eu' : 'app'
+            }.posthog.com/signup`}
+        >
+            here
+        </Link>
+    )
+}
