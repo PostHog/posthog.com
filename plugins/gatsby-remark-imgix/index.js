eeb2e8c1cefc9eeeed9c4a51468cc1db919360b9
Eli Kinsey <eli@ekinsey.dev>
2023-03-03 19:10:30 -0800

    use s3

--- plugins/gatsby-remark-imgix/index.js
+++ plugins/gatsby-remark-imgix/index.js
@@ -9,33 +9,41 @@ const isRelative = require('is-relative')
 const mime = require('mime')
 const sizeOf = require('image-size')
 const { buildGatsbyImageDataObject } = require('@imgix/gatsby/dist/pluginHelpers')
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
+const { createHash } = require('crypto')
 
-const generateImageNode = ({ publicURL, srcSet, mime, width = 0, height = 0 }) => {
-    const data = {
-        layout: 'constrained',
-        width,
-        height,
-        images: {
-            fallback: {
-                src: publicURL,
-                srcSet,
-                sizes: '(min-width: 1000px) 1000px, 100vw',
-            },
-            sources: [
-                {
-                    type: mime,
-                    srcSet,
-                    sizes: '(min-width: 1000px) 1000px, 100vw',
-                },
-            ],
-        },
+const generateAWSKey = (filePath, branch) => `${branch}/${filePath.substring(filePath.indexOf('contents'))}`
+
+const uploadFileToAWS = async (filePath, branch, cache) => {
+    const file = fs.readFileSync(filePath)
+    const key = generateAWSKey(filePath, branch)
+    const cachedETag = await cache.get(key)
+    if (cachedETag) {
+        const hash = createHash('md5').update(file).digest('hex')
+        if (cachedETag === hash) return
     }
-    return data
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: key,
+        Body: file,
+    }
+    try {
+        const data = await s3.upload(params).promise()
+        await cache.set(key, data?.ETag)
+    } catch (err) {
+        console.error(err)
+    }
+    return key
 }
 
-module.exports = ({ markdownAST, getNode, markdownNode }, { imgixDomain, imgixToken, branch }) => {
+module.exports = async ({ markdownAST, getNode, markdownNode, cache }, { imgixDomain, imgixToken, branch }) => {
     const parentNode = getNode(markdownNode?.parent)
     if (!parentNode) return markdownAST
+    const imagesToUpload = []
     // handle all image nodes
     visit(markdownAST, 'image', (node) => {
         const url = node?.url
@@ -50,28 +58,40 @@ module.exports = ({ markdownAST, getNode, markdownNode }, { imgixDomain, imgixTo
         const { width, height } = sizeOf(imagePath) ?? {}
         const mimeType = mime.getType(imagePath)
         const extension = mime.getExtension(mimeType)
-        if (extension !== 'gif' && branch && imgixDomain && imgixToken) {
-            // create imgix image
-            const gitHubURL = `https://raw.githubusercontent.com/PostHog/posthog.com/${branch}/contents/${
-                imagePath.split('contents/')[1]
-            }`
-            const data = buildGatsbyImageDataObject({
-                url: gitHubURL,
-                imgixClientOptions: {
-                    domain: imgixDomain,
-                    secureURLToken: imgixToken,
-                },
-                resolverArgs: {
-                    layout: 'constrained',
-                    imgixParams: {},
-                    placeholderImgixParams: {},
-                },
-                dimensions: { width: width ?? 0, height: height ?? 0 },
-            })
+        if (branch && imgixDomain && imgixToken) {
+            if (extension === 'gif') {
+                const key = generateAWSKey(imagePath, branch)
+                imagesToUpload.push(imagePath)
+                const publicURL = `${process.env.CLOUDFRONT_DOMAIN}/${key}`
+                html = `
+                    <img
+                        src='${publicURL}'
+                        title='${node.alt || ''}'
+                        alt='${node.alt || ''}'
+                        loading='lazy'>
+                `
+            } else {
+                // create imgix image
+                const gitHubURL = `https://raw.githubusercontent.com/PostHog/posthog.com/${branch}/contents/${
+                    imagePath.split('contents/')[1]
+                }`
+                const data = buildGatsbyImageDataObject({
+                    url: gitHubURL,
+                    imgixClientOptions: {
+                        domain: imgixDomain,
+                        secureURLToken: imgixToken,
+                    },
+                    resolverArgs: {
+                        layout: 'constrained',
+                        imgixParams: {},
+                        placeholderImgixParams: {},
+                    },
+                    dimensions: { width: width ?? 0, height: height ?? 0 },
+                })
 
-            const source = data.images.sources[0]
+                const source = data.images.sources[0]
 
-            html = `
+                html = `
                 <img
                     src='${data.images.fallback.src}'
                     ${source ? `srcSet="${source.srcSet}" sizes="${source.sizes}"` : ''}
@@ -79,19 +99,13 @@ module.exports = ({ markdownAST, getNode, markdownNode }, { imgixDomain, imgixTo
                     alt='${node.alt || ''}'
                     loading='lazy'>
             `
+            }
         } else {
             // create local image
             const imageName = imagePath.replaceAll('/', '-') + path.basename(imagePath)
             const publicPath = path.join(process.cwd(), `public`, `static`, imageName)
             fs.copyFileSync(imagePath, publicPath)
             const publicURL = `/static/${imageName}`
-            data = generateImageNode({
-                publicURL,
-                srcSet: '',
-                mime: mimeType,
-                width: width,
-                height: height,
-            })
             html = `
                 <img
                     src='${publicURL}'
@@ -101,14 +115,12 @@ module.exports = ({ markdownAST, getNode, markdownNode }, { imgixDomain, imgixTo
             `
         }
 
-        // Add a link if the image is the first thing in the line (e.g.: ![alt](/path.png))
-        // ... otherwise it's in a link already (e.g.: [![alt](/path.png)](http://link))
-        const addLink = node.position.start.column === 1
-
-        node.value = addLink ? `<a href='${node.url}'>${html}</a>` : html
-        node.type = 'html' // this breaks the node type, so always use this plugin last
+        node.value = html
+        node.type = 'html'
         node.children = undefined
     })
+
+    await Promise.all(imagesToUpload.map((imagePath) => uploadFileToAWS(imagePath, branch, cache)))
 
     return markdownAST
 }
