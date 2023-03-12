466fb184157e7c8dc5f4bf7df7a9928f0781a29e
Eli Kinsey <eli@ekinsey.dev>
2023-03-12 16:58:22 -0700

    add key

--- src/components/Docs/LinkGrid.tsx
+++ src/components/Docs/LinkGrid.tsx
@@ -11,14 +11,19 @@ export const LinkGrid: React.FC<LinkGridProps> = ({ links }) => {
         <ul className="p-0 m-0 grid md:grid-cols-2 gap-[1px]">
             {links.map((link) => {
                 return (
-                    <li className="list-none">
+                    <li key={link.name} className="list-none">
                         <Link
                             to={link.to}
                             key={link.name}
                             disablePrefetch
                             className="group -mx-2 px-2 py-2 my-1 rounded-sm flex items-center space-x-3 hover:bg-gray-accent-light dark:hover:bg-gray-accent-dark"
                         >
-                            {link.icon && <Icon className="w-10 h-10 text-gray bg-gray-accent-light group-hover:bg-gray-accent/50 rounded-sm mt-1 lg:mt-0.5 shrink-0" name={link.icon} />}
+                            {link.icon && (
+                                <Icon
+                                    className="w-10 h-10 text-gray bg-gray-accent-light group-hover:bg-gray-accent/50 rounded-sm mt-1 lg:mt-0.5 shrink-0"
+                                    name={link.icon}
+                                />
+                            )}
                             <div>
                                 <h3 className="text-base font-semibold text-red m-0">{link.name}</h3>
                                 {/* {link.description} */}
