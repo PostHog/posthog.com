cc21031ad94087d7f279333ee54fabc8dc94f00b
camerondeleone <56287180+camerondeleone@users.noreply.github.com>
2023-03-23 13:59:38 -0400

    update slack link

--- src/pages/slack.tsx
+++ src/pages/slack.tsx
@@ -5,7 +5,7 @@ import { SEO } from 'components/seo'
 function Slack() {
     /* This component will redirect the user to the Slack users group. */
     const [source, setSource] = useState<string | null>(null)
-    const slackUrl = 'https://join.slack.com/t/posthogusers/shared_invite/zt-1ou84aphe-R3H5OE9Uu6i8Fq4EHCWzbg'
+    const slackUrl = 'https://join.slack.com/t/posthogusers/shared_invite/zt-1fldtg2p0-5XasbpaWVxtu2M5QyBsQOA'
 
     useEffect(() => {
         const s = new URLSearchParams(window.location.search).get('s')
