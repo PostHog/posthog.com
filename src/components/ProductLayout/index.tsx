7a1bac0a683ef0263d1fa2d588f54f9b89fa1af0
Cory Watilo <cww@watilo.com>
2023-03-26 14:02:28 -0400

    polishing docs ui

--- src/components/ProductLayout/index.tsx
+++ src/components/ProductLayout/index.tsx
@@ -184,7 +184,7 @@ export const Documentation = ({ documentation, title, image, tutorials }) => {
         <>
             <div className="pb-4 mb-4 border-b border-dashed border-gray-accent-light flex justify-between items-end">
                 <h2 className="m-0">Docs & resources</h2>
-                <CallToAction size="md" type="secondary" to="/docs">
+                <CallToAction size="sm" type="secondary" to="/docs">
                     Visit docs
                 </CallToAction>
             </div>
@@ -196,10 +196,13 @@ export const Documentation = ({ documentation, title, image, tutorials }) => {
                             const slug = fields?.slug
                             return (
                                 <li key={slug} className="py-4 flex space-x-4">
-                                    <span className="font-semibold opacity-60">{index + 1}.</span>
-                                    <span>
-                                        <Link to={slug}>
-                                            <h5 className="mb-2">{frontmatter.title}</h5>
+                                    <span className="font-bold text-sm opacity-60 mt-1.5 ml-1.5">{index + 1}.</span>
+                                    <div className="w-full">
+                                        <Link
+                                            to={slug}
+                                            className="block font-semibold text-sm px-2 py-1 -ml-2 rounded-sm relative  hover:bg-gray-accent-light hover:text-red hover:scale-[1.01] active:scale-[1] active:top-[.5px]"
+                                        >
+                                            <h5 className="text-[17px] m-0">{frontmatter.title}</h5>
                                         </Link>
                                         <ul className="m-0 p-0 list-none">
                                             {headings
@@ -207,22 +210,27 @@ export const Documentation = ({ documentation, title, image, tutorials }) => {
                                                 .map(({ value }) => {
                                                     const id = slugger.slug(value)
                                                     return (
-                                                        <li key={id} className="mb-2 last:mb-0">
-                                                            <Link to={`${slug}#${id}`} className="text-black ">
+                                                        <li key={id} className="">
+                                                            <Link
+                                                                to={`${slug}#${id}`}
+                                                                className="block text-primary/75 font-semibold text-sm px-2 py-1 -ml-2 rounded-[3px] relative hover:bg-gray-accent-light hover:text-primary hover:scale-[1.01] active:scale-[1] active:top-[.5px]"
+                                                            >
                                                                 {value}
                                                             </Link>
                                                         </li>
                                                     )
                                                 })}
                                         </ul>
-                                    </span>
+                                    </div>
                                 </li>
                             )
                         })}
                     </ul>
                 </div>
                 <div>
-                    {gatsbyImage && <GatsbyImage alt={title} image={gatsbyImage} />}
+                    <figure className="m-0 p-4 rounded bg-gray-accent-light">
+                        {gatsbyImage && <GatsbyImage alt={title} image={gatsbyImage} />}
+                    </figure>
                     <h4 className="m-0 opacity-60 mt-6 md:mt-12">Tutorials</h4>
                     <ul className="m-0 p-0 list-none grid divide-y-1 divide-dashed divide-gray-accent-light">
                         {tutorials.map(({ frontmatter, fields }) => {
@@ -306,8 +314,9 @@ export const Testimonial = ({ author, image, quote }: ITestimonial & { image: Im
         <SectionWrapper className="!pt-0 mt-0 relative">
             <Quote className="items-end">
                 <div>
-                    <img className="text-black max-w-[200px]" src={author.company.image} />
-                    <p className="my-6">{quote}</p>
+                    <img className="text-black max-h-[45px]" src={author.company.image} />
+                    <p className="my-6 text-[17px]" dangerouslySetInnerHTML={{ __html: quote }} />
+
                     <div className="flex space-x-4 items-center">
                         <img className="rounded-full max-w-[50px]" src={author.image} />
                         <div>
