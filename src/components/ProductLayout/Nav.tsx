745ca7116403c2fd28489400ad29750b9acaf1e9
Eli Kinsey <eli@ekinsey.dev>
2023-03-29 05:46:16 -0700

    Product page enhancements (#5644)
    * better mobile nav
    
    * add more comparison tables

--- src/components/ProductLayout/Nav.tsx
+++ src/components/ProductLayout/Nav.tsx
@@ -4,6 +4,8 @@ import slugify from 'slugify'
 import { useLocation } from '@reach/router'
 import { Analytics, SessionRecording, FeatureFlags, AbTesting } from 'components/ProductIcons'
 import { Platform } from 'components/NotProductIcons'
+import Slider from 'react-slick'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const nav = [
     {
@@ -33,64 +35,69 @@ const nav = [
     },
 ]
 
+const sliderSettings = {
+    dots: false,
+    infinite: false,
+    arrows: false,
+    speed: 500,
+    slidesToScroll: 1,
+    autoplay: false,
+    variableWidth: true,
+    draggable: true,
+    centerMode: true,
+}
+
 export default function Nav() {
     const { pathname } = useLocation()
-    const [activeStyles, setActiveStyles] = useState({})
-    const activeItem = useRef<HTMLLIElement>(null)
-
-    const setStyles = useCallback(() => {
-        if (activeItem?.current) {
-            const bounding = activeItem.current.getBoundingClientRect()
-            setActiveStyles({ width: bounding?.width, left: activeItem.current.offsetLeft })
-        }
-    }, [])
+    const sliderRef = useRef(null)
 
     useEffect(() => {
-        window.addEventListener('resize', setStyles)
-        return () => {
-            window.removeEventListener('resize', setStyles)
-        }
-    }, [])
-
-    useEffect(() => {
-        setStyles()
+        sliderRef?.current && sliderRef?.current?.slickGoTo(nav.findIndex((navItem) => navItem.url === pathname))
     }, [pathname])
 
     return (
-        <nav className="relative z-10 -mx-5 overflow-x-auto">
-            <div className="relative flex max-w-screen-2xl mx-auto md:border-b md:border-gray-accent-light md:border-dashed px-4 pb-2.5 md:pb-0">
-                <ul className="list-none flex p-0 m-auto space-x-1 md:space-x-4  whitespace-nowrap">
-                    {nav.map((navItem) => {
-                        const { label, url, icon } = navItem
-                        const active = pathname === url
-                        return (
-                            <li
-                                ref={active ? activeItem : null}
-                                id={`product-nav-${slugify(url, { lower: true })}`}
-                                key={label}
-                                className="first:ml-auto last:mr-auto"
-                            >
-                                <Link
-                                    className={`flex space-x-2 items-center ${
-                                        active
-                                            ? '!text-red !font-bold'
-                                            : '!text-primary/75 hover:border-gray-accent-light hover:bg-gray-accent-light'
-                                    } px-3 py-1.5 mb-1.5 text-sm [font-variation-settings:_'wght'_700] whitespace-nowrap rounded relative hover:scale-[1.01] active:scale-[.99] tracking-[-.1px] group`}
-                                    to={url}
+        <nav className="z-10 -mx-5">
+            <div className="px-4 pb-2.5 md:pb-0 relative after:w-full after:md:border-b after:border-gray-accent-light after:border-dashed after:absolute after:bottom-0">
+                <div className="product-nav z-10 relative">
+                    <Slider ref={sliderRef} {...sliderSettings}>
+                        {nav.map((navItem) => {
+                            const { label, url, icon } = navItem
+                            const active = pathname === url
+                            return (
+                                <div
+                                    className="relative"
+                                    id={`product-nav-${slugify(url, { lower: true })}`}
+                                    key={label}
                                 >
-                                    <span className={`text-black ${active ? 'opacity-100' : 'opacity-70'} `}>
-                                        {icon}
-                                    </span>
-                                    <span>{label}</span>
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <div
-                    style={activeStyles}
-                    className="h-[3px] md:h-[2px] bg-red rounded-md absolute bottom-3 md:bottom-[-1px] z-10 transition-all duration-500"
-                />
+                                    <Link
+                                        className={`flex space-x-2 items-center ${
+                                            active
+                                                ? '!text-red !font-bold'
+                                                : '!text-primary/75 hover:border-gray-accent-light hover:bg-gray-accent-light'
+                                        } px-3 py-1.5 mb-1.5 text-sm [font-variation-settings:_'wght'_700] whitespace-nowrap rounded relative hover:scale-[1.01] active:scale-[.99] tracking-[-.1px] group`}
+                                        to={url}
+                                    >
+                                        <span className={`text-black ${active ? 'opacity-100' : 'opacity-70'} `}>
+                                            {icon}
+                                        </span>
+                                        <span>{label}</span>
+                                    </Link>
+                                    <AnimatePresence>
+                                        {active && (
+                                            <motion.div
+                                                transition={{ duration: 0.2 }}
+                                                initial={{ translateY: 50, opacity: 0 }}
+                                                animate={{ translateY: 0, opacity: '100%' }}
+                                                exit={{ translateY: 50, opacity: 0 }}
+                                                className="h-[3px] md:h-[2px] bg-red rounded-md absolute bottom-0 z-10 transition-all duration-500 w-full left-0"
+                                            />
+                                        )}
+                                    </AnimatePresence>
+                                </div>
+                            )
+                        })}
+                    </Slider>
+                </div>
             </div>
         </nav>
     )
