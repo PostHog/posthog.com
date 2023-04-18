3eee6655889eca5080ad171dee0f8d927c3904c7
Cory Watilo <cww@watilo.com>
2023-04-18 14:20:33 -0400

    styled notification bell

--- src/components/Squeak/components/SubscribeButton.tsx
+++ src/components/Squeak/components/SubscribeButton.tsx
@@ -3,6 +3,7 @@ import { useUser } from 'hooks/useUser'
 import { QuestionData, StrapiRecord } from 'lib/strapi'
 import React, { useEffect, useState } from 'react'
 import { useQuestion } from '../hooks/useQuestion'
+import { Bell } from 'components/NotProductIcons'
 
 const Button = ({
     className,
@@ -14,8 +15,18 @@ const Button = ({
     handleSubscribe: () => Promise<void>
 }) =>
     subscribed === null ? null : (
-        <button className={className} onClick={handleSubscribe}>
-            {subscribed ? 'Unsubscribe' : 'Subscribe'}
+        <button
+            className={`flex rounded-sm p-1 ${
+                subscribed
+                    ? 'relative bg-gray-accent-light hover:bg-gray-accent-light-hover/50 dark:bg-gray-accent-dark dark:hover:bg-gray-accent-dark-hover/50 text-primary/50 hover:text-primary/75 dark:text-primary-dark/50 dark:hover:text-primary-dark/75 hover:scale-[1.05] hover:top-[-.5px] active:scale-[1] active:top-[0px]'
+                    : 'bg-red text-white dark:text-white'
+            } ${className}`}
+            onClick={handleSubscribe}
+        >
+            <span className="w-6 h-6 rotate-6">
+                <Bell />
+            </span>
+            {/* {subscribed ? 'Unsubscribe' : 'Subscribe'} */}
         </button>
     )
 
@@ -52,14 +63,16 @@ export default function SubscribeButton({
                     <Button
                         subscribed={subscribed}
                         handleSubscribe={handleSubscribe}
-                        className={`${className} p-0 border-0 relative font-bold text-red`}
+                        className={`${className} p-0 border-0 relative font-bold`}
                     />
                 ) : (
                     <button
-                        className={`${className} p-0 border-0 relative opacity-50 font-bold text-black dark:text-white`}
+                        className={`${className} flex gap-1 p-0 border-0 relative opacity-50 font-bold text-black dark:text-white`}
                         disabled
                     >
-                        Subscribe
+                        <span className="w-4 h-4">
+                            <Bell />
+                        </span>
                     </button>
                 )}
             </span>
