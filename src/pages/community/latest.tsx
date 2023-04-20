984f1633b008755cfbbaeb23d8d87b5b0c353305
Cory Watilo <cww@watilo.com>
2023-04-20 12:05:56 -0400

    split new questions from my discussions

--- src/pages/community/latest.tsx
+++ src/pages/community/latest.tsx
@@ -155,25 +155,18 @@ const Subscription = ({ question }: { question: QuestionData }) => {
     )
 }
 
-const Subscriptions = () => {
-    const { user, fetchUser } = useUser()
-    const subscriptions = user?.profile?.questionSubscriptions
+const RecentQuestions = () => {
+    // const [sortBy, setSortBy] = useState<'newest' | 'activity' | 'popular'>('newest')
 
-    useEffect(() => {
-        if (user) fetchUser()
-    }, [])
+    const { questions, fetchMore, isLoading } = useQuestions({ limit: 3 })
 
-    if (!subscriptions) return null
     return (
-        <div id="my-activity" className="mb-12">
-            <SectionTitle>My discussions</SectionTitle>
-            {subscriptions?.length > 0 ? (
-                subscriptions.map((question) => {
-                    return <Subscription key={question.id} user={user} question={question} />
-                })
-            ) : (
-                <p>You're not subscribed to any questions yet</p>
-            )}
+        <div id="recent-questions" className="mb-12">
+            <SectionTitle>Recent questions</SectionTitle>
+            <QuestionsTable hideLoadMore questions={questions} fetchMore={fetchMore} isLoading={isLoading} />
+            <CallToAction className="mt-4" type="secondary" width="full" to="/questions">
+                Browse all questions
+            </CallToAction>
         </div>
     )
 }
@@ -195,7 +188,7 @@ export default function CommunityPage({ params }: PageProps) {
                     ]}
                     hideSurvey
                 >
-                    <Subscriptions />
+                    <RecentQuestions />
                 </PostLayout>
             </Layout>
         </>
