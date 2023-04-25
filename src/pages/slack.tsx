a08cb2c53e8033b1af921b63065d166207a856d9
Cory Watilo <cww@watilo.com>
2023-04-25 14:51:41 -0400

    Update community Slack invite URL (good for 2k invites) (#5657)
    * update slack URL (good for 2k invites)
    
    * added documentation about how the slack invite URL works

--- src/pages/slack.tsx
+++ src/pages/slack.tsx
@@ -5,7 +5,10 @@ import { SEO } from 'components/seo'
 function Slack() {
     /* This component will redirect the user to the Slack users group. */
     const [source, setSource] = useState<string | null>(null)
-    const slackUrl = 'https://join.slack.com/t/posthogusers/shared_invite/zt-1rstc53ir-z17iWhXIe0r0oUeNkFvVSQ'
+    const slackUrl = 'https://join.slack.com/t/posthogusers/shared_invite/zt-1salriqd0-5Y5~Xbj5v6HQuQ8nmqi2Cw'
+    // Normal Slack invited (generated through the client) work for up to 400 invites.
+    // Above link supports 2,000 invites and lasts for 1 year (until March 29, 2024).
+    // When above link stops working, we can reach out to Slack to get a new one (or to get more invites on the link.)
 
     useEffect(() => {
         const s = new URLSearchParams(window.location.search).get('s')
