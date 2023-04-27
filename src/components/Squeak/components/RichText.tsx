bc3232ae81fd88139de8e3663ff31419a6a95f3a
Eli Kinsey <eli@ekinsey.dev>
2023-04-26 18:34:47 -0700

    button placement

--- src/components/Squeak/components/RichText.tsx
+++ src/components/Squeak/components/RichText.tsx
@@ -4,6 +4,7 @@ import { useDropzone } from 'react-dropzone'
 import Spinner from 'components/Spinner'
 import Markdown from './Markdown'
 import slugify from 'slugify'
+import { Edit } from 'components/Icons'
 
 const buttons = [
     {
@@ -187,7 +188,7 @@ export default function RichText({ initialValue = '', setFieldValue, autoFocus, 
                 />
             )}
             <div className="flex items-center justify-between py-1">
-                <ul className="flex items-center list-none p-0 ml-2 space-x-1">
+                <ul className="flex items-center list-none p-0 mx-2 space-x-1 w-full">
                     {buttons.map((button, index) => {
                         return (
                             <li key={index}>
@@ -220,52 +221,59 @@ export default function RichText({ initialValue = '', setFieldValue, autoFocus, 
                             </svg>
                         </button>
                     </li>
+                    <li className="!ml-auto">
+                        <button
+                            onClick={() => setShowPreview(false)}
+                            type="button"
+                            className={`flex items-center bg-none border-none rounded-sm text-black/50 dark:text-primary-dark/50 justify-center w-[32px] h-[32px] hover:bg-black/[.15] hover:text-black/75 dark:hover:bg-primary-dark/[.15] dark:hover:text-primary-dark/75 ${
+                                showPreview
+                                    ? ''
+                                    : 'bg-black/[.15] text-black/75 dark:bg-primary-dark/[.15] dark:text-primary-dark/75'
+                            }`}
+                        >
+                            <Edit />
+                        </button>
+                    </li>
                     <li>
                         <button
-                            onClick={() => setShowPreview(!showPreview)}
+                            onClick={() => setShowPreview(true)}
                             type="button"
-                            className="flex items-center bg-none border-none rounded-sm text-black/50 dark:text-primary-dark/50 justify-center w-[32px] h-[32px] hover:bg-black/[.15] hover:text-black/75 dark:hover:bg-primary-dark/[.15] dark:hover:text-primary-dark/75"
+                            className={`flex items-center bg-none border-none rounded-sm text-black/50 dark:text-primary-dark/50 justify-center w-[32px] h-[32px] hover:bg-black/[.15] hover:text-black/75 dark:hover:bg-primary-dark/[.15] dark:hover:text-primary-dark/75 ${
+                                showPreview
+                                    ? 'bg-black/[.15] text-black/75 dark:bg-primary-dark/[.15] dark:text-primary-dark/75'
+                                    : ''
+                            }`}
                         >
-                            {showPreview ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-5 h-5"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-5 h-5"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                                    />
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                    />
-                                </svg>
-                            )}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-5 h-5"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                />
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                            </svg>
                         </button>
                     </li>
                 </ul>
-                <div className="mr-2">
+            </div>
+            {imageLoading && (
+                <div className="w-full h-full inset-0 bg-white/50 dark:bg-black/50 absolute flex justify-center items-center">
+                    <Spinner className="w-10 h-10" />
+                </div>
+            )}
+            {!value && (
+                <div className="absolute top-3 right-4">
                     <a
                         className="text-primary/30 hover:text-primary/50 dark:text-primary-dark/30 dark:hover:text-primary-dark/50"
                         href="https://www.markdownguide.org/cheat-sheet/"
@@ -275,11 +283,6 @@ export default function RichText({ initialValue = '', setFieldValue, autoFocus, 
                     >
                         <MarkdownLogo />
                     </a>
-                </div>
-            </div>
-            {imageLoading && (
-                <div className="w-full h-full inset-0 bg-white/50 dark:bg-black/50 absolute flex justify-center items-center">
-                    <Spinner className="w-10 h-10" />
                 </div>
             )}
         </div>
