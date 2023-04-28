7436ca60eef549c44c0be3416fd85ef6bbea98f0
Eli Kinsey <eli@ekinsey.dev>
2023-04-28 00:54:39 -0700

    tooltip

--- src/components/Squeak/components/Days.tsx
+++ src/components/Squeak/components/Days.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import Tooltip from 'components/Tooltip'
 dayjs.extend(relativeTime)
 
 export const Days = ({ created }: { created: string | undefined }) => {
@@ -8,7 +9,11 @@ export const Days = ({ created }: { created: string | undefined }) => {
         return null
     }
 
-    return <span className="text-sm opacity-50">{dayjs(created).fromNow()}</span>
+    return (
+        <Tooltip content={dayjs(created).format('MM/DD/YYYY - h:m A')}>
+            <span className="text-sm opacity-50 relative cursor-default">{dayjs(created).fromNow()}</span>
+        </Tooltip>
+    )
 }
 
 export default Days
