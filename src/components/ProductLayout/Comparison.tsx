ef04decfcb36b4bdea6b0d0018c4433eea94c80a
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2023-05-18 14:03:33 +0100

    Update and add comparison tables to product page (#5910)
    * Update product-os.mdx
    
    * Update product-os.mdx
    
    * Update product-os.mdx
    
    * Links to full comparisons for interested parties
    
    * Adds an experimentation chart too
    
    * Update Comparison.tsx

--- src/components/ProductLayout/Comparison.tsx
+++ src/components/ProductLayout/Comparison.tsx
@@ -2,13 +2,13 @@ import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import { IComparison } from './types'
 
-export default function Comparison({ description, children }: IComparison) {
+export default function Comparison({ children }: IComparison) {
     return (
         <div id="comparison" className="max-w-5xl mx-auto">
             <div className="flex justify-between items-end">
                 <div>
                     <h2 className="m-0">PostHog vs...</h2>
-                    <p className="m-0">{description}</p>
+                    <p className="m-0">How does PostHog compare?</p>
                 </div>
                 <div>
                     <StaticImage className="max-w-[530px]" alt="PostHog vs..." src="./images/vs.png" />
