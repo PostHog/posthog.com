a672ca63215f18f39382a52bf4b5d3929a655637
Cory Watilo <cww@watilo.com>
2023-05-21 14:55:56 -0400

    posthog vs

--- src/components/ProductLayout/Comparison.tsx
+++ src/components/ProductLayout/Comparison.tsx
@@ -10,13 +10,14 @@ export default function Comparison({ description, children }: IComparison) {
                     <h2 className="m-0">PostHog vs...</h2>
                     {/*<p className="m-0">{description}</p>*/}
                 </div>
-                <div>
-                    {/*
-                    <StaticImage className="max-w-[530px]" alt="PostHog vs..." src="./images/vs.png" />
-                    */}
-                </div>
             </div>
-            <div className="overflow-x-auto article-content mt-0">{children}</div>
+
+            <div className="-mx-12 my-4 relative">
+                <StaticImage className="w-full sm:hidden" alt="PostHog vs..." src="./images/vs-mobile.jpg" />
+                <StaticImage className="hidden sm:block w-full" alt="PostHog vs..." src="./images/vs.jpg" />
+            </div>
+
+            <div className="overflow-x-auto article-content mt-0 -mx-5 px-5">{children}</div>
         </div>
     )
 }
