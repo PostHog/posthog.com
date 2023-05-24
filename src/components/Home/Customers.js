564bc4138aa08053a5d997af7fab1befc050c730
Cory Watilo <cww@watilo.com>
2023-05-24 12:56:49 +0100

    add new homepage logo

--- src/components/Home/Customers.js
+++ src/components/Home/Customers.js
@@ -13,7 +13,7 @@ import outbrain from './images/outbrain.svg'
 import phantom from './images/phantom.svg'
 import questdb from './images/questdb.svg'
 import staples from './images/staples.svg'
-import vendasta from './images/vendasta.svg'
+import dhl from './images/dhl.svg'
 import yCombinator from './images/y-combinator.svg'
 import { motion } from 'framer-motion'
 
@@ -49,15 +49,15 @@ export default function Customers() {
                     <Customer className="max-h-[44px]" image={yCombinator} />
                     <Customer className="max-h-[36px]" image={staples} />
                     <Customer className="max-h-[36px]" image={airbus} />
-                    <Customer className="max-h-[46px]" image={phantom} />
+                    <Customer className="max-h-[35px]" image={dhl} />
 
+                    <Customer className="max-h-[46px]" image={phantom} />
                     <Customer className="max-h-[51px]" image={hasura} />
                     <Customer className="max-h-[50px]" image={assemblyai} />
                     <Customer className="max-h-[40px]" image={outbrain} />
-                    <Customer className="max-h-[35px]" image={clickhouse} />
 
+                    <Customer className="max-h-[35px]" image={clickhouse} />
                     <Customer className="max-h-[50px]" image={joybird} />
-                    <Customer className="max-h-[35px]" image={vendasta} />
                     <Customer className="max-h-[45px]" image={questdb} />
                     <Customer className="max-h-[50px]" image={landmark} />
                 </ul>
