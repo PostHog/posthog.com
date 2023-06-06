d120c919dca724e2b633e7364e9dde03a6380768
Cory Watilo <cww@watilo.com>
2023-06-06 11:32:08 -0500

    Revert "removed old (singular) product page"
    This reverts commit fc8d71f9714d5e49bf8a5b7ca06b7bf64cb18ca5.

---
+++ src/components/ProductPage/Section.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AnimateIntoView from './AnimateIntoView'
+
+export default function Section({ children, ...other }) {
+    return (
+        <section {...other} className="my-12 md:my-24 px-5 max-w-[960px] mx-auto justify-between">
+            <AnimateIntoView>
+                <div className="flex items-center md:space-x-4 space-y-6 md:space-y-0 flex-col md:flex-row">
+                    {children}
+                </div>
+            </AnimateIntoView>
+        </section>
+    )
+}
