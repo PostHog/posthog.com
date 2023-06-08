5f5ca2f28ed1d82d4eb8fbc2f3b5628e9d600fc8
Cory Watilo <cww@watilo.com>
2023-06-08 16:13:16 -0700

    fixed sticky header dark mode color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import MainNav from '../MainNav'
 
 export const Header = (): JSX.Element => {
     return (
-        <header className="relative z-[9999] reasonable:sticky reasonable:top-0 bg-gradient-to-b from-primary-dark/100 via-primary-dark/100 to-primary-dark/50 dark:from-primary/100 dark:via-primary/100 dark:to-primary/50 dark:from-dark/100 dark:to-dark/50 backdrop-blur">
+        <header className="relative z-[9999] reasonable:sticky reasonable:top-0 bg-gradient-to-b from-primary-dark/100 via-primary-dark/100 to-primary-dark/50 dark:from-dark/100 dark:via-dark/100 dark:to-dark/50 backdrop-blur">
             <MainNav />
         </header>
     )
