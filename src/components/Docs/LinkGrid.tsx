d7c7c4649de4efcd78df8e2f034cd147d3bd6cf2
Cory Watilo <cww@watilo.com>
2023-06-19 14:06:21 -0400

    colorize product analytics icons

--- src/components/Docs/LinkGrid.tsx
+++ src/components/Docs/LinkGrid.tsx
@@ -1,29 +1,35 @@
 import React from 'react'
 import Link from 'components/Link'
 import * as ProductIcons from '../ProductIcons'
+import * as NewIcons from '../NewIcons'
 
 type LinkGridProps = {
-    links: { name: string; to: string; description: string; icon?: string }[]
+    links: { name: string; to: string; description: string; icon?: string; color?: string }[]
 }
 
 export const LinkGrid: React.FC<LinkGridProps> = ({ links }) => {
     return (
         <ul className="p-0 m-0 grid md:grid-cols-2 gap-[1px]">
             {links.map((link) => {
-                const Icon = link.icon && ProductIcons[link.icon]
+                const Icon = link.icon && NewIcons[link.icon]
+                const Color = link.color
                 return (
                     <li key={link.name} className="list-none">
                         <Link
                             to={link.to}
                             key={link.name}
                             disablePrefetch
-                            className="group flex items-center relative px-2 py-0.5 mb-1 gap-2 rounded  border border-b-3 border-transparent hover:border-light dark:hover:border-dark hover:translate-y-[-1px] active:translate-y-[1px] active:transition-all"
+                            className="group flex items-center relative px-2 py-1 mb-1 gap-2 rounded  border border-b-3 border-transparent hover:border-light dark:hover:border-dark hover:translate-y-[-1px] active:translate-y-[1px] active:transition-all"
                         >
                             {Icon && (
-                                <Icon className="w-10 h-10 p-2 text-primary dark:text-primary-dark bg-accent dark:bg-accent-dark group-hover:bg-accent dark:group-hover:bg-accent-dark rounded-sm mt-1 lg:mt-0.5 shrink-0" />
+                                <Icon
+                                    className={`w-10 h-10 p-2 text-primary  text-${Color} dark:text-${Color} bg-accent dark:bg-accent-dark group-hover:bg-accent dark:group-hover:bg-accent-dark rounded-sm shrink-0`}
+                                />
                             )}
                             <div>
-                                <h3 className="text-base font-semibold m-0">{link.name}</h3>
+                                <h3 className="text-base text-primary dark:text-primary-dark font-semibold m-0">
+                                    {link.name}
+                                </h3>
                                 {/* {link.description} */}
                             </div>
                         </Link>
