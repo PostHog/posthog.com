7495ef9c242411384bfbac3f199e4ec455da43b3
Cory Watilo <cww@watilo.com>
2023-06-20 18:06:13 -0400

    reduce font size of text in blockquotes, inline code
    (and hoping this doesn't have negative repercussions...)

--- src/components/InlineCode/index.js
+++ src/components/InlineCode/index.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const InlineCode = (props) => (
     <code
         {...props}
-        className="text-inherit p-1 rounded bg-accent dark:bg-accent-dark border border-light dark:border-dark"
+        className="text-inherit p-1 rounded bg-accent dark:bg-accent-dark border border-light dark:border-dark !text-[13px]"
     />
 )
