4e9f9296c7ac6b501d55ade76204e158c88f2f8f
Eli Kinsey <eli@ekinsey.dev>
2023-06-30 12:37:53 -0700

    product page quick links

--- src/components/ProductLayout/Documentation.tsx
+++ src/components/ProductLayout/Documentation.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { IDocumentation } from './types'
-import { Intro as SessionReplayIntro } from '../../pages/docs/session-replay'
+import { Content as SessionReplayIntro } from '../../pages/docs/session-replay'
 import { Content as ProductAnalyticsIntro } from '../../pages/docs/product-analytics'
-import { Intro as FeatureFlagIntro } from '../../pages/docs/feature-flags'
-import { Intro as ExperimentsIntro } from '../../pages/docs/experiments'
+import { Content as FeatureFlagIntro } from '../../pages/docs/feature-flags'
+import { Content as ExperimentsIntro } from '../../pages/docs/experiments'
 
 const quickLinks = {
-    'session replay': SessionReplayIntro,
-    'product analytics': ProductAnalyticsIntro,
-    'feature flags': FeatureFlagIntro,
-    'a/b testing': ExperimentsIntro,
+    'session replay': <SessionReplayIntro quickLinks />,
+    'product analytics': <ProductAnalyticsIntro quickLinks />,
+    'feature flags': <FeatureFlagIntro quickLinks />,
+    'a/b testing': <ExperimentsIntro quickLinks />,
 }
 
 export default function Documentation({ title }: IDocumentation) {
     const Intro = title && quickLinks[title.toLowerCase()]
     return (
         <div id="documentation" className="max-w-screen-2xl mx-auto">
-            {Intro && <Intro image={false} />}
+            {Intro}
         </div>
     )
 }
