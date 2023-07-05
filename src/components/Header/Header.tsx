0aba250fca0b976c9053d2616463ee1aa99651d6
Eli Kinsey <eli@ekinsey.dev>
2023-07-05 07:43:42 -0700

    header z-index

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Main } from '../MainNav'
 export const Header = (): JSX.Element => {
     return (
         <>
-            <header className="relative z-[9999] reasonable:sticky reasonable:top-0 bg-gradient-to-b from-primary-dark/100 via-primary-dark/100 to-primary-dark/50 dark:from-dark/100 dark:via-dark/100 dark:to-dark/50 backdrop-blur">
+            <header className="relative z-[999999] reasonable:sticky reasonable:top-0 bg-gradient-to-b from-primary-dark/100 via-primary-dark/100 to-primary-dark/50 dark:from-dark/100 dark:via-dark/100 dark:to-dark/50 backdrop-blur">
                 <Main />
             </header>
             {/* viewport debugging
