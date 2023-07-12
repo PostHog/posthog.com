320f030bcd68351337f4390afd134b5b4bad6680
Cory Watilo <cww@watilo.com>
2023-07-12 15:39:44 -0400

    moved team name

--- src/templates/Job.tsx
+++ src/templates/Job.tsx
@@ -145,8 +145,8 @@ export default function Job({
                 >
                     <div className="relative">
                         <div>
+                            {teamName && <p className="m-0 opacity-60 pb-2">{teamName} Team</p>}
                             <h1 className="m-0 text-5xl">{jobTitle}</h1>
-                            {teamName && <p className="m-0 text-xl opacity-70">{teamName}</p>}
                             <ul className="list-none m-0 p-0 md:items-center text-black/50 dark:text-white/50 mt-6 flex md:flex-row flex-col md:space-x-12 md:space-y-0 space-y-6">
                                 <Detail title="Department" value={departmentName} icon={<Department />} />
                                 <Detail title="Location" value={locationName} icon={<Location />} />
