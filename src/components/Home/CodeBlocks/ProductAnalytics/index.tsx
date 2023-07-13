32c55f303cc0b657cd51a0e4e97a494b1cec57fc
Eli Kinsey <eli@ekinsey.dev>
2023-07-13 15:40:27 -0700

    simplify

--- src/components/Home/CodeBlocks/ProductAnalytics/index.tsx
+++ src/components/Home/CodeBlocks/ProductAnalytics/index.tsx
@@ -1,5 +1,35 @@
-import IdentifyUser from './IdentifyUser'
-import TrackEvent from './TrackEvent'
+import CodeBlock from 'components/Home/CodeBlock'
+import React from 'react'
+
+function IdentifyUser() {
+    return (
+        <div className="grid grid-cols-2 gap-x-6">
+            <div>
+                <h4>Your code</h4>
+                <CodeBlock code={`console.log('test')`} language="js" />
+            </div>
+            <div>
+                <h4>Available in PostHog</h4>
+                <CodeBlock code={`console.log('test')`} language="js" />
+            </div>
+        </div>
+    )
+}
+
+function TrackEvent() {
+    return (
+        <div className="grid grid-cols-2 gap-x-6">
+            <div>
+                <h4>Your code</h4>
+                <CodeBlock code={`console.log('test')`} language="js" />
+            </div>
+            <div>
+                <h4>Available in PostHog</h4>
+                <CodeBlock code={`console.log('test')`} language="js" />
+            </div>
+        </div>
+    )
+}
 
 export default [
     {
