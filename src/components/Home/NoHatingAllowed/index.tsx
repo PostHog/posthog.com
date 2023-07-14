0e10b7910075371993a6521f9fd24418f67df212
Eli Kinsey <eli@ekinsey.dev>
2023-07-14 13:24:01 -0700

    card component

--- src/components/Home/NoHatingAllowed/index.tsx
+++ src/components/Home/NoHatingAllowed/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from '@posthog/icons'
 import { StaticImage } from 'gatsby-plugin-image'
 import React, { useRef } from 'react'
 
@@ -15,8 +16,25 @@ const cards = [
     },
 ]
 
+const Card = ({ top, bottom, Image, color }) => {
+    return (
+        <li
+            style={{ backgroundColor: color || 'white' }}
+            className="h-[400px] w-[300px] flex flex-col justify-between p-5 rounded-md relative even:rotate-3 odd:-rotate-3 flex-shrink-0 md:first:ml-[20vw]"
+        >
+            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full">{Image}</div>
+            <h5 className="m-0 text-2xl text-black relative">{top}</h5>
+            <p className="text-sm text-black m-0 relative">{bottom}</p>
+        </li>
+    )
+}
+
 export default function NoHatingAllowed() {
     const listRef = useRef<HTMLUListElement>(null)
+
+    const handleNext = () => {
+        return null
+    }
 
     return (
         <div className="relative">
@@ -24,22 +42,18 @@ export default function NoHatingAllowed() {
                 ref={listRef}
                 className="list-none m-0 p-0 flex space-x-12 w-full px-5 snap-x overflow-x-auto overflow-y-hidden py-12"
             >
-                {cards.map(({ top, bottom, Image, color }, index) => {
-                    return (
-                        <li
-                            style={{ backgroundColor: color || 'white' }}
-                            className="h-[400px] w-[300px] flex flex-col justify-between p-5 rounded-md relative even:rotate-3 odd:-rotate-3 flex-shrink-0 md:first:ml-[20vw]"
-                            key={index}
-                        >
-                            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full">
-                                {Image}
-                            </div>
-                            <h5 className="m-0 text-2xl text-black relative">{top}</h5>
-                            <p className="text-sm text-black m-0 relative">{bottom}</p>
-                        </li>
-                    )
+                {new Array(20).fill(cards[0]).map((card, index) => {
+                    return <Card {...card} key={index} />
                 })}
             </ul>
+            <div className="absolute top-1/2 right-0 -translate-y-1/2">
+                <button
+                    onClick={handleNext}
+                    className="relative hover:scale-[1.01] hover:top-[-1px] active:top-[.5px] active:scale-[.99] md:z-30 p-8"
+                >
+                    <ArrowRight className="w-10" />
+                </button>
+            </div>
         </div>
     )
 }
