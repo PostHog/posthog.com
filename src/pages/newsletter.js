4ef5d5eed7b755fc08d7ef4fa90030bd2572854e
Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>
2023-07-14 10:05:40 +0100

    Update to newsletter signup page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -22,26 +22,26 @@ export const NewsletterPage = () => {
                 <div className="font-semibold pb-2 text-lg opacity-60 text-center">Subscribe to...</div>
                 <h1 className="text-5xl lg:text-6xl mb-3 text-center">Product for Engineers</h1>
                 <p className="font-semibold text-lg opacity-60 text-center leading-tight">
-                    Our newsletter about making better products
+                    Helping engineers and founders flex their product muscles
                 </p>
 
                 <div className="md:grid md:grid-cols-5 md:gap-12 px-4 lg:px-12 pb-8 max-w-6xl mx-auto items-center">
                     <div className="col-span-2 text-right">
                         <StaticImage
-                            src="../images/newsletter-signup.png"
+                            src="../images/newsletter-signup.jpeg"
                             objectFit="contain"
                             className="w-full h-full"
                         />
                     </div>
                     <div className="col-span-3 pt-6">
                         <p className="mb-3 text-lg">
-                            <strong>Here's what you'll find inside:</strong>
+                            <strong>Recent issues include:</strong>
                         </p>
 
                         <ul className="space-y-2 m-0 mb-8 p-0">
-                            <Benefit text="Info to help engineers build better products" />
-                            <Benefit text="The latest tutorials and tips from fast-growing startups" />
-                            <Benefit text="Curated advice on building great products" />
+                            <Benefit text="6 mistakes we've made at PostHog (and what we learned)" />
+                            <Benefit text="What we've learned about product-market fit" />
+                            <Benefit text="How to build new features users love" />
                         </ul>
 
                         <h4 className="relative text-lg">
