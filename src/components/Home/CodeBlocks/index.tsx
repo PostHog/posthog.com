853bbc342d3ea36dfd2ea9dc99f53e16c52be633
Cory Watilo <cww@watilo.com>
2023-07-17 11:52:03 -0400

    updated code snippet tagline

--- src/components/Home/CodeBlocks/index.tsx
+++ src/components/Home/CodeBlocks/index.tsx
@@ -7,6 +7,7 @@ import productAnalyticsContent from './ProductAnalytics'
 import sessionReplayContent from './SessionReplay'
 import featureFlagsContent from './FeatureFlags'
 import abTestingContent from './ABTesting'
+import { Link } from 'gatsby'
 
 const content = [productOSContent, productAnalyticsContent, sessionReplayContent, featureFlagsContent, abTestingContent]
 
@@ -15,8 +16,16 @@ export default function CodeBlocks() {
     return (
         <div className="max-w-screen-2xl mx-auto relative pb-12">
             <h2 className="text-4xl lg:text-6xl text-center mb-1">Mmmm, code examples</h2>
-            <p className="text-center text-lg font-medium opacity-75 pb-4">
-                The PostHog code snippet offers granular control over features and data privacy.
+            <p className="text-center text-lg font-medium opacity-75 mb-2">
+                Here's what you can do with the PostHog JS code snippet and your product.
+            </p>
+            <p className="text-center text-sm pb-4">
+                <Link
+                    to="https://posthog.com/docs/getting-started/install?tab=sdks"
+                    className="text-primary/50 dark:text-primary-dark/50 font-normal"
+                >
+                    (Not using JavaScript?)
+                </Link>
             </p>
             <InternalMenu
                 scrollOnRender={false}
