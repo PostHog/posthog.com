400c45581ee944f0425973f964a426651715982e
Cory Watilo <cww@watilo.com>
2023-07-19 13:17:52 -0400

    colors and placement

--- src/components/Home/Community.js
+++ src/components/Home/Community.js
@@ -18,22 +18,28 @@ export default function Community() {
     return (
         <>
             <section className="relative">
-                <StaticImage src="./images/community-light.png" className="w-full dark:hidden" />
-                <StaticImage src="./images/community-dark.png" className="w-full light:hidden" />
-                <div className="absolute border border-white left-[20vw] bottom-[29vw] text-center -rotate-[4deg]">
-                    <h4 className="text-2xl mb-0">81k+</h4>
-                    <p className="m-0">
+                <StaticImage
+                    src="./images/community-light.png"
+                    className="w-full transition-opacity opacity-100 dark:hidden dark:opacity-0"
+                />
+                <StaticImage
+                    src="./images/community-dark.png"
+                    className="w-full transition-opacity hidden opacity-0 dark:block dark:opacity-100"
+                />
+                <div className="absolute border-white left-[21.5vw] top-[30vw] xl:left-[19.5vw] xl:top-[31vw] text-center -rotate-[4deg] text-[#392116] dark:text-[#E7F1FF]">
+                    <h4 className="text-[2.5vw] lg:text-[2.75vw] xl:text-[3vw] leading-none mb-0">81k+</h4>
+                    <p className="text-[1.5vw] lg:text-[1vw] xl:text-[1vw] leading-tight m-0">
                         Developer <br className="xl:hidden" />
                         community
                     </p>
                 </div>
-                <div className="absolute border border-white left-[44vw] bottom-[19vw] text-center -rotate-1">
-                    <h4 className="text-2xl mb-0">411</h4>
-                    <p className="m-0">Contributors</p>
+                <div className="absolute border-white left-[43.5vw] top-[40.75vw] text-center -rotate-[1.5deg] text-[#392116] dark:text-[#E7F1FF]">
+                    <h4 className="text-[2.5vw] lg:text-[2.75vw] xl:text-[3vw] leading-none mb-0">411</h4>
+                    <p className="text-[1.5vw] lg:text-[1vw] xl:text-[1vw] m-0">Contributors</p>
                 </div>
-                <div className="absolute border border-white left-[65vw] bottom-[9vw] text-center -rotate-1">
-                    <h4 className="text-2xl mb-0">50b+</h4>
-                    <p className="m-0">Events tracked</p>
+                <div className="absolute border-white left-[63.5vw] top-[50.75vw] text-center -rotate-1 text-[#392116] dark:text-[#E7F1FF]">
+                    <h4 className="text-[2.5vw] lg:text-[2.75vw] xl:text-[3vw] leading-none mb-0">50b+</h4>
+                    <p className="text-[1.5vw] lg:text-[1vw] xl:text-[1vw] m-0">Events tracked</p>
                 </div>
             </section>
             <section className={section('text-center')}>
