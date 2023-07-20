468c8d892ae1c9365300ee8c8ecf79e26d8cdc65
Cory Watilo <cww@watilo.com>
2023-07-20 15:32:17 -0400

    Adds library comparison for feature flags (#6296)
    * add library comparison for feature flags
    
    * Fix typos
    
    * tweaks
    
    ---------
    
    Co-authored-by: PostHog <hey@posthog.com>

--- src/navs/index.js
+++ src/navs/index.js
@@ -1997,6 +1997,15 @@ export const docsMenu = {
                     icon: 'Question',
                     color: 'seagreen',
                 },
+                {
+                    name: 'Resources',
+                },
+                {
+                    name: 'Library comparison',
+                    url: '/docs/feature-flags/libraries',
+                    icon: 'Book',
+                    color: 'blue',
+                },
             ],
         },
         {
