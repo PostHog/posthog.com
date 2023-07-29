27225c630f642e8acee301fbbf67073f0a002c4d
Eli Kinsey <eli@ekinsey.dev>
2023-07-29 15:12:38 -0700

    fix scroll to

--- src/components/Docs/InternalSidebarLink.tsx
+++ src/components/Docs/InternalSidebarLink.tsx
@@ -2,15 +2,17 @@ import { useActions } from 'kea'
 import { scrollspyCaptureLogic } from 'logic/scrollspyCaptureLogic'
 import React from 'react'
 import { Link } from 'react-scroll'
+import { useBreakpoint } from 'gatsby-plugin-breakpoints'
 
 export default function InternalSidebarLink({ url, name, depth, onClick, className = '', style = {} }) {
     const key = typeof window !== 'undefined' ? window.location.pathname : 'gatsby-ssr-context'
     const { reportScrollUpdated } = useActions(scrollspyCaptureLogic({ key }))
-    // Remove HTML tags from the name and trim
+    const breakpoints = useBreakpoint()
 
     return (
         <span className="block" style={{ marginLeft: `${depth / 1.5}rem` }}>
             <Link
+                offset={breakpoints.md ? -56 : -108}
                 style={style}
                 smooth
                 duration={300}
