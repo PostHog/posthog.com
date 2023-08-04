0cf772d0779e06cbb83fa4f440e593d8816581b8
Eli Kinsey <eli@ekinsey.dev>
2023-08-04 11:32:47 -0700

    scroll

--- src/components/Home/Timeline.js
+++ src/components/Home/Timeline.js
@@ -1,9 +1,8 @@
-import { CallToAction } from 'components/CallToAction'
 import { graphql, useStaticQuery } from 'gatsby'
-import Link from 'components/Link'
-import { heading, section } from './classes'
+import { heading } from './classes'
 import groupBy from 'lodash.groupby'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { ArrowLeft, ArrowRight } from '@posthog/icons'
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 const categories = {
@@ -13,6 +12,7 @@ const categories = {
 }
 
 export default function Timeline() {
+    const listRef = useRef(null)
     const {
         allSqueakRoadmap: { nodes },
     } = useStaticQuery(graphql`
@@ -50,7 +50,7 @@ export default function Timeline() {
     )
 
     return (
-        <section className="px-4 mb-12 md:mb-20">
+        <section className="px-4 mb-12 md:mb-20 overflow-hidden">
             <h2 className={heading()}>
                 We ship <br className="sm:hidden" />
                 <span className="text-orange">weirdly fast</span>
@@ -73,77 +73,97 @@ export default function Timeline() {
                     </div>
                 </div>
             </div>
-
-            <div className="-mx-4 px-4 overflow-x-auto flex flex-nowrap gap-4">
-                {Object.keys(pastEvents).map((year) => {
-                    const pastMonths = groupBy(pastEvents[year], (node) => {
-                        const date = new Date(node.dateCompleted || node.projectedCompletion)
-                        return months[date.getUTCMonth()]
-                    })
-                    const futureQuarters = groupBy(futureEvents[year], (node) => {
-                        const date = node.dateCompleted || node.projectedCompletion
-                        return Math.floor(new Date(date).getUTCMonth() / 3 + 1)
-                    })
-                    return (
-                        <div key={year} className="w-[80vw] md:w-[90vw] max-w-5xl shrink-0 mb-4">
-                            <h4 className="text-2xl py-1 font-bold text-center">{year}</h4>
-                            <div className="p-4 bg-white dark:bg-dark border border-light dark:border-dark">
-                                <ul role="list" className="py-1 px-0 grid grid-cols-2 md:grid-cols-4 gap-4">
-                                    {Object.keys(pastMonths).map((month) => {
-                                        return (
-                                            <li
-                                                key={month}
-                                                className="timeline-entry bg-accent dark:bg-accent-dark list-none px-4 pb-4 text-center min-h-[6rem] md:min-h-[8rem] lg:min-h-[10rem]"
-                                            >
-                                                <p className="text-lg font-bold border-b border-light dark:border-dark capitalize py-2 mb-2">
-                                                    {month}
-                                                </p>
-                                                <ul className="m-0 p-0">
-                                                    {pastMonths[month].map(({ title, category }) => {
-                                                        return (
-                                                            <li
-                                                                key={title}
-                                                                className="relative list-none text-sm text-left pl-4 content-none before:inline-block before:absolute before:w-[10px] before:h-[10px] before:left-0 before:top-[5px] before:rounded-full before:mr-2 mt-1 first:mt-0"
-                                                                data-type={categories[category]}
-                                                            >
-                                                                {title}
-                                                            </li>
-                                                        )
-                                                    })}
-                                                </ul>
-                                            </li>
-                                        )
-                                    })}
-                                    {Object.keys(futureQuarters).map((quarter) => {
-                                        return (
-                                            <li
-                                                key={quarter}
-                                                className="timeline-entry list-none bg-accent dark:bg-accent-dark px-4 pb-4"
-                                            >
-                                                <p className="text-lg font-bold border-b border-light dark:border-dark capitalize py-2 mb-2 text-center">
-                                                    Q{quarter}
-                                                </p>
-                                                <ul className="list-none m-0 p-0">
-                                                    {futureQuarters[quarter].map(({ title, category }) => {
-                                                        return (
-                                                            <li
-                                                                key={title}
-                                                                className="flex-auto relative text-[14px] text-left pl-4 content-none before:inline-block before:absolute before:w-[10px] before:h-[10px] before:left-0 before:top-[5px] before:rounded-full before:mr-2 mt-1 first:mt-0"
-                                                                data-type={categories[category]}
-                                                            >
-                                                                {title}
-                                                            </li>
-                                                        )
-                                                    })}
-                                                </ul>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
+            <div className="relative -mx-4 pr-4">
+                <div className="absolute z-20 top-1/2 left-0 -translate-y-1/2">
+                    <button
+                        onClick={() => listRef?.current?.scrollBy({ left: -50, behavior: 'smooth' })}
+                        className="relative hover:scale-[1.01] hover:top-[-1px] active:top-[.5px] active:scale-[.99] md:z-30 px-2 py-8"
+                    >
+                        <ArrowLeft className="w-10" />
+                    </button>
+                </div>
+                <div
+                    ref={listRef}
+                    className="-mr-4 px-4 snap-x snap-mandatory flex flex-nowrap gap-4 overflow-auto relative"
+                >
+                    {Object.keys(pastEvents).map((year, index) => {
+                        const pastMonths = groupBy(pastEvents[year], (node) => {
+                            const date = new Date(node.dateCompleted || node.projectedCompletion)
+                            return months[date.getUTCMonth()]
+                        })
+                        const futureQuarters = groupBy(futureEvents[year], (node) => {
+                            const date = node.dateCompleted || node.projectedCompletion
+                            return Math.floor(new Date(date).getUTCMonth() / 3 + 1)
+                        })
+                        return (
+                            <div key={year} className="w-[80vw] md:w-[90vw] max-w-5xl shrink-0 mb-4 snap-center">
+                                <h4 className="text-2xl py-1 font-bold text-center">{year}</h4>
+                                <div className="p-4 bg-white dark:bg-dark border border-light dark:border-dark">
+                                    <ul role="list" className="py-1 px-0 grid grid-cols-2 md:grid-cols-4 gap-4">
+                                        {Object.keys(pastMonths).map((month) => {
+                                            return (
+                                                <li
+                                                    key={month}
+                                                    className="timeline-entry bg-accent dark:bg-accent-dark list-none px-4 pb-4 text-center min-h-[6rem] md:min-h-[8rem] lg:min-h-[10rem]"
+                                                >
+                                                    <p className="text-lg font-bold border-b border-light dark:border-dark capitalize py-2 mb-2">
+                                                        {month}
+                                                    </p>
+                                                    <ul className="m-0 p-0">
+                                                        {pastMonths[month].map(({ title, category }) => {
+                                                            return (
+                                                                <li
+                                                                    key={title}
+                                                                    className="relative list-none text-sm text-left pl-4 content-none before:inline-block before:absolute before:w-[10px] before:h-[10px] before:left-0 before:top-[5px] before:rounded-full before:mr-2 mt-1 first:mt-0"
+                                                                    data-type={categories[category]}
+                                                                >
+                                                                    {title}
+                                                                </li>
+                                                            )
+                                                        })}
+                                                    </ul>
+                                                </li>
+                                            )
+                                        })}
+                                        {Object.keys(futureQuarters).map((quarter) => {
+                                            return (
+                                                <li
+                                                    key={quarter}
+                                                    className="timeline-entry list-none bg-accent dark:bg-accent-dark px-4 pb-4"
+                                                >
+                                                    <p className="text-lg font-bold border-b border-light dark:border-dark capitalize py-2 mb-2 text-center">
+                                                        Q{quarter}
+                                                    </p>
+                                                    <ul className="list-none m-0 p-0">
+                                                        {futureQuarters[quarter].map(({ title, category }) => {
+                                                            return (
+                                                                <li
+                                                                    key={title}
+                                                                    className="flex-auto relative text-[14px] text-left pl-4 content-none before:inline-block before:absolute before:w-[10px] before:h-[10px] before:left-0 before:top-[5px] before:rounded-full before:mr-2 mt-1 first:mt-0"
+                                                                    data-type={categories[category]}
+                                                                >
+                                                                    {title}
+                                                                </li>
+                                                            )
+                                                        })}
+                                                    </ul>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
+                <div className="absolute top-1/2 right-0 -translate-y-1/2">
+                    <button
+                        onClick={() => listRef?.current?.scrollBy({ left: 50, behavior: 'smooth' })}
+                        className="relative hover:scale-[1.01] hover:top-[-1px] active:top-[.5px] active:scale-[.99] md:z-30 px-2 py-8"
+                    >
+                        <ArrowRight className="w-10" />
+                    </button>
+                </div>
             </div>
         </section>
     )
