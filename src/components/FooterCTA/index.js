d2c6568e76e430253bcc932f39deb7d3fba2c8b4
Cory Watilo <cww@watilo.com>
2023-09-14 14:12:39 -0400

    don't block text with graphic

--- src/components/FooterCTA/index.js
+++ src/components/FooterCTA/index.js
@@ -7,13 +7,13 @@ import blurb from './images/blurb.svg'
 
 export default function FooterCTA() {
     return (
-        <div className="flex py-12 px-6 sm:px-12 md:pr-24 lg:pr-6 xl:pr-24 text-primary dark:text-primary-dark bg-accent dark:bg-accent-dark border border-light dark:border-dark items-center dark rounded-[10px] mt-6 md:mt-12 relative">
+        <div className="flex pt-16 pb-12 px-6 sm:px-12 md:pr-24 lg:pr-6 xl:pr-24 text-primary dark:text-primary-dark bg-accent dark:bg-accent-dark border border-light dark:border-dark items-center dark rounded-[10px] mt-6 md:mt-12 relative">
             <div className="w-full text-center sm:text-left">
                 <h2 className="text-5xl">Try it free.</h2>
                 <p className="w-">It takes less than 5 minutes.</p>
                 <div className="flex flex-col sm:flex-row sm:space-x-2 space-y-2 sm:space-y-0 items-center">
                     <SignupCTA className="w-full sm:w-48" size="md" />
-                    <CallToAction to="/book-a-demo" type="secondary" size="md" className="">
+                    <CallToAction to="/book-a-demo" type="secondary" size="md">
                         Schedule a demo
                     </CallToAction>
                 </div>
