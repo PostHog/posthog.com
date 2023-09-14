e2fb2069f92b23b1fc116b11d39c79fe0e3f8fb5
Cory Watilo <cww@watilo.com>
2023-09-14 13:44:15 -0400

    match postcard changes in customers view

--- src/components/Edition/Views/Customers.tsx
+++ src/components/Edition/Views/Customers.tsx
@@ -22,15 +22,15 @@ const Row = ({ title, featuredImage, date, excerpt, slug, fetchMore }) => {
         }
     }, [inView])
     return (
-        <div ref={fetchMore ? ref : null}>
+        <div className="h-full" ref={fetchMore ? ref : null}>
             <Link
-                className="!text-inherit p-3 border border-transparent hover:border-border dark:hover:border-dark rounded-md hover:bg-accent dark:hover:bg-accent-dark transition-colors block h-full relative active:top-[1px] active:scale-[.99]"
+                className="!text-inherit py-2 md:p-3 font-normal border border-transparent hover:border-border dark:hover:border-dark rounded-md hover:bg-accent dark:hover:bg-accent-dark hover:scale-[1.01] transition-colors block h-full relative active:top-[1px] active:scale-[.99]"
                 to={slug}
             >
-                <div className="w-full aspect-video rounded-md overflow-hidden">
+                <div className="w-full aspect-video rounded-md overflow-hidden bg-accent dark:bg-accent-dark">
                     <img className="w-full h-full object-cover" src={imageURL || '/banner.png'} />
                 </div>
-                <p className="m-0 text-sm mt-2">{postDate}</p>
+                <p className="m-0 text-sm mt-3 opacity-75">{postDate}</p>
                 <h3 className="my-1 text-xl leading-tight">{title}</h3>
             </Link>
         </div>
