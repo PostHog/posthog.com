3ce5172260f4258492f21f89f1a8766dbd27c37e
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2023-09-26 09:38:21 -0700

    Add link and sidebar

--- src/navs/index.js
+++ src/navs/index.js
@@ -2359,6 +2359,10 @@ export const docsMenu = {
                             name: 'BigQuery',
                             url: '/docs/cdp/batch-exports/bigquery',
                         },
+                        {
+                            name: 'Postgres',
+                            url: '/docs/cdp/batch-exports/postgres',
+                        },
                     ],
                 },
                 {
