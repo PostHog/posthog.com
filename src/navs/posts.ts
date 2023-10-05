dbcb86f9926b01c8db1e470f8ca421944339cef3
Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>
2023-10-05 13:40:28 +0100

    Updating posts sidebar

--- src/navs/posts.ts
+++ src/navs/posts.ts
@@ -2,23 +2,155 @@ import { IMenu } from 'components/PostLayout/types'
 
 export const postsMenu: IMenu[] = [
     {
-        name: 'Blog',
-        url: '/blog',
+        name: 'Founders',
+        url: '/founders',
         children: [
             {
-                name: 'Product updates',
-                url: '/blog/product-updates',
+                name: 'Being a founder',
+                url: '/founders/being-a-founder',
+            },
+            {
+                name: 'Culture',
+                url: '/founders/culture',
+            },
+            {
+                name: 'Fundraising',
+                url: '/founders/fundraising',
+            },
+            {
+                name: 'Growth',
+                url: '/founders/growth',
+            },
+            {
+                name: 'Marketing',
+                url: '/founders/marketing',
+            },
+            {
+                name: 'Ops & finance',
+                url: '/founders/ops-finance',
+            },
+            {
+                name: 'People',
+                url: '/founders/people',
+            },
+            {
+                name: 'Product',
+                url: '/founders/product',
+            },
+            {
+                name: 'Product-market fit',
+                url: '/founders/product-market-fit',
+            },
+            {
+                name: 'Revenue',
+                url: '/founders/revenue',
+            },
+            {
+                name: 'Sales & CS',
+                url: '/founders/sales-customer-success',
             },
         ],
     },
     {
-        name: 'Guides',
+        name: 'Product engineers',
+        url: '/product-engineers',
+        children: [
+            {
+                name: 'AB testing',
+                url: '/product-engineers/ab-testing',
+            },
+            {
+                name: 'Feature management',
+                url: '/product-engineers/feature-management',
+            },
+            {
+                name: 'Growth engineering',
+                url: '/product engineers/growth',
+            },
+            {
+                name: 'Product analytics',
+                url: '/product-engineers/product-analytics',
+            },
+            {
+                name: 'User research',
+                url: '/product-engineers/user-research',
+            },
+            {
+                name: 'Engineering',
+                url: '/product-engineers/engineering',
+            },
+        ],
+    },
+    {
+        name: 'Blog',
+        url: '/blog/',
+        children: [
+            {
+                name: 'CEO diaries',
+                url: '/blog/categories/ceo-diaries',
+            },
+            {
+                name: 'PostHog news',
+                url: '/blog/categories/posthog-news',
+            },
+            {
+                name: 'Inside PostHog',
+                url: '/blog/categories/inside-posthog',
+            },
+        ],
+    },
+    {
+        name: 'Changelog',
+        url: '/changelog/',
+    },
+    {
+        name: 'Customer stories',
+        url: '/customers/',
+    },
+    {
+        name: 'Guides & tutorials',
         url: '/tutorials',
         children: [
             {
-                name: 'Actions',
-                url: '/tutorials/actions',
+                name: 'Product OS',
+                url: '/tutorials/product-os',
+            },
+            {
+                name: 'Product analytics',
+                url: '/tutorials/product-analytics',
+            },
+            {
+                name: 'Session replay',
+                url: '/tutorials/session-replay',
+            },
+            {
+                name: 'Feature flags',
+                url: '/tutorials/feature-flags',
+            },
+            {
+                name: 'A/B testing',
+                url: '/tutorials/ab-testing',
+            },
+            {
+                name: 'Surveys',
+                url: '/tutorials/surveys',
+            },
+            {
+                name: 'CDP',
+                url: '/tutorials/cdp',
             },
         ],
+    },
+    {
+        name: 'Newsletter',
+        url: '/newsletter/',
+    },
+    {
+        name: 'PostHog news',
+        url: '/blog/categories/posthog-news',
+    },
+    {
+        name: 'Spotlight',
+        url: '/blog/spotlight',
     },
 ]
