241bf123f52681eb899b6107188f4d522df70322
Cory Watilo <cww@watilo.com>
2023-10-21 15:01:12 +0200

    inline componentized "pairs with"

--- src/components/Product/SessionReplay/index.tsx
+++ src/components/Product/SessionReplay/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Layout from '../../Layout'
 import Link from 'components/Link'
 import { StaticImage } from 'gatsby-plugin-image'
-import { IconRewindPlay, IconMinus, IconPlus, IconGraph, IconCheck, IconX } from '@posthog/icons'
+import { IconRewindPlay, IconMinus, IconPlus, IconGraph, IconFlask, IconToggle } from '@posthog/icons'
 import { CallToAction } from 'components/CallToAction'
 import { CustomerCard } from 'components/Products/CustomerCard'
 import { Hero } from 'components/Products/Hero'
@@ -257,6 +257,54 @@ const Accordion = ({ children, label, active, initialOpen = false, className = '
     )
 }
 
+const WorksWith = ({ children, items }) => {
+    return (
+        <section className="mb-20">
+            <h3 className="text-3xl lg:text-4xl text-center">Pairs with...</h3>
+
+            <p className="text-center">PostHog products are natively designed to be interoperable using Product OS.</p>
+            <div className={`grid gap-8 md:grid-cols-${items}`}>{children}</div>
+        </section>
+    )
+}
+
+const WorksWithArray = [
+    {
+        icon: <IconGraph />,
+        product: 'Product analytics',
+        description: 'Jump into a playlist of session recordings directly from any time series in a graph',
+        url: '/product-analytics',
+    },
+    {
+        icon: <IconToggle />,
+        product: 'Feature flags',
+        description: "See which feature flags are enabled for a user's session",
+        url: '/feature-flags',
+    },
+    {
+        icon: <IconFlask />,
+        product: 'A/B testing',
+        description:
+            'Generate a playlist of recordings limited to an A/B test or specific group within a multivariate experiment.',
+        url: '/ab-testing',
+    },
+]
+
+const WorksWithItem = ({ icon, product, description, url }) => {
+    return (
+        <>
+            <Link
+                to={url}
+                className="bg-accent dark:bg-accent-dark rounded-md p-6 relative hover:top-[-1px] hover:scale-[1.01] active:top-[1px] active:scale-[1] transition-all"
+            >
+                <span className="inline-block w-8 opacity-50 text-primary dark:text-primary-dark">{icon}</span>
+                <h4 className="mt-2 mb-0">{product}</h4>
+                <p className="text-primary dark:text-primary-dark mb-0 text-[15px] opacity-75">{description}</p>
+            </Link>
+        </>
+    )
+}
+
 const DocLinks = ({ menu }) => {
     const organized = {}
     let currentMenu
@@ -273,16 +321,19 @@ const DocLinks = ({ menu }) => {
     const menuOrganized = Object.keys(organized)
 
     return (
-        <ul className={`list-none m-0 p-0 flex justify-center space-x-20`}>
+        <ul className={`list-none m-0 p-0 flex flex-col md:flex-row justify-center gap-4 md:gap-20`}>
             {menuOrganized.map((title) => {
                 return (
                     <li key={title}>
                         <p className="opacity-50 m-0 font-semibold">{title}</p>
-                        <ul className="list-none m-0 p-0 mt-2 gap-y-4 flex flex-col">
+                        <ul className="list-none m-0 p-0 mt-2 flex flex-col">
                             {organized[title].map(({ name, icon, color, url, badge }) => {
                                 return (
                                     <li key={name + url} to={url}>
-                                        <Link to={url}>
+                                        <Link
+                                            to={url}
+                                            className="flex items-center relative px-2 pt-1.5 pb-1 mb-1 rounded hover:bg-light/50 hover:dark:bg-dark/50 border border-b-3 border-transparent md:hover:border-light dark:md:hover:border-dark hover:translate-y-[-1px] active:translate-y-[1px] active:transition-all"
+                                        >
                                             <MenuItem badge={badge} color={color} icon={icon} name={name} />
                                         </Link>
                                     </li>
@@ -596,7 +647,7 @@ export const ProductSessionReplay = () => {
                 <section className="mb-20">
                     <h3 className="text-3xl lg:text-4xl text-center mb-0">Explore the docs</h3>
                     <p className="mt-0 opacity-70 text-center">
-                        Get a more technical overview of how everything works in our docs.
+                        Get a more technical overview of how everything works <Link to="/docs">in our docs</Link>.
                     </p>
                     <DocLinks menu={docsMenu.children[2].children} />
                 </section>
@@ -642,26 +693,12 @@ export const ProductSessionReplay = () => {
                     <Questions topicId={20} />
                 </section>
 
-                <section className="mb-20">
-                    <h3 className="text-3xl lg:text-4xl text-center">Pairs with...</h3>
+                <WorksWith items={3}>
+                    {WorksWithArray.map((card, index) => {
+                        return <WorksWithItem {...card} key={index} />
+                    })}
+                </WorksWith>
 
-                    <p className="text-center">
-                        PostHog products are natively designed to be interoperable using Product OS.
-                    </p>
-
-                    <div className="grid md:grid-cols-3 gap-8">
-                        <Link
-                            to="/product-analytics"
-                            className="bg-accent dark:bg-accent-dark rounded-md p-6 relative hover:top-[-1px] hover:scale-[1.01] active:top-[1px] active:scale-[1] transition-all"
-                        >
-                            <IconGraph className="w-8 opacity-50 text-primary dark:text-primary-dark" />
-                            <h4 className="mt-2 mb-0">Product analytics</h4>
-                            <p className="text-primary dark:text-primary-dark mb-0 text-[15px]">
-                                Jump into a playlist of session recordings directly from any time series in a graph
-                            </p>
-                        </Link>
-                    </div>
-                </section>
                 <section className="mb-20">
                     <CTA />
                 </section>
