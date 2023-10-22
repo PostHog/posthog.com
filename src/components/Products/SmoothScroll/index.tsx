1a08a7305de1ca41a12199c15f7900e1f9fa01f7
Eli Kinsey <eli@ekinsey.dev>
2023-10-22 16:58:03 -0700

    initial active tab

--- src/components/Products/SmoothScroll/index.tsx
+++ src/components/Products/SmoothScroll/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-scroll'
 
 const menuItems: { label: string; id: string }[] = [
@@ -23,6 +23,7 @@ const menuItems: { label: string; id: string }[] = [
 ]
 
 export const SmoothScroll = ({ className = '', exclude = [] }): JSX.Element => {
+    const [activeTab, setActiveTab] = useState(0)
     return (
         <section
             className={`hidden md:block sticky top-[-1px] reasonable:top-[107px] bg-accent dark:bg-accent-dark border-y border-border dark:border-border-dark z-50 mb-12 ${className}`}
@@ -30,18 +31,22 @@ export const SmoothScroll = ({ className = '', exclude = [] }): JSX.Element => {
             <ul className="list-none flex gap-4 justify-center pt-1">
                 {menuItems
                     .filter(({ label }) => !exclude.includes(label))
-                    .map(({ label, id }) => {
+                    .map(({ label, id }, index) => {
                         return (
                             <li key={id}>
                                 <Link
                                     offset={-169}
-                                    className="!text-inherit inline-block text-sm py-2 px-3 border border-transparent border-b-transparent -mb-px text-opacity-60 hover:border hover:border-light hover:dark:border-dark hover:bg-light hover:dark:bg-dark hover:rounded-tl-sm hover:rounded-tr-md cursor-pointer"
+                                    className={`!text-inherit inline-block text-sm py-2 px-3 border border-transparent border-b-transparent -mb-px text-opacity-60 hover:border hover:border-light hover:dark:border-dark hover:bg-light hover:dark:bg-dark hover:rounded-tl-sm hover:rounded-tr-md cursor-pointer ${
+                                        index === activeTab
+                                            ? '!border-border dark:!border-border-dark !border-b-transparent dark:!border-b-transparent font-bold bg-light dark:bg-dark rounded-tl-sm rounded-tr-md'
+                                            : ''
+                                    }`}
                                     smooth
                                     duration={300}
                                     to={id}
                                     hashSpy
                                     spy
-                                    activeClass="!border-border dark:!border-border-dark !border-b-transparent dark:!border-b-transparent font-bold bg-light dark:bg-dark rounded-tl-sm rounded-tr-md"
+                                    onSetActive={() => setActiveTab(index)}
                                 >
                                     {label}
                                 </Link>
