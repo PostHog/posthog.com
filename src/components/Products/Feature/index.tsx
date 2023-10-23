2d13e4ed90e68bc82e593ccc9b7931d4da6a54d5
Cory Watilo <cww@watilo.com>
2023-10-23 08:36:08 +0100

    updated survey screenshots

--- src/components/Products/Feature/index.tsx
+++ src/components/Products/Feature/index.tsx
@@ -10,7 +10,7 @@ interface FeatureProps {
 export const Feature = ({ title, description, image }: FeatureProps): JSX.Element => {
     return (
         <li className="text-center">
-            <div className="mb-2 w-full relative after:absolute after:w-full after:h-24 after:bottom-0 after:left-0 after:bg-gradient-to-b after:from-transparent dark:after:via-dark/80 dark:after:to-dark after:via-light/80 after:to-light after:z-10">
+            <div className="bg-accent dark:bg-accent-dark rounded mb-2 w-full relative after:absolute after:w-full after:h-24 after:bottom-0 after:left-0 after:bg-gradient-to-b after:from-transparent dark:after:via-dark/80 dark:after:to-dark after:via-light/80 after:to-light after:z-10">
                 {image}
             </div>
             <h4 className="mb-0 text-lg">{title}</h4>
