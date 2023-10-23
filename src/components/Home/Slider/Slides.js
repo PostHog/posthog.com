17beefb4e9c475dcbc09a047a0d59824c7e1ca01
Cory Watilo <cww@watilo.com>
2023-10-23 14:40:30 +0100

    updated surveys homepage slide image

--- src/components/Home/Slider/Slides.js
+++ src/components/Home/Slider/Slides.js
@@ -387,7 +387,7 @@ export const Surveys = () => {
                             quality={100}
                             objectFit="contain"
                             className="w-full h-full py-4 md:py-10 max-h-96"
-                            src="../../Product/surveys/images/survey-hog.png"
+                            src="./images/surveys-hog.png"
                         />
                     </div>
                 </ImageContainer>
