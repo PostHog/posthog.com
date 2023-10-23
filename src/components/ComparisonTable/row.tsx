c3e2aa00c5bf1e922e949917ec588d1b6844cda6
Cory Watilo <cww@watilo.com>
2023-10-23 18:26:46 +0100

    converted amplitude vs posthog

--- src/components/ComparisonTable/row.tsx
+++ src/components/ComparisonTable/row.tsx
@@ -10,7 +10,7 @@ export const ComparisonRow = ({ feature, description, column1, column2 }) => {
                 <p className="!mb-0 pb-0.5 !leading-tight">
                     <strong>{feature}</strong>
                 </p>
-                <p className="!mb-0 !text-sm text-opacity-75 leading-none">{description}</p>
+                {description && <p className="!mb-0 !text-sm text-opacity-75 leading-none">{description}</p>}
             </td>
             <td className="text-center">{typeof column1 === 'string' ? column1 : column1 ? <True /> : <False />}</td>
             <td className="text-center">{typeof column2 === 'string' ? column2 : column2 ? <True /> : <False />}</td>
