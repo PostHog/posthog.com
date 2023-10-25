282f889db897bbefea5846dfbbc58671daa24f20
Cory Watilo <cww@watilo.com>
2023-10-25 13:47:50 +0100

    Client post fixes (#7024)
    * split post markdown component from question componnt
    
    * open links in new window
    
    * more padding at end of post

--- src/components/Edition/ClientPost.tsx
+++ src/components/Edition/ClientPost.tsx
@@ -1,6 +1,6 @@
 import { CallToAction } from 'components/CallToAction'
 import Modal from 'components/Modal'
-import Markdown from 'components/Squeak/components/Markdown'
+import ClientPostMarkdown from 'components/Squeak/components/ClientPostMarkdown'
 import { ZoomImage } from 'components/ZoomImage'
 import SEO from 'components/seo'
 import dayjs from 'dayjs'
@@ -79,7 +79,7 @@ export default function ClientPost({
             </Modal>
             <SEO title={title + ' - PostHog'} />
             <article
-                className={`article-content mx-auto transition-all pb-6 md:py-6 md:px-8 2xl:px-12 ${
+                className={`article-content mx-auto transition-all pb-6 md:py-6 md:pb-20 md:px-8 2xl:px-12 ${
                     fullWidthContent ? 'max-w-full' : 'max-w-2xl'
                 }`}
             >
@@ -116,10 +116,10 @@ export default function ClientPost({
                     </p>
                 </div>
                 <div className="my-2 article-content">
-                    <Markdown>{body}</Markdown>
+                    <ClientPostMarkdown>{body}</ClientPostMarkdown>
                 </div>
                 {CTA?.label && CTA?.url && (
-                    <CallToAction size="md" type="outline" to={CTA.url}>
+                    <CallToAction size="md" type="outline" externalNoIcon to={CTA.url}>
                         {CTA.label}
                     </CallToAction>
                 )}
