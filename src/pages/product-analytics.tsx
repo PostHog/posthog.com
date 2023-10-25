aa740d5d94fd55bf9f080d045d222ffa9dbdb43d
Eli Kinsey <eli@ekinsey.dev>
2023-10-25 07:28:11 -0700

    Make wide mode actually work on product pages

--- src/pages/product-analytics.tsx
+++ src/pages/product-analytics.tsx
@@ -1,2 +1,10 @@
+import React from 'react'
+import Layout from 'components/Layout'
 import ProductProductAnalytics from 'components/Product/ProductAnalytics'
-export default ProductProductAnalytics
+export default function ProductAnalytics(): JSX.Element {
+    return (
+        <Layout>
+            <ProductProductAnalytics />
+        </Layout>
+    )
+}
