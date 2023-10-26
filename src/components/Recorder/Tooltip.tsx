3f4b04ad46b6d78cc060eca97b0be02710cb6a67
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-10-26 13:19:49 +0200

    adjust toolbar styling

--- src/components/Recorder/Tooltip.tsx
+++ src/components/Recorder/Tooltip.tsx
@@ -12,18 +12,18 @@ const Tooltip = ({ title, children }: Props): React.ReactElement => {
         <TooltipPrimitive.Provider delayDuration={0} disableHoverableContent>
             <TooltipPrimitive.Root>
                 <TooltipPrimitive.Trigger asChild>
-                    <div className="group radix-state-delayed-open:bg-gray-50 radix-state-instant-open:bg-gray-50 radix-state-on:bg-gray-900 radix-state-open:bg-gray-900">
+                    <div className="group radix-state-delayed-open:bg-[#f9fafb] radix-state-instant-open:bg-[#f9fafb] radix-state-on:bg-[#111827] radix-state-open:bg-[#111827]">
                         {children}
                     </div>
                 </TooltipPrimitive.Trigger>
                 <TooltipPrimitive.Content
                     sideOffset={4}
                     className={
-                        'radix-side-top:animate-slide-down-fade radix-side-right:animate-slide-left-fade radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade inline-flex items-center rounded-md bg-gray-800 px-4 py-2.5'
+                        'radix-side-top:animate-slide-down-fade radix-side-right:animate-slide-left-fade radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade inline-flex items-center rounded-md bg-[#1f2937] px-4 py-2.5'
                     }
                 >
-                    <TooltipPrimitive.Arrow className="fill-current text-gray-800" />
-                    <span className="block text-xs leading-none text-gray-100">{title}</span>
+                    <TooltipPrimitive.Arrow className="fill-current text-[#1f2937]" />
+                    <span className="block text-xs leading-none text-[#f3f4f6]">{title}</span>
                 </TooltipPrimitive.Content>
             </TooltipPrimitive.Root>
         </TooltipPrimitive.Provider>
