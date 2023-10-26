e55795d9ff094c8df1da84d1c7755119998996cf
Cory Watilo <cww@watilo.com>
2023-10-26 11:29:56 +0100

    Update src/components/Edition/Intro.tsx
    Co-authored-by: Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>

--- src/components/Edition/Intro.tsx
+++ src/components/Edition/Intro.tsx
@@ -7,15 +7,15 @@ const Founders = () => {
         <div className="bg-accent dark:bg-accent-dark px-6 py-4 mt-8 md:mt-0 rounded border border-light dark:border-dark">
             <h2 className="text-lg mb-2">Welcome to our founder's hub</h2>
             <p className="text-sm">
-                These are the best pieces of content we can find on the internet for all the major disciplines you need
-                to build a successful company around your product – and what we've learned from each link along the way.
+                We've curated the best advice in all the major disciplines you need
+                to build a successful company – and what we've learned from building PostHog to product-market fit and beyond.
             </p>
             <p className="text-sm mb-0">
                 Long term, we want{' '}
                 <Link to="https://github.com/PostHog/posthog.com/issues/7011">
                     anyone to be able to post stuff here
                 </Link>
-                . In the meantime, if we are missing a topic you’d like to learn more about –
+                . In the meantime, if we're missing a topic you’d like to learn more about, 
                 <Link to="https://x.com/posthog" external>
                     tell us on X
                 </Link>
