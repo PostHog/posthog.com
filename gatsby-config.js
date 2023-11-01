0e28cf0e80a5dbb253fd718eb1ce0f85b3687e58
kevin frilot <kevinfrilot@icloud.com>
2023-11-01 11:49:11 +0700

    feat(shopify): add gatsby-source-shopify, source shopify products

--- gatsby-config.js
+++ gatsby-config.js
@@ -405,6 +405,16 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: 'gatsby-source-shopify',
+            options: {
+                password: process.env.SHOPIFY_APP_PASSWORD,
+                storeUrl: process.env.GATSBY_MYSHOPIFY_URL,
+                downloadImages: true,
+
+                // salesChannel: process.env.SHOPIFY_APP_ID, // Optional but recommended
+            },
+        },
         ...(!process.env.GATSBY_ALGOLIA_APP_ID || !process.env.ALGOLIA_API_KEY || !process.env.GATSBY_ALGOLIA_INDEX_NAME
             ? []
             : [algoliaConfig]),
