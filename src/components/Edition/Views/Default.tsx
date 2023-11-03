65845af3777b68ea453549bfa5efdc6c9d51004f
Eli Kinsey <eli@ekinsey.dev>
2023-11-03 09:18:16 -0700

    improved client post pages

--- src/components/Edition/Views/Default.tsx
+++ src/components/Edition/Views/Default.tsx
@@ -374,7 +374,7 @@ function PostsListing() {
                 transition-all 
                 ${
                     articleView
-                        ? 'flex flex-col h-[calc(100vh_-_108px)] sticky top-0 z-10 reasonable:top-[108px] w-full md:w-[20rem] lg:w-[24rem] flex-shrink-0 border-r border-border dark:border-dark 2xl:border-l'
+                        ? 'flex flex-col h-auto sticky top-0 z-10 reasonable:top-[108px] w-full md:w-[20rem] lg:w-[24rem] flex-shrink-0 border-r border-border dark:border-dark 2xl:border-l'
                         : 'flex-grow md:px-8 2xl:px-12'
                 }
             `}
