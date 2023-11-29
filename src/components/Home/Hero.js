26e69fb6a81056f4a04979344a9a43f3af29d1cb
Eli Kinsey <eli@ekinsey.dev>
2023-11-29 13:53:47 -0800

    add back exported components

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -19,6 +19,39 @@ const ctaVariants = {
     },
 }
 
+export const FeatureStrip = ({ className = '' }) => {
+    return (
+        <div className="text-center mt-0 mb-4">
+            <ul
+                className={`list-none m-0 p-0 pb-2 inline-grid mx-auto grid-cols-3 md:grid-cols-5 justify-start gap-y-0 md:gap-y-4 md:gap-x-1 ${className}`}
+            >
+                <Feature icon="analytics" title="Product analytics" url="/product/#product-analytics" />
+                <Feature icon="session-recording" title="Session recording" url="/product/session-recording" />
+                <Feature icon="feature-flags" title="Feature flags" url="/product/feature-flags" />
+                <Feature icon="heatmaps" title="Heatmaps" url="/product/heatmaps" />
+                <Feature icon="experiments" title="Experiments" url="/product/experimentation-suite" />
+            </ul>
+            <p className="mt-4 text-sm">
+                Plus 50-ish apps available in the <a href="/apps">PostHog App Store</a>
+            </p>
+        </div>
+    )
+}
+
+const Feature = ({ title, icon, url }) => {
+    return (
+        <li className="w-24">
+            <a
+                href={url}
+                className="flex flex-col py-4 px-6 h-full space-y-1 font-semibold items-center justify-start text-black hover:text-black rounded hover:bg-gray-accent-light"
+            >
+                <Icon className="w-5 h-5 mr-1 md:mr-0" name={icon} />
+                <div className="text-[14px] lg:text-[15px] mt-2 leading-tight">{title}</div>
+            </a>
+        </li>
+    )
+}
+
 export default function Hero() {
     return (
         <section className="flex flex-col justify-center items-center">
