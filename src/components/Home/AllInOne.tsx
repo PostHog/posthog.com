cfff0a080c5e53afd053bf1397624eaf736a0097
Eli Kinsey <eli@ekinsey.dev>
2023-12-05 13:28:18 -0800

    responsiveness

--- src/components/Home/AllInOne.tsx
+++ src/components/Home/AllInOne.tsx
@@ -31,9 +31,9 @@ const HogZilla = () => {
 export default function AllInOne() {
     return (
         <section className="relative mb-12">
-            <div className="md:absolute top-0 left-0 lg:top-24 lg:left-0 xl:left-8 max-w-md mx-auto lg:mx-0 mt-12 lg:mt-0 lg:max-w-2xl z-50">
-                <h2 className="m-0 text-4xl px-4 md:text-6xl text-center md:text-left leading-tight md:leading-none">
-                    PostHog is 7+ tools <br className="xl:hidden" />
+            <div className="md:absolute top-0 left-0 md:top-0 lg:top-8 xl:top-24 lg:left-0 xl:left-8 max-w-md mx-auto lg:mx-0 md:mt-12 lg:mt-0 lg:max-w-2xl z-50 md:mb-0 mb-8">
+                <h2 className="m-0 text-4xl px-4 lg:text-6xl text-center md:text-left leading-tight md:leading-none">
+                    PostHog is 7+ tools <br className="hidden lg:inline-block xl:hidden" />
                     in one
                 </h2>
                 <p className="text-center md:text-left px-4 m-0 mt-1 font-semibold text-primary/75 dark:text-primary-dark/75 text-base sm:text-lg">
