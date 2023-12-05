898500133269890b00a7ae176ad66f8e897a4e56
Eli Kinsey <eli@ekinsey.dev>
2023-12-05 13:49:40 -0800

    just use css

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,23 +1,13 @@
 import React from 'react'
-import { CallToAction, TrackedCTA } from '../CallToAction'
-import { Link } from 'gatsby'
+import { TrackedCTA } from '../CallToAction'
 import { heading, section } from './classes'
 import Icon from './Icon'
 import Slider from './Slider'
 import { SignupCTA } from 'components/SignupCTA'
 import Accordion from './Accordion'
-import { motion } from 'framer-motion'
 import './hero.scss'
 
 const heroTitle = 'How engineers build better products'
-const ctaVariants = {
-    hidden: {
-        opacity: 0,
-    },
-    visible: {
-        opacity: 1,
-    },
-}
 
 export const FeatureStrip = ({ className = '' }) => {
     return (
@@ -69,43 +59,20 @@ export default function Hero() {
                             </span>
                         ))}
                     </h1>
-                    <motion.h2
-                        transition={{ delay: 0.8, duration: 1.5 }}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        className={heading('subtitle', 'primary', 'mt-0 mb-6')}
-                    >
+                    <h2 className={`${heading('subtitle', 'primary', 'mt-0 mb-6')} home-hero-subtitle`}>
                         The single platform to analyze, test, observe, and deploy new features
-                    </motion.h2>
-
-                    <motion.div
-                        initial="hidden"
-                        animate="visible"
-                        variants={{
-                            hidden: {
-                                opacity: 0,
-                            },
-                            visible: {
-                                opacity: 1,
-                            },
-                        }}
-                        transition={{ delay: 1.5, staggerChildren: 2 }}
-                        className="flex justify-center items-center gap-2"
-                    >
-                        <motion.div variants={ctaVariants} transition={{ duration: 5 }}>
-                            <SignupCTA className="" />
-                        </motion.div>
-                        <motion.div variants={ctaVariants} transition={{ duration: 2 }}>
-                            <TrackedCTA
-                                event={{ name: `clicked Get a demo` }}
-                                href="/book-a-demo"
-                                type="secondary"
-                                size="lg"
-                            >
-                                Get a demo
-                            </TrackedCTA>
-                        </motion.div>
-                    </motion.div>
+                    </h2>
+                    <div className="flex justify-center items-center gap-2 home-hero-cta">
+                        <SignupCTA />
+                        <TrackedCTA
+                            event={{ name: `clicked Get a demo` }}
+                            href="/book-a-demo"
+                            type="secondary"
+                            size="lg"
+                        >
+                            Get a demo
+                        </TrackedCTA>
+                    </div>
                 </div>
                 <Slider />
                 <Accordion />
