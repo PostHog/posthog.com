90064692467284f67548fc506a468514650c8a63
Eli Kinsey <eli@ekinsey.dev>
2023-12-19 22:02:41 -0800

    wrap in nav

--- src/components/Edition/Breadcrumbs.tsx
+++ src/components/Edition/Breadcrumbs.tsx
@@ -11,22 +11,24 @@ type Props = {
 export default function Breadcrumbs({ category, tags }: Props): JSX.Element {
     const hasTags = tags && tags.length > 0
     return (
-        <ul className="m-0 p-0 !mb-2 flex space-x-2 list-none items-center whitespace-nowrap">
-            <li className={`after:content-['→'] after:ml-2`}>
-                <Link className="text-base" to={`https://posthog.com/posts`}>
-                    Posts
-                </Link>
-            </li>
-            <li className={`${hasTags ? `after:content-['→'] after:ml-2` : ''} overflow-hidden text-ellipsis`}>
-                <Link className="text-base" to={`/${slugify(category, { lower: true })}`}>
-                    {category}
-                </Link>
-            </li>
-            {hasTags && (
-                <li className="!text-base font-semibold overflow-hidden text-ellipsis">
-                    {tags.map((tag) => capitalize(tag)).join(', ')}
+        <nav>
+            <ul className="m-0 p-0 !mb-2 flex space-x-2 list-none items-center whitespace-nowrap">
+                <li className={`after:content-['→'] after:ml-2`}>
+                    <Link className="text-base" to={`https://posthog.com/posts`}>
+                        Posts
+                    </Link>
                 </li>
-            )}
-        </ul>
+                <li className={`${hasTags ? `after:content-['→'] after:ml-2` : ''} overflow-hidden text-ellipsis`}>
+                    <Link className="text-base" to={`/${slugify(category, { lower: true })}`}>
+                        {category}
+                    </Link>
+                </li>
+                {hasTags && (
+                    <li className="!text-base font-semibold overflow-hidden text-ellipsis">
+                        {tags.map((tag) => capitalize(tag)).join(', ')}
+                    </li>
+                )}
+            </ul>
+        </nav>
     )
 }
