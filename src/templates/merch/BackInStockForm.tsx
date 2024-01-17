b43ae1c4a81a9ea811e97897005a340fb1aa2b64
Kevin Frilot <kevinfrilot@icloud.com>
2024-01-17 16:13:37 +0700

    feat(merch): add back in stock form styling and logic

--- src/templates/merch/BackInStockForm.tsx
+++ src/templates/merch/BackInStockForm.tsx
@@ -16,6 +16,8 @@ export function BackInStockForm(props: BackInStockFormProps): React.ReactElement
     const { className } = props
     const [email, setEmail] = useState('')
     const [emailError, setEmailError] = useState('')
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+    const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
 
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
@@ -23,23 +25,51 @@ export function BackInStockForm(props: BackInStockFormProps): React.ReactElement
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        if (!validateEmail(email)) {
-            setEmailError('Invalid email format')
-        } else {
-            setEmailError('')
-            // handle form submission logic here
-            console.log(`Form submitted with: ${email}`)
-        }
+        setIsSubmitting(true)
+
+        setTimeout(() => {
+            if (!validateEmail(email)) {
+                setEmailError('Invalid email format')
+                setIsSubmitting(false)
+            } else {
+                setEmailError('')
+                setIsSubmitting(false)
+                setIsSubmitted(true)
+                // handle form submission logic here
+                console.log(`Form submitted with: ${email}`)
+            }
+        }, 1000)
     }
 
-    const classes = cn('', className)
+    const classes = cn('flex gap-4', className)
 
-    // TODO add "submitting" & "submitted" states when posting to upcoming endpoint
+    if (isSubmitted) {
+        return <div>Submitted! Get back to you soon!</div>
+    }
+
     return (
-        <form className={classes} onSubmit={handleSubmit}>
-            <input type="text" placeholder="Email Address" value={email} onChange={handleEmailChange} />
-            {emailError && <div style={{ color: 'red' }}>{emailError}</div>}
-            <button type="submit">Submit</button>
-        </form>
+        <>
+            <h3>Get notified!</h3>
+            <p>Sign up and we'll get back to you as soon as this product is back in stock</p>
+            <form className={classes} onSubmit={handleSubmit}>
+                <div className="px-4 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue sm:max-w-md">
+                    <input
+                        type="text"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={handleEmailChange}
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                </div>
+
+                {emailError && <div style={{ color: 'red' }}>{emailError}</div>}
+                <button
+                    type="submit"
+                    className="flex-shrink-0 rounded-md bg-blue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue"
+                >
+                    {isSubmitting ? 'Submitting...' : 'Submit'}
+                </button>
+            </form>
+        </>
     )
 }
