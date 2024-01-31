4f0d1a949c7bd11fe6f3b90b0e0564c4156c6984
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2024-01-31 16:40:52 +0000

    Update yc-onboarding.tsx

--- src/pages/yc-onboarding.tsx
+++ src/pages/yc-onboarding.tsx
@@ -62,7 +62,7 @@ export const YCOnboarding = () => {
                                 <iframe
                                     width="560"
                                     height="315"
-                                    src="https://www.youtube.com/embed/BPDmpepEwSY"
+                                    src="https://www.youtube-nocookie.com/embed/BPDmpepEwSY"
                                     title="YouTube video player"
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
