b2eceb5a5da91c1dcd7bd585beec08b0a3e80c53
Cory Watilo <cww@watilo.com>
2024-02-07 08:56:55 -0500

    adds comments api to api docs nav

--- src/navs/index.js
+++ src/navs/index.js
@@ -1483,6 +1483,10 @@ export const docsMenu = {
                             url: '/docs/api/cohorts',
                         },
                         {
+                            name: 'Comments',
+                            url: '/docs/api/comments',
+                        },
+                        {
                             name: 'Dashboards',
                             url: '/docs/api/dashboards',
                         },
