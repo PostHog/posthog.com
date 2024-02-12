e3b999cfbf0f849eb05ae8d3327f6a03acd12a63
Jasmine Zou <54626845+jasmineyas@users.noreply.github.com>
2024-02-12 05:28:57 +0000

    For Ian to review (1 doc 1 tutorial updated) for issue 7616

--- src/components/ProductScreenshot/index.js
+++ src/components/ProductScreenshot/index.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 export const ProductScreenshot = ({ imageLight, imageDark, alt, classes }) => {
     return (
-        <div className="mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark">
-            <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
-            {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
+        <div
+            className={`mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark ${classes}`}
+            style={{ display: 'flex', justifyContent: 'center' }}
+        >
+            {imageLight && (
+                <img
+                    src={imageLight}
+                    alt={alt}
+                    className={`dark:hidden ${classes}`}
+                    style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            )}
+            {imageDark && (
+                <img
+                    src={imageDark}
+                    alt={alt}
+                    className={`hidden dark:block ${classes}`}
+                    style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            )}
         </div>
     )
 }
