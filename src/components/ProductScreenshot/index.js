42c56b1f8285d4e7e7d8325d68b43ace5349ad7e
Jasmine Zou <54626845+jasmineyas@users.noreply.github.com>
2024-02-17 11:46:08 -0800

    Update src/components/ProductScreenshot/index.js
    Co-authored-by: Ian Vanagas <34755028+ivanagas@users.noreply.github.com>

--- src/components/ProductScreenshot/index.js
+++ src/components/ProductScreenshot/index.js
@@ -3,8 +3,7 @@ import React from 'react'
 export const ProductScreenshot = ({ imageLight, imageDark, alt, classes }) => {
     return (
         <div
-            className={`mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark ${classes}`}
-            style={{ display: 'flex', justifyContent: 'center' }}
+            className={`mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark flex justify-center ${classes}`}
         >
             {imageLight && (
                 <img
