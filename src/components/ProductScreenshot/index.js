b22b5b5708c2f6033d6bf6bfc7ffd4358ef1a42e
Cory Watilo <cww@watilo.com>
2024-02-20 10:30:15 -0500

    allow ProductScreenshot images to be zoomable

--- src/components/ProductScreenshot/index.js
+++ src/components/ProductScreenshot/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { ZoomImage } from 'components/ZoomImage'
 
-export const ProductScreenshot = ({ imageLight, imageDark, alt, classes }) => {
+export const ProductScreenshot = ({ imageLight, imageDark, alt, classes, zoom }) => {
     return (
         <div className="mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark">
-            <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
-            {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
+            {zoom === false ? (
+                <>
+                    <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
+                    {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
+                </>
+            ) : (
+                <ZoomImage>
+                    <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
+                    {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
+                </ZoomImage>
+            )}
         </div>
     )
 }
