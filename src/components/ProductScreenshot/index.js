5f82b11a15440c88dc6b29ccb34b16a4bef3dfb7
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2024-02-20 10:08:25 -0800

    revert product screenshot change

--- src/components/ProductScreenshot/index.js
+++ src/components/ProductScreenshot/index.js
@@ -2,23 +2,9 @@ import React from 'react'
 
 export const ProductScreenshot = ({ imageLight, imageDark, alt, classes }) => {
     return (
-        <div
-            className={`mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark flex justify-center ${classes}`}
-        >
-            {imageLight && (
-                <img
-                    src={imageLight}
-                    alt={alt}
-                    className={`dark:hidden max-w-full  ${classes}`}
-                />
-            )}
-            {imageDark && (
-                <img
-                    src={imageDark}
-                    alt={alt}
-                    className={`hidden dark:block max-w-full ${classes}`}
-                />
-            )}
+        <div className="mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark">
+            <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
+            {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
         </div>
     )
 }
