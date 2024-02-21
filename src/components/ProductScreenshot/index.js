96067d2e82e3e27aec2ff88a32a7c0ac22f4fe72
Eli Kinsey <eli@ekinsey.dev>
2024-02-21 08:17:57 -0800

    simplify

--- src/components/ProductScreenshot/index.js
+++ src/components/ProductScreenshot/index.js
@@ -2,19 +2,13 @@ import React from 'react'
 import { ZoomImage } from 'components/ZoomImage'
 
 export const ProductScreenshot = ({ imageLight, imageDark, alt, classes, zoom }) => {
+    const Container = zoom === false ? React.Fragment : ZoomImage
     return (
         <div className="mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark">
-            {zoom === false ? (
-                <>
-                    <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
-                    {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
-                </>
-            ) : (
-                <ZoomImage>
-                    <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
-                    {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
-                </ZoomImage>
-            )}
+            <Container>
+                <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
+                {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
+            </Container>
         </div>
     )
 }
