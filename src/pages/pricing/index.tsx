bc906b8dac26efe825ca0f58442f2e156f21f85f
Eli Kinsey <eli@ekinsey.dev>
2024-02-28 12:16:18 -0800

    use usePosthog

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -2,14 +2,15 @@ import Pricing from 'components/Pricing/Pricing'
 import { Pricing as PricingControl } from 'components/Pricing/experiment-control/Pricing'
 import { RenderInClient } from 'components/RenderInClient'
 import Spinner from 'components/Spinner'
-import posthog from 'posthog-js'
 import React from 'react'
+import usePostHog from 'hooks/usePostHog'
 
 const PricingPage = (): JSX.Element => {
+    const posthog = usePostHog()
     return (
         <RenderInClient
             render={() => {
-                return posthog.getFeatureFlag('teams-pricing-page') !== 'test' ? <PricingControl /> : <Pricing />
+                return posthog?.getFeatureFlag('teams-pricing-page') !== 'test' ? <PricingControl /> : <Pricing />
             }}
             placeholder={<Spinner />}
         />
