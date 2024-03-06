a0e1acde4cd90f71836455d0e382df5126f3a897
Eli Kinsey <eli@ekinsey.dev>
2024-03-06 11:27:43 -0800

    add update on wip page

--- src/components/TeamUpdate/index.tsx
+++ src/components/TeamUpdate/index.tsx
@@ -7,21 +7,14 @@ import Toggle from 'components/Toggle'
 import { useFormik } from 'formik'
 import { useUser } from 'hooks/useUser'
 import React, { useEffect, useState } from 'react'
-import qs from 'qs'
 import RoadmapSelect from 'components/RoadmapSelect'
+import qs from 'qs'
+import TeamSelect from 'components/TeamSelect'
 
-type Update = {
-    thingOfTheWeek: boolean
-    roadmap: boolean
-    roadmapID: number | null
-    question: number
-    team: number
-}
-
-export default function TeamUpdate({ teamName }: { teamName: string }) {
+export default function TeamUpdate({ teamName, onSubmit }: { teamName?: string; onSubmit?: () => void }) {
     const [teamID, setTeamID] = useState<number | null>(null)
-    const [updates, setUpdates] = useState<Update[]>([])
     const { getJwt, user } = useUser()
+    const [updateCount, setUpdateCount] = useState(0)
     const { handleSubmit, values, setFieldValue, initialValues, submitForm, isSubmitting, resetForm } = useFormik({
         validateOnMount: false,
         initialValues: {
@@ -64,9 +57,7 @@ export default function TeamUpdate({ teamName }: { teamName: string }) {
                 }).then((res) => res.json())
 
                 const transformedValues = await transformValues({ body, images: images ?? [] }, profileID, jwt)
-                const {
-                    data: { id: questionID },
-                } = await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/questions`, {
+                await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/questions`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -84,16 +75,16 @@ export default function TeamUpdate({ teamName }: { teamName: string }) {
                         },
                     }),
                 }).then((res) => res.json())
-
-                setUpdates([{ thingOfTheWeek, question: questionID, team: teamID, roadmap, roadmapID }, ...updates])
                 resetForm()
+                setUpdateCount(updateCount + 1)
+                onSubmit?.()
             } catch (err) {
                 console.error(err)
             }
         },
     })
 
-    const fetchUpdates = async () => {
+    const fetchTeam = async () => {
         const {
             data: [{ id: teamID }],
         } = await fetch(
@@ -109,89 +100,61 @@ export default function TeamUpdate({ teamName }: { teamName: string }) {
             )}`
         ).then((res) => res.json())
         setTeamID(teamID)
-        const { data } = await fetch(
-            `${process.env.GATSBY_SQUEAK_API_HOST}/api/team-updates?${qs.stringify(
-                {
-                    populate: ['question.id', 'roadmap.id', 'team.id'],
-                    sort: ['createdAt:desc'],
-                    filters: {
-                        team: {
-                            id: {
-                                $eq: teamID,
-                            },
-                        },
-                    },
-                },
-                { encodeValuesOnly: true }
-            )}`
-        ).then((res) => res.json())
-
-        const updates = data?.map(({ attributes: { thingOfTheWeek, roadmap, question, team } }) => {
-            return {
-                thingOfTheWeek,
-                roadmap: roadmap?.data?.id,
-                question: question?.data.id,
-                team: team?.data?.id,
-            }
-        })
-        setUpdates(updates ?? [])
     }
 
     useEffect(() => {
-        fetchUpdates()
-    }, [])
+        if (teamName) {
+            fetchTeam()
+        }
+    })
 
-    return (
-        <div>
-            {user?.role?.type === 'moderator' && (
-                <form onSubmit={handleSubmit} className="m-0">
-                    <Avatar className="w-[40px] mr-[10px]" image={getAvatarURL(user?.profile)} />
-                    <div className="ml-[50px]">
-                        <div className="bg-white dark:bg-accent-dark border border-light dark:border-dark rounded-md overflow-hidden mb-4">
-                            <div className="leading-[0]">
-                                <RichText
-                                    key={updates.length}
-                                    onSubmit={submitForm}
-                                    autoFocus
-                                    setFieldValue={setFieldValue}
-                                    initialValue={initialValues?.body}
-                                    values={values}
-                                />
-                            </div>
+    return user?.role?.type === 'moderator' ? (
+        <form onSubmit={handleSubmit} className="m-0">
+            <Avatar className="w-[40px] mr-[10px]" image={getAvatarURL(user?.profile)} />
+            <div className="ml-[50px]">
+                <div className="bg-white dark:bg-accent-dark border border-light dark:border-dark rounded-md overflow-hidden mb-4">
+                    {!teamName && (
+                        <div className="border-b border-border dark:border-dark">
+                            <TeamSelect value={teamID} onChange={(teamID) => setTeamID(teamID)} />
                         </div>
-                        <Toggle
-                            checked={values.thingOfTheWeek}
-                            onChange={(checked) => setFieldValue('thingOfTheWeek', checked)}
-                            label="Thing of the week"
+                    )}
+                    <div className="leading-[0]">
+                        <RichText
+                            key={updateCount}
+                            onSubmit={submitForm}
+                            autoFocus
+                            setFieldValue={setFieldValue}
+                            initialValue={initialValues?.body}
+                            values={values}
                         />
-                        <div className="mt-4 mb-6">
-                            <Toggle
-                                checked={values.roadmap}
-                                onChange={(checked) => setFieldValue('roadmap', checked)}
-                                label="This is connected to a roadmap item"
-                            />
-                            {values.roadmap && teamID && (
-                                <div className="border border-border dark:border-dark rounded-md mt-4">
-                                    <RoadmapSelect
-                                        onChange={(value) => setFieldValue('roadmapID', value)}
-                                        value={values.roadmapID}
-                                    />
-                                </div>
-                            )}
-                        </div>
                     </div>
-                    <span className="ml-[50px]">
-                        <Button
-                            loading={isSubmitting}
-                            disabled={isSubmitting}
-                            type="submit"
-                            className="w-[calc(100%_-_50px)]"
-                        >
-                            Post update
-                        </Button>
-                    </span>
-                </form>
-            )}
-        </div>
-    )
+                </div>
+                <Toggle
+                    checked={values.thingOfTheWeek}
+                    onChange={(checked) => setFieldValue('thingOfTheWeek', checked)}
+                    label="Thing of the week"
+                />
+                <div className="mt-4 mb-6">
+                    <Toggle
+                        checked={values.roadmap}
+                        onChange={(checked) => setFieldValue('roadmap', checked)}
+                        label="This is connected to a roadmap item"
+                    />
+                    {values.roadmap && teamID && (
+                        <div className="border border-border dark:border-dark rounded-md mt-4">
+                            <RoadmapSelect
+                                onChange={(value) => setFieldValue('roadmapID', value)}
+                                value={values.roadmapID}
+                            />
+                        </div>
+                    )}
+                </div>
+            </div>
+            <span className="ml-[50px]">
+                <Button loading={isSubmitting} disabled={isSubmitting} type="submit" className="w-[calc(100%_-_50px)]">
+                    Post update
+                </Button>
+            </span>
+        </form>
+    ) : null
 }
--- src/pages/wip.tsx
+++ src/pages/wip.tsx
@@ -8,125 +8,103 @@ import SEO from 'components/seo'
 import { companyMenu } from '../navs'
 import { useUser } from 'hooks/useUser'
 import { Skeleton } from 'components/Questions/QuestionsTable'
+import TeamUpdate from 'components/TeamUpdate'
 
 export default function TeamUpdates() {
-  const [loading, setLoading] = useState(true)
-  const [copied, setCopied] = useState(false)
-  const [updates, setUpdates] = useState<any>(null)
-  const { user } = useUser()
+    const [loading, setLoading] = useState(true)
+    const [updates, setUpdates] = useState<any>(null)
+    const { user } = useUser()
+    const isModerator = user?.role?.type === 'moderator'
 
-  const fetchUpdates = async () => {
-    const { data } = await fetch(
-      `${process.env.GATSBY_SQUEAK_API_HOST}/api/team-updates?${qs.stringify(
-        {
-          populate: ['question', 'team'],
-          sort: ['createdAt:desc'],
-          filters: {
-            thingOfTheWeek: true,
-            question: {
-              createdAt: {
-                $gte: dayjs().startOf('week').format(),
-              },
-            },
-          },
-        },
-        { encodeValuesOnly: true }
-      )}`
-    ).then((res) => res.json())
-
-    const updates = groupBy(data, (update) => update?.attributes?.team?.data?.attributes?.name)
-    setUpdates(updates)
-    setLoading(false)
-  }
-
-  const handleCopy = () => {
-    const text = Object.keys(updates)
-      .map((teamName) => {
-        return (
-          `**${teamName}**\n` +
-          updates[teamName]
-            .map((update) => {
-              const {
-                attributes: {
-                  question: {
-                    data: {
-                      attributes: { body },
-                    },
-                  },
-                },
-              } = update
-              return body
-            })
-            .join('\n\n')
-        )
-      })
-      .join('\n\n')
-
-    navigator.clipboard.writeText(text)
-    setCopied(true)
-    setTimeout(() => {
-      setCopied(false)
-    }, 3000)
-  }
-
-  useEffect(() => {
-    fetchUpdates()
-  }, [])
-
-  const hasUpdates = updates && Object.keys(updates).length > 0
-
-  return (
-    <Layout parent={companyMenu}>
-      <SEO title="WIP - PostHog" noindex />
-      <section className="max-w-[700px] mx-auto px-5 mt-8">
-        <div className="relative pb-6 mb-6 border-b border-border dark:border-dark flex justify-between items-end">
-          <div>
-            <h1 className="font-bold text-5xl my-0">Work in progress</h1>
-            <p className="my-0 font-semibold opacity-70 mt-2">
-              Here's what we're building right now (week of {dayjs().startOf('week').format('MM/DD/YYYY')})
-            </p>
-          </div>
-        </div>
-        <div className="pb-12">
-          {loading ? (
-            <Skeleton />
-          ) : hasUpdates ? (
-            <ul className="list-none m-0 p-0 pb-4 grid gap-y-4">
-              {Object.keys(updates).map((teamName) => {
-                return (
-                  <li key={teamName}>
-                    <h2>{teamName}</h2>
-                    <ul>
-                      {updates[teamName].map((update) => {
-                        const {
-                          id,
-                          attributes: {
-                            question: {
-                              data: {
-                                attributes: { body },
-                              },
+    const fetchUpdates = async () => {
+        const { data } = await fetch(
+            `${process.env.GATSBY_SQUEAK_API_HOST}/api/team-updates?${qs.stringify(
+                {
+                    populate: ['question', 'team'],
+                    sort: ['createdAt:desc'],
+                    filters: {
+                        thingOfTheWeek: true,
+                        question: {
+                            createdAt: {
+                                $gte: dayjs().startOf('week').format(),
                             },
-                          },
-                        } = update
-                        return (
-                          <li key={id}>
-                            <Markdown>{body}</Markdown>
-                          </li>
-                        )
-                      })}
-                    </ul>
-                  </li>
-                )
-              })}
-            </ul>
-          ) : (
-            <>
-              <h1 className="m-0 mb-1">We're still cookin'</h1>
-              <p className="m-0 opacity-70 font-semibold">Check back later!</p>
-            </>
-          )}
-        </div>
-      </section>
-    </Layout>
-  )
+                        },
+                    },
+                },
+                { encodeValuesOnly: true }
+            )}`
+        ).then((res) => res.json())
+
+        const updates = groupBy(data, (update) => update?.attributes?.team?.data?.attributes?.name)
+        setUpdates(updates)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchUpdates()
+    }, [])
+
+    const hasUpdates = updates && Object.keys(updates).length > 0
+
+    return (
+        <Layout parent={companyMenu}>
+            <SEO title="WIP - PostHog" noindex />
+            <section className="max-w-[700px] mx-auto px-5 mt-8">
+                <div className="relative pb-6 mb-6 border-b border-border dark:border-dark flex justify-between items-end">
+                    <div>
+                        <h1 className="font-bold text-5xl my-0">Work in progress</h1>
+                        <p className="my-0 font-semibold opacity-70 mt-2">
+                            Here's what we're building right now (week of {dayjs().startOf('week').format('MM/DD/YYYY')}
+                            )
+                        </p>
+                    </div>
+                </div>
+                <div className="pb-12">
+                    {isModerator && (
+                        <div className="mb-8 pb-8 border-b border-border dark:border-dark">
+                            <h4>Post an update</h4>
+                            <TeamUpdate onSubmit={fetchUpdates} />
+                        </div>
+                    )}
+                    {loading ? (
+                        <Skeleton />
+                    ) : hasUpdates ? (
+                        <ul className="list-none m-0 p-0 pb-4 grid gap-y-4">
+                            {Object.keys(updates).map((teamName) => {
+                                return (
+                                    <li key={teamName}>
+                                        <h2>{teamName}</h2>
+                                        <ul>
+                                            {updates[teamName].map((update) => {
+                                                const {
+                                                    id,
+                                                    attributes: {
+                                                        question: {
+                                                            data: {
+                                                                attributes: { body },
+                                                            },
+                                                        },
+                                                    },
+                                                } = update
+                                                return (
+                                                    <li key={id}>
+                                                        <Markdown>{body}</Markdown>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    ) : (
+                        <>
+                            <h1 className="m-0 mb-1">We're still cookin'</h1>
+                            <p className="m-0 opacity-70 font-semibold">Check back later!</p>
+                        </>
+                    )}
+                </div>
+            </section>
+        </Layout>
+    )
 }
