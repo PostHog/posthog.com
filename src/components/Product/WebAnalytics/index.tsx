cc1341b69e32bde7a057bf50b8caa9e0f58cc97a
Cory Watilo <cww@watilo.com>
2024-03-07 10:14:50 -0500

    web analytics topic

--- src/components/Product/WebAnalytics/index.tsx
+++ src/components/Product/WebAnalytics/index.tsx
@@ -555,7 +555,7 @@ export const ProductWebAnalytics = () => {
                         Get a more technical overview of how everything works <Link to="/docs">in our docs</Link>.
                     </p>
                     <DocLinks
-                        menu={docsMenu.children.find(({ name }) => name.toLowerCase() === 'session replay').children}
+                        menu={docsMenu.children.find(({ name }) => name.toLowerCase() === 'web analytics').children}
                     />
                 </section>
 
@@ -599,7 +599,7 @@ export const ProductWebAnalytics = () => {
                         </CallToAction>
                     </div>
 
-                    <Questions topicIds={[20]} />
+                    <Questions topicIds={[65]} />
                 </section>
 
                 <PairsWith items={pairsWithItemCount}>
