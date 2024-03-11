ea3ba77765606c5001cd8c9850231d4956fc23c6
Eli Kinsey <eli@ekinsey.dev>
2024-03-11 15:37:30 -0700

    remove scripts

--- script.mjs
+++
@@ -1,9 +0,0 @@
-import { glob } from 'glob'
-import matter from 'gray-matter'
-import path from 'path'
-import fs from 'fs'
-
-const files = await glob('contents/**/*.{jpg,jpeg,png,gif,mp4,avi,mov}')
-for (const file of files) {
-    fs.unlinkSync(file)
-}
