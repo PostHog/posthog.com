6e90c989dc344ddbf76e044ef808ed5a53b2d346
Cory Watilo <cww@watilo.com>
2024-03-14 22:16:58 -0400

    sizing/spacing

--- src/components/Roadmap/index.tsx
+++ src/components/Roadmap/index.tsx
@@ -117,7 +117,7 @@ const Feature = ({ id, title, teams, description, likeCount, onLike, onUpdate })
                                 {teamName} Team
                             </Link>
                         )}
-                        <div className="mt-1">
+                        <div className="mt-1 text-[15px]">
                             <Markdown>{description}</Markdown>
                         </div>
                         <div className="mt-2">
@@ -369,7 +369,7 @@ export default function Roadmap() {
                                         const roadmaps = roadmapsGroupedByTeam[team]
                                         return (
                                             <li className="relative" key={team}>
-                                                <h4 className="m-0 mb-6 pr-2 inline-flex items-center bg-light dark:bg-dark after:-z-10 after:absolute after:w-full after:h-[1px] after:bg-border after:dark:bg-border-dark after:translate-y-[2px]">
+                                                <h4 className="text-lg m-0 mb-6 pr-2 inline-flex items-center bg-light dark:bg-dark after:-z-10 after:absolute after:w-full after:h-[1px] after:bg-border after:dark:bg-border-dark after:translate-y-[2px]">
                                                     {team}
                                                 </h4>
                                                 <ul className="m-0 p-0 list-none space-y-8">
