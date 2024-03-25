4091f23c2baa66e031c0cbf70f38a767fb81bf91
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2024-03-25 15:43:13 +0000

    Update index.tsx (#8128)

--- src/components/Product/WebAnalytics/index.tsx
+++ src/components/Product/WebAnalytics/index.tsx
@@ -353,7 +353,7 @@ export const ProductWebAnalytics = () => {
                 <section id="customers" className="-mt-56 pt-36">
                     <ul className="list-none p-0 grid md:grid-cols-3 gap-4 mb-10 md:mb-20">
                         <CustomerCard
-                            outcome="gathers 30% more data than with Google Analytics"
+                            outcome="gets 30% more data than with Google Analytics"
                             quote="We could autocapture... events using the JS snippet and... configure custom events."
                             customer={ycombinator}
                         />
