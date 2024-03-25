f3847f5794c4b7e719ffd71cb5c16418a0f80b86
Cory Watilo <cww@watilo.com>
2024-03-25 17:37:00 -0400

    you'll love posthog if...

--- src/components/Home/NoHatingAllowed/index.tsx
+++ src/components/Home/NoHatingAllowed/index.tsx
@@ -2,6 +2,7 @@ import { IconChevronDown } from '@posthog/icons'
 import { StaticImage } from 'gatsby-plugin-image'
 import React, { useRef } from 'react'
 import Link from 'components/Link'
+import { useLayoutData } from '../../Layout/hooks'
 
 const cards = [
     {
@@ -189,6 +190,7 @@ const cards = [
 ]
 
 const Card = ({ top, bottom, Image, ImageSize, color }) => {
+    const { enterpriseMode } = useLayoutData()
     return (
         <li
             style={{ backgroundColor: color || 'white' }}
@@ -198,18 +200,20 @@ const Card = ({ top, bottom, Image, ImageSize, color }) => {
                 {Image}
             </div>
             <h5 className="m-0 text-2xl text-black relative leading-7">{top}</h5>
-            <p className="text-sm text-black m-0 relative">{bottom}</p>
+            {!enterpriseMode && <p className="text-sm text-black m-0 relative">{bottom}</p>}
         </li>
     )
 }
 
 export default function NoHatingAllowed() {
     const listRef = useRef<HTMLUListElement>(null)
+    const { enterpriseMode } = useLayoutData()
 
     return (
         <div className="relative pt-8 mb-12 overflow-hidden">
             <h2 className="text-4xl lg:text-6xl text-center mb-5">
-                <span className="text-red uppercase block md:inline">Warning:</span> You'll hate PostHog if...
+                {!enterpriseMode && <span className="text-red uppercase block md:inline">Warning</span>} You'll{' '}
+                {enterpriseMode ? <span className="text-red">LOVE</span> : 'hate'} PostHog if...
             </h2>
             <div className="absolute z-10 -left-10 top-64 bottom-32 w-48 bg-gradient-radial from-light/30 via-light/0 to-light/0 dark:from-dark/30 dark:via-dark/0 dark:to-dark/0" />
             <div className="absolute z-20 top-1/2 left-0 -translate-y-1/2 mt-16">
