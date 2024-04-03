bb92808642f62f1b3914564210d65494cb7b240d
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2024-04-02 17:07:01 -0700

    unify app docs

--- src/navs/index.js
+++ src/navs/index.js
@@ -2495,24 +2495,16 @@ export const docsMenu = {
                     icon: 'IconGraduationCap',
                     children: [
                         {
-                            name: 'Tutorial',
-                            url: '/docs/cdp/build/tutorial',
-                        },
-                        {
-                            name: 'Troubleshooting',
-                            url: '/docs/cdp/enabling',
+                            name: 'Overview',
+                            url: '/docs/cdp/build',
                         },
                         {
                             name: 'Developer reference',
                             url: '/docs/cdp/build/reference',
                         },
                         {
-                            name: 'Testing',
-                            url: '/docs/cdp/build/testing',
-                        },
-                        {
-                            name: 'TypeScript types',
-                            url: '/docs/cdp/build/types',
+                            name: 'Tutorial',
+                            url: '/docs/cdp/build/tutorial',
                         },
                     ],
                 },
