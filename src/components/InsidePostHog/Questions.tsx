1df3c2900350745cb09116d26b599aef4027f607
Cory Watilo <cww@watilo.com>
2024-04-17 21:28:46 -0400

    line height

--- src/components/InsidePostHog/Questions.tsx
+++ src/components/InsidePostHog/Questions.tsx
@@ -35,7 +35,7 @@ const Thread = ({ title, status, url }: { title: string; status: string; url: st
     return (
         <>
             <div>
-                <Link to={url} className="font-bold text-sm @2xs:py-1 inline-block">
+                <Link to={url} className="font-semibold text-sm @2xs:py-1 inline-block leading-tight line-clamp-3">
                     {title}
                 </Link>
             </div>
