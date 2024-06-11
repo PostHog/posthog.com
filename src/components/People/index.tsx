eec95265b23d2ad0d80001d4cf3ba3697564d96c
Eli Kinsey <eli@ekinsey.dev>
2024-06-11 09:30:40 -0700

    add bios snippets to the people page

--- src/components/People/index.tsx
+++ src/components/People/index.tsx
@@ -10,18 +10,21 @@ import Layout from 'components/Layout'
 import { SEO } from '../seo'
 import TeamStat, { pineappleOnPizzaStat } from './TeamStat'
 import { StaticImage } from 'gatsby-plugin-image'
+import ReactMarkdown from 'react-markdown'
 
 export const TeamMember = (teamMember) => {
-    const { avatar, lastName, firstName, companyRole, country, squeakId, location, compact } = teamMember
+    const { avatar, lastName, firstName, companyRole, country, squeakId, location, biography } = teamMember
     const name = [firstName, lastName].filter(Boolean).join(' ')
 
     return (
-        <li className="bg-accent dark:bg-accent-dark border border-light dark:border-dark rounded h-40 relative hover:-translate-y-0.5 active:translate-y-0 hover:transition-all hover:border-b-[4px] active:border-b-1 active:top-[2px] @container">
+        <li className="bg-accent dark:bg-accent-dark border border-light dark:border-dark rounded h-40 relative @container group click">
             <Link
                 to={`/community/profiles/${squeakId}`}
-                className="flex justify-between h-full relative text-primary dark:text-primary-dark hover:text-primary dark:hover:text-primary-dark"
+                className={`flex justify-between h-full relative text-primary dark:text-primary-dark hover:text-primary dark:hover:text-primary-dark w-full transition-transform preserve-3d ${
+                    biography ? 'group-hover:[transform:rotateY(180deg)]' : ''
+                }`}
             >
-                <div className="flex flex-col justify-between px-6 py-4 w-full mr-32 xl:mr-40">
+                <div className="flex flex-col justify-between px-6 py-4 w-full mr-32 xl:mr-40 absolute h-full [backface-visibility:hidden]">
                     <div>
                         <h3
                             className="mb-0 text-[15px] @sm:text-base @md:text-[17px] leading-tight"
@@ -40,17 +43,20 @@ export const TeamMember = (teamMember) => {
                             {country === 'world' ? 'Planet Earth' : location || country}
                         </span>
                     </span>
+                    <figure className="m-0 -mt-8 p-0 absolute right-0 bottom-0">
+                        <img
+                            src={
+                                avatar?.url ||
+                                'https://res.cloudinary.com/dmukukwp6/image/upload/v1698231117/max_6942263bd1.png'
+                            }
+                            className="w-[200px]"
+                        />
+                    </figure>
                 </div>
-
-                <figure className="m-0 -mt-8 p-0 absolute right-0 bottom-0">
-                    <img
-                        src={
-                            avatar?.url ||
-                            'https://res.cloudinary.com/dmukukwp6/image/upload/v1698231117/max_6942263bd1.png'
-                        }
-                        className="w-[200px]"
-                    />
-                </figure>
+                <div className="absolute h-full w-full [backface-visibility:hidden] [transform:rotateY(180deg)] overflow-hidden p-4">
+                    <ReactMarkdown>{biography}</ReactMarkdown>
+                    <div className="bg-gradient-to-t from-tan to-transparent absolute inset-0 w-full h-full" />
+                </div>
             </Link>
         </li>
     )
@@ -169,6 +175,7 @@ export const teamQuery = graphql`
                 avatar {
                     url
                 }
+                biography
                 lastName
                 firstName
                 companyRole
