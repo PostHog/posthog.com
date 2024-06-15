571310d0ec4b866c3f252cf76fb6a3d4f03c1ede
Eli Kinsey <eli@ekinsey.dev>
2024-06-15 07:21:58 -0700

    add field validation to teaupdate form

--- src/components/TeamUpdate/index.tsx
+++ src/components/TeamUpdate/index.tsx
@@ -10,6 +10,11 @@ import React, { useEffect, useState } from 'react'
 import RoadmapSelect from 'components/RoadmapSelect'
 import qs from 'qs'
 import TeamSelect from 'components/TeamSelect'
+import * as Yup from 'yup'
+
+const ValidationSchema = Yup.object().shape({
+    team: Yup.mixed().required('Please select a team'),
+})
 
 export default function TeamUpdate({
     teamName,
@@ -20,83 +25,86 @@ export default function TeamUpdate({
     onSubmit?: () => void
     roadmapID?: number
 }) {
-    const [team, setTeam] = useState<any>(null)
     const { getJwt, user } = useUser()
     const [updateCount, setUpdateCount] = useState(0)
-    const { handleSubmit, values, setFieldValue, initialValues, submitForm, isSubmitting, resetForm } = useFormik({
-        validateOnMount: false,
-        initialValues: {
-            body: '',
-            thingOfTheWeek: false,
-            roadmap: !!roadmapID,
-            roadmapID,
-            images: [],
-            impersonate: false,
-        },
-        onSubmit: async ({ body, thingOfTheWeek, roadmap, roadmapID, images, impersonate }) => {
-            if (!team) return
-            try {
-                const jwt = await getJwt()
-                const profileID = user?.profile?.id
-                if (!profileID || !jwt) return
+    const { handleSubmit, values, setFieldValue, initialValues, submitForm, isSubmitting, resetForm, errors } =
+        useFormik({
+            validationSchema: ValidationSchema,
+            validateOnBlur: false,
+            validateOnChange: false,
+            validateOnMount: false,
+            initialValues: {
+                body: '',
+                thingOfTheWeek: false,
+                roadmap: !!roadmapID,
+                roadmapID,
+                images: [],
+                impersonate: false,
+                team: null,
+            },
+            onSubmit: async ({ body, thingOfTheWeek, roadmap, roadmapID, images, impersonate, team }) => {
+                try {
+                    const jwt = await getJwt()
+                    const profileID = user?.profile?.id
+                    if (!profileID || !jwt) return
 
-                const {
-                    data: { id: updateID },
-                } = await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/team-updates`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${jwt}`,
-                    },
-                    body: JSON.stringify({
-                        data: {
-                            thingOfTheWeek,
-                            team: {
-                                connect: [team.id],
-                            },
-                            ...(roadmap && roadmapID
-                                ? {
-                                      roadmap: {
-                                          connect: [roadmapID],
-                                      },
-                                  }
-                                : null),
+                    const {
+                        data: { id: updateID },
+                    } = await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/team-updates`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Authorization: `Bearer ${jwt}`,
                         },
-                    }),
-                }).then((res) => res.json())
+                        body: JSON.stringify({
+                            data: {
+                                thingOfTheWeek,
+                                team: {
+                                    connect: [team.id],
+                                },
+                                ...(roadmap && roadmapID
+                                    ? {
+                                          roadmap: {
+                                              connect: [roadmapID],
+                                          },
+                                      }
+                                    : null),
+                            },
+                        }),
+                    }).then((res) => res.json())
 
-                const transformedValues = await transformValues({ body, images: images ?? [] }, profileID, jwt)
-                await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/questions`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${jwt}`,
-                    },
-                    body: JSON.stringify({
-                        data: {
-                            subject: '',
-                            body: transformedValues?.body,
-                            slugs: [],
-                            permalink: '',
-                            update: {
-                                connect: [updateID],
-                            },
-                            ...(impersonate
-                                ? {
-                                      profile: team.attributes.leadProfiles.data[0].id,
-                                  }
-                                : null),
+                    const transformedValues = await transformValues({ body, images: images ?? [] }, profileID, jwt)
+                    await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/questions`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Authorization: `Bearer ${jwt}`,
                         },
-                    }),
-                }).then((res) => res.json())
-                resetForm()
-                setUpdateCount(updateCount + 1)
-                onSubmit?.()
-            } catch (err) {
-                console.error(err)
-            }
-        },
-    })
+                        body: JSON.stringify({
+                            data: {
+                                subject: '',
+                                body: transformedValues?.body,
+                                slugs: [],
+                                permalink: '',
+                                update: {
+                                    connect: [updateID],
+                                },
+                                ...(impersonate
+                                    ? {
+                                          profile: team.attributes.leadProfiles.data[0].id,
+                                      }
+                                    : null),
+                            },
+                        }),
+                    }).then((res) => res.json())
+                    resetForm()
+                    setUpdateCount(updateCount + 1)
+                    onSubmit?.()
+                } catch (err) {
+                    console.error(err)
+                }
+            },
+        })
 
     const fetchTeam = async () => {
         const {
@@ -113,7 +121,7 @@ export default function TeamUpdate({
                 { encodeValuesOnly: true }
             )}`
         ).then((res) => res.json())
-        setTeam(team)
+        setFieldValue('team', team)
     }
 
     useEffect(() => {
@@ -129,7 +137,7 @@ export default function TeamUpdate({
                 <div className="bg-white dark:bg-accent-dark border border-light dark:border-dark rounded-md overflow-hidden mb-4">
                     {!teamName && (
                         <div className="border-b border-border dark:border-dark">
-                            <TeamSelect value={team} onChange={(team) => setTeam(team)} />
+                            <TeamSelect value={values.team} onChange={(team) => setFieldValue('team', team)} />
                         </div>
                     )}
                     <div className="leading-[0]">
@@ -149,7 +157,7 @@ export default function TeamUpdate({
                     label="Thing of the week"
                     tooltip="Toggle this ON if you want this update to appear in the TOTW (Thing of the Week) page at /team-updates"
                 />
-                {team && (
+                {values.team && (
                     <>
                         <div className="my-4">
                             <Toggle
@@ -169,7 +177,7 @@ export default function TeamUpdate({
                                 {values.roadmap && (
                                     <div className="border border-border dark:border-dark rounded-md mt-4">
                                         <RoadmapSelect
-                                            teamID={team.id}
+                                            teamID={values.team.id}
                                             onChange={(value) => setFieldValue('roadmapID', value)}
                                             value={values.roadmapID}
                                         />
@@ -179,6 +187,7 @@ export default function TeamUpdate({
                         )}
                     </>
                 )}
+                {errors.team && <p className="text-red mt-6 font-bold mb-0">{errors.team}</p>}
             </div>
             <span className="ml-[50px] mt-6 inline-block">
                 <Button loading={isSubmitting} disabled={isSubmitting} type="submit" className="w-[calc(100%_-_50px)]">
