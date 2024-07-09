76afe5135b6b80c6a9d658cebbe386fd352cb1ac
Eli Kinsey <eli@ekinsey.dev>
2024-07-08 17:17:54 -0700

    remember volume

--- src/components/Pricing/PricingCalculator/Tabbed.tsx
+++ src/components/Pricing/PricingCalculator/Tabbed.tsx
@@ -286,33 +286,6 @@ const Addon = ({ type, name, description, plans, addons, setAddons, volume, incl
     )
 }
 
-const SliderToggle = ({ label = '', children, onChange, ...other }) => {
-    const [checked, setChecked] = useState(other.checked || false)
-    return (
-        <div className={`mt-2 grid ${checked ? 'mb-10' : 'mb-2 grid-cols-5'}`}>
-            <div className="space-y-3 col-span-3">
-                <Checkbox
-                    className="!text-base"
-                    checked={checked}
-                    onChange={() => {
-                        const newChecked = !checked
-                        setChecked(!checked)
-                        onChange(newChecked)
-                    }}
-                    value={label}
-                />
-                {checked && children}
-            </div>
-            {!checked && (
-                <>
-                    <span className="opacity-25 text-right">--</span>
-                    <span className="opacity-25 text-right">--</span>
-                </>
-            )}
-        </div>
-    )
-}
-
 const AnalyticsSlider = ({ marks, min, max, className = '', label, onChange, value }) => {
     return (
         <div className={`${className} ml-6 relative ${label ? 'pt-7' : ''}`}>
@@ -325,6 +298,69 @@ const AnalyticsSlider = ({ marks, min, max, className = '', label, onChange, val
                 onChange={(value) => onChange(reverseNonLinearCurve(value))}
                 value={nonLinearCurve(value)}
             />
+        </div>
+    )
+}
+
+const SliderToggle = ({ label = '', types, activeProduct, setAnalyticsVolume, analyticsData, ...other }) => {
+    const [volume, setVolume] = useState({})
+    const [checked, setChecked] = useState(other.checked || false)
+
+    const handleCheck = () => {
+        if (checked) {
+            const volume = {}
+            types.forEach(({ type }) => {
+                volume[type] = analyticsData[type].volume
+                setAnalyticsVolume(type, 0)
+            })
+            setVolume(volume)
+        } else {
+            types.forEach(({ type }) => {
+                setAnalyticsVolume(type, volume[type] || 0)
+            })
+        }
+        setChecked(!checked)
+    }
+
+    return (
+        <div className={`mt-2 grid ${checked ? 'mb-10' : 'mb-2 grid-cols-5'}`}>
+            <div className="space-y-3 col-span-3">
+                <Checkbox className="!text-base" checked={checked} onChange={handleCheck} value={label} />
+                {checked && (
+                    <div className="space-y-8">
+                        {types.map(({ type, label }) => (
+                            <div key={type}>
+                                <div className="grid grid-cols-5">
+                                    <AnalyticsSlider
+                                        {...activeProduct.slider}
+                                        onChange={(value) => setAnalyticsVolume(type, value)}
+                                        value={analyticsData[type].volume}
+                                        className="col-span-3"
+                                        label={label}
+                                    />
+                                    <div className="text-right font-bold m-0 self-end -mb-1.5 flex justify-end">
+                                        <NumericFormat
+                                            className="bg-transparent text-right p-0 focus:ring-0 max-w-[103px] border-t-0 border-x-0 border-dashed border-border dark:border-dark"
+                                            value={analyticsData[type].volume}
+                                            thousandSeparator=","
+                                            onValueChange={({ floatValue }) => setAnalyticsVolume(type, floatValue)}
+                                        />
+                                    </div>
+                                    <p className="text-right font-bold m-0 self-end -mb-1.5">
+                                        {formatUSD(analyticsData[type].cost)}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            {!checked && (
+                <>
+                    <span className="opacity-25 text-right">--</span>
+                    <span className="opacity-25 text-right">--</span>
+                </>
+            )}
         </div>
     )
 }
@@ -371,8 +407,6 @@ const analyticsSliders = [
 ]
 
 const ProductAnalyticsTab = ({ activeProduct, analyticsData, setAnalyticsVolume }) => {
-    const { slider } = activeProduct
-
     return (
         <div className="mb-4 pr-3">
             <div className="grid grid-cols-5 items-end mb-2">
@@ -381,42 +415,14 @@ const ProductAnalyticsTab = ({ activeProduct, analyticsData, setAnalyticsVolume 
                 <p className="m-0 text-right opacity-70 text-sm">Subtotal</p>
             </div>
 
-            {analyticsSliders.map(({ label, types, checked }) => (
+            {analyticsSliders.map((slider) => (
                 <SliderToggle
-                    key={label}
-                    label={label}
-                    onChange={(checked) =>
-                        types.forEach(({ type }) => setAnalyticsVolume(type, checked ? slider.min : 0))
-                    }
-                    checked={checked}
-                >
-                    <div className="space-y-8">
-                        {types.map(({ type, label }) => (
-                            <div key={type}>
-                                <div className="grid grid-cols-5">
-                                    <AnalyticsSlider
-                                        {...slider}
-                                        onChange={(value) => setAnalyticsVolume(type, value)}
-                                        value={analyticsData[type].volume}
-                                        className="col-span-3"
-                                        label={label}
-                                    />
-                                    <div className="text-right font-bold m-0 self-end -mb-1.5 flex justify-end">
-                                        <NumericFormat
-                                            className="bg-transparent text-right p-0 focus:ring-0 max-w-[103px] border-t-0 border-x-0 border-dashed border-border dark:border-dark"
-                                            value={analyticsData[type].volume}
-                                            thousandSeparator=","
-                                            onValueChange={({ floatValue }) => setAnalyticsVolume(type, floatValue)}
-                                        />
-                                    </div>
-                                    <p className="text-right font-bold m-0 self-end -mb-1.5">
-                                        {formatUSD(analyticsData[type].cost)}
-                                    </p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </SliderToggle>
+                    key={slider.label}
+                    analyticsData={analyticsData}
+                    setAnalyticsVolume={setAnalyticsVolume}
+                    activeProduct={activeProduct}
+                    {...slider}
+                />
             ))}
         </div>
     )
