00741530e56b5a758f6408b82aed55fdbc410468
Cory Watilo <cww@watilo.com>
2024-07-25 10:41:09 +0300

    modal loading state

--- src/components/Pricing/Test/PlanContent.tsx
+++ src/components/Pricing/Test/PlanContent.tsx
@@ -4,6 +4,7 @@ import { Link as ScrollLink } from 'react-scroll'
 import { TrackedCTA } from 'components/CallToAction'
 import usePostHog from 'hooks/usePostHog'
 import Modal from 'components/Modal'
+import Lottie from 'react-lottie'
 
 export const FreePlanContent = ({ onFreeTierClick }) => {
     return (
@@ -114,6 +115,19 @@ export default function PlanContent({ activePlan, onFreeTierClick }) {
     const [signupCountToday, setSignupCountToday] = useState(0)
     const [signupCoundLoading, setSignupCountLoading] = useState(true)
     const [modalOpen, setModalOpen] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleOpenModal = () => {
+        setIsLoading(true)
+        setModalOpen(true)
+        setTimeout(() => setIsLoading(false), 3000)
+    }
+
+    const [hogData, setHogData] = useState<any | null>(null)
+
+    useEffect(() => {
+        import('../../../../static/lotties/loading.json').then((data) => setHogData(data.default))
+    }, [])
 
     useEffect(() => {
         if (posthog?.isFeatureEnabled('direct-to-eu-cloud')) {
@@ -134,16 +148,34 @@ export default function PlanContent({ activePlan, onFreeTierClick }) {
     return (
         <>
             <Modal open={modalOpen} setOpen={setModalOpen}>
-                <div className="px-5">
-                    <div className="max-w-4xl mx-auto mt-12 relative rounded-md overflow-hidden">
-                        <iframe
-                            className="m-0"
-                            width="100%"
-                            height="400"
-                            src="https://app.posthog.com/embedded/gQMqaRP0ZH0V3P3XXrSDnNcqDGoe7Q?refresh=true"
-                        />
+                {isLoading ? (
+                    <>
+                        {hogData ? (
+                            <div className="size-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                                <Lottie
+                                    options={{
+                                        loop: true,
+                                        autoplay: true,
+                                        animationData: hogData,
+                                    }}
+                                />
+                            </div>
+                        ) : (
+                            <>Loading...</>
+                        )}
+                    </>
+                ) : (
+                    <div className="px-5">
+                        <div className="max-w-4xl mx-auto mt-12 relative rounded-md overflow-hidden">
+                            <iframe
+                                className="m-0"
+                                width="100%"
+                                height="400"
+                                src="https://app.posthog.com/embedded/gQMqaRP0ZH0V3P3XXrSDnNcqDGoe7Q?refresh=true"
+                            />
+                        </div>
                     </div>
-                </div>
+                )}
             </Modal>
             {activePlan === 'free' ? (
                 <FreePlanContent onFreeTierClick={onFreeTierClick} />
@@ -182,7 +214,7 @@ export default function PlanContent({ activePlan, onFreeTierClick }) {
                 >
                     {signupCountToday ? <strong>{signupCountToday}</strong> : 'Tons of'} companies signed up{' '}
                     {signupCountToday ? (
-                        <button className="font-bold text-red dark:text-yellow" onClick={() => setModalOpen(true)}>
+                        <button className="font-bold text-red dark:text-yellow" onClick={handleOpenModal}>
                             today
                         </button>
                     ) : (
