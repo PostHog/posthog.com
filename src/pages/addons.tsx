42d06258a17535ffe4e7e7f4570a6b6c9fc54ce1
Cory Watilo <cww@watilo.com>
2024-09-04 11:27:24 -0400

    array of all teams features

--- src/pages/addons.tsx
+++ src/pages/addons.tsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { pricingMenu } from '../navs'
 import Layout from 'components/Layout'
-import { SectionHeader } from 'components/Pricing/Test/Sections'
-import Link from 'components/Link'
-import Tooltip from 'components/Tooltip'
-import { IconAdvanced, IconInfo } from '@posthog/icons'
-import { CTA as PlanCTA } from 'components/Pricing/Plans'
-import { StaticImage } from 'gatsby-plugin-image'
+import { IconAdvanced } from '@posthog/icons'
 
-const FeatureItem = ({ icon: Icon, title, description }) => (
+// ... other imports ...
+
+const FeatureItem = ({ icon: Icon, name, description }) => (
     <div className="flex gap-2">
         <div className="shrink-0">
             <Icon className="size-8" />
         </div>
         <div className="flex-1">
-            <h4 className="text-base my-1">{title}</h4>
+            <h4 className="text-base my-1">{name}</h4>
             <p className="text-sm text-primary/75 dark:text-primary-dark/75">{description}</p>
         </div>
     </div>
@@ -22,20 +19,100 @@ const FeatureItem = ({ icon: Icon, title, description }) => (
 
 const features = [
     {
-        icon: IconAdvanced,
-        title: 'Unlimited projects',
-        description: 'Create silos of data within PostHog. All data belongs to a single project and all queries are project-specific.',
+        "group": "Features",
+        "items": [
+            {
+                "name": "Unlimited projects",
+                "description": "Create silos of data within PostHog. All data belongs to a single project and all queries are project-specific.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Managed reverse proxy",
+                "description": "Use our managed proxy service to send events through your own domain. Capture more usage data that might otherwise be intercepted by ad blockers without needing to set up and maintain the proxy yourself.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "White labeling",
+                "description": "Use your own branding on surveys, shared dashboards, shared insights, and more.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Ingestion taxonomy",
+                "description": "Mark events as verified or unverified to help you understand the quality of your data.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Dashboard tags",
+                "description": "Organize dashboards with tags.",
+                "icon": IconAdvanced
+            }
+        ]
     },
     {
-        icon: IconAdvanced,
-        title: 'Unlimited projects',
-        description: 'Create silos of data within PostHog. All data belongs to a single project and all queries are project-specific.',
+        "group": "Security",
+        "items": [
+            {
+                "name": "Enforce SSO login",
+                "description": "Users can only sign up and log in to your PostHog organization with your specified SSO provider.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Enforce 2FA",
+                "description": "Require all users in your organization to enable two-factor authentication.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Automatic provisioning",
+                "description": "Verify your domains to enforce SSO and automatically add users with matching email addresses to your organization.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Project permissions",
+                "description": "Restrict access to data within the organization to only those who need it.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Advanced permissions",
+                "description": "Control who can access and modify data and features within your organization.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Security assessment",
+                "description": "Security assessment",
+                "icon": IconAdvanced
+            }
+        ]
     },
     {
-        icon: IconAdvanced,
-        title: 'Unlimited projects',
-        description: 'Create silos of data within PostHog. All data belongs to a single project and all queries are project-specific.',
+        "group": "Compliance",
+        "items": [
+            {
+                "name": "Audit logs",
+                "description": "See who in your organization has accessed or modified entities within PostHog.",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "HIPAA BAA",
+                "description": "Get a signed HIPAA Business Associate Agreement (BAA) to use PostHog in a HIPAA-compliant manner.",
+                "icon": IconAdvanced
+            }
+        ]
     },
+    {
+        "group": "Support",
+        "items": [
+            {
+                "name": "Priority support",
+                "description": "Get help from our team faster than other customers with a 12-hour response time target",
+                "icon": IconAdvanced
+            },
+            {
+                "name": "Personalized onboarding",
+                "description": "Get help from our team to create dashboards that will help you understand your data and your business.",
+                "icon": IconAdvanced
+            }
+        ]
+    }
 ]
 
 const Addons = (): JSX.Element => {
@@ -54,16 +131,21 @@ const Addons = (): JSX.Element => {
                 </div>
                 <div className="col-span-12 md:col-span-8">
                     <div className="grid md:grid-cols-2 gap-x-8 gap-y-4">
-                        <div className="col-span-2">
-                            <h3 className="text-lg pt-1 mb-0">Features</h3>
-                        </div>
-                        {features.map((feature, index) => (
-                            <FeatureItem key={index} {...feature} />
+                        {features.map((featureGroup, groupIndex) => (
+                            <React.Fragment key={groupIndex}>
+                                <div className="col-span-2">
+                                    <h3 className="text-lg pt-1 mb-0">{featureGroup.group}</h3>
+                                </div>
+                                {featureGroup.items.map((feature, itemIndex) => (
+                                    <FeatureItem key={`${groupIndex}-${itemIndex}`} {...feature} />
+                                ))}
+                            </React.Fragment>
                         ))}
                     </div>
                 </div>
             </div>
         </section>
+
     </Layout>
 }
 
