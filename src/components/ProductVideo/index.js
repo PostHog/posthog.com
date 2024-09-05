1b2783ddb6745e91a5b8eb00dc4f3ffe84da8e6e
Eli Kinsey <eli@ekinsey.dev>
2024-09-05 16:24:04 -0700

    fix autoplay

--- src/components/ProductVideo/index.js
+++ src/components/ProductVideo/index.js
@@ -1,24 +1,26 @@
 import React from 'react'
 
-export const ProductVideo = ({ videoLight, videoDark, autoplay = true, classes = '' }) => {
+export const ProductVideo = ({ videoLight, videoDark, autoPlay = true, classes = '' }) => {
     return (
         <div className="mb-4 border border-light dark:border-dark rounded bg-accent dark:bg-accent-dark">
             <video
-                autoPlay={autoplay}
+                autoPlay={autoPlay}
                 loop
                 muted
                 playsInline
                 src={videoLight}
                 className={`${videoDark ? 'dark:hidden' : ''} ${classes}`}
+                controls={!autoPlay}
             ></video>
             {videoDark && (
                 <video
-                    autoPlay={autoplay}
+                    autoPlay={autoPlay}
                     loop
                     muted
                     playsInline
                     src={videoDark}
                     className={`hidden dark:block ${classes}`}
+                    controls={!autoPlay}
                 ></video>
             )}
         </div>
