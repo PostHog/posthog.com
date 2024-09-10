bb6d94ccd0a2fee909c508aac0c49b1c061c9459
Eli Kinsey <eli@ekinsey.dev>
2024-09-09 18:39:35 -0700

    source teams features from API

--- src/pages/addons.tsx
+++ src/pages/addons.tsx
@@ -8,16 +8,16 @@ import { PricingTiers } from 'components/Pricing/Plans'
 import { Link } from 'react-scroll'
 import { StaticImage } from 'gatsby-plugin-image'
 import { CallToAction } from 'components/CallToAction'
+import groupBy from 'lodash.groupby'
 
-const FeatureItem = ({ icon: Icon, name, description, size }) => (
+const FeatureItem = ({ name, description, size }) => (
     <div className="flex gap-2">
-        <div className="shrink-0 hidden">
-            <Icon className="size-8" />
-        </div>
         <div className="flex-1">
             <h4 className="text-base my-1">{name}</h4>
             <p
-                className={`${size === 'small' ? 'text-sm [&_li]:text-sm' : 'text-[15px] [&_li]:text-[15px]'} text-primary/75 dark:text-primary-dark/75 mb-0`}
+                className={`${
+                    size === 'small' ? 'text-sm [&_li]:text-sm' : 'text-[15px] [&_li]:text-[15px]'
+                } text-primary/75 dark:text-primary-dark/75 mb-0`}
                 dangerouslySetInnerHTML={{ __html: description }}
             />
         </div>
@@ -25,114 +25,6 @@ const FeatureItem = ({ icon: Icon, name, description, size }) => (
 )
 
 const addons = [
-    {
-        name: 'Teams',
-        features: [
-            {
-                group: 'Features',
-                items: [
-                    {
-                        name: 'Unlimited projects',
-                        description:
-                            'Create silos of data within PostHog. All data belongs to a single project and all queries are project-specific.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Managed reverse proxy',
-                        description:
-                            'Use our managed proxy service to send events through your own domain. Capture more usage data that might otherwise be intercepted by ad blockers without needing to set up and maintain the proxy yourself.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'White labeling',
-                        description: 'Use your own branding on surveys, shared dashboards, shared insights, and more.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Ingestion taxonomy',
-                        description:
-                            'Mark events as verified or unverified to help you understand the quality of your data.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Dashboard tags',
-                        description: 'Organize dashboards with tags.',
-                        icon: IconAdvanced,
-                    },
-                ],
-            },
-            {
-                group: 'Security',
-                items: [
-                    {
-                        name: 'Enforce SSO login',
-                        description:
-                            'Users can only sign up and log in to your PostHog organization with your specified SSO provider.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Enforce 2FA',
-                        description: 'Require all users in your organization to enable two-factor authentication.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Automatic provisioning',
-                        description:
-                            'Verify your domains to enforce SSO and automatically add users with matching email addresses to your organization.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Project permissions',
-                        description: 'Restrict access to data within the organization to only those who need it.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Advanced permissions',
-                        description: 'Control who can access and modify data and features within your organization.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Security assessment',
-                        description: 'Security assessment',
-                        icon: IconAdvanced,
-                    },
-                ],
-            },
-            {
-                group: 'Compliance',
-                items: [
-                    {
-                        name: 'Audit logs',
-                        description: 'See who in your organization has accessed or modified entities within PostHog.',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'HIPAA BAA',
-                        description:
-                            'Get a signed HIPAA Business Associate Agreement (BAA) to use PostHog in a HIPAA-compliant manner.',
-                        icon: IconAdvanced,
-                    },
-                ],
-            },
-            {
-                group: 'Support',
-                items: [
-                    {
-                        name: 'Priority support',
-                        description:
-                            'Get help from our team faster than other customers with a 12-hour response time target',
-                        icon: IconAdvanced,
-                    },
-                    {
-                        name: 'Personalized onboarding',
-                        description:
-                            'Get help from our team to create dashboards that will help you understand your data and your business.',
-                        icon: IconAdvanced,
-                    },
-                ],
-            },
-        ],
-    },
     {
         name: 'Data pipelines',
         features: [
@@ -204,13 +96,12 @@ const addons = [
                     {
                         name: 'Merge anonymous users with their eventual identified user',
                         description:
-                            'Like when they sign up for your product or use different devices - enables analyzing the user\'s path',
+                            "Like when they sign up for your product or use different devices - enables analyzing the user's path",
                         icon: IconAdvanced,
                     },
                     {
                         name: 'Store custom properties on users',
-                        description:
-                            'Use these properties in cohorts, session replay, experiments, and feature flags',
+                        description: 'Use these properties in cohorts, session replay, experiments, and feature flags',
                         icon: IconAdvanced,
                     },
                     {
@@ -224,6 +115,23 @@ const addons = [
         ],
     },
 ]
+
+const Features = ({ title, addonName, features }) => {
+    return (
+        <React.Fragment>
+            <div className={addonName === 'Teams' ? 'md:col-span-2 pt-8 first:pt-0' : ''}>
+                <h3 className="text-xl pt-1 mb-0">{title}</h3>
+            </div>
+            {features.map((feature, itemIndex) => (
+                <FeatureItem
+                    key={`${addonName}-${title}-${itemIndex}`}
+                    size={addonName === 'Teams' ? 'small' : ''}
+                    {...feature}
+                />
+            ))}
+        </React.Fragment>
+    )
+}
 
 const Addons = (): JSX.Element => {
     const platform = usePlatform()
@@ -239,7 +147,8 @@ const Addons = (): JSX.Element => {
                     <div className="flex-1">
                         <h1 className="text-4xl xl:text-5xl mb-2">Add-ons</h1>
                         <p className="text-[17px] 2xl:text-lg font-semibold opacity-75">
-                            We've moved specialized functionality into add-ons so you never pay for things you don't need.
+                            We've moved specialized functionality into add-ons so you never pay for things you don't
+                            need.
                         </p>
                         <div className="max-w-sm rounded border border-light dark:border-dark bg-accent dark:bg-accent-dark p-4">
                             <div className="font-semibold opacity-70 mb-1">Jump to:</div>
@@ -263,72 +172,93 @@ const Addons = (): JSX.Element => {
                         </div>
                     </div>
                     <aside className="max-w-full -mt-6 md:mt-0 md:max-w-xs mdlg:max-w-sm lg:max-w-lg lg:-my-6 flex justify-end">
-                        <StaticImage src="https://res.cloudinary.com/dmukukwp6/image/upload/add_ons_a700ab577f.png" alt="Add-ons" objectFit="contain" />
+                        <StaticImage
+                            src="https://res.cloudinary.com/dmukukwp6/image/upload/add_ons_a700ab577f.png"
+                            alt="Add-ons"
+                            objectFit="contain"
+                        />
                     </aside>
                 </div>
-                {
-                    allAddons.map((addon) => {
-                        const { name, description, plans, unit, type } = addon
-                        const plan = plans[plans.length - 1]
-                        const freeAllocation = plan?.tiers?.find((tier) => tier.unit_amount_usd === '0')?.up_to
+                {allAddons.map((addon) => {
+                    const { name, description, plans, unit, type, features } = addon
+                    const plan = plans[plans.length - 1]
+                    const freeAllocation = plan?.tiers?.find((tier) => tier.unit_amount_usd === '0')?.up_to
+                    const featuresByCategory = groupBy(features, (feature) => feature.category || 'Features')
+                    const customAddon = addons.find((a) => a.name === name)
 
-                        return (
-                            <div key={name} className="grid md:grid-cols-12 gap-x-12 gap-y-4 mt-12" id={name.toLowerCase().replace(/\s+/g, '-')}>
-                                <div className="md:col-span-12 border-b border-light dark:border-dark pb-2">
-                                    <h2 className="mb-1">{name}</h2>
-                                </div>
-                                <div className="md:col-span-4 md:sticky top-[120px] self-start">
-                                    <p className="text-[15px]">{description}</p>
-                                    {plan?.flat_rate ? (
-                                        <div className="flex items-baseline">
-                                            <strong className="text-xl">${plan.unit_amount_usd.replace('.00', '')}</strong>
-                                            <span className="text-[15px] opacity-60">/mo</span>
-                                        </div>
-                                    ) : (
-                                        <>
-                                            <span className="opacity-70 text-sm">Pricing starts at</span>{' '}
-                                            <strong>
-                                                $
-                                                {plan?.tiers?.find((tier) => tier.unit_amount_usd !== '0')?.unit_amount_usd}
-                                            </strong>
-                                            <span className="opacity-70 text-sm">/{unit}</span>
-                                        </>
-                                    )}
-                                    {freeAllocation && (
-                                        <p className="m-0 text-green text-sm">
-                                            First <strong>{freeAllocation?.toLocaleString()}</strong> {unit}s/mo free
-                                        </p>
-                                    )}
-                                </div>
-                                <div className="md:col-span-8">
-                                    <div className={`grid gap-x-8 gap-y-3 ${name === "Teams" ? 'md:grid-cols-2' : 'md:grid-cols-1'}`}>
-                                        {addons
-                                            .find((addon) => addon.name === name)
-                                            ?.features?.map((featureGroup, groupIndex) => (
-                                                <React.Fragment key={groupIndex}>
-                                                    <div className={name === "Teams" ? 'md:col-span-2 pt-8 first:pt-0' : ''}>
-                                                        <h3 className="text-xl pt-1 mb-0">{featureGroup.group}</h3>
-                                                    </div>
-                                                    {featureGroup.items.map((feature, itemIndex) => (
-                                                        <FeatureItem key={`${groupIndex}-${itemIndex}`} size={name === "Teams" ? 'small' : ''} {...feature} />
-                                                    ))}
-
-                                                </React.Fragment>
-                                            ))}
-                                    </div>
-                                    {!plan?.flat_rate && (
-                                        <div className="max-w-[400px] mt-8">
-                                            <h5 className="mb-2 text-lg">Pricing breakdown</h5>
-                                            <div className="border border-light dark:border-dark rounded divide-y divide-light dark:divide-dark bg-accent/50 dark:bg-accent-dark">
-                                                <PricingTiers plans={plans} type={type} unit={unit} test />
-                                            </div>
-                                        </div>
-                                    )}
-                                </div>
+                    return (
+                        <div
+                            key={name}
+                            className="grid md:grid-cols-12 gap-x-12 gap-y-4 mt-12"
+                            id={name.toLowerCase().replace(/\s+/g, '-')}
+                        >
+                            <div className="md:col-span-12 border-b border-light dark:border-dark pb-2">
+                                <h2 className="mb-1">{name}</h2>
                             </div>
-                        )
-                    })
-                }
+                            <div className="md:col-span-4 md:sticky top-[120px] self-start">
+                                <p className="text-[15px]">{description}</p>
+                                {plan?.flat_rate ? (
+                                    <div className="flex items-baseline">
+                                        <strong className="text-xl">${plan.unit_amount_usd.replace('.00', '')}</strong>
+                                        <span className="text-[15px] opacity-60">/mo</span>
+                                    </div>
+                                ) : (
+                                    <>
+                                        <span className="opacity-70 text-sm">Pricing starts at</span>{' '}
+                                        <strong>
+                                            $
+                                            {plan?.tiers?.find((tier) => tier.unit_amount_usd !== '0')?.unit_amount_usd}
+                                        </strong>
+                                        <span className="opacity-70 text-sm">/{unit}</span>
+                                    </>
+                                )}
+                                {freeAllocation && (
+                                    <p className="m-0 text-green text-sm">
+                                        First <strong>{freeAllocation?.toLocaleString()}</strong> {unit}s/mo free
+                                    </p>
+                                )}
+                            </div>
+                            <div className="md:col-span-8">
+                                <div
+                                    className={`grid gap-x-8 gap-y-3 ${
+                                        name === 'Teams' ? 'md:grid-cols-2' : 'md:grid-cols-1'
+                                    }`}
+                                >
+                                    {customAddon
+                                        ? customAddon.features?.map((feature) => {
+                                              return (
+                                                  <Features
+                                                      key={`${name}-${feature.group}`}
+                                                      addonName={name}
+                                                      features={feature.items}
+                                                      title={feature.group}
+                                                  />
+                                              )
+                                          })
+                                        : Object.keys(featuresByCategory)?.map((category) => {
+                                              const features = featuresByCategory[category]
+                                              return (
+                                                  <Features
+                                                      key={`${name}-${category}`}
+                                                      title={category}
+                                                      addonName={name}
+                                                      features={features}
+                                                  />
+                                              )
+                                          })}
+                                </div>
+                                {!plan?.flat_rate && (
+                                    <div className="max-w-[400px] mt-8">
+                                        <h5 className="mb-2 text-lg">Pricing breakdown</h5>
+                                        <div className="border border-light dark:border-dark rounded divide-y divide-light dark:divide-dark bg-accent/50 dark:bg-accent-dark">
+                                            <PricingTiers plans={plans} type={type} unit={unit} test />
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    )
+                })}
                 <div className="my-12 border-t border-light dark:border-dark pt-6 flex flex-col items-center">
                     <p>Subscribe to add-ons after signing up.</p>
 
@@ -342,8 +272,8 @@ const Addons = (): JSX.Element => {
                         Get started
                     </CallToAction>
                 </div>
-            </section >
-        </Layout >
+            </section>
+        </Layout>
     )
 }
 
