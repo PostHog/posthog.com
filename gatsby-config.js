5b68de9108163399eca70a4f5f9011825b599881
Eli Kinsey <eli@ekinsey.dev>
2024-09-12 05:07:32 -0700

    disable mermaid

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,9 +77,9 @@ module.exports = {
                 extensions: ['.mdx', '.md'],
                 gatsbyRemarkPlugins: [
                     { resolve: 'gatsby-remark-autolink-headers', options: { icon: false } },
-                    {
-                        resolve: require.resolve(`./plugins/gatsby-remark-mermaid`),
-                    },
+                    // {
+                    //     resolve: require.resolve(`./plugins/gatsby-remark-mermaid`),
+                    // },
                     {
                         resolve: require.resolve('./plugins/gatsby-remark-video'),
                     },
