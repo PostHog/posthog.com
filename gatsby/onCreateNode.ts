4f00983458fc12d2951c06fe657eca2e397efe77
Eli Kinsey <eli@ekinsey.dev>
2024-09-12 04:18:34 -0700

    conditionally fetch image metadata

--- gatsby/onCreateNode.ts
+++ gatsby/onCreateNode.ts
@@ -115,15 +115,22 @@ export const onCreateNode: GatsbyNode['onCreateNode'] = async ({
                 if (!cloudinaryData) {
                     console.warn(`Cloudinary data not found for ${publicId}`)
                 }
+
+                const hasData = publicId && cloudinaryData?.format && cloudinaryData?.width && cloudinaryData?.height
+
                 node.frontmatter[field] = {
                     publicURL: node.frontmatter?.[field],
-                    childImageSharp: {
-                        cloudName: process.env.GATSBY_CLOUDINARY_CLOUD_NAME,
-                        publicId,
-                        originalFormat: cloudinaryData?.format,
-                        originalWidth: cloudinaryData?.width,
-                        originalHeight: cloudinaryData?.height,
-                    },
+                    ...(hasData
+                        ? {
+                              childImageSharp: {
+                                  cloudName: process.env.GATSBY_CLOUDINARY_CLOUD_NAME,
+                                  publicId,
+                                  originalFormat: cloudinaryData?.format,
+                                  originalWidth: cloudinaryData?.width,
+                                  originalHeight: cloudinaryData?.height,
+                              },
+                          }
+                        : {}),
                 }
             }
         })
