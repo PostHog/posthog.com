e33faa6ebe6025298623e387565c67ce36abbd8b
Cory Watilo <cww@watilo.com>
2024-09-19 12:09:15 -0400

    copied wip page into roadmap page

--- src/components/Roadmap/index.tsx
+++ src/components/Roadmap/index.tsx
@@ -20,6 +20,7 @@ import { companyMenu } from '../../navs'
 import Fuse from 'fuse.js'
 import Tooltip from 'components/Tooltip'
 import Spinner from 'components/Spinner'
+import { InProgress } from 'components/Roadmap/InProgress'
 
 interface IGitHubPage {
     title: string
@@ -282,6 +283,53 @@ export default function Roadmap() {
     const teams = Object.keys(roadmapsGroupedByTeam).sort()
     const isModerator = user?.role?.type === 'moderator'
 
+    // WIP logic
+    const wipRoadmaps = useRoadmaps({
+        params: {
+            filters: {
+                $and: [
+                    {
+                        complete: {
+                            $ne: true,
+                        },
+                    },
+                    {
+                        projectedCompletion: {
+                            $notNull: true,
+                        },
+                    },
+                ],
+            },
+        },
+    }).roadmaps
+
+    const wipRoadmapsGroupedByTeam = groupBy(
+        wipRoadmaps,
+        (roadmap) => `${roadmap.attributes.teams?.data?.[0]?.attributes?.name ?? 'Any'} Team`
+    )
+    const wipTeams = Object.keys(wipRoadmapsGroupedByTeam).sort()
+    const wipIsLoading = useRoadmaps({
+        params: {
+            filters: {
+                $and: [
+                    {
+                        complete: {
+                            $ne: true,
+                        },
+                    },
+                    {
+                        projectedCompletion: {
+                            $notNull: true,
+                        },
+                    },
+                ],
+            },
+        },
+    }).isLoading
+
+    const params = new URLSearchParams(search)
+    const roadmapID = params.get('id')
+
     return (
         <CommunityLayout
             parent={companyMenu}
@@ -434,7 +482,83 @@ export default function Roadmap() {
                         )}
                     </div>
                     <div>
-                        column 2
+                        <section>
+                            <div className="relative mb-6">
+                                <div className="flex gap-4 items-center">
+                                    <h1 className="font-bold text-3xl sm:text-4xl my-0">Work in progress</h1>
+                                    {isModerator && !adding && (
+                                        <div className="relative top-1">
+                                            <CallToAction onClick={() => setAdding(true)} size="xs" type="secondary">
+                                                <Tooltip content="Only moderators can see this" placement="top">
+                                                    <IconShieldLock className="w-6 h-6 inline-block mr-1" />
+                                                </Tooltip>
+                                                Add a feature
+                                            </CallToAction>
+                                        </div>
+                                    )}
+                                </div>
+                                <p className="my-0 font-semibold mt-2 mb-4">
+                                    <span className="opacity-70">
+                                        Here's what we're working on right now.
+                                    </span>
+                                </p>
+                            </div>
+
+                            {isModerator && adding && (
+                                <div className="mt-2 mb-8">
+                                    <RoadmapForm
+                                        status="in-progress"
+                                        onSubmit={() => {
+                                            mutate()
+                                            setAdding(false)
+                                        }}
+                                    />
+                                </div>
+                            )}
+
+                            <div className="pb-12">
+                                {wipIsLoading ? (
+                                    <Skeleton />
+                                ) : (
+                                    <ul className="@container list-none m-0 p-0 pb-4 grid gap-y-8">
+                                        {wipTeams.map((team) => {
+                                            const roadmaps = wipRoadmapsGroupedByTeam[team]
+                                            return (
+                                                <li className="relative" key={team}>
+                                                    <h4 className="text-lg m-0 mb-6 pr-2 inline-flex items-center bg-light dark:bg-dark after:-z-10 after:absolute after:w-full after:h-[1px] after:bg-border after:dark:bg-border-dark after:translate-y-[2px]">
+                                                        {team}
+                                                    </h4>
+                                                    <ul className="m-0 p-0 list-none flex flex-col gap-8">
+                                                        {roadmaps.map((roadmap) => {
+                                                            const { id, attributes } = roadmap
+                                                            return (
+                                                                <li key={id}>
+                                                                    <UpdateWrapper
+                                                                        key={id}
+                                                                        id={id}
+                                                                        roundButton={true}
+                                                                        status="in-progress"
+                                                                        formClassName="mb-4"
+                                                                        editButtonClassName={'absolute top-3 right-0 z-10'}
+                                                                        onSubmit={() => mutate()}
+                                                                    >
+                                                                        <InProgress
+                                                                            {...attributes}
+                                                                            squeakId={id}
+                                                                            modalOpen={roadmapID == id}
+                                                                        />
+                                                                    </UpdateWrapper>
+                                                                </li>
+                                                            )
+                                                        })}
+                                                    </ul>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                )}
+                            </div>
+                        </section>
                     </div>
                 </div>
             </section>
