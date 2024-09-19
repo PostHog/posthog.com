cb084ba260e183bcde74dbbebf33e52de4f68071
Cory Watilo <cww@watilo.com>
2024-09-19 15:46:42 -0400

    show job summaries at top of page (unstyled)

--- src/components/Careers/CareersHero/index.tsx
+++ src/components/Careers/CareersHero/index.tsx
@@ -1,53 +1,87 @@
-import React from 'react'
-import scrollTo from 'gatsby-plugin-smoothscroll'
+import React, { useState, useEffect } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
+import Link from 'components/Link'
 
-import { Header } from '../../Header'
-import { CallToAction } from '../../CallToAction'
-
+const query = graphql`
+    query CareersHero {
+        allAshbyJobPosting(filter: { isListed: { eq: true } }) {
+            jobs: nodes {
+                fields {
+                    title
+                    slug
+                    html
+                }
+                parent {
+                    ... on AshbyJob {
+                        customFields {
+                            value
+                            title
+                        }
+                    }
+                }
+                externalLink
+                departmentName
+                info {
+                    descriptionHtml
+                }
+            }
+            departments: group(field: departmentName) {
+                title: fieldValue
+            }
+        }
+    }
+`
 export const CareersHero = () => {
-    const { jobs } = useStaticQuery(query)
+    const {
+        allAshbyJobPosting: { departments, jobs },
+    } = useStaticQuery(query)
+
+    const [selectedJob, setSelectedJob] = useState(jobs[0])
+    const [processedHtml, setProcessedHtml] = useState('')
+
+    useEffect(() => {
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(selectedJob.fields.html, 'text/html')
+
+        const whoWereLookingFor = doc.querySelector('details:has(h2[id="who-we\'re-looking-for"])')
+        const whatYoullBeDoing = doc.querySelector('details:has(h2[id="what-you\'ll-be-doing"])')
+
+        let content = ''
+        if (whoWereLookingFor) {
+            content = whoWereLookingFor.outerHTML
+        } else if (whatYoullBeDoing) {
+            content = whatYoullBeDoing.outerHTML
+        }
+
+        setProcessedHtml(content)
+    }, [selectedJob])
 
     return (
-        <div className="careers-hero lg:pt-12 pb-12 md:pb-18">
-            <div className="relative rounded">
-                <div className="md:absolute -top-8 xl:-top-6 2xl:-top-8 right-0 md:w-1/2 lg:w-[45%] 2xl:w-[45%] max-w-5xl">
-                    <StaticImage
-                        src="../../../images/construction-hogs-with-text.png"
-                        alt="Construction hogs taking a lunch break"
-                        className="w-full h-full"
-                        placeholder="blurred"
-                    />
-                </div>
-
-                <div className="text-center md:text-left max-w-7xl mx-auto relative z-10 px-4 md:pr-0 lg:pl-8">
-                    <h1 className="mb-0 pt-4 md:pt-0 pb-0 text-4xl leading-none lg:text-5xl 2xl:text-6xl md:w-2/3 lg:w-[55%] xl:[w-2/3]">
-                        We're working to increase the number of successful products{' '}
-                        <br className="hidden md:block mdlg:hidden" />
-                        in the world.
-                        <br />
-                        <span className="text-red">We could use your help.</span>
-                    </h1>
-
-                    <CallToAction
-                        onClick={() => scrollTo('#open-roles')}
-                        type="primary"
-                        width="72"
-                        className="my-6 sm:my-12"
-                    >
-                        Jump to open roles ({jobs.totalCount})
-                    </CallToAction>
+        <div className="flex flex-col md:flex-row">
+            <div className="w-full md:w-1/4">
+                <ul className="list-none p-0 m-0">
+                    {jobs.map((job) => (
+                        <li key={job.fields.title} className="mb-2">
+                            <button
+                                className={`w-full text-left p-4 ${selectedJob.fields.title === job.fields.title ? 'bg-gray-200' : 'bg-white'}`}
+                                onClick={() => setSelectedJob(job)}
+                            >
+                                {job.fields.title}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="w-full md:w-3/4 p-4">
+                <h2 className="text-2xl font-bold">{selectedJob.fields.title}</h2>
+                <p className="text-gray-600">{selectedJob.departmentName}</p>
+                <div className="mt-4">
+                    <div dangerouslySetInnerHTML={{ __html: processedHtml }} />
+                    <Link to={selectedJob.fields.slug} className="mt-4 inline-block bg-yellow-500 text-white px-4 py-2 rounded">
+                        Read more
+                    </Link>
                 </div>
             </div>
         </div>
     )
 }
-
-const query = graphql`
-    {
-        jobs: allAshbyJobPosting {
-            totalCount
-        }
-    }
-`
