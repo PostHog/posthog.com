b0119e2c57ca8c1f723dd2c349289f4d7284df5c
Cory Watilo <cww@watilo.com>
2024-09-23 13:39:47 -0700

    equity terms

--- src/components/Careers/Compensation/index.tsx
+++ src/components/Careers/Compensation/index.tsx
@@ -3,9 +3,29 @@ import Link from 'components/Link'
 import { CompensationCalculator } from 'components/CompensationCalculator'
 import { IconChevronDown } from '@posthog/icons'
 
-
-
 const Compensation: React.FC = () => {
+  const equityTerms = [
+    {
+      title: '10 years to exercise your options',
+      subtitle: '(if you leave)',
+      description: 'Most companies give you 90 days.',
+    },
+    {
+      title: 'Double-trigger acceleration',
+      description: "You receive all your options in the event you're let go due to the company being acquired.",
+    },
+    {
+      title: 'Option to trade cash for equity',
+      subtitle: '(when getting a raise)',
+      description: 'You can trade up to 100% of any pay rise you receive for share options at a 1:1 ratio of cash to equity.',
+    },
+    {
+      title: 'Employee secondaries',
+      subtitle: '(when offered)',
+      description: 'In 2024, we held our first secondary for early employees to sell some of their shares. We plan on doing more in the future.',
+    },
+  ]
+
   return (
     <section className="max-w-7xl mx-auto px-4 py-8">
       <h2 className="text-4xl font-bold mb-2 text-center">Compensation &amp; equity</h2>
@@ -20,17 +40,17 @@ const Compensation: React.FC = () => {
         <div className="bg-accent dark:bg-accent-dark p-4 lg:p-6 rounded">
           <h3 className="text-xl mb-4">(Really) employee-friendly equity terms</h3>
 
-          <ul className="list-none p-0 grid gap-2">
-            <li>
-              <p className="relative pl-7">
+          <ul className="list-none p-0 pb-4 grid gap-3">
+            {equityTerms.map((term, index) => (
+              <li key={index} className="relative pl-7 leading-snug">
                 <IconChevronDown className="w-6 h-6 inline-block -rotate-90 absolute top-0 left-0 opacity-50" />
-                <strong>10 years to exercise your options</strong> <span className="opacity-60">(if you leave)</span><br />
-                <span className="opacity-60 text-[15px]">Most companies give you 90 days</span>
-              </p>
-            </li>
+                <strong>{term.title}</strong> {term.subtitle && <span className="opacity-60">{term.subtitle}</span>}<br />
+                <span className="opacity-60 text-[15px]">{term.description}</span>
+              </li>
+            ))}
           </ul>
 
-          <p>Learn more about <Link href="handbook/people/share-options">share options</Link>.</p>
+          <p><Link href="handbook/people/share-options" className="font-semibold text-red dark:text-yellow">Learn more about share options</Link></p>
         </div>
       </div>
     </section>
