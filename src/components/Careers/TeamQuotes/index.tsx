35c3ee9d2d63a2b89f64d48ce109250e518b8d7e
Cory Watilo <cww@watilo.com>
2024-09-23 18:38:44 -0700

    quote ui

--- src/components/Careers/TeamQuotes/index.tsx
+++ src/components/Careers/TeamQuotes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'components/Link'
-
+import { StaticImage } from 'gatsby-plugin-image'
 
 const TeamQuotes: React.FC = () => {
   return (
@@ -8,12 +8,29 @@ const TeamQuotes: React.FC = () => {
       <h2 className="text-center text-4xl mb-2">Things we definitely didn't coerce anyone into saying...</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-accent dark:bg-accent-dark p-4 rounded-lg">
-          <div className="bg-white dark:bg-accent-dark rounded-md p-2">
-            <p className="mb-0">Quote goes here</p>
+        <div className="flex flex-col items-center">
+          <div className="bg-white dark:bg-accent-dark rounded px-6 py-4 mb-4 shadow-lg">
+            <p className="mb-0 text-center">
+              I wake up so excited to get to work that sometimes I pinch myself to make sure I'm not dreaming. The combination of high expectations, high trust, high autonomy, and high reward is so unique and drives me to improve every day.
+            </p>
           </div>
 
-          <p className="text-sm opacity-75">- John Doe, Product Manager</p>
+          <div className="p-0.5 bg-white dark:bg-accent-dark rounded-full border-2 border-light dark:border-dark">
+            <div className="bg-red size-20 rounded-full overflow-hidden">
+              <StaticImage
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/v1688575125/paul_64ee2de98e.png"
+                alt="John Doe"
+                width={80}
+                height={80}
+                className=""
+              />
+            </div>
+          </div>
+
+          <div className="leading-tight text-center mt-1">
+            <Link href="/handbook/team/paul" className="text-red dark:text-yellow font-semibold inline-block">Paul</Link><br />
+            <span className="text-sm opacity-75">Product Manager</span>
+          </div>
         </div>
       </div>
 
