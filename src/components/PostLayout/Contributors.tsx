de7422e31310eebf127c904fdea2590aba745b38
Eli Kinsey <eli@ekinsey.dev>
2024-09-24 17:30:34 -0700

    optimize cloud images

--- src/components/PostLayout/Contributors.tsx
+++ src/components/PostLayout/Contributors.tsx
@@ -3,15 +3,38 @@ import Tooltip from 'components/Tooltip'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import React from 'react'
 import { IContributor } from './types'
+import { Image, Transformation } from 'cloudinary-react'
+
+const isCloudinaryImage = (url: string): boolean => {
+    const cloudinaryUrlPattern = new RegExp(`https://res.cloudinary.com/${process.env.GATSBY_CLOUDINARY_CLOUD_NAME}/`)
+    return cloudinaryUrlPattern.test(url)
+}
+
+const getCloudinaryPublicId = (url: string): string | null => {
+    const cloudinaryUrlPattern = /https:\/\/res\.cloudinary\.com\/[^/]+\/(?:image|video)\/upload\/(?:v\d+\/)?([^/.]+)/
+    const match = url.match(cloudinaryUrlPattern)
+    return match ? match[1] : null
+}
 
 export const ContributorImageSmall = ({ image, name, className = '', imgClassName = '' }) => {
     const gatsbyImage = image && getImage(image)
+    const cloudinaryPublicId = typeof image === 'string' && isCloudinaryImage(image) && getCloudinaryPublicId(image)
     return (
         <div
             className={`w-[32px] h-[32px] relative rounded-full overflow-hidden border-2 border-tan dark:border-primary transition-all ${className}`}
         >
             {typeof image === 'string' ? (
-                <img className={`rounded-full ${imgClassName}`} src={image} />
+                cloudinaryPublicId ? (
+                    <Image
+                        className={`rounded-full ${imgClassName}`}
+                        publicId={cloudinaryPublicId}
+                        cloudName={process.env.GATSBY_CLOUDINARY_CLOUD_NAME}
+                    >
+                        <Transformation width="50" crop="scale" />
+                    </Image>
+                ) : (
+                    <img className={`rounded-full ${imgClassName}`} src={image} />
+                )
             ) : gatsbyImage ? (
                 <GatsbyImage
                     imgClassName={`rounded-full ${imgClassName}`}
@@ -39,13 +62,15 @@ export const ContributorImage = ({ image, name, compact, rounded }) => {
     const gatsbyImage = image && getImage(image)
     return (
         <figure
-            className={`${compact
-                ? `flex-shrink-0 relative size-12 ${rounded ? '' : 'self-end'}`
-                : 'm-0 -mt-8 p-0 absolute right-0 bottom-0'
-                } ${rounded
+            className={`${
+                compact
+                    ? `flex-shrink-0 relative size-12 ${rounded ? '' : 'self-end'}`
+                    : 'm-0 -mt-8 p-0 absolute right-0 bottom-0'
+            } ${
+                rounded
                     ? `rounded-full overflow-hidden border-2 border-tan dark:border-primary ${compact ? 'mr-1' : ''}`
                     : ''
-                }`}
+            }`}
         >
             {typeof image === 'string' ? (
                 <img className={compact ? 'absolute w-full h-full object-cover' : 'w-24 h-24'} src={image} />
@@ -97,9 +122,11 @@ export const Contributor = ({
     return (
         <Container
             {...(url ? { to: url, state } : {})}
-            className={`${compact ? 'overflow-hidden' : ''
-                } flex bg-${color ? color : 'accent'} dark:bg-${color ? color : 'accent-dark'} border border-light dark:border-dark md:mx-4 rounded relative hover:-translate-y-0.5 active:translate-y-0 hover:transition-all hover:border-b-[4px] active:border-b-1 active:top-[2px] justify-between text-primary dark:text-primary-dark hover:text-primary dark:hover:text-primary-dark ${roundedImage ? 'items-center' : ''
-                }`}
+            className={`${compact ? 'overflow-hidden' : ''} flex bg-${color ? color : 'accent'} dark:bg-${
+                color ? color : 'accent-dark'
+            } border border-light dark:border-dark md:mx-4 rounded relative hover:-translate-y-0.5 active:translate-y-0 hover:transition-all hover:border-b-[4px] active:border-b-1 active:top-[2px] justify-between text-primary dark:text-primary-dark hover:text-primary dark:hover:text-primary-dark ${
+                roundedImage ? 'items-center' : ''
+            }`}
         >
             <div className={compact ? '' : 'pr-20'}>
                 <div className="flex flex-col justify-between px-4 py-2 w-full gap-0.5">
@@ -108,8 +135,9 @@ export const Contributor = ({
                     </h3>
                     {role && (
                         <p
-                            className={`text-primary/50 dark:text-primary-dark/50 m-0 leading-tight text-sm line-clamp-1 ${compact ? 'text-[13px]' : 'text-sm'
-                                }`}
+                            className={`text-primary/50 dark:text-primary-dark/50 m-0 leading-tight text-sm line-clamp-1 ${
+                                compact ? 'text-[13px]' : 'text-sm'
+                            }`}
                         >
                             {role}
                         </p>
@@ -136,8 +164,9 @@ export default function Contributors({
                 <h3 className="text-black dark:text-white font-semibold opacity-25 m-0 text-sm flex space-x-1 items-center">
                     <span>Contributors</span>
                     <span
-                        className={`w-[24px] h-[24px] bg-black/40 dark:bg-white/40 flex items-center justify-center ${contributors.length > maxContributorsToShow ? 'text-xs' : ''
-                            }`}
+                        className={`w-[24px] h-[24px] bg-black/40 dark:bg-white/40 flex items-center justify-center ${
+                            contributors.length > maxContributorsToShow ? 'text-xs' : ''
+                        }`}
                     >
                         {contributors.length > maxContributorsToShow
                             ? `${maxContributorsToShow}+`
