4b828f6b92aa0cd4a20935fa2a5939bc37be5d25
Cory Watilo <cww@watilo.com>
2024-09-24 21:22:47 -0700

    small teams polish

--- src/components/Careers/SmallTeams/index.tsx
+++ src/components/Careers/SmallTeams/index.tsx
@@ -32,33 +32,25 @@ export const SmallTeams = () => {
     `)
 
   return (
-    <div className="px-4 max-w-7xl mx-auto my-12">
-      <div className="grid md:grid-cols-2 gap-8">
+    <div className="px-4 max-w-7xl mx-auto mb-12 flex justify-center">
+      <div className="inline-flex flex-col md:flex-row md:gap-8">
         <div>
-          <div className="opacity-60 mb-2">Small teams</div>
-          <h2 className="text-3xl xl:text-4xl font-bold mb-2">
-            Join a startup <em className="text-red dark:text-yellow">within a startup</em>
+          <h2 className="text-5xl font-bold mb-2">
+            Join a startup <em className="text-red dark:text-yellow block">within a startup</em>
           </h2>
           <div className="max-w-md">
             <p>
-              Joining a company of 50+ people can be intimidating! With our{' '}
-              <Link to="/handbook/company/small-teams">small teams structure</Link>, it's like operating a
-              startup <em>within</em> a startup.
+              Starting a job at a company of 50+ people can be intimidating!
             </p>
+            <p>With our <Link to="/handbook/company/small-teams">small teams</Link> structure, it's like working at a startup <em>within</em> a startup. This allows each team to operate as autonomously as possible.</p>
 
-            <p>
-              Many of our team members are former founders. At PostHog, they enjoy huge upside potential
-              without the distraction of running payroll, fundraising, and all the other stuff that comes
-              with running a startup <em>outside</em> of building a product.
-            </p>
-
-            <p>We adjust our small teams periodically based on the products we're building.</p>
+            <p>Many of our team members are former founders. Here they can focus on building great products without the distraction of running payroll, fundraising, etc - while still enjoying huge upside potential.</p>
           </div>
         </div>
 
         <div>
-          <h3 className="md:hidden text-base font-bold mb-2">Small teams</h3>
-          <div className="grid grid-cols-2 text-center max-w-xl">
+          <h3 className="text-base font-bold mb-2 md:ml-4 pt-1">Our small teams</h3>
+          <div className="grid grid-cols-2 text-center max-w-xl -mx-4 md:mx-0">
             {allTeams.nodes.map(({ id, name, profiles, miniCrest, leadProfiles }) => {
               const gatsbyImageMiniCrest = getImage(miniCrest)
               return (
@@ -67,23 +59,25 @@ export const SmallTeams = () => {
                     remove: /and/,
                   })}`}
                   key={id}
-                  className="items-center text-left flex gap-2 w-full px-2 py-1 rounded-md border border-b-3 hover:bg-white/50 hover:dark:bg-accent-dark border-transparent md:hover:border-light dark:md:hover:border-dark hover:translate-y-[-1px] active:translate-y-[1px] active:transition-all"
+                  className="items-start md:items-center text-left flex gap-2 w-full px-2 py-1 rounded-md border border-b-3 hover:bg-white/50 hover:dark:bg-accent-dark border-transparent md:hover:border-light dark:md:hover:border-dark hover:translate-y-[-1px] active:translate-y-[1px] active:transition-all"
                 >
-                  {gatsbyImageMiniCrest ? (
-                    <GatsbyImage
-                      image={gatsbyImageMiniCrest}
-                      alt={`${name} Team`}
-                      className="w-16"
-                    />
-                  ) : (
-                    <CloudinaryImage
-                      alt={`${name} Team`}
-                      className="w-16"
-                      src="https://res.cloudinary.com/dmukukwp6/image/upload/crest_mini_default_def12aa14a.png"
-                    />
-                  )}
-                  <div>
-                    <h3 className="text-[15px] my-0 leading-snug text-primary dark:text-primary-dark font-semibold">
+                  <div className="size-8">
+                    {gatsbyImageMiniCrest ? (
+                      <GatsbyImage
+                        image={gatsbyImageMiniCrest}
+                        alt={`${name} Team`}
+                        className="size-8"
+                      />
+                    ) : (
+                      <CloudinaryImage
+                        alt={`${name} Team`}
+                        className="size-8"
+                        src="https://res.cloudinary.com/dmukukwp6/image/upload/crest_mini_default_def12aa14a.png"
+                      />
+                    )}
+                  </div>
+                  <div className="flex-1">
+                    <h3 className="text-sm md:text-[15px] my-0 pt-1.5 md:pt-0 leading-snug text-primary dark:text-primary-dark font-semibold">
                       {name} Team
                     </h3>
                   </div>
