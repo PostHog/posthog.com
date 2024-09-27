1ca04fdd5c46c694ffa2e18159b37a8a4198a8b8
Marcus Hof <13001502+MarconLP@users.noreply.github.com>
2024-09-27 09:09:59 +0200

    Update src/templates/Handbook.tsx
    Co-authored-by: Ian Vanagas <34755028+ivanagas@users.noreply.github.com>

--- src/templates/Handbook.tsx
+++ src/templates/Handbook.tsx
@@ -246,7 +246,7 @@ export const TemplateParametersFactory: (params: TemplateParametersProps) => Rea
                         return (
                             <tr key={input.key}>
                                 <td>
-                                    <div className="mb-6">
+                                    <div className="mb-6 w-40">
                                         <code className="dark:bg-gray-accent-dark dark:text-white bg-gray-accent-light text-inherit p-1 rounded">
                                             {input.label}
                                         </code>
