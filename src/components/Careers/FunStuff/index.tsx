b5ac6a6f47af4e55e90c2f7cc47b09f7eb52cf5c
Cory Watilo <cww@watilo.com>
2024-09-28 12:59:56 -0400

    more fun stuff

--- src/components/Careers/FunStuff/index.tsx
+++ src/components/Careers/FunStuff/index.tsx
@@ -20,7 +20,7 @@ const FunStuff: React.FC = () => {
 
     return (
         <section className="max-w-7xl mx-auto px-4 py-12">
-            <h2 className="text-center text-4xl xl:text-5xl">
+            <h2 className="text-center text-4xl xl:text-5xl text-balance">
                 Live, Laugh, LEquip every developer to build successful products
             </h2>
             <p className="text-center text-lg mb-8 font-semibold opacity-75">Our jobs are zero fun.</p>
@@ -46,7 +46,7 @@ const FunStuff: React.FC = () => {
 
                 <Frame>
                     <div className="px-4 pt-4">
-                        <h3 className="text-xl mb-1">Found favor Hacker News</h3>
+                        <h3 className="text-xl mb-1"><strike>That one time</strike> we found favor on Hacker News</h3>
                         <p className="text-[15px] mb-0">
                             We appear on Hacker News quite often. And we don't even submit all of our own posts!
                         </p>
@@ -69,6 +69,9 @@ const FunStuff: React.FC = () => {
                 </Frame>
 
                 <Frame>
+                    <Link href="https://www.youtube.com/watch?v=SD7B2teuLXk" externalNoIcon>
+                        <StaticImage src="./images/thanking-stars.jpg" alt="Thanking 10,000 GitHub stars" />
+                    </Link>
                     <div className="px-4 pt-4">
                         <h3 className="text-xl mb-1">6+ hour GitHub star live stream</h3>
                         <p className="text-[15px] mb-4">
@@ -80,22 +83,34 @@ const FunStuff: React.FC = () => {
                             .
                         </p>
                     </div>
-                    <StaticImage src="./images/thanking-stars.jpg" alt="Thanking 10,000 GitHub stars" />
-                    {/* <StaticImage src="./images/github-stars.png" alt="GitHub stars" /> */}
                 </Frame>
 
                 <Frame>
+                    <StaticImage src="./images/enterprise-mode.png" alt="Enterprise mode on PostHog.com" />
                     <div className="px-4 pt-4">
                         <h3 className="text-xl mb-1">
                             Enterprise mode<sup className="text-xs">TM</sup> on PostHog.com
                         </h3>
                         <p className="text-[15px] mb-4">
-                            Same website, just bigger words to make the enterprise audience more comfortable.
+                            Same website, just with bigger words. It makes us more approachable to the suits.
                         </p>
                         <p className="text-sm mb-4">Find it under the Account menu (top right) on the homepage.</p>
                     </div>
-                    <StaticImage src="./images/enterprise-mode.png" alt="Enterprise mode on PostHog.com" />
-                    {/* <StaticImage src="./images/github-stars.png" alt="GitHub stars" /> */}
+                </Frame>
+
+                <Frame>
+                    <StaticImage src="./images/github-stars.png" alt="GitHub stars" />
+                    <div className="p-4">
+                        <h3 className="text-xl mb-1">
+                            20k+ GitHub stars
+                        </h3>
+                        <p className="text-[15px] mb-2">
+                            We are honored to be the most popular open source Product OS on GitHub. (Yes, we defined the category, but still...)
+                        </p>
+                        <p className="text-[15px] mb-0">
+                            Won't you <Link href="https://github.com/PostHog/posthog" externalNoIcon>be our next star?</Link>
+                        </p>
+                    </div>
                 </Frame>
             </Masonry>
         </section>
