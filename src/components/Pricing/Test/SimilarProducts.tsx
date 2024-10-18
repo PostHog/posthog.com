013154d5fdffbd65a54577ce6d1374f6951e9610
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2024-10-18 14:41:14 -0700

    use width instead of height

--- src/components/Pricing/Test/SimilarProducts.tsx
+++ src/components/Pricing/Test/SimilarProducts.tsx
@@ -12,7 +12,13 @@ const comparison = [
     {
         name: 'Amplitude',
         logo: (
-            <CloudinaryImage src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_amp_4d0e457978.png" className="" height={32} placeholder="blurred" alt="Amplitude" />
+            <CloudinaryImage
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_amp_4d0e457978.png"
+                className=""
+                width={32}
+                placeholder="blurred"
+                alt="Amplitude"
+            />
         ),
         products: {
             'Product analytics': true,
@@ -27,7 +33,13 @@ const comparison = [
     {
         name: 'Mixpanel',
         logo: (
-            <CloudinaryImage src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_mp_6404158e7f.png" className="" height={32} placeholder="blurred" alt="Mixpanel" />
+            <CloudinaryImage
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_mp_6404158e7f.png"
+                className=""
+                width={32}
+                placeholder="blurred"
+                alt="Mixpanel"
+            />
         ),
         products: {
             'Product analytics': true,
@@ -44,8 +56,7 @@ const comparison = [
         logo: (
             <CloudinaryImage
                 src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_heep_7d9bb0f55a.png"
-                className="h-full rotate-3"
-                height={40}
+                width={16}
                 placeholder="blurred"
                 alt="Heap"
             />
@@ -63,7 +74,13 @@ const comparison = [
     {
         name: 'Pendo',
         logo: (
-            <CloudinaryImage src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_pnd0_dc3ea0f56f.png" className="" height={32} placeholder="blurred" alt="Pendo" />
+            <CloudinaryImage
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_pnd0_dc3ea0f56f.png"
+                className=""
+                width={32}
+                placeholder="blurred"
+                alt="Pendo"
+            />
         ),
         products: {
             'Product analytics': true,
@@ -78,7 +95,13 @@ const comparison = [
     {
         name: 'FullStory',
         logo: (
-            <CloudinaryImage src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_fs_542ca7eb05.png" className="" height={32} placeholder="blurred" alt="FullStory" />
+            <CloudinaryImage
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/icon_fs_542ca7eb05.png"
+                className=""
+                width={32}
+                placeholder="blurred"
+                alt="FullStory"
+            />
         ),
         products: {
             'Product analytics': true,
