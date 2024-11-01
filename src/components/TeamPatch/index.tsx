1b0dc07fc0a3c197093f03525ddafccb80b67aca
Cory Watilo <cww@watilo.com>
2024-11-01 13:43:53 -0400

    patch sizing

--- src/components/TeamPatch/index.tsx
+++ src/components/TeamPatch/index.tsx
@@ -15,10 +15,10 @@ export default function TeamPatch({ name, imageUrl, color, textColor, textShadow
   return (
     <div className={`size-${size === 'lg' ? '72' : size === 'md' ? '56' : size === 'sm' ? '36' : '72'} mx-auto flex justify-center`}>
       <div
-        className={`relative ${size === 'lg' ? 'w-48 h-64' : size === 'md' ? 'w-36 h-48' : size === 'sm' ? 'w-24 h-32' : 'w-48 h-64'} shadow-xl border-4 mx-auto flex items-center justify-center border-team-${color}-border bg-team-${color}-bg ${frame === 'half-round'
+        className={`relative ${size === 'lg' ? 'w-48 h-64' : size === 'md' ? 'w-36 h-48' : size === 'sm' ? 'w-24 h-32' : 'w-48 h-64'} shadow-xl ${size === 'lg' ? 'border-4' : size === 'md' ? 'border-3' : size === 'sm' ? 'border-2' : 'border-4'} mx-auto flex items-center justify-center border-team-${color}-border bg-team-${color}-bg ${frame === 'half-round'
           ? 'rounded-tl-[100px] rounded-tr-[100px]'
           : frame === 'rounded'
-            ? 'rounded-lg'
+            ? size === 'lg' ? 'rounded-lg' : size === 'md' ? 'rounded-md' : size === 'sm' ? 'rounded-sm' : 'rounded-lg'
             : frame === 'square'
               ? ''
               : ''
@@ -27,13 +27,13 @@ export default function TeamPatch({ name, imageUrl, color, textColor, textShadow
         {plaque === 'straight' && (
           <>
             <div
-              className={`left-round-behind w-4 h-1.5 border-l-2 border-b-2 border-team-${color}-border bg-team-${color}-plaque-shadow rounded-bl-full absolute left-[calc(-1rem_-_0px)] bottom-1.5 z-[-1]`}
+              className={`left-round-behind w-4 h-1.5 border-l-2 border-b-2 border-team-${color}-border bg-team-${color}-plaque-shadow rounded-bl-full absolute left-[calc(-1rem_-_0px)] ${size === 'lg' ? 'bottom-1.5' : size === 'md' ? 'bottom-1' : size === 'sm' ? 'bottom-0' : 'bottom-1.5'} z-[-1]`}
             />
             <div
-              className={`right-round-behind w-4 h-1.5 border-r-2 border-b-2 border-team-${color}-border bg-team-${color}-plaque-shadow rounded-br-full absolute right-[calc(-1rem_-_0px)] bottom-1.5 z-[-1]`}
+              className={`right-round-behind w-4 h-1.5 border-r-2 border-b-2 border-team-${color}-border bg-team-${color}-plaque-shadow rounded-br-full absolute right-[calc(-1rem_-_0px)] ${size === 'lg' ? 'bottom-1.5' : size === 'md' ? 'bottom-1' : size === 'sm' ? 'bottom-0' : 'bottom-1.5'} z-[-1]`}
             />
             <div
-              className={`banner-text-bg bg-team-${color}-plaque-bg bg-opacity-80 border-2 border-team-${color}-border h-8 rounded-tl rounded-tr absolute bottom-4 -left-4 -right-4 z-20 flex items-center justify-center text-center leading-none uppercase ${size === 'lg' ? 'text-xl' : size === 'md' ? 'text-lg' : size === 'sm' ? 'text-sm' : 'text-xl'} font-bold font-squeak [font-variant:none] text-${textColor ? textColor : 'white [text-shadow:0_1px_0_rgba(0,0,0,.5)]'
+              className={`banner-text-bg bg-team-${color}-plaque-bg bg-opacity-80 border-2 border-team-${color}-border h-8 rounded-tl rounded-tr absolute ${size === 'lg' ? 'bottom-4' : size === 'md' ? 'bottom-3' : size === 'sm' ? 'bottom-2' : 'bottom-4'} -left-4 -right-4 z-20 flex items-center justify-center text-center leading-none uppercase ${size === 'lg' ? 'text-xl' : size === 'md' ? 'text-lg' : size === 'sm' ? 'text-sm' : 'text-xl'} font-bold font-squeak [font-variant:none] text-${textColor ? textColor : 'white [text-shadow:0_1px_0_rgba(0,0,0,.5)]'
                 } ${textShadow === 'light'
                   ? '0_1px_0_rgba(255,255,255,.5)'
                   : textShadow === 'dark'
@@ -43,26 +43,26 @@ export default function TeamPatch({ name, imageUrl, color, textColor, textShadow
             >
               {name}
             </div>
-            <div className="fake-corners absolute bottom-4 -left-3.5 -right-3.5">
+            <div className={`fake-corners absolute ${size === 'lg' ? 'bottom-4' : size === 'md' ? 'bottom-3' : size === 'sm' ? 'bottom-2' : 'bottom-4'} -left-3.5 -right-3.5`}>
               <div className="relative mt-[-2px]">
                 <div
                   className={`left-line bg-team-${color}-plaque-bg dark:team-${color}-plaque-bg-dark border-l-2 border-team-${color}-border w-2 h-3 absolute -left-0.5 -bottom-1 z-20 [clip-path:polygon(6px_8px,0px_6px,0px_12px,10px_12px)]`}
                 />
                 <div
-                  className={`left-rounded-corner bg-team-${color}-plaque-shadow border-l-2 border-t-2 rounded-tl-full border-team-${color}-border w-3 h-1.5 absolute -left-0.5 -bottom-1.5 z-20 `}
+                  className={`left-rounded-corner bg-team-${color}-plaque-shadow border-l-2 border-t-2 rounded-tl-full border-team-${color}-border ${size === 'lg' ? 'w-3' : size === 'md' ? 'w-3.5' : size === 'sm' ? 'w-3.5' : 'w-3'} h-1.5 absolute -left-0.5 -bottom-1.5 z-20 `}
                 />
                 <div
                   className={`right-line bg-team-${color}-plaque-bg dark:team-${color}-plaque-bg-dark border-r-2 border-team-${color}-border w-2 h-3 absolute -right-0.5 -bottom-1 z-20 [clip-path:polygon(4px_8px,10px_6px,10px_12px,0px_12px)]`}
                 />
                 <div
-                  className={`right-rounded-corner bg-team-${color}-plaque-shadow border-r-2 border-t-2 rounded-tr-full border-team-${color}-border w-3 h-1.5 absolute -right-0.5 -bottom-1.5 z-20`}
+                  className={`right-rounded-corner bg-team-${color}-plaque-shadow border-r-2 border-t-2 rounded-tr-full border-team-${color}-border ${size === 'lg' ? 'w-3' : size === 'md' ? 'w-3.5' : size === 'sm' ? 'w-3.5' : 'w-3'} h-1.5 absolute -right-0.5 -bottom-1.5 z-20`}
                 />
               </div>
             </div>
           </>
         )}
         {plaque === 'curved' && (
-          <div className="absolute bottom-4 -left-3.5 -right-3.5 z-20">
+          <div className={`absolute ${size === 'lg' ? 'bottom-4' : size === 'md' ? 'bottom-3' : size === 'sm' ? 'bottom-2' : 'bottom-4'} -left-3.5 -right-3.5 z-20`}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 970 274" fill="none" className="w-full">
               <path
                 className={`fill-team-${color}-plaque-bg`}
