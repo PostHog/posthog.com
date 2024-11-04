e6e422480334466136d8ea3bcc24be837f814936
Cory Watilo <cww@watilo.com>
2024-11-04 10:12:28 +0100

    link back to small teams

--- src/templates/Team.tsx
+++ src/templates/Team.tsx
@@ -22,7 +22,7 @@ import getAvatarURL from 'components/Squeak/util/getAvatar'
 import Markdown from 'markdown-to-jsx'
 import { AddTeamMember } from 'components/TeamMembers'
 import useTeam from 'hooks/useTeam'
-import { IconInfo, IconX } from '@posthog/icons'
+import { IconArrowLeft, IconInfo, IconX } from '@posthog/icons'
 import { useUser } from 'hooks/useUser'
 import { useFormik } from 'formik'
 import TeamUpdate from 'components/TeamUpdate'
@@ -252,6 +252,10 @@ export default function Team({
                         className="mt-8"
                     />
                     <div className="max-w-xl w-full">
+                        <Link href="/teams" className="-ml-2 mb-1 inline-flex items-center gap-1 text-sm text-primary/50 dark:text-primary-dark/50 hover:text-primary dark:hover:text-primary-dark relative px-2 pt-1.5 pb-1 rounded hover:bg-light/50 hover:dark:bg-dark/50 border border-b-3 border-transparent md:hover:border-light dark:md:hover:border-dark hover:translate-y-[-1px] active:translate-y-[1px] active:transition-all">
+                            <IconArrowLeft className="size-5" />
+                            <span>Teams</span>
+                        </Link>
                         <h1 className="m-0">{teamName} Team</h1>
                         {editingDescription ? (
                             <DescriptionForm
