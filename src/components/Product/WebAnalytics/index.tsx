96ed3e9cfbb8b95955bf4305b6920a8e621ed841
Cory Watilo <cww@watilo.com>
2024-11-10 20:23:38 +0100

    componentized features

--- src/components/Product/WebAnalytics/index.tsx
+++ src/components/Product/WebAnalytics/index.tsx
@@ -47,6 +47,7 @@ import { useLayoutData } from 'components/Layout/hooks'
 import Plans from 'components/Pricing/Plans'
 import { Screenshot } from 'components/Products/Screenshot'
 import ProductHero from 'components/Products/Hero'
+import ProductFeatures from 'components/Products/Features'
 
 const product = {
     slug: 'web-analytics',
@@ -376,30 +377,21 @@ export const ProductWebAnalytics = () => {
 
             <SmoothScroll exclude={['Installation', 'Meet the team']} />
 
-            <div id="features" className="@container">
-                <section className={`transition-all ${fullWidthContent ? 'max-w-full' : 'max-w-7xl'} px-4 @5xl:px-8 mb-10 md:mb-20`}>
-                    <h3 className="text-2xl mb-4">Features</h3>
-                    <ul className={`list-none p-0 grid @xs:grid-cols-2 @6xl:grid-cols-3 gap-4 @6xl:gap-6 @5xl:gap-8 mb-8`}>
-                        {features.map((feature, index) => {
-                            return <Feature {...feature} key={index} />
-                        })}
-                    </ul>
+            <ProductFeatures
+                featuresArray={features.map((feature, index) => {
+                    return <Feature {...feature} key={index} />
+                })}
+                subfeaturesArray={subfeatures.map((subfeature, index) => {
+                    return <Subfeature {...subfeature} key={index} />
+                })}
+                subfeaturesItemCount={subfeaturesItemCount}
+                marquee={<Marquee product={product.capitalized}>
+                {questions.map((question, index) => {
+                    return <Question {...question} key={index} />
+                })}
+            </Marquee>}
+            />
 
-                    <ul className={`list-none p-0 grid grid-cols-2 md:grid-cols-${subfeaturesItemCount} gap-4`}>
-                        {subfeatures.map((subfeature, index) => {
-                            return <Subfeature {...subfeature} key={index} />
-                        })}
-                    </ul>
-                </section>
-
-                <section className="bg-accent dark:bg-accent-dark">
-                    <Marquee product={product.capitalized}>
-                        {questions.map((question, index) => {
-                            return <Question {...question} key={index} />
-                        })}
-                    </Marquee>
-                </section>
-            </div>
             <section
                 id="pricing"
                 className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl'} mx-auto px-5 pt-20 pb-10`}
---
+++ src/components/Products/Features/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ProductHeader } from 'components/Products/ProductHeader'
+import { useLayoutData } from 'components/Layout/hooks'
+
+interface ProductFeaturesProps {
+    featuresArray: React.ReactNode
+    subfeaturesArray: React.ReactNode
+    subfeaturesItemCount: number
+    marquee: React.ReactNode
+}
+
+export const ProductFeatures = ({ featuresArray, subfeaturesArray, subfeaturesItemCount, marquee }: ProductFeaturesProps): JSX.Element => {
+  const { fullWidthContent } = useLayoutData()
+
+  return (
+    <div id="features" className="@container">
+        <section className={`transition-all ${fullWidthContent ? 'max-w-full' : 'max-w-7xl'} px-4 @5xl:px-8 mb-10 md:mb-20`}>
+            <h3 className="text-2xl mb-4">Features</h3>
+            <ul className={`list-none p-0 grid @xs:grid-cols-2 @6xl:grid-cols-3 gap-4 @6xl:gap-6 @5xl:gap-8 mb-8`}>
+              {featuresArray}
+            </ul>
+            <ul className={`list-none p-0 grid grid-cols-2 md:grid-cols-${subfeaturesItemCount} gap-4`}>
+              {subfeaturesArray}
+            </ul>
+        </section>
+
+        <section className="bg-accent dark:bg-accent-dark">
+            {marquee}
+        </section>
+    </div>
+  )
+}
+
+export default ProductFeatures
