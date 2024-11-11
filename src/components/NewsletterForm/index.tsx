7c3e2d70d1d366985cf0a0565a04bb1ae8a0fb2a
Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>
2024-11-11 15:38:11 +0000

    Changing image and copy

--- src/components/NewsletterForm/index.tsx
+++ src/components/NewsletterForm/index.tsx
@@ -56,7 +56,7 @@ export const NewsletterForm = ({ className = '', placement }: NewsletterFormProp
                     <>
                         <div className="float-right md:hidden -mt-6 2xs:-mt-12">
                             <CloudinaryImage
-                                src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/swole-hog.png"
+                                src="https://res.cloudinary.com/dmukukwp6/image/upload/engineer_47d6638eae.png"
                                 objectFit="contain"
                                 className="w-full h-full max-w-[170px]"
                             />
@@ -64,7 +64,8 @@ export const NewsletterForm = ({ className = '', placement }: NewsletterFormProp
                         <p className="!text-sm opacity-50 !m-0">Subscribe to our newsletter</p>
                         <h4 className="relative !text-2xl !m-0 !leading-tight">Product for Engineers</h4>
                         <p className="!m-0 !text-sm md:!text-[15px] !leading-normal !pt-1">
-                            Join 25k+ subscribers learning how to build successful products and become better engineers.
+                            Practical tips for becoming a better engineer and building successful products. Read by 25k
+                            founders and engineers.
                         </p>
                         <div className="">
                             <form
