f4b6852cf158d7c86434183d1d0e2cec246ef64e
Bijan <bijan@posthog.com>
2024-11-19 15:42:18 -0800

    Sketch initial page structure

--- src/pages/posthug.tsx
+++ src/pages/posthug.tsx
@@ -7,9 +7,26 @@ const PostHugPage = () => {
         <Layout>
             <SEO title="PostHug" description="Free hedgehugs." image={`/images/about.png`} />
 
-            <div>Need a hug? 🦔</div>
+            <div>
+                <div className="max-w-2xl mx-auto py-8 px-4 md:px-8">
+                    <h1 className="text-5xl text-center">
+                        Were you looking for
+                        <div className="text-center my-8">
+                            <img src="/brand/posthog-logo.svg" className="w-full" />
+                        </div>
+                    </h1>
 
-            <a href="http://posthog.com">Go to PostHog</a>
+                    <p className="m-2 text-center text-balance">Even engineers need hugs every now and again.</p>
+
+                    <p className="m-2 text-center text-balance">
+                        Seeing as how we can't literally give you a hug, here is the next best thing:
+                    </p>
+
+                    <button>Get a Hug</button>
+
+                    <p>🦔 ❤️ 🦔</p>
+                </div>
+            </div>
         </Layout>
     )
 }
