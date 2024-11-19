5cc63feea3bec20a9c22c4f154078d53a99900c4
Eli Kinsey <eli@ekinsey.dev>
2024-11-18 20:44:47 -0800

    careers hogzilla

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,6 +1,6 @@
 import Layout from 'components/Layout'
 import { graphql, useStaticQuery } from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 import { CareersHero } from '../components/Careers/CareersHero'
 import { OpenRoles } from '../components/Careers/OpenRoles'
 import '../components/Careers/styles/index.scss'
@@ -21,13 +21,54 @@ import TeamQuotes from 'components/Careers/TeamQuotes'
 import { SmallTeams } from 'components/Careers/SmallTeams'
 import { Pizza } from 'components/Careers/Pizza'
 import FunStuff from 'components/Careers/FunStuff'
+import { DotLottiePlayer, PlayerEvents } from '@dotlottie/react-player'
+import { motion } from 'framer-motion'
+
+const Hogzilla = ({ onComplete }: { onComplete: () => void }) => {
+    const [ready, setReady] = useState(false)
+    return (
+        <div>
+            {!ready && <div className="h-screen w-full bg-tan fixed z-40" />}
+            {ready && (
+                <>
+                    <motion.div
+                        initial={{ opacity: 1 }}
+                        animate={{ opacity: 0 }}
+                        transition={{ delay: 3.8 }}
+                        className="h-screen w-full bg-tan fixed z-40"
+                    />
+                    <motion.div
+                        initial={{ width: '50%' }}
+                        animate={{ width: 0 }}
+                        transition={{ duration: 1, delay: 3.8 }}
+                        className="h-screen bg-tan fixed z-50"
+                    />
+                </>
+            )}
+            <div className="fixed top-0 left-0 w-full h-full z-[999999]">
+                <DotLottiePlayer
+                    onEvent={(event) => {
+                        if (event === PlayerEvents.Complete) {
+                            onComplete()
+                        }
+                        if (event === PlayerEvents.Ready) {
+                            setReady(true)
+                        }
+                    }}
+                    src="/lotties/careers/hogzilla.lottie"
+                    autoplay
+                />
+            </div>
+        </div>
+    )
+}
 
 const IndexPage = () => {
     const data = useStaticQuery(query)
     const latestJob = data?.allAshbyJobPosting?.nodes && data.allAshbyJobPosting.nodes[0]
     const latestJobCreatedAt = latestJob && new Date(latestJob['publishedDate'])
+    const [showHogzilla, setShowHogzilla] = useState(true)
 
-    const breakpoints = useBreakpoint()
     return (
         <Layout>
             <SEO
@@ -39,7 +80,7 @@ const IndexPage = () => {
                 }`}
                 imageType="absolute"
             />
-
+            {showHogzilla && <Hogzilla onComplete={() => setShowHogzilla(false)} />}
             <CareersHero />
             <SmallTeams />
             <Pizza />
