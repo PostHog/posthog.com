c6778feb80e38863d6c8e6f71ea4f29e9fef6963
Bijan Boustani <bijan@posthog.com>
2024-11-19 17:46:06 -0800

    [Hackathon] Set up Page for PostHug.com (#9924)
    * Add a new page for posthug.com
    * Initial setup for a hedgehog hugging animation
    * Artwork coming soon in a separate PR

---
+++ src/pages/posthug.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import HugHog from 'components/HugHog'
+import Layout from 'components/Layout'
+import { SEO } from 'components/seo'
+
+const PostHugPage = () => {
+    const [active, setActive] = useState(false)
+
+    return (
+        <Layout>
+            <SEO title="PostHug" description="Free hedgehugs." image={`/images/about.png`} />
+
+            <div>
+                <div className="max-w-2xl mx-auto py-8 px-4 md:px-8">
+                    <h1 className="text-5xl text-center">
+                        Were you looking for
+                        <div className="text-center my-8">
+                            <img src="/brand/posthog-logo.svg" className="dark:hidden w-full" />
+                            <img src="/brand/posthog-logo-white.svg" className="hidden dark:block w-full" />
+                        </div>
+                    </h1>
+
+                    <div className="my-16 space-y-2 text-balance text-center">
+                        <p>Even engineers need hugs every now and again. 🥺</p>
+
+                        <p>Seeing as how we can't literally give you a hug, here is the next best thing:</p>
+
+                        <HugHog onClick={() => setActive(!active)} active={active} className="w-full" />
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default PostHugPage
