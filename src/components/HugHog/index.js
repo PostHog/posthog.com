4948d03efe223b98dd3819cf3b84aa9b29428945
Eli Kinsey <eli@ekinsey.dev>
2024-11-21 11:47:56 -0800

    quick fix

--- src/components/HugHog/index.js
+++ src/components/HugHog/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+
+const HugHogImage = () => {
+    return <img src="https://res.cloudinary.com/dmukukwp6/image/upload/max_hug_2139d9320c.png" />
+}
+
 export default function HugHog({ onClick, className, active }) {
     return (
         <button className={className} onClick={onClick}>
-            <div className="flex items-center">
-                <motion.img
-                    className="max-w-[300px]"
-                    src="https://res.cloudinary.com/dmukukwp6/image/upload/max_hug_2139d9320c.png"
-                    animate={{ translateX: active ? 120 : 0 }}
-                    initial={{ x: -60, y: 0 }}
+            <div className="grid grid-cols-2 items-center">
+                <motion.div
+                    animate={{ translateX: active ? '35%' : 0 }}
                     transition={{ type: 'spring', duration: 0.5, bounce: 0.7 }}
-                />
-                <motion.img
-                    className="max-w-[300px]"
-                    src="https://res.cloudinary.com/dmukukwp6/image/upload/max_hug_2139d9320c.png"
-                    animate={{ translateX: active ? -120 : 0 }}
-                    initial={{ x: 60, y: 0, scaleX: -1 }}
+                >
+                    <HugHogImage />
+                </motion.div>
+                <motion.div
+                    animate={{ translateX: active ? '-35%' : 0 }}
+                    initial={{ scaleX: -1 }}
                     transition={{ type: 'spring', duration: 0.5, bounce: 0.7 }}
-                />
+                >
+                    <HugHogImage />
+                </motion.div>
             </div>
         </button>
     )
