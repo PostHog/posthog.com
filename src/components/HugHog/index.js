bb73ee51bfe5d1d5ce1ac2e1df54e26586dbe4bf
Bijan <bijan@posthog.com>
2024-11-21 11:34:03 -0800

    Start resizing

--- src/components/HugHog/index.js
+++ src/components/HugHog/index.js
@@ -5,15 +5,17 @@ export default function HugHog({ onClick, className, active }) {
         <button className={className} onClick={onClick}>
             <div className="flex items-center">
                 <motion.img
+                    className="max-w-[300px]"
                     src="https://res.cloudinary.com/dmukukwp6/image/upload/max_hug_2139d9320c.png"
                     animate={{ translateX: active ? 120 : 0 }}
-                    initial={{ height: 300, x: -60, y: 0 }}
+                    initial={{ x: -60, y: 0 }}
                     transition={{ type: 'spring', duration: 0.5, bounce: 0.7 }}
                 />
                 <motion.img
+                    className="max-w-[300px]"
                     src="https://res.cloudinary.com/dmukukwp6/image/upload/max_hug_2139d9320c.png"
                     animate={{ translateX: active ? -120 : 0 }}
-                    initial={{ height: 300, x: 60, y: 0, scaleX: -1 }}
+                    initial={{ x: 60, y: 0, scaleX: -1 }}
                     transition={{ type: 'spring', duration: 0.5, bounce: 0.7 }}
                 />
             </div>
