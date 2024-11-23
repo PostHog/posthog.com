39942fbf443180fd27e14b788582b10a63bf0e6a
Eli Kinsey <eli@ekinsey.dev>
2024-11-23 11:51:24 -0800

    restore home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,3 @@
-import React from 'react'
-import JobList from 'components/Jobs/JobList'
+import Home from '../components/Home'
 
-export default function Home({ jobs }) {
-    return (
-        <main>
-            <h1>Open Positions</h1>
-            <JobList jobs={jobs} />
-        </main>
-    )
-}
+export default Home
