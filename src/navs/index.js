166e4f09d4a87ddfea3f000d392c46073c7dface
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2024-11-25 11:37:33 -0800

    add to nav

--- src/navs/index.js
+++ src/navs/index.js
@@ -2846,6 +2846,10 @@ export const docsMenu = {
                             url: '/docs/cdp/destinations/sendgrid',
                         },
                         {
+                            name: 'Snapchat Ads',
+                            url: '/docs/cdp/destinations/snapchat-ads',
+                        },
+                        {
                             name: 'Zapier',
                             url: '/docs/cdp/destinations/zapier',
                         },
