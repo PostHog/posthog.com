0fb0811aeff769a1ab08cdc4d38c81b8700fc73e
Cory Watilo <corywatilo@gmail.com>
2024-12-11 15:46:02 -0500

    Adds a "Why posthog?" page (#10096)
    Co-authored-by: Eli Kinsey <eli@ekinsey.dev>
    Co-authored-by: Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>

--- src/components/Home/OnePlatform.js
+++ src/components/Home/OnePlatform.js
@@ -1,6 +1,7 @@
 import CloudinaryImage from 'components/CloudinaryImage'
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
+import { PRODUCT_COUNT } from '../../constants'
 
 export default function OnePlatform() {
     return (
@@ -13,11 +14,15 @@ export default function OnePlatform() {
             </p>
             <ul className="p-0 grid md:grid-cols-3 gap-6 md:gap-12">
                 <li className="list-none">
-                    <CloudinaryImage src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/components/Home/images/mds.png" alt="Drake Hog" className="max-w-[179px] h-[200px]" />
+                    <CloudinaryImage
+                        src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/components/Home/images/mds.png"
+                        alt="Drake Hog"
+                        className="max-w-[179px] h-[200px]"
+                    />
                     <h3 className="text-xl pt-4 mb-2 text-red">Replaces the modern data stack</h3>
                     <p>
-                        With 8+ products on one platform, you'll spend less time engineering your data integrations and
-                        more time building your product.
+                        With {PRODUCT_COUNT}+ products on one platform, you'll spend less time engineering your data
+                        integrations and more time building your product.
                     </p>
                 </li>
                 <li className="list-none">
