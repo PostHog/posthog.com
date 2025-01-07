951eb369fed9559ef8aa859597efc32e06079327
Eli Kinsey <eli@ekinsey.dev>
2025-01-07 10:22:11 -0800

    add forum suggestion

--- src/components/Chat/Inkeep.tsx
+++ src/components/Chat/Inkeep.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 import useInkeepSettings from 'hooks/useInkeepSettings'
 import { DotLottiePlayer } from '@dotlottie/react-player'
+import { AIChatFunctions } from '@inkeep/uikit'
 
 export default function InkeepEmbeddedChat(): JSX.Element {
     const { baseSettings, aiChatSettings } = useInkeepSettings()
-    const embeddedChatRef = useRef(null)
+    const embeddedChatRef = useRef<AIChatFunctions | null>(null)
     const lottieRef = useRef(null)
     const [loading, setLoading] = useState(true)
+    const [hasFirstResponse, setHasFirstResponse] = useState(false)
 
     useEffect(() => {
         import('@inkeep/uikit-js').then((inkeepJS) => {
@@ -16,13 +18,34 @@ export default function InkeepEmbeddedChat(): JSX.Element {
                 componentType: 'EmbeddedChat',
                 targetElement: '#embedded-chat-target',
                 properties: {
-                    baseSettings,
+                    baseSettings: {
+                        ...baseSettings,
+                        logEventCallback: (event) => {
+                            if (!hasFirstResponse && event?.eventName === 'chat_message_bot_response_received') {
+                                setHasFirstResponse(true)
+                            }
+                        },
+                    },
                     aiChatSettings,
                 },
             })
             setLoading(false)
         })
     }, [])
+
+    useEffect(() => {
+        if (hasFirstResponse && embeddedChatRef.current) {
+            const shadowRoot = document.querySelector('#embedded-chat-target>div')?.shadowRoot
+            if (shadowRoot) {
+                const chatBubbleActions = shadowRoot.querySelector('.ikp-chat-bubble__message-actions')
+                if (chatBubbleActions) {
+                    const el = document.createElement('p')
+                    el.innerHTML = `Not what you were looking for? Try <a target="_blank" style="text-decoration: underline;" href="/questions">posting a community question</a>.`
+                    chatBubbleActions.insertAdjacentElement('beforebegin', el)
+                }
+            }
+        }
+    }, [hasFirstResponse])
 
     return (
         <>
