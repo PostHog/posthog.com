850566833e1ea53f466317e9b4a3a5187f434670
Cory Watilo <corywatilo@gmail.com>
2025-01-07 14:08:03 -0500

    alignment

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -174,7 +174,7 @@ function Tooltip({
                 <button
                     ref={setReferenceElement}
                     onClick={() => setOpen(!open)}
-                    className={`ml-2 flex items-center rounded-full border border-light dark:border-dark relative active:scale-[.99] ${
+                    className={`flex items-center rounded-full border border-light dark:border-dark relative active:scale-[.99] ${
                         open
                             ? 'border-primary/50 dark:border-primary-dark/50'
                             : 'hover:border-primary/25 hover:dark:border-primary-dark/25 hover:scale-[1.05]'
@@ -186,7 +186,7 @@ function Tooltip({
                 <button
                     ref={setReferenceElement}
                     onClick={() => setOpen(!open)}
-                    className={`ml-2 flex items-center p-2 rounded hover:bg-border dark:hover:bg-border-dark relative active:top-[1px] active:scale-[.99] ${
+                    className={`flex items-center p-2 rounded hover:bg-border dark:hover:bg-border-dark relative active:top-[1px] active:scale-[.99] ${
                         open ? 'bg-border dark:bg-border-dark' : ' hover:scale-[1.05]'
                     }`}
                 >
