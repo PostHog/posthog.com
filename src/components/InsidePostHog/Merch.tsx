19d48c2918f707caa0eea2bffed355157c5248f8
Bijan <bijan@posthog.com>
2025-01-10 20:22:21 -0500

    Replace merch store image

--- src/components/InsidePostHog/Merch.tsx
+++ src/components/InsidePostHog/Merch.tsx
@@ -28,15 +28,15 @@ export default function Merch() {
     return (
         <div>
             <div className="bg-white dark:bg-accent-dark p-4 border border-light dark:border-dark mt-4 mb-0 rounded">
-                <h3 className="text-lg text-center italic leading-tight">
-                    "Some of the best company swag I've ever seen"
+                <h3 className="text-sm text-center italic leading-tight">
+                    "This merch store has some of the best company swag I've ever seen"
                 </h3>
                 {/* quote source: https://posthog.slack.com/archives/C011L071P8U/p1710758940243199 */}
 
                 <Link to="/merch">
                     <CloudinaryImage
-                        src="https://res.cloudinary.com/dmukukwp6/image/upload/v1715109076/dw.jpg"
-                        alt="PostHog t-shirt"
+                        src="https://cdn.shopify.com/s/files/1/0452/0935/4401/files/DSC07383_3265x4897_crop_center.jpg?v=1709570954"
+                        alt="PostHog stickers"
                         className="w-full"
                     />
                     {/* <GatsbyImage image={getImage(gatsbyImageData)} /> */}
