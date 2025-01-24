268fa3622ebfc9c50e85c23b0ff126d265427155
Cory Watilo <corywatilo@gmail.com>
2025-01-23 20:02:03 -0500

    created founder stack page

--- src/navs/index.js
+++ src/navs/index.js
@@ -3352,6 +3352,12 @@ export const pricingMenu = {
             color: 'green',
             url: '/sales',
         },
+        {
+            name: 'Founder stack',
+            icon: 'IconStack',
+            color: 'salmon',
+            url: '/founder-stack',
+        },
     ],
 }
 
