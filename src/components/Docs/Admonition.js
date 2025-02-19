9d38ce7939a76a3957a6c97b972c81fc6bae874a
Danilo Campos <danilo@posthog.com>
2025-02-19 15:08:42 -0500

    Creation Docusaurus-style admonition component

---
+++ src/components/Docs/Admonition.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import * as Icons from '@posthog/icons'
+
+const typeStyles = {
+    info: 'bg-yellow/10 border-l-yellow',
+    tip: 'bg-gray/10 border-l-gray',
+    success: 'bg-green/10 border-l-green',
+    warning: 'bg-orange/10 border-l-orange',
+    caution: 'bg-red/10 border-l-red',
+}
+
+export const Admonition = ({ icon, title, type = 'info', children }) => {
+    const Icon = Icons[icon]
+    const styles = typeStyles[type] || typeStyles.info
+
+    return (
+        <div className={`my-4 mb-8 p-4 border-l-[10px] rounded-sm ${styles}`}>
+            <div className="flex items-center gap-2 mb-2">
+                {Icon && <Icon className="w-5 h-5" />}
+                <strong>{title}</strong>
+            </div>
+            <div className="[&>p:last-child]:mb-0">{children}</div>
+        </div>
+    )
+}
+
+export default Admonition
--- src/components/Docs/AnalyticsPlannerTip.js
+++ src/components/Docs/AnalyticsPlannerTip.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Admonition } from './Admonition'
 
 export const AnalyticsPlannerTip = () => {
     return (
-        <blockquote>
-            <strong>Tip</strong>: Get this intro to PostHog as a{' '}
+        <Admonition icon="IconGithub" title="Plan with your teammates" type="tip">
+            Get this intro to PostHog as a{' '}
             <a href="https://github.com/PostHog/posthog-integration-planner">private repo</a> you can share with your
             team. Learn the basics while documenting the specifics of your integration.
-        </blockquote>
+        </Admonition>
     )
 }
 
