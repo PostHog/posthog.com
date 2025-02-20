57e335229fa404bc9fd9355464c18d525324c267
Cory Watilo <corywatilo@gmail.com>
2025-02-20 11:06:33 -0400

    slider margin, button ux

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -16,19 +16,19 @@ const sliderSettings = {
 export default function ImageSlider({ children, ...other }: { children: JSX.Element[] | JSX.Element }) {
     const ref = useRef(null)
     return (
-        <div className="relative group image-slider">
+        <div className="relative group image-slider mb-4">
             <Slider {...sliderSettings} {...other} ref={ref}>
                 {children}
             </Slider>
             <button
                 onClick={() => ref.current?.slickPrev()}
-                className="absolute top-1/2 left-0 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity rounded border border-light dark:border-dark bg-accent dark:bg-accent-dark p-1 border-b-2"
+                className="absolute top-1/2 left-0 -translate-y-1/2 opacity-75 group-hover:opacity-100 transition-opacity rounded border border-light dark:border-dark bg-accent dark:bg-accent-dark p-1 border-b-2 hover:-mt-px active:mt-0 active:scale-[.99]"
             >
                 <IconArrowLeft className="size-5 opacity-70" />
             </button>
             <button
                 onClick={() => ref.current?.slickNext()}
-                className="absolute top-1/2 right-0 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity rounded border border-light dark:border-dark bg-accent dark:bg-accent-dark p-1 border-b-2"
+                className="absolute top-1/2 right-0 -translate-y-1/2 opacity-75 group-hover:opacity-100 transition-opacity rounded border border-light dark:border-dark bg-accent dark:bg-accent-dark p-1 border-b-2 hover:-mt-px active:mt-0 active:scale-[.99]"
             >
                 <IconArrowRight className="size-5 opacity-70" />
             </button>
