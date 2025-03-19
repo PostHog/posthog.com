3ea52e2863ff6fc5c4b408adb4d56c468b60b6bd
Michael Matloka <dev@twixes.com>
2025-03-19 19:58:56 +0100

    Add initial Max docs (#10995)
    * Add initial Max docs
    
    * Fix Tailwind classes not being detected in MDX
    
    * Fix ProductScreenshot sizing
    
    * Also rename path to `/max-ai`

--- src/components/ProductScreenshot/index.js
+++ src/components/ProductScreenshot/index.js
@@ -4,7 +4,7 @@ import { ZoomImage } from 'components/ZoomImage'
 export const ProductScreenshot = ({ imageLight, imageDark, alt, classes = '', zoom }) => {
     const Container = zoom === false ? React.Fragment : ZoomImage
     return (
-        <div className="mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark">
+        <div className="mb-4 border border-light dark:border-dark rounded p-4 bg-accent dark:bg-accent-dark max-w-fit">
             <Container>
                 <img src={imageLight} alt={alt} className={`${imageDark ? 'dark:hidden' : ''} ${classes}`} />
                 {imageDark && <img src={imageDark} alt={alt} className={`hidden dark:block ${classes}`} />}
