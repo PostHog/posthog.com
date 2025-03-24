4f553b5953f77e24de0550ad6faf2b7247662bb3
Cory Watilo <corywatilo@gmail.com>
2025-03-24 13:02:41 -0400

    Portugal flag, added Max AI to teams nav (#11052)
    * add max ai team to nav
    
    * added portugal flag (PT)

--- src/navs/index.js
+++ src/navs/index.js
@@ -1499,6 +1499,10 @@ export const companyMenu = {
                     url: '/teams/llm-observability',
                 },
                 {
+                    name: 'Max AI',
+                    url: '/teams/max-ai',
+                },
+                {
                     name: 'Messaging',
                     url: '/teams/messaging',
                 },
