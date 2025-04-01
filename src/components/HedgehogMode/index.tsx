693c01099b92ebc6119ff8c1eab300f53e64d7e6
Eli Kinsey <eli@ekinsey.dev>
2025-04-01 09:46:57 -0700

    ignore server side

--- src/components/HedgehogMode/index.tsx
+++ src/components/HedgehogMode/index.tsx
@@ -2,14 +2,15 @@ import React, { lazy, Suspense } from 'react'
 
 import { useLayoutData } from 'components/Layout/hooks'
 
-
-const HedgeHogModeRenderer = lazy(() => import('@posthog/hedgehog-mode').then((module) => ({ default: module.HedgehogModeRenderer })))
+const HedgeHogModeRenderer =
+    typeof window !== 'undefined'
+        ? lazy(() => import('@posthog/hedgehog-mode').then((module) => ({ default: module.HedgehogModeRenderer })))
+        : () => null
 
 export default function HedgeHogModeEmbed(): JSX.Element | null {
     const { hedgehogModeEnabled } = useLayoutData()
 
-
-    return hedgehogModeEnabled ? (
+    return typeof window !== 'undefined' && hedgehogModeEnabled ? (
         <Suspense fallback={<span>Loading...</span>}>
             <HedgeHogModeRenderer
                 config={{
