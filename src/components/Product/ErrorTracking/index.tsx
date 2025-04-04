20c7b710d17ad385fbebc0fd0ba3680e2e11eb30
Cory Watilo <corywatilo@gmail.com>
2025-04-04 13:01:50 -0700

    organized page content, added screenshots

--- src/components/Product/ErrorTracking/index.tsx
+++ src/components/Product/ErrorTracking/index.tsx
@@ -1,23 +1,3 @@
-// Everything you’d expect in an error tracking product, but *10x better in the PostHog ecosystem*
-
-// Sure you can use error tracking solo, but it's 10x better with other PostHog products.
-
-// - Session replay: Watch session recordings of users who triggered errors for more context about how to reproduce an issue.
-// - Product analytics: Graph the `$exception` event and use filtering and grouping to determine how errors happen and what to prioritize.
-// - Feature flags:  Test potential fixes by rolling out code changes only to affected users.
-// - User profiles: See all `$exception` events for specific users in their event history log and find which feature flags were enabled at the time an error occurred.
-
-// Find, inspect, fix
-
-// - Alerts
-//   Get notified of new errors by email, Slack, or webhook
-// - Assign errors
-//   Task issues to teams or individual members
-// - Organize and prioritize
-//   Merge issues, sort by frequency or recency, or use text search to find specific errors
-// - Stack traces
-//   Get code context automatically with PostHog's server-side libraries, or upload source maps for front-end frameworks
-
 import CloudinaryImage from 'components/CloudinaryImage'
 import React, { useState } from 'react'
 import Link from 'components/Link'
@@ -99,7 +79,7 @@ const features = [
         description: 'Get notifications of new errors by email, Slack, or webhook',
         image: (
             <CloudinaryImage
-                src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/products/error-tracking-alerts.png"
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/screenshot_alerts_00824b03f5.png"
                 width={428}
                 placeholder="none"
             />
@@ -110,7 +90,7 @@ const features = [
         description: 'Task issues to teams or individual members',
         image: (
             <CloudinaryImage
-                src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/products/error-tracking-monitoring.png"
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/screenshot_assign_4c9bb9ee60.png"
                 width={428}
                 placeholder="none"
             />
@@ -121,7 +101,7 @@ const features = [
         description: 'Merge issues, sort by frequency or recency, or use text search to find specific errors',
         image: (
             <CloudinaryImage
-                src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/products/error-tracking-stack.png"
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/screenshot_organize_94b4d00ea2.png"
                 width={428}
                 placeholder="none"
             />
@@ -133,7 +113,7 @@ const features = [
             "Get code context automatically with PostHog's server-side libraries, or upload source maps for front-end frameworks",
         image: (
             <CloudinaryImage
-                src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/products/error-tracking-assignment.png"
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/screenshot_trace_3fc569059c.png"
                 width={428}
                 placeholder="none"
             />
@@ -408,18 +388,22 @@ export const ProductErrorTracking = () => {
                         <li className="hidden md:block"></li>
                     </ul>
                 </section>
+            </div>
 
+            <SmoothScroll exclude={['Tutorials', 'Installation', 'Roadmap & changelog']} />
+
+            <div className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 py-4 pb-0`}>
                 <section id="features" className="-mt-36 pt-36">
                     <h2 className="text-4xl md:text-5xl text-center">
                         All the features you'd expect, but{' '}
                         <span className="text-red dark:text-yellow">10x better in the PostHog ecosystem</span>
                     </h2>
-                    <p className="text-lg opacity-75">
+                    <p className="text-lg opacity-75 text-center">
                         Sure you can use error tracking solo, but it's better with other PostHog products.
                     </p>
                 </section>
 
-                <div className="mt-20">
+                <div className="mt-12">
                     <ul className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 mt-12 list-none p-0">
                         {subfeatures.map((subfeature, index) => (
                             <Subfeature key={index} {...subfeature} />
@@ -427,7 +411,7 @@ export const ProductErrorTracking = () => {
                     </ul>
                 </div>
 
-                <div className="mt-20">
+                <div className="py-12">
                     <ul className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 list-none p-0">
                         {features.map((feature, index) => (
                             <Feature key={index} {...feature} />
@@ -435,9 +419,7 @@ export const ProductErrorTracking = () => {
                     </ul>
                 </div>
 
-                <section
-                    className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 py-10 md:pt-20 pb-0`}
-                >
+                <section>
                     <div className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 pb-0`}>
                         <div className="bg-accent dark:bg-accent-dark -mx-5 md:-mx-8">
                             <Marquee product={product.capitalized} shortFade>
@@ -449,10 +431,7 @@ export const ProductErrorTracking = () => {
                     </div>
                 </section>
 
-                <section
-                    id="pricing"
-                    className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl'} mx-auto px-5 py-20`}
-                >
+                <section id="pricing" className="pt-20">
                     <div className="flex flex-col-reverse md:flex-row md:gap-12">
                         <div className="flex-1">
                             <h2 className="text-4xl md:text-5xl">Usage-based pricing</h2>
@@ -460,7 +439,7 @@ export const ProductErrorTracking = () => {
                                 Use the {product.lowercase} for free up to {product.freeTier}.
                             </p>
                         </div>
-                        <div className="md:w-96 md:text-right mb-8 md:mb-0 -mt-8">
+                        <div className="md:w-96 md:text-right mb-8 md:mb-0 -mt-16">
                             <CloudinaryImage
                                 alt="Just another hedgehog"
                                 placeholder="blurred"
@@ -476,43 +455,71 @@ export const ProductErrorTracking = () => {
                         </div>
 
                         <div className="px-5 md:px-0 lg:w-96 lg:mt-4">
+                            {/* 
                             <h4 className="text-3xl">FAQs</h4>
                             {faqs.map((faq, index) => {
                                 return <FAQ {...faq} key={index} />
                             })}
+                             */}
                         </div>
                     </div>
                 </section>
 
-                <div
-                    className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 py-10 md:pt-20 pb-0`}
-                >
-                    <PairsWith items={pairsWithItemCount}>
-                        {PairsWithArray.map((card, index) => {
-                            return <PairsWithItem {...card} key={index} />
-                        })}
-                    </PairsWith>
-                </div>
+                <section id="posthog-vs">
+                    <h2 className="text-center text-3xl lg:text-4xl">PostHog vs...</h2>
+                    <Comparison comparison={comparison} columnCount={comparisonColumnCount} truncate />
 
-                <section
-                    id="team"
-                    className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 py-10 md:pt-20 pb-0`}
-                >
-                    <div className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 pb-0`}>
-                        <h3 className="text-3xl lg:text-4xl text-center">Meet the team</h3>
-
-                        <p className="text-center mb-2">
-                            PostHog works in small teams. The <Link to={teamSlug}>{team}</Link> team is responsible for
-                            building this product.
-                        </p>
-                        <TeamMembers teamName={team} setActiveProfile={setActiveProfile} />
+                    <h3 className="text-center mb-8">So, what's best for you?</h3>
+                    <div className="@container mb-8 mx-5 md:mx-0 grid md:grid-cols-2 gap-4">
+                        <VsCompetitor
+                            title="Reasons a competitor may be best for you (for now...)"
+                            image={
+                                <CloudinaryImage
+                                    src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/products/competitors-ff.png"
+                                    className="max-w-[176px]"
+                                />
+                            }
+                        >
+                            <ul>
+                                <li>
+                                    Uptime monitoring
+                                    <ul className="pl-6">
+                                        <li className="text-sm">We don't have uptime monitoring. Yet.</li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    Advanced alerting
+                                    <ul className="pl-6">
+                                        <li className="text-sm">
+                                            We currently only support Slack and email alerts on custom criteria.
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>Advanced error grouping systems</li>
+                            </ul>
+                        </VsCompetitor>
+                        <VsPostHog>
+                            <ul>
+                                <li>Integration with other PostHog products</li>
+                                <li>
+                                    Feature flags for error recovery
+                                    <ul className="pl-6">
+                                        <li className="text-sm">Quickly roll back features that cause errors.</li>
+                                    </ul>
+                                </li>
+                                <li>Simple, transparent pricing</li>
+                            </ul>
+                        </VsPostHog>
                     </div>
+
+                    <p className="text-center text-sm font-medium">
+                        Have questions about PostHog? <br className="md:hidden" />
+                        <Link to={`/questions/${product.slug}`}>Ask the community</Link> or{' '}
+                        <Link to="/talk-to-a-human">book a demo</Link>.
+                    </p>
                 </section>
 
-                <section
-                    id="docs"
-                    className={`${fullWidthContent ? 'max-w-full px-8' : 'max-w-7xl mx-auto'} px-5 py-10 md:pt-20 pb-0`}
-                >
+                <section id="docs" className="py-16">
                     <h3 className="text-3xl lg:text-4xl text-center mb-2">Explore the docs</h3>
                     <p className="mt-0 text-opacity-70 text-center">
                         Get a more technical overview of how everything works <Link to="/docs">in our docs</Link>.
@@ -525,73 +532,43 @@ export const ProductErrorTracking = () => {
                     />
                 </section>
 
-                <div className={`${fullWidthContent ? 'max-w-full px-0 md:px-8' : 'max-w-7xl'} mx-auto`}>
-                    <div id="posthog-vs">
-                        <section>
-                            <h2 className="text-center text-3xl lg:text-4xl">PostHog vs...</h2>
-                            <Comparison comparison={comparison} columnCount={comparisonColumnCount} truncate />
-                        </section>
+                <section id="team">
+                    <h3 className="text-3xl lg:text-4xl text-center">Meet the team</h3>
 
-                        <section className="mb-20">
-                            <h3 className="text-center mb-8">So, what's best for you?</h3>
-                            <div className="@container mb-8 mx-5 md:mx-0 grid md:grid-cols-2 gap-4">
-                                <VsCompetitor
-                                    title="Reasons a competitor may be best for you (for now...)"
-                                    image={
-                                        <CloudinaryImage
-                                            src="https://res.cloudinary.com/dmukukwp6/image/upload/posthog.com/src/images/products/competitors-ff.png"
-                                            className="max-w-[176px]"
-                                        />
-                                    }
-                                >
-                                    <ul>
-                                        <li>
-                                            Uptime monitoring
-                                            <ul className="pl-6">
-                                                <li className="text-sm">We don't have uptime monitoring. Yet.</li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            Advanced alerting
-                                            <ul className="pl-6">
-                                                <li className="text-sm">
-                                                    We currently only support Slack and email alerts on custom criteria.
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li>Advanced error grouping systems</li>
-                                    </ul>
-                                </VsCompetitor>
-                                <VsPostHog>
-                                    <ul>
-                                        <li>Integration with other PostHog products</li>
-                                        <li>
-                                            Feature flags for error recovery
-                                            <ul className="pl-6">
-                                                <li className="text-sm">
-                                                    Quickly roll back features that cause errors.
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li>Simple, transparent pricing</li>
-                                    </ul>
-                                </VsPostHog>
-                            </div>
+                    <p className="text-center mb-0">
+                        PostHog works in small teams. The <Link to={teamSlug}>{team}</Link> team is responsible for
+                        building this product.
+                    </p>
+                    <TeamMembers teamName={team} setActiveProfile={setActiveProfile} />
+                </section>
 
-                            <p className="text-center text-sm font-medium">
-                                Have questions about PostHog? <br className="md:hidden" />
-                                <Link to={`/questions/${product.slug}`}>Ask the community</Link> or{' '}
-                                <Link to="/talk-to-a-human">book a demo</Link>.
-                            </p>
-                        </section>
+                <section id="questions" className="my-20 px-5">
+                    <h3 className="text-3xl lg:text-4xl text-center mb-2">Questions?</h3>
+
+                    <p className="text-center mb-4">See more questions (or ask your own!) in our community forums.</p>
+
+                    <div className="text-center mb-8">
+                        <CallToAction href={`/questions/${product.slug}`} type="secondary" size="sm">
+                            View {product.lowercase} questions
+                        </CallToAction>
                     </div>
-                </div>
 
-                <div className={`${fullWidthContent ? 'max-w-full px-8' : 'mx-auto'} relative px-5 py-10 pb-0`}>
-                    <section className="mb-20">
-                        <CTA />
-                    </section>
-                </div>
+                    <Questions topicIds={[389]} />
+                </section>
+
+                <section className="pb-12">
+                    <PairsWith items={pairsWithItemCount}>
+                        {PairsWithArray.map((card, index) => {
+                            return <PairsWithItem {...card} key={index} />
+                        })}
+                    </PairsWith>
+                </section>
+            </div>
+
+            <div className="max-w-7xl mx-auto relative">
+                <section className="mb-12">
+                    <CTA />
+                </section>
             </div>
         </>
     )
