1162c13f757b8901f7d4373d265e626c7b763a21
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2025-04-08 18:07:13 +0100

    Tweaks

--- src/components/Product/ErrorTracking/index.tsx
+++ src/components/Product/ErrorTracking/index.tsx
@@ -294,14 +294,6 @@ const comparison = [
             PostHog: true,
         },
     },
-    {
-        feature: 'Boring, purple website with corporate vibes',
-        companies: {
-            Sentry: true,
-            LogRocket: true,
-            PostHog: false,
-        },
-    },
 ]
 
 export const ProductErrorTracking = () => {
