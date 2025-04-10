07b6042266467bb9961e7a9b0de48aa72c537daa
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2025-04-10 11:34:59 +0100

    Update src/pages/error-tracking.tsx

--- src/pages/error-tracking.tsx
+++ src/pages/error-tracking.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ProductErrorTracking } from 'components/Product/ErrorTracking'
 import Layout from 'components/Layout'
-
 export default function ErrorTracking(): JSX.Element {
     return (
         <Layout>
