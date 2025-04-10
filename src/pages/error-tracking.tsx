d56875c8acce4e19aabc09d9354a2784b9b30903
Joe Martin <84011561+joethreepwood@users.noreply.github.com>
2025-04-10 11:34:46 +0100

    Update src/pages/error-tracking.tsx

--- src/pages/error-tracking.tsx
+++ src/pages/error-tracking.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { ProductErrorTracking } from 'components/Product/ErrorTracking'
-import { MaxCTA } from 'components/MaxCTA'
 import Layout from 'components/Layout'
 
 export default function ErrorTracking(): JSX.Element {
     return (
         <Layout>
-                <p className="mt-2">Try asking Max to help you set up error tracking in your application.</p>
-            </MaxCTA>
             <ProductErrorTracking />
         </Layout>
     )
