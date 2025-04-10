c742b92dac46699363274608c3e4d3272e9feb9a
Cory Watilo <corywatilo@gmail.com>
2025-04-09 22:00:37 -0400

    use slug in features page

--- src/pages/product-analytics/features.tsx
+++ src/pages/product-analytics/features.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import Layout from 'components/Layout'
 import ProductProductAnalytics from 'components/Product/ProductAnalytics'
 import Explorer from 'components/Explorer'
+import SEO from 'components/seo'
 
 export default function ProductAnalyticsFeatures(): JSX.Element {
     return (
-        <Explorer>
+      <>
+        <SEO
+            title="Features – Product Analytics"
+            description="PostHog is the only product analytics platform built to natively work with Session Replay, Feature Flags, Experiments, and Surveys."
+            image={`/images/og/product-analytics.jpg`}
+        />
+        <Explorer slug="product-analytics" title="Features">
           features go here!
         </Explorer>
+      </>
     )
 }
