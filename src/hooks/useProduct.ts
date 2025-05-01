d0dd7c57238dc72eddd6261cb89be9eea717eac4
Cory Watilo <corywatilo@gmail.com>
2025-05-01 10:45:46 -0400

    define features customers per product

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -29,6 +29,29 @@ export default function useProduct({ type } = {}) {
             ...products.find((product) => product.type === 'product_analytics'),
             type: 'product_analytics',
             name: 'Product analytics',
+            slug: '/product-analytics',
+            customers: [
+                {
+                    name: 'Y Combinator',
+                    title: 'gathers 30% more data than with Google Analytics',
+                    description: 'We could autocapture... events using the JS snippet and... configure custom events.'
+                },
+                {
+                    name: 'Hasura',
+                    title: 'improved conversion rates by 10-20%',
+                    description: 'we observed drop-offs at very particular stages of our onboarding flow.'
+                },
+                {
+                    name: 'Contra',
+                    title: 'increased registrations by 30%',
+                    description: 'From [funnels], we could easily jump to session replays to see the drop-off point.'
+                },
+                {
+                    name: 'Speakeasy',
+                    title: 'manages features and developer relations',
+                    description: '...top-to-bottom view of conversion rates and user paths, without... extra setup time.'
+                },
+            ],
         },
         ...products,
     ]
