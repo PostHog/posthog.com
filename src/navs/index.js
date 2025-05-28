09adada1c4ea9aae13f451d1ed3d0e89ace85822
Chris <141421870+CEMcNEill@users.noreply.github.com>
2025-05-28 06:36:37 -0400

    Update index.js (#11529)

--- src/navs/index.js
+++ src/navs/index.js
@@ -1175,6 +1175,10 @@ export const handbookSidebar = [
                 name: 'Expansion & Retention',
                 url: '/handbook/growth/sales/expansion-and-retention',
             },
+                        {
+                name: 'Account Planning',
+                url: '/handbook/growth/sales/account-planning',
+            },
             {
                 name: 'How we work',
                 url: '/handbook/growth/sales/how-we-work',
