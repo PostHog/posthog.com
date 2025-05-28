0ac54f1f5049bd6c1143fd68cdc9acfc0df39400
Edgar Cerecerez <edgar@hey.com>
2025-05-27 21:37:36 -0700

    Saving baseline wip of revenue-analytics
    Honestly, just duplicated the page from Web Analytics to serve as a starting point.

---
+++ src/pages/revenue-analytics.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductRevenueAnalytics from 'components/Product/RevenueAnalytics'
+import Layout from 'components/Layout'
+
+export default function ProductRevenueAnalyticsPage(): JSX.Element {
+    return (
+        <Layout>
+            <ProductRevenueAnalytics />
+        </Layout>
+    )
+} 
