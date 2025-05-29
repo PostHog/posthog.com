bb509fc5a3a1051ec72876f794c10d8d534600d4
Eli Kinsey <eli@ekinsey.dev>
2025-05-29 07:10:30 -0700

    side-by-side view

--- src/components/Inbox/index.tsx
+++ src/components/Inbox/index.tsx
@@ -10,7 +10,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import { Question } from 'components/Squeak'
 import { useLocation } from '@reach/router'
 import OSButton from 'components/OSButton'
-import { IconCheck, IconCornerDownRight } from '@posthog/icons'
+import { IconCheck, IconCornerDownRight, IconSidePanel, IconBottomPanel } from '@posthog/icons'
 import Switch from 'components/RadixUI/Switch'
 import { useToast } from '../../context/Toast'
 import { QuestionData, StrapiRecord } from 'lib/strapi'
@@ -19,6 +19,7 @@ import { navigate } from 'gatsby'
 import Lottie from 'lottie-react'
 import hourglassAnimation from 'images/icons8-hourglass.json'
 import { useInView } from 'react-intersection-observer'
+import Tooltip from 'components/Tooltip'
 
 dayjs.extend(relativeTime)
 
@@ -75,12 +76,19 @@ export default function Inbox(props) {
         filters,
     })
     const [bottomHeight, setBottomHeight] = useState(300)
+    const [sideWidth, setSideWidth] = useState(600)
     const [notificationsEnabled, setNotificationsEnabled] = useState(false)
     const [question, setQuestion] = useState<StrapiRecord<QuestionData>>()
     const containerRef = useRef<HTMLDivElement>(null)
     const bottomContainerRef = useRef<HTMLDivElement>(null)
     const [isDragging, setIsDragging] = useState(false)
     const [lastQuestionRef, inView] = useInView({ threshold: 0.1 })
+    const [sideBySide, setSideBySide] = useState(false)
+
+    const handleSideBySide = (sideBySide: boolean) => {
+        setSideBySide(sideBySide)
+        localStorage.setItem('sideBySide', sideBySide.toString())
+    }
 
     useEffect(() => {
         if (inView && hasMore) {
@@ -116,17 +124,30 @@ export default function Inbox(props) {
         }
     }, [isValidating, props.path])
 
+    useEffect(() => {
+        const sideBySide = localStorage.getItem('sideBySide')
+        if (sideBySide) {
+            setSideBySide(sideBySide === 'true')
+        }
+    }, [])
+
     return (
         ready && (
             <div className="@container w-full h-full flex flex-col">
                 <HeaderBar showHome showBack showForward showSearch />
                 <div data-scheme="secondary" className="flex flex-grow border-t border-primary min-h-0">
-                    <aside data-scheme="secondary" className="w-64 bg-primary border-r border-primary h-full p-2">
+                    <aside
+                        data-scheme="secondary"
+                        className="w-64 bg-primary border-r border-primary h-full p-2 flex-shrink-0"
+                    >
                         <Menu />
                     </aside>
                     <main data-scheme="primary" className="flex-1 bg-primary">
-                        <div ref={containerRef} className="flex flex-col h-full">
-                            <div className="flex-1 min-h-0">
+                        <div
+                            ref={containerRef}
+                            className={`flex flex-row h-full ${sideBySide ? 'flex-row' : 'flex-col'}`}
+                        >
+                            <div className={`flex-1 min-h-0 ${sideBySide ? 'w-0' : 'w-full'}`}>
                                 <ScrollArea className="h-full">
                                     <div className="flex items-center px-3.5 py-2 border-b border-primary font-medium bg-secondary text-sm bg-accent-2 sticky top-0">
                                         <div className="flex-1">Subject</div>
@@ -198,34 +219,75 @@ export default function Inbox(props) {
                             {permalink && (
                                 <div
                                     ref={bottomContainerRef}
-                                    className={`flex-none relative min-h-0 ${
-                                        !isDragging ? 'transition-[height] duration-200 ease-out' : ''
-                                    }`}
-                                    style={{ height: bottomHeight }}
+                                    className={`flex-none relative min-h-0 min-w-[600px] ${
+                                        !isDragging ? 'transition-all duration-200 ease-out' : ''
+                                    } ${sideBySide ? 'border-l border-border' : ''}`}
+                                    style={{
+                                        height: sideBySide ? '100%' : bottomHeight,
+                                        width: sideBySide ? sideWidth : '100%',
+                                    }}
                                 >
-                                    <motion.div
-                                        data-scheme="tertiary"
-                                        className="h-1.5 cursor-ns-resize top-0 left-0 !transform-none absolute z-20 w-full hover:bg-accent active:bg-accent"
-                                        drag="y"
-                                        dragMomentum={false}
-                                        dragConstraints={{ top: 0, bottom: 0 }}
-                                        onDragStart={() => setIsDragging(true)}
-                                        onDragEnd={() => setIsDragging(false)}
-                                        onDrag={(_event, info) => {
-                                            if (!containerRef.current) return
-                                            const containerHeight = containerRef.current.getBoundingClientRect().height
-                                            const newBottomHeight = Math.min(
-                                                Math.max(bottomHeight - info.delta.y, 57),
-                                                containerHeight
-                                            )
-                                            setBottomHeight(newBottomHeight)
-                                        }}
-                                        onDoubleClick={() => {
-                                            if (!containerRef.current) return
-                                            const containerHeight = containerRef.current.getBoundingClientRect().height
-                                            setBottomHeight(bottomHeight <= 57 ? containerHeight : 57)
-                                        }}
-                                    />
+                                    {sideBySide ? (
+                                        <motion.div
+                                            data-scheme="tertiary"
+                                            className="w-1.5 cursor-ew-resize top-0 left-0 !transform-none absolute z-20 h-full hover:bg-accent active:bg-accent"
+                                            drag="x"
+                                            dragMomentum={false}
+                                            dragConstraints={{ left: 0, right: 0 }}
+                                            onDragStart={() => setIsDragging(true)}
+                                            onDragEnd={() => setIsDragging(false)}
+                                            onDrag={(_event, info) => {
+                                                if (!containerRef.current) return
+                                                const containerWidth =
+                                                    containerRef.current.getBoundingClientRect().width
+                                                const newSideWidth = Math.min(
+                                                    Math.max(sideWidth - info.delta.x, 300),
+                                                    containerWidth
+                                                )
+                                                setSideWidth(newSideWidth)
+                                            }}
+                                            onDoubleClick={() => {
+                                                if (!containerRef.current) return
+                                                const containerWidth =
+                                                    containerRef.current.getBoundingClientRect().width
+                                                setSideWidth(
+                                                    sideWidth <= containerWidth / 3
+                                                        ? containerWidth
+                                                        : containerWidth / 3
+                                                )
+                                            }}
+                                        />
+                                    ) : (
+                                        <motion.div
+                                            data-scheme="tertiary"
+                                            className="h-1.5 cursor-ns-resize top-0 left-0 !transform-none absolute z-20 w-full hover:bg-accent active:bg-accent"
+                                            drag="y"
+                                            dragMomentum={false}
+                                            dragConstraints={{ top: 0, bottom: 0 }}
+                                            onDragStart={() => setIsDragging(true)}
+                                            onDragEnd={() => setIsDragging(false)}
+                                            onDrag={(_event, info) => {
+                                                if (!containerRef.current) return
+                                                const containerHeight =
+                                                    containerRef.current.getBoundingClientRect().height
+                                                const newBottomHeight = Math.min(
+                                                    Math.max(bottomHeight - info.delta.y, 57),
+                                                    containerHeight
+                                                )
+                                                setBottomHeight(newBottomHeight)
+                                            }}
+                                            onDoubleClick={() => {
+                                                if (!containerRef.current) return
+                                                const containerHeight =
+                                                    containerRef.current.getBoundingClientRect().height
+                                                setBottomHeight(
+                                                    bottomHeight <= containerHeight / 1.5
+                                                        ? containerHeight
+                                                        : containerHeight / 1.5
+                                                )
+                                            }}
+                                        />
+                                    )}
                                     <ScrollArea className="h-full">
                                         <div className="bg-accent border-y border-border px-4 py-2 flex gap-2 items-center sticky top-0 z-10">
                                             <OSButton
@@ -251,38 +313,73 @@ export default function Inbox(props) {
                                             >
                                                 Reply
                                             </OSButton>
-
-                                            {question?.id && user && (
-                                                <Switch
-                                                    className="ml-auto"
-                                                    checked={notificationsEnabled}
-                                                    onChange={(checked) => {
-                                                        setNotificationsEnabled(checked)
-                                                        setSubscription({
-                                                            contentType: 'question',
-                                                            id: question.id,
-                                                            subscribe: checked,
-                                                        })
-                                                        addToast({
-                                                            description: checked
-                                                                ? "You'll be notified of replies by email."
-                                                                : "You won't receive notifications for this thread.",
-                                                            title: checked
-                                                                ? 'Thread notifications enabled'
-                                                                : 'Thread notifications disabled',
-                                                            onUndo: () => {
-                                                                setNotificationsEnabled(!checked)
-                                                                setSubscription({
-                                                                    contentType: 'question',
-                                                                    id: question.id,
-                                                                    subscribe: !checked,
-                                                                })
-                                                            },
-                                                        })
-                                                    }}
-                                                    label="Thread notifications"
-                                                />
-                                            )}
+                                            <div className="ml-auto flex space-x-4">
+                                                {question?.id && user && (
+                                                    <Switch
+                                                        checked={notificationsEnabled}
+                                                        onChange={(checked) => {
+                                                            setNotificationsEnabled(checked)
+                                                            setSubscription({
+                                                                contentType: 'question',
+                                                                id: question.id,
+                                                                subscribe: checked,
+                                                            })
+                                                            addToast({
+                                                                description: checked
+                                                                    ? "You'll be notified of replies by email."
+                                                                    : "You won't receive notifications for this thread.",
+                                                                title: checked
+                                                                    ? 'Thread notifications enabled'
+                                                                    : 'Thread notifications disabled',
+                                                                onUndo: () => {
+                                                                    setNotificationsEnabled(!checked)
+                                                                    setSubscription({
+                                                                        contentType: 'question',
+                                                                        id: question.id,
+                                                                        subscribe: !checked,
+                                                                    })
+                                                                },
+                                                            })
+                                                        }}
+                                                        label="Thread notifications"
+                                                    />
+                                                )}
+                                                <div>
+                                                    <Tooltip
+                                                        content={`${
+                                                            sideBySide
+                                                                ? 'Switch to stacked view'
+                                                                : 'Switch to side-by-side view'
+                                                        }`}
+                                                    >
+                                                        <span>
+                                                            <OSButton
+                                                                active={!sideBySide}
+                                                                variant="ghost"
+                                                                size="sm"
+                                                                icon={<IconBottomPanel />}
+                                                                onClick={() => handleSideBySide(false)}
+                                                            />
+                                                        </span>
+                                                    </Tooltip>
+                                                    <Tooltip content={`Side-by-side view`}>
+                                                        <span>
+                                                            <OSButton
+                                                                active={sideBySide}
+                                                                className={`${
+                                                                    sideBySide
+                                                                        ? '!skin-classic:bg-red-500 !skin-modern:bg-red-500'
+                                                                        : ''
+                                                                }`}
+                                                                variant="ghost"
+                                                                size="sm"
+                                                                icon={<IconSidePanel />}
+                                                                onClick={() => handleSideBySide(true)}
+                                                            />
+                                                        </span>
+                                                    </Tooltip>
+                                                </div>
+                                            </div>
                                         </div>
                                         <div className="p-5">
                                             <Question
