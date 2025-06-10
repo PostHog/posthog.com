72071b6d361914a0477cb08f53ba861e115d043d
Cory Watilo <corywatilo@gmail.com>
2025-06-10 14:36:09 -0400

    improved desktop icon sizing

--- src/components/Desktop/index.tsx
+++ src/components/Desktop/index.tsx
@@ -58,7 +58,7 @@ const productLinks: AppItem[] = [
         label: 'demo.mov',
         Icon: IconDemoThumb,
         url: '/demo',
-        className: '!size-14 -mt-4 -mb-3',
+        className: 'size-14 -my-1',
     },
     {
         label: 'Docs',
@@ -110,35 +110,37 @@ const AppLink = ({ Icon, type, color, label, url, className }: AppItem) => {
 
     const renderIcon = () => {
         if (typeof Icon === 'string') {
-            return <IconImage url={Icon} className={`size-7 text-${color} ${className}`} />
+            return <IconImage url={Icon} className={`text-${color} ${className}`} />
         }
 
         if (React.isValidElement(Icon)) {
             return React.cloneElement(Icon as React.ReactElement<any>, {
-                className: `size-7 text-${color} ${className}`
+                className: `text-${color} ${className}`,
             })
         }
 
         // Icon is a ComponentType
         const IconComponent = Icon as React.ComponentType<any>
-        return <IconComponent className={`size-7 text-${color} ${className}`} />
+        return <IconComponent className={`text-${color} ${className}`} />
     }
 
     return (
-        <span ref={ref}>
+        <figure ref={ref}>
             <Link
                 to={url}
                 state={{ newWindow: true }}
-                className="group inline-flex flex-col justify-center items-center space-y-1 w-auto max-w-28 text-center select-none"
+                className="group inline-flex flex-col justify-center items-center w-auto max-w-40 text-center select-none space-y-1"
             >
                 {renderIcon()}
-                <p className="text-sm font-medium leading-tight">
-                    <span className="bg-[rgba(238,239,233,0.75)] group-hover:bg-[rgba(238,239,233,1)] dark:bg-[rgba(1,1,1,0.75)] dark:group-hover:bg-[rgba(1,1,1,1)] rounded-[2px] px-0.5">
-                        <span className="skin-classic:underline decoration-dotted decoration-primary underline-offset-[3px]">{label}</span>
+                <figcaption className="text-sm font-medium leading-tight">
+                    <span className="inline-block bg-[rgba(238,239,233,0.75)] group-hover:bg-[rgba(238,239,233,1)] dark:bg-[rgba(1,1,1,0.75)] dark:group-hover:bg-[rgba(1,1,1,1)] rounded-[2px] px-0.5 py-0 leading-tight">
+                        <span className="skin-classic:underline decoration-dotted decoration-primary underline-offset-[3px]">
+                            {label}
+                        </span>
                     </span>
-                </p>
+                </figcaption>
             </Link>
-        </span>
+        </figure>
     )
 }
 
@@ -175,7 +177,7 @@ export default function Desktop() {
             >
                 <ul className="py-1 px-0 m-0 list-none flex flex-col flex-wrap h-full content-start gap-x-8 gap-y-5">
                     {productLinks.map((app, index) => (
-                        <li key={app.label + index} className="w-[110px] flex justify-center">
+                        <li key={app.label + index} className="w-28 flex justify-center">
                             <ZoomHover>
                                 <AppLink {...app} />
                             </ZoomHover>
@@ -199,7 +201,7 @@ export default function Desktop() {
                 </ul>
                 <ul className="py-1 px-0 m-0 list-none flex flex-col flex-wrap h-full content-start gap-x-8 gap-y-5">
                     {apps.map((app, index) => (
-                        <li key={app.label + index} className="w-[110px] flex justify-center">
+                        <li key={app.label + index} className="w-28 flex justify-center">
                             <ZoomHover>
                                 <AppLink {...app} />
                             </ZoomHover>
