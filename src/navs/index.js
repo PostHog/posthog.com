3a7052637f0e71c61c51a9c301a5efbdc64f5130
Juraj Majerik <juro.majerik@gmail.com>
2025-06-12 11:52:16 +0200

    init

--- src/navs/index.js
+++ src/navs/index.js
@@ -3322,6 +3322,21 @@ export const docsMenu = {
                     name: 'Under the hood',
                 },
                 {
+                    name: 'Traffic allocation',
+                    url: '/docs/experiments/traffic-allocation',
+                    icon: 'IconPeople',
+                    color: 'orange',
+                },
+                {
+                    name: 'Sample size and running time',
+                    url: '/docs/experiments/sample-size-running-time',
+                    icon: 'IconDashboard',
+                    color: 'blue',
+                },
+                {
+                    name: 'Bayesian method',
+                },
+                {
                     name: 'Statistics overview',
                     url: '/docs/experiments/statistics',
                     icon: 'IconBook',
@@ -3346,15 +3361,12 @@ export const docsMenu = {
                     color: 'red',
                 },
                 {
-                    name: 'Traffic allocation',
-                    url: '/docs/experiments/traffic-allocation',
-                    icon: 'IconPeople',
-                    color: 'orange',
+                    name: 'Frequentist method',
                 },
                 {
-                    name: 'Sample size and running time',
-                    url: '/docs/experiments/sample-size-running-time',
-                    icon: 'IconDashboard',
+                    name: 'Overview',
+                    url: '/docs/experiments/frequentist-method',
+                    icon: 'IconBook',
                     color: 'blue',
                 },
             ],
