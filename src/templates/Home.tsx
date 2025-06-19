f5cd36de125028793b7e2c31062ececef0a7f972
Eli Kinsey <eli@ekinsey.dev>
2025-06-19 08:54:18 -0700

    create mdx editor component

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,8 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { MDXProvider } from '@mdx-js/react'
-import Editor from 'components/Editor'
+import React, { useEffect, useState } from 'react'
 import Link from 'components/Link'
 import OSTable from 'components/OSTable'
 import { useCustomers } from 'hooks/useCustomers'
@@ -13,40 +9,13 @@ import Pricing from 'components/Home/New/Pricing'
 import OSButton from 'components/OSButton'
 import useProduct from 'hooks/useProduct'
 import { Accordion } from 'components/RadixUI/Accordion'
-import {
-    JsxComponentDescriptor,
-    MDXEditor,
-    frontmatterPlugin,
-    headingsPlugin,
-    jsxPlugin,
-    linkPlugin,
-    listsPlugin,
-    toolbarPlugin,
-    usePublisher,
-    applyFormat$,
-    currentFormat$,
-    useCellValues,
-    FORMAT,
-    IS_BOLD,
-    IS_ITALIC,
-    IS_STRIKETHROUGH,
-    activeEditor$,
-} from '@mdxeditor/editor'
-import {
-    LexicalEditor,
-    REDO_COMMAND,
-    UNDO_COMMAND,
-    FORMAT_ELEMENT_COMMAND,
-    $getSelection,
-    $isRangeSelection,
-    COMMAND_PRIORITY_CRITICAL,
-    CAN_REDO_COMMAND,
-    CAN_UNDO_COMMAND,
-} from 'lexical'
-import { mergeRegister } from '@lexical/utils'
+import { JsxComponentDescriptor } from '@mdxeditor/editor'
+
 import Logo from 'components/Logo'
 import { useApp } from '../context/App'
 import { useWindow } from '../context/Window'
+import MDXEditor from 'components/MDXEditor'
+import { graphql } from 'gatsby'
 
 interface ProductButtonsProps {
     productTypes: string[]
@@ -136,7 +105,9 @@ const Products = () => {
                 { content: 3 },
                 { content: 'talk to customers', className: 'font-bold' },
                 {
-                    content: <ProductButtons productTypes={['Surveys', 'messaging', 'User interviews', 'Broadcasts']} />,
+                    content: (
+                        <ProductButtons productTypes={['Surveys', 'messaging', 'User interviews', 'Broadcasts']} />
+                    ),
                     className: 'text-sm',
                 },
             ],
@@ -367,68 +338,9 @@ const jsxComponentDescriptors: JsxComponentDescriptor[] = [
     },
 ]
 
-export default function Home({
-    data,
-}: {
-    data: { mdx: { frontmatter: { title: string; template: string }; body: string; rawBody: string } }
-}) {
-    const [currentFormat, setCurrentFormat] = useState<FORMAT>(0)
-    const [activeEditor, setActiveEditor] = useState<LexicalEditor>()
-    const applyFormatRef = React.useRef<((value: any) => void) | null>(null)
-    const [currentAlignment, setCurrentAlignment] = useState<'left' | 'center' | 'right' | 'justify'>('left')
-    const [canUndo, setCanUndo] = React.useState(false)
-    const [canRedo, setCanRedo] = React.useState(false)
+export default function Home({ data }: { data: { mdx: { rawBody: string } } }) {
     const { appWindow } = useWindow()
     const { setWindowTitle } = useApp()
-
-    useEffect(() => {
-        if (activeEditor) {
-            mergeRegister(
-                activeEditor.registerCommand<boolean>(
-                    CAN_UNDO_COMMAND,
-                    (payload) => {
-                        setCanUndo(payload)
-                        return false
-                    },
-                    COMMAND_PRIORITY_CRITICAL
-                ),
-                activeEditor.registerCommand<boolean>(
-                    CAN_REDO_COMMAND,
-                    (payload) => {
-                        setCanRedo(payload)
-                        return false
-                    },
-                    COMMAND_PRIORITY_CRITICAL
-                )
-            )
-
-            const removeUpdateListener = activeEditor.registerUpdateListener(({ editorState }) => {
-                editorState.read(() => {
-                    const selection = $getSelection()
-                    if (selection && $isRangeSelection(selection)) {
-                        const anchorNode = selection.anchor.getNode()
-                        const element = anchorNode.getParent()
-                        if (element) {
-                            const format = element.getFormat()
-                            const alignment = format & 0x7
-                            setCurrentAlignment(
-                                alignment === 1
-                                    ? 'left'
-                                    : alignment === 2
-                                        ? 'center'
-                                        : alignment === 3
-                                            ? 'right'
-                                            : alignment === 4
-                                                ? 'justify'
-                                                : 'left'
-                            )
-                        }
-                    }
-                })
-            })
-            return () => removeUpdateListener()
-        }
-    }, [activeEditor])
 
     useEffect(() => {
         if (appWindow) {
@@ -436,84 +348,12 @@ export default function Home({
         }
     }, [])
 
-    return (
-        <Editor
-            type="mdx"
-            slug="/"
-            actionButtons={{
-                undo: {
-                    onClick: () => activeEditor?.dispatchCommand(UNDO_COMMAND, undefined),
-                    disabled: !canUndo,
-                },
-                redo: {
-                    onClick: () => activeEditor?.dispatchCommand(REDO_COMMAND, undefined),
-                    disabled: !canRedo,
-                },
-                bold: {
-                    onClick: () => applyFormatRef.current?.('bold'),
-                    active: (currentFormat & IS_BOLD) !== 0,
-                },
-                italic: {
-                    onClick: () => applyFormatRef.current?.('italic'),
-                    active: (currentFormat & IS_ITALIC) !== 0,
-                },
-                strikethrough: {
-                    onClick: () => applyFormatRef.current?.('strikethrough'),
-                    active: (currentFormat & IS_STRIKETHROUGH) !== 0,
-                },
-                leftAlign: {
-                    onClick: () => activeEditor?.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'left'),
-                    active: currentAlignment === 'left',
-                },
-                centerAlign: {
-                    onClick: () => activeEditor?.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'center'),
-                    active: currentAlignment === 'center',
-                },
-                rightAlign: {
-                    onClick: () => activeEditor?.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'right'),
-                    active: currentAlignment === 'right',
-                },
-            }}
-        >
-            <MDXEditor
-                contentEditableClassName="outline-none"
-                markdown={data.mdx.rawBody}
-                plugins={[
-                    headingsPlugin(),
-                    frontmatterPlugin(),
-                    listsPlugin(),
-                    linkPlugin(),
-                    jsxPlugin({ jsxComponentDescriptors }),
-                    toolbarPlugin({
-                        toolbarContents: () => {
-                            const [currentFormat, activeEditor] = useCellValues(currentFormat$, activeEditor$)
-                            const applyFormat = usePublisher(applyFormat$)
-                            useEffect(() => {
-                                applyFormatRef.current = applyFormat
-                            }, [])
-                            useEffect(() => {
-                                setCurrentFormat(currentFormat)
-                            }, [currentFormat])
-                            useEffect(() => {
-                                setActiveEditor(activeEditor)
-                            }, [activeEditor])
-                            return null
-                        },
-                    }),
-                ]}
-            />
-        </Editor>
-    )
+    return <MDXEditor jsxComponentDescriptors={jsxComponentDescriptors} body={data.mdx.rawBody} />
 }
 
 export const query = graphql`
     query HomeQuery($id: String!) {
         mdx(id: { eq: $id }) {
-            frontmatter {
-                title
-                template
-            }
-            body
             rawBody
         }
     }
