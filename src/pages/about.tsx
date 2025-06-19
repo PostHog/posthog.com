800a2c76a40365743d4b611b94bc7cbd3e00b6fc
Eli Kinsey <eli@ekinsey.dev>
2025-06-19 09:01:38 -0700

    about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,37 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import MDXEditor from 'components/MDXEditor'
+import Logo from 'components/Logo'
+import { JsxComponentDescriptor } from '@mdxeditor/editor'
+
+const jsxComponentDescriptors: JsxComponentDescriptor[] = [
+    {
+        name: 'HappyHog',
+        kind: 'flow',
+        props: [],
+        Editor: () => (
+            <img
+                src="https://res.cloudinary.com/dmukukwp6/image/upload/happy_hog_ebc59e4658.png"
+                alt="happy hog"
+                className="float-right max-w-[400px] max-h-48 -mt-2 -mr-2"
+            />
+        ),
+    },
+    {
+        name: 'Logo',
+        kind: 'flow',
+        props: [],
+        Editor: () => <Logo noText className="inline-block" />,
+    },
+]
+export default function About({ data }: { data: { mdx: { rawBody: string } } }) {
+    return <MDXEditor jsxComponentDescriptors={jsxComponentDescriptors} body={data.mdx.rawBody} />
+}
+
+export const query = graphql`
+    {
+        mdx(fields: { slug: { eq: "/about" } }) {
+            rawBody
+        }
+    }
+`
