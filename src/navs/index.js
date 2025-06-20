d60bea2458c34449f4cb146e3abf4efc0dd2869b
Annika 🦔 <14750837+annikaschmid@users.noreply.github.com>
2025-06-20 23:41:57 +0100

    Remove 'beta' label from query runner (#11892)

--- src/navs/index.js
+++ src/navs/index.js
@@ -3316,10 +3316,6 @@ export const docsMenu = {
                     url: '/docs/experiments/new-experimentation-engine',
                     icon: 'IconTestTube',
                     color: 'green',
-                    badge: {
-                        title: 'Beta',
-                        className: 'uppercase !bg-blue/10 !text-blue !dark:text-white !dark:bg-blue/50',
-                    },
                 },
                 {
                     name: 'Data warehouse',
