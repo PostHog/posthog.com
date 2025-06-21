1152849cf267b40276ef5a09ca308366acc293d6
Cory Watilo <corywatilo@gmail.com>
2025-06-21 11:49:40 -0400

    don't use editor layout on about page (but use md)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,46 +1,60 @@
 import { graphql } from 'gatsby'
 import React from 'react'
-import MDXEditor from 'components/MDXEditor'
-import Logo from 'components/Logo'
-import { JsxComponentDescriptor } from '@mdxeditor/editor'
 import ReaderView from 'components/ReaderView'
 import { TreeMenu } from 'components/TreeMenu'
 import { companyMenu } from '../navs'
+import { YC } from 'components/About/v2/YC'
+import Logo from 'components/Logo'
+import { shortcodes } from '../mdxGlobalComponents'
+import { Blockquote } from 'components/BlockQuote'
+import { MdxCodeBlock } from 'components/CodeBlock'
+import { Heading } from 'components/Heading'
+import { InlineCode } from 'components/InlineCode'
+import { ZoomImage } from 'components/ZoomImage'
+import Link from 'components/Link'
 
-const jsxComponentDescriptors: JsxComponentDescriptor[] = [
-    {
-        name: 'HappyHog',
-        kind: 'flow',
-        props: [],
-        Editor: () => (
-            <img
-                src="https://res.cloudinary.com/dmukukwp6/image/upload/happy_hog_ebc59e4658.png"
-                alt="happy hog"
-                className="float-right max-w-[400px] max-h-48 -mt-2 -mr-2"
-            />
-        ),
-    },
-    {
-        name: 'Logo',
-        kind: 'flow',
-        props: [],
-        Editor: () => <Logo noText className="inline-block" />,
-    },
-]
-export default function About({ data }: { data: { mdx: { rawBody: string } } }) {
+const HappyHog = () => (
+    <img
+        src="https://res.cloudinary.com/dmukukwp6/image/upload/happy_hog_ebc59e4658.png"
+        alt="happy hog"
+        className="float-right max-w-[400px] max-h-48 -mt-2 -mr-2"
+    />
+)
+
+const A = (props: any) => <Link {...props} className="text-red hover:text-red font-semibold" />
+
+export default function About({ data }: { data: { mdx: { body: string } } }) {
+    const components = {
+        HappyHog,
+        Logo: () => <Logo noText className="inline-block" />,
+        YC,
+        inlineCode: InlineCode,
+        blockquote: Blockquote,
+        pre: MdxCodeBlock,
+        h1: (props: any) => Heading({ as: 'h1', ...props }),
+        h2: (props: any) => Heading({ as: 'h2', ...props }),
+        h3: (props: any) => Heading({ as: 'h3', ...props }),
+        h4: (props: any) => Heading({ as: 'h4', ...props }),
+        h5: (props: any) => Heading({ as: 'h5', ...props }),
+        h6: (props: any) => Heading({ as: 'h6', ...props }),
+        img: ZoomImage,
+        a: A,
+        ...shortcodes,
+    }
+
     return (
         <ReaderView
+            body={{ type: 'mdx', content: data.mdx.body }}
             leftSidebar={<TreeMenu items={companyMenu.children.map((child) => ({ ...child, children: [] }))} />}
-        >
-            <MDXEditor jsxComponentDescriptors={jsxComponentDescriptors} body={data.mdx.rawBody} />
-        </ReaderView>
+            mdxComponents={components}
+        />
     )
 }
 
 export const query = graphql`
     {
         mdx(fields: { slug: { eq: "/about" } }) {
-            rawBody
+            body
         }
     }
 `
