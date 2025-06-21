2d72f3c85fd99285b7e33b6e4703327df64fe2dc
Eli Kinsey <eli@ekinsey.dev>
2025-06-21 14:12:46 -0700

    new window on auth

--- src/context/App.tsx
+++ src/context/App.tsx
@@ -70,22 +70,22 @@ interface AppProviderProps {
 
 export const Context = createContext<AppContextType>({
     windows: [],
-    closeWindow: () => { },
-    bringToFront: () => { },
+    closeWindow: () => {},
+    bringToFront: () => {},
     setWindowTitle: () => null,
     focusedWindow: undefined,
     location: {},
-    minimizeWindow: () => { },
+    minimizeWindow: () => {},
     taskbarHeight: 0,
-    addWindow: () => { },
-    updateWindowRef: () => { },
-    updateWindow: () => { },
+    addWindow: () => {},
+    updateWindowRef: () => {},
+    updateWindow: () => {},
     getPositionDefaults: () => ({ x: 0, y: 0 }),
     getDesktopCenterPosition: () => ({ x: 0, y: 0 }),
-    openSearch: () => { },
-    handleSnapToSide: () => { },
+    openSearch: () => {},
+    handleSnapToSide: () => {},
     constraintsRef: { current: null },
-    expandWindow: () => { },
+    expandWindow: () => {},
 })
 
 const appSettings = {
@@ -267,12 +267,12 @@ export const Provider = ({ children, element, location }: AppProviderProps) => {
                     windows.map((w) =>
                         w === focusedWindow
                             ? {
-                                ...w,
-                                element: newWindow.element,
-                                path: newWindow.path,
-                                fromHistory: newWindow.fromHistory,
-                                props: newWindow.props,
-                            }
+                                  ...w,
+                                  element: newWindow.element,
+                                  path: newWindow.path,
+                                  fromHistory: newWindow.fromHistory,
+                                  props: newWindow.props,
+                              }
                             : w
                     )
                 )
@@ -353,9 +353,9 @@ export const Provider = ({ children, element, location }: AppProviderProps) => {
             (key.startsWith('ask-max')
                 ? appSettings['ask-max']?.size?.max
                 : {
-                    width: window.innerWidth * 0.9,
-                    height: window.innerHeight * 0.9,
-                })
+                      width: window.innerWidth * 0.9,
+                      height: window.innerHeight * 0.9,
+                  })
         return {
             width: Math.min(defaultSize.width, window.innerWidth * 0.9),
             height: Math.min(defaultSize.height, window.innerHeight * 0.9),
@@ -372,6 +372,7 @@ export const Provider = ({ children, element, location }: AppProviderProps) => {
     }
 
     const updatePages = (element: WindowElement) => {
+        console.log(element)
         const existingWindow = windows.find((w) => w.path === element.props.location.pathname)
         const size = getInitialSize(element.key)
         const position = getPositionDefaults(element.key, size, windows)
@@ -402,9 +403,9 @@ export const Provider = ({ children, element, location }: AppProviderProps) => {
             fixedSize: settings?.size.fixed || false,
             fromOrigin: lastClickedElementRect
                 ? {
-                    x: lastClickedElementRect.x - size.width / 2,
-                    y: lastClickedElementRect.y - taskbarHeight - size.height / 2,
-                }
+                      x: lastClickedElementRect.x - size.width / 2,
+                      y: lastClickedElementRect.y - taskbarHeight - size.height / 2,
+                  }
                 : undefined,
             minimal: element.props.minimal ?? false,
         }
@@ -449,24 +450,24 @@ export const Provider = ({ children, element, location }: AppProviderProps) => {
             windows.map((w) =>
                 w === appWindow
                     ? {
-                        ...appWindow,
-                        position: {
-                            ...appWindow.position,
-                            ...(updates.position || {}),
-                        },
-                        size: {
-                            ...appWindow.size,
-                            ...(updates.size || {}),
-                        },
-                        previousPosition: {
-                            ...appWindow.previousPosition,
-                            ...(updates.previousPosition || {}),
-                        },
-                        previousSize: {
-                            ...appWindow.previousSize,
-                            ...(updates.previousSize || {}),
-                        },
-                    }
+                          ...appWindow,
+                          position: {
+                              ...appWindow.position,
+                              ...(updates.position || {}),
+                          },
+                          size: {
+                              ...appWindow.size,
+                              ...(updates.size || {}),
+                          },
+                          previousPosition: {
+                              ...appWindow.previousPosition,
+                              ...(updates.previousPosition || {}),
+                          },
+                          previousSize: {
+                              ...appWindow.previousSize,
+                              ...(updates.previousSize || {}),
+                          },
+                      }
                     : w
             )
         )
