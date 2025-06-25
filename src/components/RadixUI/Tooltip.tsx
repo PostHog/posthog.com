14baa47636b4324edf8f66614882d71df3fa27a7
Cory Watilo <corywatilo@gmail.com>
2025-06-25 11:58:01 -0700

    polished tooltips

--- src/components/RadixUI/Tooltip.tsx
+++ src/components/RadixUI/Tooltip.tsx
@@ -17,11 +17,16 @@ const Tooltip = ({ trigger, children, open, onOpenChange }: TooltipProps) => {
                 </RadixTooltip.Trigger>
                 <RadixTooltip.Portal>
                     <RadixTooltip.Content
-                        className="select-none rounded bg-white px-[15px] py-2.5 text-[15px] leading-none text-violet11 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] min-w-0 min-h-0 max-w-full max-h-full transition-all will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
-                        sideOffset={5}
+                        data-scheme="secondary"
+                        className="select-none rounded bg-primary border border-primary text-primary text-sm px-3 py-2.5 text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] min-w-0 min-h-0 max-w-full max-h-full transition-all will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
+                        sideOffset={0}
                     >
                         {children}
-                        <RadixTooltip.Arrow className="fill-white" />
+                        <RadixTooltip.Arrow asChild>
+                            <div className="w-5 h-2.5 overflow-hidden">
+                                <div className="w-3 h-3 border-r border-b border-primary bg-primary rotate-45 rounded-xs relative left-[3px] top-[-7px]" />
+                            </div>
+                        </RadixTooltip.Arrow>
                     </RadixTooltip.Content>
                 </RadixTooltip.Portal>
             </RadixTooltip.Root>
