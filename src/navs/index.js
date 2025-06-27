18beb05bb0a4da1f9cadd5916ca2dc0b1c6da95e
Charles Cook <charles@posthog.com>
2025-06-27 10:57:51 +0100

    Sales 2.0 handbook update (#11994)
    * Update overview.md
    
    * Update new-sales.md
    
    * Update index.js
    
    * Update product-led-sales.md
    
    * Update expansion-and-retention.md
    
    * Update account-planning.md
    
    * Update new-hire-onboarding.md
    
    * Update how-we-work.md
    
    * Create outbound-sales.md
    
    * Update outbound-sales.md
    
    * Fix typos
    
    * Update contents/handbook/growth/sales/how-we-work.md
    
    Co-authored-by: Simon Fisher <simon@posthog.com>
    
    ---------
    
    Co-authored-by: PostHog <hey@posthog.com>
    Co-authored-by: Simon Fisher <simon@posthog.com>

--- src/navs/index.js
+++ src/navs/index.js
@@ -806,6 +806,10 @@ export const handbookSidebar = [
                 name: 'Tracking feature requests',
                 url: '/handbook/cs-and-onboarding/feature-requests',
             },
+            {
+                name: 'YC onboarding',
+                url: '/handbook/growth/sales/yc-onboarding',
+            },
         ],
     },
     {
@@ -1260,7 +1264,7 @@ export const handbookSidebar = [
                 url: '/handbook/growth/sales/why-buy-posthog',
             },
             {
-                name: 'New sales',
+                name: 'Inbound sales',
                 url: '/handbook/growth/sales/new-sales',
             },
             {
@@ -1268,21 +1272,31 @@ export const handbookSidebar = [
                 url: '/handbook/growth/sales/product-led-sales',
             },
             {
-                name: 'Expansion & Retention',
-                url: '/handbook/growth/sales/expansion-and-retention',
+                name: 'Outbound sales',
+                url: '/handbook/growth/sales/outbound-sales',
             },
             {
-                name: 'Account planning',
-                url: '/handbook/growth/sales/account-planning',
+                name: 'Expansion, cross-sell & retention',
+                url: '/handbook/growth/sales/expansion-and-retention',
+                children: [
+                    {
+                    name: 'Account planning',
+                    url: '/handbook/growth/sales/account-planning',
+                    },
+                ],
             },
+
             {
                 name: 'How we work',
                 url: '/handbook/growth/sales/how-we-work',
+                children: [
+                    {
+                    name: 'New team member onboarding',
+                    url: '/handbook/growth/sales/new-hire-onboarding',
+                    },
+                ],  
             },
-            {
-                name: 'New team member onboarding',
-                url: '/handbook/growth/sales/new-hire-onboarding',
-            },
+
             {
                 name: 'Customer FAQs',
                 url: '/handbook/growth/sales/customer-faqs',
@@ -1300,15 +1314,11 @@ export const handbookSidebar = [
                         url: '/handbook/growth/sales/lead-scoring',
                     },
                     {
-                        name: 'YC onboarding',
-                        url: '/handbook/growth/sales/yc-onboarding',
-                    },
-                    {
-                        name: 'Contract Rules',
+                        name: 'Contract rules',
                         url: '/handbook/growth/sales/contract-rules',
                     },
                     {
-                        name: 'Creating Contracts',
+                        name: 'Creating contracts',
                         url: '/handbook/growth/sales/contracts',
                     },
                     {
