745f70747ce96c4a42e64a3d98ba60ad5ef9b59f
Cory Watilo <corywatilo@gmail.com>
2025-07-01 14:15:27 -0400

    cleaned up small teams section

--- src/components/Careers/SmallTeams/index.tsx
+++ src/components/Careers/SmallTeams/index.tsx
@@ -4,8 +4,9 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Link from 'components/Link'
 import slugify from 'slugify'
 import CloudinaryImage from 'components/CloudinaryImage'
-import { slugifyTeamName } from 'lib/utils'
+
 import OSButton from 'components/OSButton'
+import { DebugContainerQuery } from 'components/DebugContainerQuery'
 
 export const SmallTeams = () => {
     const {
@@ -17,7 +18,7 @@ export const SmallTeams = () => {
                 nodes {
                     id
                     name
-
+                    slug
                     miniCrest {
                         gatsbyImageData(width: 64, height: 64)
                     }
@@ -33,40 +34,44 @@ export const SmallTeams = () => {
     const roundedProfilesTotalCount = Math.floor(profilesTotalCount / 10) * 10
 
     return (
-        <div className="@container">
-                <div className="mb-8">
-                    <h2 className="text-3xl font-bold mb-4">
-                        Join a startup <em className="text-red dark:text-yellow">within a startup</em>
-                    </h2>
-                    <div className="max-w-xl">
-                        <p>Starting a job at a company of {roundedProfilesTotalCount}+ people can be intimidating!</p>
-                        <p>
-                            With our <Link to="/handbook/company/small-teams">small teams</Link> structure, it's like
-                            working at a startup <em>within</em> a startup. This allows each team to operate as
-                            autonomously as possible.
-                        </p>
+        <section id="small-teams" className="@container px-8 @3xl:px-8">
+            <DebugContainerQuery />
+            <div className="mb-8">
+                <h2 className="text-3xl font-bold mb-4">
+                    Join a startup <em className="text-red dark:text-yellow">within a startup</em>
+                </h2>
+                <div className="not-prose @2xl:columns-2 gap-8 [orphans:3]">
+                    <p>Starting a job at a company of {roundedProfilesTotalCount}+ people can be intimidating!</p>
+                    <p>
+                        With our <Link to="/handbook/company/small-teams">small teams</Link> structure, it's like
+                        working at a startup <em>within</em> a startup. This allows each team to operate as autonomously
+                        as possible.
+                    </p>
 
-                        <p>
-                            Many of our team members are former founders. Here they can focus on building great products
-                            without the distraction of running payroll, fundraising, etc - while still enjoying huge
-                            upside potential.
-                        </p>
-                    </div>
+                    <p>
+                        Many of our team members are former founders. Here they can focus on building great products
+                        without the distraction of running payroll, fundraising, etc - while still enjoying huge upside
+                        potential.
+                    </p>
                 </div>
+            </div>
 
-                <fieldset>
-                    <legend>Our small teams</legend>
-                    <div className="grid @xl:grid-cols-2 @3xl:grid-cols-3 text-center -mx-4 md:mx-0 md:gap-x-8">
-                        {allTeams.nodes.map(({ id, name, miniCrest }) => {
+            <fieldset>
+                <legend>Our small teams</legend>
+                <div className="@xl:columns-2 @3xl:columns-3 text-center -mx-4 md:mx-0 md:gap-x-8">
+                    {allTeams.nodes
+                        .sort((a: any, b: any) => a.name.localeCompare(b.name))
+                        .map(({ id, name, slug, miniCrest }: any) => {
                             const gatsbyImageMiniCrest = getImage(miniCrest)
                             return (
                                 <OSButton
-                                    to={`/teams/${slugifyTeamName(name)}`}
+                                    to={`/teams/${slug}`}
                                     key={id}
                                     variant="ghost"
                                     align="left"
                                     width="full"
                                     zoomHover="md"
+                                    state={{ newWindow: true }}
                                 >
                                     <div className="size-8">
                                         {gatsbyImageMiniCrest ? (
@@ -91,8 +96,8 @@ export const SmallTeams = () => {
                                 </OSButton>
                             )
                         })}
-                    </div>
-                </fieldset>
-        </div>
+                </div>
+            </fieldset>
+        </section>
     )
 }
