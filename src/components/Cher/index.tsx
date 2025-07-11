ad3666a680690d69b51f222f1043e98bc794a5f0
Eli Kinsey <eli@ekinsey.dev>
2025-07-10 18:05:38 -0700

    italic

--- src/components/Cher/index.tsx
+++ src/components/Cher/index.tsx
@@ -31,7 +31,7 @@ export default function Cher() {
             >
                 {/* Comic speech bubble */}
                 <div className="absolute -left-[98px] top-6 bg-primary border-2 border-accent rounded-lg px-2.5 py-1.5 text-xs font-semibold text-primary shadow-lg before:content-[''] before:absolute before:right-[-8px] before:top-3 before:w-0 before:h-0 before:border-l-[8px] before:border-l-accent before:border-t-[6px] before:border-t-transparent before:border-b-[6px] before:border-b-transparent">
-                    Looking for Cher?
+                    Looking for <span className="italic">Cher</span>?
                 </div>
                 <CloudinaryImage
                     src="https://res.cloudinary.com/dmukukwp6/image/upload/cher_hog_5cdafbe899.png"
