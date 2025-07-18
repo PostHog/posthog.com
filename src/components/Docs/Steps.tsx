c7f09a12da30e158137131bf7ad175b5cbce2ed2
edwinyjlim <edwinyjlim@gmail.com>
2025-07-18 01:05:36 -0400

    anchor links for each step heading

--- src/components/Docs/Steps.tsx
+++ src/components/Docs/Steps.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CopyAnchor } from 'components/Heading'
+import Slugger from 'github-slugger'
 
 const badgeClasses =
     'bg-gray-accent/50 text-primary/75 dark:text-primary-dark/60 dark:bg-gray-accent-dark text-xs m-[-2px] font-medium rounded-sm px-1 py-0.5 inline-block'
@@ -23,7 +25,7 @@ export const Steps: React.FC<{ children: React.ReactNode }> = ({ children }) => 
         <ol className="ml-0">
             {React.Children.map(children, (child, i) =>
                 React.isValidElement(child)
-                    ? React.cloneElement(child as React.ReactElement<any>, {
+                    ? React.cloneElement(child as React.ReactElement<StepProps & { number?: number }>, {
                           number: i + 1,
                       })
                     : null
@@ -39,35 +41,50 @@ export const Step: React.FC<StepProps & { number?: number }> = ({
     titleSize,
     children,
     number,
-}) => (
-    <li className="mb-10 flex w-full">
-        <span className="flex flex-col items-center mr-4 relative">
-            <span className="flex items-center justify-center w-8 h-8 rounded-md bg-gray-accent-light dark:bg-gray-accent-dark text-primary dark:text-primary-dark font-bold text-base z-10 border border-light dark:border-dark border-b-4 border-b-gray-accent dark:border-b-gray-accent-dark">
-                {number}
-            </span>
+}) => {
+    const [hovered, setHovered] = useState(false)
+    const slugger = new Slugger()
+    const anchorId = slugger.slug(title)
 
-            <span className="mt-2 w-[1px] top-4 bg-gray-accent dark:bg-gray-accent-dark h-[calc(100%_-_3rem)]"></span>
-        </span>
-        <div className="min-w-0 flex-1">
-            <div className="relative">
-                <div className="absolute -top-[108px]" id={title.toLowerCase().replace(/ /g, '-')}></div>
-                <div className="flex items-center gap-2 font-semibold text-base text-primary dark:text-primary-dark">
-                    {!titleSize || titleSize === 'h2' ? (
-                        <h2 className="!my-0 !text-2xl truncate">{title}</h2>
-                    ) : (
-                        <h3 className="!my-0 !text-xl truncate">{title}</h3>
-                    )}
-                    {badge && badgeMap[badge] && (
-                        <span className={`${badgeMap[badge].className} shrink-0`}>{badgeMap[badge].text}</span>
-                    )}
-                </div>
+    return (
+        <li
+            className="mb-10 flex w-full group"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <div className="flex items-start mr-4 relative">
+                <span className="flex flex-col items-center relative">
+                    <CopyAnchor id={anchorId} hovered={hovered} />
+                    <span className="flex items-center justify-center w-8 h-8 rounded-md bg-gray-accent-light dark:bg-gray-accent-dark text-primary dark:text-primary-dark font-bold text-base z-10 border border-light dark:border-dark border-b-4 border-b-gray-accent dark:border-b-gray-accent-dark">
+                        {number}
+                    </span>
+                    <span className="mt-2 w-[1px] top-4 bg-gray-accent dark:bg-gray-accent-dark h-[calc(100%_-_3rem)]"></span>
+                </span>
             </div>
-            {subtitle && subtitle.trim() && (
-                <div className="text-sm text-primary/75 dark:text-primary-dark/75 mt-1 mb-2">
-                    <em>{subtitle}</em>
+            <div className="min-w-0 flex-1">
+                <div className="relative">
+                    <div className="flex items-center gap-2 font-semibold text-base text-primary dark:text-primary-dark">
+                        {!titleSize || titleSize === 'h2' ? (
+                            <h2 id={anchorId} className="!my-0 !text-2xl truncate">
+                                {title}
+                            </h2>
+                        ) : (
+                            <h3 id={anchorId} className="!my-0 !text-xl truncate">
+                                {title}
+                            </h3>
+                        )}
+                        {badge && badgeMap[badge] && (
+                            <span className={`${badgeMap[badge].className} shrink-0`}>{badgeMap[badge].text}</span>
+                        )}
+                    </div>
                 </div>
-            )}
-            <div className="mt-4 mb-4 overflow-x-auto overflow-y-hidden">{children}</div>
-        </div>
-    </li>
-)
+                {subtitle && subtitle.trim() && (
+                    <div className="text-sm text-primary/75 dark:text-primary-dark/75 mt-1 mb-2">
+                        <em>{subtitle}</em>
+                    </div>
+                )}
+                <div className="mt-4 mb-4 overflow-x-auto overflow-y-hidden">{children}</div>
+            </div>
+        </li>
+    )
+}
