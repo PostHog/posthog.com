224f0100023b495eb2a8eeebbde5d770e995a6b1
Eli Kinsey <eli@ekinsey.dev>
2025-07-18 08:54:49 -0700

    nit

--- src/components/TaskBarMenu/index.tsx
+++ src/components/TaskBarMenu/index.tsx
@@ -86,7 +86,9 @@ export default function TaskBarMenu() {
                             <div className="relative">
                                 <CloudinaryImage
                                     src={getAvatarURL(user?.profile)}
-                                    className={`size-6 rounded-full bg-${user?.profile?.color ?? 'white dark:bg-dark'}`}
+                                    className={`size-6 rounded-full overflow-hidden bg-${
+                                        user?.profile?.color ?? 'white dark:bg-dark'
+                                    }`}
                                     width={48}
                                     alt=""
                                 />
