8546752200e4669eb67960cf2beb2149a54fce8a
Vincent (Wen Yu) Ge <29069505+gewenyu99@users.noreply.github.com>
2025-07-23 16:51:08 -0400

    Fix some floater docs pages that don't exist in nav (#12230)

--- src/navs/index.js
+++ src/navs/index.js
@@ -376,6 +376,7 @@ export const dataPipelines = {
             ],
             dynamicChildren: 'data-pipeline-transformations',
         },
+
         {
             name: 'Troubleshooting and FAQs',
             url: '/docs/cdp/common-questions',
@@ -2469,6 +2470,24 @@ export const docsMenu = {
                         {
                             name: 'Account settings',
                             url: '/docs/settings/account-settings',
+                        },
+                        {
+                            name: 'Site Apps',
+                            url: '/docs/site-apps',
+                            children: [
+                                {
+                                    name: 'Overview',
+                                    url: '/docs/site-apps',
+                                },
+                                {
+                                    name: 'Notification Bar',
+                                    url: '/docs/site-apps/notification-bar',
+                                },
+                                {
+                                    name: 'Pineapple Mode',
+                                    url: '/docs/site-apps/pineapple-mode',
+                                },
+                            ],
                         },
                     ],
                 },
