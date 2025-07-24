71fb702a0879d6d06b3f9cbabf17ec15c6e7f5d4
Cory Watilo <corywatilo@gmail.com>
2025-07-24 14:56:58 +0300

    fixed classic overrides

--- src/components/TaskBarMenu/index.tsx
+++ src/components/TaskBarMenu/index.tsx
@@ -200,7 +200,7 @@ export default function TaskBarMenu() {
             <div
                 id="taskbar"
                 data-scheme="primary"
-                className="w-full bg-accent/75 backdrop-blur border-b border-primary top-0 z-50 flex justify-between pl-0.5 pr-2"
+                className="w-full bg-accent/75 skin-classic:bg-accent backdrop-blur border-b border-primary top-0 z-50 flex justify-between pl-0.5 pr-2"
             >
                 <MenuBar menus={menuData} className="[&_button]:px-2" />
                 <aside className="flex items-center gap-px py-0.5">
