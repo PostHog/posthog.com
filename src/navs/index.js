181066582f1bc7aa7b71560beb801fc3676180c2
Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
2025-08-19 16:20:23 +0100

    Docs: node:http reverse proxy (#12153)
    Co-authored-by: Vincent (Wen Yu) Ge <29069505+gewenyu99@users.noreply.github.com>

--- src/navs/index.js
+++ src/navs/index.js
@@ -2739,6 +2739,10 @@ export const docsMenu = {
                             url: '/docs/advanced/proxy/nginx',
                         },
                         {
+                            name: 'Node',
+                            url: '/docs/advanced/proxy/node',
+                        },
+                        {
                             name: 'Nuxt',
                             url: '/docs/advanced/proxy/nuxt',
                         },
