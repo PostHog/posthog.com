319a7f5e97ebe343135b7084fef6ad549c815347
Vincent (Wen Yu) Ge <gewenyu99@gmail.com>
2025-08-26 15:59:22 -0400

    Add to nav

--- src/navs/index.js
+++ src/navs/index.js
@@ -1998,6 +1998,16 @@ export const docsMenu = {
                         {
                             name: 'Node.js',
                             url: '/docs/libraries/node',
+                            children: [
+                                {
+                                    name: 'Setup and features',
+                                    url: '/docs/libraries/node',
+                                },
+                                {
+                                    name: 'Reference',
+                                    url: '/docs/references/posthog-node',
+                                },
+                            ],
                         },
                         {
                             name: 'PHP',
