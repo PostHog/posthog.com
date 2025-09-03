0c7999d4e575b643c432230b0ce8953bb99c05eb
Eli Kinsey <eli@ekinsey.dev>
2025-09-03 12:38:25 -0700

    scroll

--- src/components/Legal/index.tsx
+++ src/components/Legal/index.tsx
@@ -1,36 +1,39 @@
 import OSTabs from 'components/OSTabs'
+import ScrollArea from 'components/RadixUI/ScrollArea'
 import { navigate } from 'gatsby'
 import React from 'react'
 
 export default function Legal({ children, defaultTab }: { children: React.ReactNode; defaultTab: string }) {
     return (
-        <OSTabs
-            tabs={[
-                {
-                    label: 'Terms',
-                    value: '/terms',
-                    content: children,
-                },
-                {
-                    label: 'Privacy',
-                    value: '/privacy',
-                    content: children,
-                },
-                {
-                    label: 'DPA generator',
-                    value: '/dpa',
-                    content: children,
-                },
-                {
-                    label: 'BAA generator',
-                    value: '/baa',
-                    content: children,
-                },
-            ]}
-            defaultValue={defaultTab}
-            onValueChange={(value) => {
-                navigate(value)
-            }}
-        />
+        <ScrollArea>
+            <OSTabs
+                tabs={[
+                    {
+                        label: 'Terms',
+                        value: '/terms',
+                        content: children,
+                    },
+                    {
+                        label: 'Privacy',
+                        value: '/privacy',
+                        content: children,
+                    },
+                    {
+                        label: 'DPA generator',
+                        value: '/dpa',
+                        content: children,
+                    },
+                    {
+                        label: 'BAA generator',
+                        value: '/baa',
+                        content: children,
+                    },
+                ]}
+                defaultValue={defaultTab}
+                onValueChange={(value) => {
+                    navigate(value)
+                }}
+            />
+        </ScrollArea>
     )
 }
