4f6dd809b4f01d99a4e71be95410d306deaf8161
Eli Kinsey <eli@ekinsey.dev>
2025-09-03 16:02:57 -0700

    fix broken links

--- src/pages/sparks-joy/index.tsx
+++ src/pages/sparks-joy/index.tsx
@@ -65,28 +65,33 @@ export default function SparkJoy(): JSX.Element {
                 }
             >
                 <div className="@container">
-                    <div className={`@md:pl-4 grid ${isListLayout
-                        ? '@lg:grid-cols-2 @3xl:grid-cols-3 gap-y-4'
-                        : explorerGridColumns + ' gap-y-4 items-start justify-items-center'
-                        } gap-x-1 @md:gap-x-4 relative [&>div]:mx-auto [&_figure]:text-center pt-4`}>
+                    <div
+                        className={`@md:pl-4 grid ${
+                            isListLayout
+                                ? '@lg:grid-cols-2 @3xl:grid-cols-3 gap-y-4'
+                                : explorerGridColumns + ' gap-y-4 items-start justify-items-center'
+                        } gap-x-1 @md:gap-x-4 relative [&>div]:mx-auto [&_figure]:text-center pt-4`}
+                    >
                         <Link
-                            to="/games/hedgehog-mode"
+                            to="/sparks-joy/hedgehog-mode"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary`}
                         >
                             <AppIcon name="hedgehog_mode" className="!size-10 -mt-2" />
                             <p className="text-sm font-medium">Hedgehog mode</p>
                         </Link>
                         <Link
-                            to="/games/dictator-or-tech-bro"
+                            to="/sparks-joy/dictator-or-tech-bro"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary relative top-0`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary relative top-0`}
                         >
                             <IconDictator className="!size-10 -mt-2" />
                             <p className="text-sm font-medium">Dictator or tech bro?</p>
@@ -94,10 +99,11 @@ export default function SparkJoy(): JSX.Element {
                         <Link
                             to="/paint"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary`}
                         >
                             <AppIcon name="hogpaint" className="!size-10 -mt-2" />
                             <p className="text-sm font-medium">HogPaint</p>
@@ -105,10 +111,11 @@ export default function SparkJoy(): JSX.Element {
                         <Link
                             to="/photobooth"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary`}
                         >
                             <AppIcon name="photobooth" className="!size-10 -mt-2" />
                             <p className="text-sm font-medium">Photobooth</p>
@@ -116,10 +123,11 @@ export default function SparkJoy(): JSX.Element {
                         <Link
                             to="/merch"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary`}
                         >
                             <AppIcon name="shoppingBag" className="!size-10 -mt-2" />
                             <p className="text-sm font-medium">Store</p>
@@ -127,10 +135,11 @@ export default function SparkJoy(): JSX.Element {
                         <Link
                             to="/bookmarks"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary`}
                         >
                             <IconBookmarkSolid className="!size-10 -mt-2 text-blue" />
                             <p className="text-sm font-medium">Bookmarks</p>
@@ -138,10 +147,11 @@ export default function SparkJoy(): JSX.Element {
                         <Link
                             to="/coloring-book.pdf"
                             state={{ newWindow: true }}
-                            className={`${isListLayout
-                                ? 'flex flex-row items-center space-x-3 w-full text-left'
-                                : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
-                                } text-primary hover:text-primary`}
+                            className={`${
+                                isListLayout
+                                    ? 'flex flex-row items-center space-x-3 w-full text-left'
+                                    : 'flex flex-col justify-center items-center space-y-1 w-28 text-center'
+                            } text-primary hover:text-primary`}
                         >
                             <IconDocument className="!size-10 -mt-2 text-red" />
                             <p className="text-sm font-medium">Coloring book.pdf</p>
