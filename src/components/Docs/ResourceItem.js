5252cf0064f3c84acb54559b189584b1ea9844fc
Cory Watilo <corywatilo@gmail.com>
2025-09-08 10:30:36 -0700

    cleaned up tracks page

--- src/components/Docs/ResourceItem.js
+++ src/components/Docs/ResourceItem.js
@@ -4,12 +4,12 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 export default function ResourceItem({ title, description, Image, gatsbyImage, url, type }) {
     return (
-        <li className="list-none bg-accent border border-primary rounded relative hover:top-[-2px] active:top-[1px] hover:transition-all overflow-hidden">
-            <Link to={url} className="block">
-                <div className={`px-4 py-3 ${Image ? 'pb-0' : ''}`}>
-                    {type && <h6 className="text-sm text-muted font-medium mb-1 p-0">{type}</h6>}
-                    <h4 className="m-0 text-lg leading-tight text-primary dark:text-primary-dark">{title}</h4>
-                    <p className="text-secondary text-[15px] mt-1 mb-0 !leading-tight">{description}</p>
+        <li className="">
+            <Link to={url} className="block" state={{ newWindow: true }}>
+                <div className={`${Image ? 'pb-0' : ''}`}>
+                    {type && <h6 className="text-xs text-muted font-medium mb-1 p-0">{type}</h6>}
+                    <h4 className="font-semibold my-0">{title}</h4>
+                    <p className="text-secondary text-sm">{description}</p>
                 </div>
                 {Image && (
                     <div className="flex justify-end w-full h-24">
