8c6d14138e319c2dacff56e70b75032a85edd08a
Eli Kinsey <eli@ekinsey.dev>
2025-09-09 22:06:51 -0700

    mobile chat scroll

--- src/components/Chat/Inkeep.tsx
+++ src/components/Chat/Inkeep.tsx
@@ -1,16 +1,23 @@
 import React, { useRef } from 'react'
 import { DotLottiePlayer } from '@dotlottie/react-player'
 import { useChat } from 'hooks/useChat'
+import ScrollArea from 'components/RadixUI/ScrollArea'
+import { useApp } from '../../context/App'
 
 export default function InkeepEmbeddedChat(): JSX.Element {
     const lottieRef = useRef(null)
     const { EmbeddedChat, aiChatSettings, baseSettings } = useChat()
+    const { isMobile } = useApp()
+
+    const Container = isMobile ? ScrollArea : React.Fragment
 
     return (
         <>
             {EmbeddedChat ? (
                 <div id="embedded-chat-target" className="h-full">
-                    <EmbeddedChat aiChatSettings={aiChatSettings} baseSettings={baseSettings} />
+                    <Container>
+                        <EmbeddedChat aiChatSettings={aiChatSettings} baseSettings={baseSettings} />
+                    </Container>
                 </div>
             ) : (
                 <div className="flex-grow size-full flex items-center justify-center">
