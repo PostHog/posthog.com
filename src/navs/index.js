e9d513fb86b6504888eb49abccfbd4fe6fbf92c9
Javier Bahamondes <javier@posthog.com>
2025-09-12 10:42:29 -0300

    feat: adding reddit docs (#12760)
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    Co-authored-by: Ian Vanagas <34755028+ivanagas@users.noreply.github.com>
    Co-authored-by: Javier Bahamondes <javierbahamondes@Javiers-MacBook-Pro.local>

--- src/navs/index.js
+++ src/navs/index.js
@@ -85,6 +85,10 @@ export const dataPipelines = {
                     url: '/docs/cdp/sources/linkedin-ads',
                 },
                 {
+                    name: 'Reddit Ads',
+                    url: '/docs/cdp/sources/reddit-ads',
+                },
+                {
                     name: 'Google Sheets',
                     url: '/docs/cdp/sources/google-sheets',
                 },
@@ -4149,6 +4153,10 @@ export const docsMenu = {
                         {
                             name: 'LinkedIn Ads',
                             url: '/docs/data-warehouse/sources/linkedin-ads',
+                        },
+                        {
+                            name: 'Reddit Ads',
+                            url: '/docs/data-warehouse/sources/reddit-ads',
                         },
                         {
                             name: 'Google Sheets',
