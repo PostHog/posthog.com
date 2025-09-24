aae52a9d1381e405ae1ad14c690fb34fac79d0ed
Eli Kinsey <eli@ekinsey.dev>
2025-09-24 16:34:48 -0700

    pull all company info

--- src/api/customer.ts
+++ src/api/customer.ts
@@ -1,17 +1,19 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby'
 
 const handler = async (req: GatsbyFunctionRequest, res: GatsbyFunctionResponse) => {
-    const domain = req.query.domain
-    let data = {}
-    if (domain) {
-        const clearbitData = await fetch(`https://company.clearbit.com/v2/companies/find?domain=${domain}`, {
-            headers: { Authorization: `Bearer ${process.env.CLEARBIT_API_KEY}` },
-        }).then((res) => res.json())
-        data = {
-            ...clearbitData,
+    try {
+        const domain = req.query.domain
+        if (!domain) {
+            return res.status(400).json({ error: 'Domain is required' })
         }
+        const data = await fetch(`${process.env.GATSBY_SQUEAK_API_HOST}/api/customers/${domain}`, {
+            headers: { Authorization: `Bearer ${process.env.GATSBY_SQUEAK_CUSTOMERS_API_KEY}` },
+        }).then((res) => res.json())
+        return res.status(200).json(data)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: 'Failed to fetch customer data' })
     }
-    return res.status(200).json(data)
 }
 
 export default handler
