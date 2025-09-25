0798789b6f677799b9085ad6a5221903c059a751
steventruong <steventruong@users.noreply.github.com>
2025-09-25 09:35:33 -0700

    adding foundation model checklist (#12909)

--- src/navs/index.js
+++ src/navs/index.js
@@ -1397,6 +1397,10 @@ export const handbookSidebar = [
                         url: '/handbook/cs-and-onboarding/getting-to-know-your-customers',
                     },
                     {
+                        name: 'Basic foundation model checklist',
+                        url: '/handbook/cs-and-onboarding/foundation-check',
+                    },
+                    {
                         name: 'Template for onboarding success plan',
                         url: '/handbook/cs-and-onboarding/onboarding-success-plan',
                     },
