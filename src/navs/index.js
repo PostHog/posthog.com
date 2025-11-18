789fcf6193488cd97599b173832627a29b9c7c70
steventruong <steventruong@users.noreply.github.com>
2025-11-18 09:24:30 -0800

    Consolidating our CSM getting started docs to remove redundancy (#13699)
    * Consolidating our CSM getting started docs to remove redundancy
    
    * spelling and grammar fixes
    
    * greptile grammar fixes

--- src/navs/index.js
+++ src/navs/index.js
@@ -1339,15 +1339,7 @@ export const handbookSidebar = [
                     },
                     {
                         name: 'Getting started with newly assigned customers',
-                        url: '/handbook/cs-and-onboarding/newly-assigned-customer',
-                    },
-                    {
-                        name: 'Saying hi to your customers',
-                        url: '/handbook/cs-and-onboarding/saying-hi-to-your-customers',
-                    },
-                    {
-                        name: 'Getting to know your customers',
-                        url: '/handbook/cs-and-onboarding/getting-to-know-your-customers',
+                        url: '/handbook/cs-and-onboarding/getting-started-with-customers',
                     },
                     {
                         name: 'Basic account review checklist',
