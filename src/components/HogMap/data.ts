b24792d91e8c828c85742c13dc8fb502157a57e4
Alex V <alexander.veryaysky@gmail.com>
2025-11-20 15:18:27 +0100

    getters setters

--- src/components/HogMap/data.ts
+++ src/components/HogMap/data.ts
@@ -1,3 +1,14 @@
 import { EventItem, PlaceItem } from './types'
-export const events: EventItem[] = []
-export const places: PlaceItem[] = []
+
+let _events: EventItem[] = []
+let _places: PlaceItem[] = []
+
+export const getEvents = (): EventItem[] => _events
+export const setEvents = (next: EventItem[]): void => {
+    _events = Array.isArray(next) ? next : []
+}
+
+export const getPlaces = (): PlaceItem[] => _places
+export const setPlaces = (next: PlaceItem[]): void => {
+    _places = Array.isArray(next) ? next : []
+}
