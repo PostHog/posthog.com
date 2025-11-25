d767fc17f96b03658c8a928aab3f6623161653e0
Michael Matloka <michael@matloka.com>
2025-11-25 19:27:04 +0100

    Revert border radius change

--- tailwind.config.js
+++ tailwind.config.js
@@ -99,7 +99,6 @@ module.exports = {
             borderRadius: {
                 xs: '2px',
                 sm: '4px',
-                DEFAULT: '6px',
                 lg: '20px',
             },
             borderWidth: {
