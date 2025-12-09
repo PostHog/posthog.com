6b7b95f04b70c24bdd7e988c34703d82dfa9a4e7
edwinyjlim <edwinyjlim@gmail.com>
2025-12-09 17:15:48 -0500

    non empty

---
+++ src/pages/startups/overworld.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Overworld(): JSX.Element {
+    return <div className="container mx-auto max-w-screen-md my-12">Overworld goes here</div>
+}
+
+export default Overworld
