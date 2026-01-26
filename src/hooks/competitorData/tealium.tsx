463f92172ae031a200018c239af719b65b10df5e
Natalia Amorim <natalia@posthog.com>
2026-01-26 11:14:57 -0500

    Tealium table updates

--- src/hooks/competitorData/tealium.tsx
+++ src/hooks/competitorData/tealium.tsx
@@ -17,4 +17,25 @@ export const tealium = {
             available: false,
         },
     },
+    platform: {
+    pricing: {
+        free_tier: false,
+        transparent_pricing: false,
+        usage_based_pricing: true,
+    },
+    security: {
+        bot_blocking: true,
+        cookieless_tracking: false,
+        data_anonymization: true,
+        data_retention: true,
+        gdpr_ready: true,
+        hipaa_ready: 'BAA available',
+        history_audit_logs: true,
+        reverse_proxy: false,
+        saml_sso: true,
+        soc2_certified: true,
+        two_factor_auth: true,
+        user_privacy_options: true,
+    },
+},
 }
