4b795beb8494e87f740b89c2f999040690f09afa
Arthur Moreira de Deus <arthur@posthog.com>
2026-01-28 12:18:17 -0300

    docs(customer-analytics): Saved views (#14587)
    * docs(customer-analytics): Saved views
    
    * chore: Adjustments

--- src/navs/index.js
+++ src/navs/index.js
@@ -5214,6 +5214,13 @@ export const docsMenu = {
                     featured: true,
                 },
                 {
+                    name: 'Saved views',
+                    url: '/docs/customer-analytics/saved-views',
+                    icon: 'IconBookmark',
+                    color: 'purple',
+                    featured: true,
+                },
+                {
                     name: 'Guides',
                 },
                 {
