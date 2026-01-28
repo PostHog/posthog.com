12aca6c2c54a6473b3806488ddb961baa7c97a89
Abheek Basu <abheek94@gmail.com>
2026-01-28 17:13:27 +0000

    update to remove beta and include pricing (#14610)

--- src/pages/docs/logs/index.tsx
+++ src/pages/docs/logs/index.tsx
@@ -6,14 +6,12 @@ import ReaderView from 'components/ReaderView'
 import Link from 'components/Link'
 import { ProductScreenshot } from 'components/ProductScreenshot'
 import { Caption } from 'components/Caption'
-import LogsBetaSnippet from '../../../../contents/docs/logs/_snippets/beta.mdx'
 
 export const Content = () => {
     return (
         <>
             <section className="mb-8">
                 <h2 className="mb-4">Overview</h2>
-                <LogsBetaSnippet />
                 <div>
                     <p>
                         PostHog logs is a powerful logging solution that works with the OpenTelemetry Protocol (OTLP).
@@ -76,10 +74,9 @@ export const Content = () => {
                 <h2 className="mb-4 mt-0" id="pricing">
                     Pricing
                 </h2>
-                <p>Logs is currently in beta. While in beta, Logs is free to use.</p>
                 <p>
-                    After the beta, pricing will be based on GB/ingest. We will offer 30 days standard retention. You'll
-                    get 50GB ingest free each month, with discounts as your usage grows.
+                    Every month, the first 50GB is free. After that, pricing is $0.25 per GB up to 300GB ingested, then
+                    $0.15/GB for 300GB+. Have a look at our <Link to="/pricing">pricing page</Link> for more details.
                 </p>
             </section>
 
