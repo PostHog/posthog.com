8de636efbac855f97fd0b7e1fa76c64ca9b8fc08
Andy Vandervell <92976667+andyvan-ph@users.noreply.github.com>
2026-02-11 18:57:36 +0000

    newsletter form fix (hopefully)

--- src/templates/Handbook.tsx
+++ src/templates/Handbook.tsx
@@ -27,6 +27,7 @@ import IsEU from 'components/IsEU'
 import IsUS from 'components/IsUS'
 import { CallToAction } from 'components/CallToAction'
 import Tooltip from 'components/Tooltip'
+import NewsletterForm from 'components/NewsletterForm'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { MDXProvider } from '@mdx-js/react'
 import { useState } from 'react'
@@ -337,6 +338,7 @@ export default function Handbook({ data: { post }, pageContext: { breadcrumbBase
                 </OverflowXSection>
             </p>
         ),
+        NewsletterForm,
         ...shortcodes,
     }
 
