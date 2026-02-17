352f4baa5170d50789417b4ac252fb2f1ed8b3d1
Eli Kinsey <eli@ekinsey.dev>
2026-02-17 07:38:34 -0800

    modal

--- src/context/App.tsx
+++ src/context/App.tsx
@@ -1101,6 +1101,9 @@ const appSettings: AppSettings = {
         position: {
             center: true,
         },
+        modal: {
+            type: 'standard',
+        },
     },
     '/fm': {
         size: {
