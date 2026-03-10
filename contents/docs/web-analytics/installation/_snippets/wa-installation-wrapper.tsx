3eedc4cc1bbe2d888e93a1bfb5b4ccbbd2491182
Vincent (Wen Yu) Ge <29069505+gewenyu99@users.noreply.github.com>
2026-03-10 16:41:48 -0400

    chore: migrate Surveys onboarding (#15457)
    * Surveys onboarding
    
    * Fix build
    
    * Cache lol
    
    * Combine wrappers
    
    * revert to master on gatsby

--- contents/docs/web-analytics/installation/_snippets/wa-installation-wrapper.tsx
+++ contents/docs/web-analytics/installation/_snippets/wa-installation-wrapper.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import {
     // Web SDK installations
-    JSWebInstallation,
+    WebInstallation,
     NextJSInstallation,
-    HTMLSnippetInstallation,
     ReactInstallation,
     VueInstallation,
     AngularInstallation,
@@ -43,7 +42,7 @@ const MOBILE_SNIPPETS = {
 // Web SDK wrappers
 export const WAJSWebInstallationWrapper = () => (
     <OnboardingContentWrapper snippets={WEB_SNIPPETS}>
-        <JSWebInstallation modifySteps={addNextStepsStep} />
+        <WebInstallation modifySteps={addNextStepsStep} />
     </OnboardingContentWrapper>
 )
 
@@ -55,7 +54,7 @@ export const WANextJSInstallationWrapper = () => (
 
 export const WAHTMLSnippetInstallationWrapper = () => (
     <OnboardingContentWrapper snippets={WEB_SNIPPETS}>
-        <HTMLSnippetInstallation modifySteps={addNextStepsStep} />
+        <WebInstallation modifySteps={addNextStepsStep} />
     </OnboardingContentWrapper>
 )
 
