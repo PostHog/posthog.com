88f5f70d1915bf7e1551bbbe6ccb66f48039a5ad
Vincent (Wen Yu) Ge <29069505+gewenyu99@users.noreply.github.com>
2026-03-24 10:40:58 -0400

    feat: Adapt to use JSSnippet (#15715)
    * Adapt to use JSSnippet
    
    * snippet fixes
    
    * revert branch
    
    * more clean ups

--- contents/docs/error-tracking/installation/_snippets/error-tracking-js-web-installation-wrapper.tsx
+++ contents/docs/error-tracking/installation/_snippets/error-tracking-js-web-installation-wrapper.tsx
@@ -3,10 +3,17 @@ import { WebInstallation } from 'onboarding/error-tracking'
 import { JSEventCapture } from 'onboarding/product-analytics'
 import { OnboardingContentWrapper } from 'components/Docs/OnboardingContentWrapper'
 import { addNextStepsStep } from './shared-helpers'
+import { WebsiteJSHtmlSnippet, WebsiteJSInitSnippet } from 'product-analytics/installation/_snippets/js-web-snippets'
 
 export const ErrorTrackingJSWebInstallationWrapper = () => {
     return (
-        <OnboardingContentWrapper snippets={{ JSEventCapture }}>
+        <OnboardingContentWrapper
+            snippets={{
+                JSEventCapture,
+                JSHtmlSnippet: WebsiteJSHtmlSnippet,
+                JSInitSnippet: WebsiteJSInitSnippet,
+            }}
+        >
             <WebInstallation modifySteps={(steps) => addNextStepsStep(steps, 'web')} />
         </OnboardingContentWrapper>
     )
