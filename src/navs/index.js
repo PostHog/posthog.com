246cf186591840be7fcf540fdde09f2c458bc480
Sachin Ranchod <sachin@posthog.com>
2026-03-25 18:19:13 -0700

    Add handbook page for running customer-facing training sessions (#15739)
    * add handbook page for running customer-facing training sessions
    
    * fix double title; user numbers consistently
    
    * Update customer training document with product training info
    
    Added information about product training as an optional activity to enhance customer value.
    
    * Remove duplicate title

--- src/navs/index.js
+++ src/navs/index.js
@@ -1756,6 +1756,10 @@ export const handbookSidebar = [
                         name: 'Turning knowledge into agent skills',
                         url: '/handbook/growth/sales/turning-knowledge-into-agent-skills',
                     },
+                    {
+                        name: 'Running product training sessions',
+                        url: '/handbook/growth/sales/customer-training',
+                    },
                 ],
             },
             {
