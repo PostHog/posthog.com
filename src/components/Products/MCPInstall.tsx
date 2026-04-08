606d949fc9c57376dfb13716fd79d24cb2cf6542
Eli Kinsey <eli@ekinsey.dev>
2026-04-08 12:36:16 -0700

    use singlecodeblock

--- src/components/Products/MCPInstall.tsx
+++ src/components/Products/MCPInstall.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CodeBlock from 'components/Home/CodeBlock'
+import { SingleCodeBlock } from 'components/CodeBlock'
 import Link from 'components/Link'
 
 export default function MCPInstall() {
@@ -7,7 +7,9 @@ export default function MCPInstall() {
         <div className="mt-4 pt-4 border-t border-border">
             <p className="text-sm font-semibold mb-2 opacity-70">Install the MCP server</p>
             <div className="[&_.code-block]:mb-0 [&_.code-block]:mt-0 [&_.pb-2]:pb-0">
-                <CodeBlock code="npx @posthog/wizard mcp add" language="bash" />
+                <SingleCodeBlock language="bash" showAskAI={false}>
+                    npx @posthog/wizard mcp add
+                </SingleCodeBlock>
             </div>
             <p className="text-sm opacity-60 mt-2">
                 The recommended way to install is with the AI wizard. See the{' '}
