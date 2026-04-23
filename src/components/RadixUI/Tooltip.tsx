303e5d17f8a43b3cfe0df3457151dfbc7952e3ff
Cory Watilo <corywatilo@gmail.com>
2026-04-22 18:21:11 -0700

    round the tooltip

--- src/components/RadixUI/Tooltip.tsx
+++ src/components/RadixUI/Tooltip.tsx
@@ -42,7 +42,7 @@ const Tooltip = ({
                     <RadixTooltip.Content
                         collisionBoundary={appContainer}
                         data-scheme="secondary"
-                        className="select-none rounded bg-primary border border-primary text-primary text-sm px-3 py-2.5 text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] min-w-0 min-h-0 max-w-full max-h-full transition-all will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade z-[51]"
+                        className="select-none rounded-md bg-primary border border-primary text-primary text-sm px-3 py-2.5 text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] min-w-0 min-h-0 max-w-full max-h-full transition-all will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade z-[51]"
                         sideOffset={sideOffset}
                         side={side}
                     >
