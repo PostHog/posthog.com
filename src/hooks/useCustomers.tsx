c5f29070bf5bb5a56f9279e8873eb3b7ef2f6ac3
Lizzie Epton <36413849+Lizzieepton@users.noreply.github.com>
2026-06-16 18:19:38 +0100

    add light mode logo

--- src/hooks/useCustomers.tsx
+++ src/hooks/useCustomers.tsx
@@ -406,7 +406,7 @@ const CUSTOMER_DATA: Record<string, BaseCustomer> = {
         notes: 'A conversion rate optimization platform',
         featured: false,
         logo: {
-            light: 'https://res.cloudinary.com/dmukukwp6/image/upload/FASTR_Logo_full_color_a8db0d8cb5.png',
+            light: 'https://res.cloudinary.com/dmukukwp6/image/upload/FASTR_Logo_white_3c093b166c.png',
             dark: 'https://res.cloudinary.com/dmukukwp6/image/upload/FASTR_Logo_full_color_a8db0d8cb5.png',
         },
         height: 10,
