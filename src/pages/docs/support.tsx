720ecddb0a93b898f2dd53184fcb37b6afd396ad
Alex V <alexander.veryaysky@gmail.com>
2026-07-13 10:49:46 +0200

    feat: support ga

--- src/pages/docs/support.tsx
+++ src/pages/docs/support.tsx
@@ -3,20 +3,11 @@ import ReaderView from 'components/ReaderView'
 import SEO from 'components/seo'
 import Intro from 'components/Docs/Intro'
 import ResourceItem from 'components/Docs/ResourceItem'
-import { CalloutBox } from 'components/Docs/CalloutBox'
 
 const Support: React.FC = () => {
     return (
         <ReaderView>
             <SEO title="Support - Docs - PostHog" />
-            <CalloutBox icon="IconFlask" title="Support is in beta" type="info">
-                <p>
-                    <strong>Note:</strong> Support is currently in{' '}
-                    <span className="rounded-sm bg-highlight py-0.5 px-1 text-xs font-bold text-red dark:text-yellow">
-                        Beta
-                    </span>
-                </p>
-            </CalloutBox>
             <Intro
                 subheader="Getting started"
                 title="Support"
