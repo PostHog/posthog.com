b970cd1d3e85986c2260572835a021e24b12133d
ianmatson <ian.m@posthog.com>
2026-07-23 10:14:03 -0500

    fix(merch): loop product carousel

--- src/templates/merch/ProductCarousel.tsx
+++ src/templates/merch/ProductCarousel.tsx
@@ -33,6 +33,7 @@ export function ProductCarousel(props: ProductCarouselProps): React.ReactElement
     const [loaded, setLoaded] = useState(false)
     const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
         initial: 0,
+        loop: true,
         slideChanged(slider) {
             setCurrentSlide(slider.track.details.rel)
         },
@@ -88,7 +89,6 @@ export function ProductCarousel(props: ProductCarouselProps): React.ReactElement
                                 e.stopPropagation()
                                 instanceRef.current?.prev()
                             }}
-                            disabled={currentSlide === 0}
                         />
 
                         <Arrow
@@ -96,7 +96,6 @@ export function ProductCarousel(props: ProductCarouselProps): React.ReactElement
                                 e.stopPropagation()
                                 instanceRef.current?.next()
                             }}
-                            disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
                         />
                     </>
                 )}
@@ -124,26 +123,19 @@ export function ProductCarousel(props: ProductCarouselProps): React.ReactElement
 
 function Arrow(props: {
     className?: string
-    disabled: boolean
     left?: boolean
     onClick: (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => void
 }) {
-    const { className, disabled, left } = props
+    const { className, left } = props
 
     const classes = cn(
-        'relative absolute h-8 w-8 top-1/2 -translate-y-1/2 left-auto right-2',
-        disabled ? 'opacity-50 cursor-default pointer-events-none' : 'cursor-pointer',
+        'relative absolute h-8 w-8 top-1/2 -translate-y-1/2 cursor-pointer left-auto right-2',
         left && 'left-2 right-auto',
         className
     )
 
     return (
-        <svg
-            className={classes}
-            onClick={disabled ? undefined : props.onClick}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-        >
+        <svg className={classes} onClick={props.onClick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             {props.left && <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />}
             {!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
         </svg>
