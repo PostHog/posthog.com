d6379b4574f3132fd7d8ab5493190208075b734a
ianmatson <ian.m@posthog.com>
2026-07-29 17:12:16 +0100

    Remove 'WIP' item from Korean and general TaskBarMenu components

--- src/components/Korean/KoreanTaskBarMenu/menuData.tsx
+++ src/components/Korean/KoreanTaskBarMenu/menuData.tsx
@@ -522,11 +522,6 @@ export function useMenuData(): MenuType[] {
                 },
                 {
                     type: 'item',
-                    label: 'WIP',
-                    link: '/wip',
-                },
-                {
-                    type: 'item',
                     label: 'Changelog',
                     link: '/changelog',
                 },
